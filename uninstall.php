27130e3bced94ee544ec48f80b8e708cb9b18fd6
Matty <hello@matty.co.za>
2013-04-19 13:44:58 +0200

    Code standards in uninstall.php.

--- uninstall.php
+++ uninstall.php
@@ -2,7 +2,7 @@
 /**
  * WooThemes Sensei Uninstall
  *
- * Uninstalls the plugin and associated data
+ * Uninstalls the plugin and associated data.
  *
  * @package WordPress
  * @subpackage Sensei
@@ -10,12 +10,12 @@
  * @author WooThemes
  * @since 1.0.0
  */
-if( !defined('WP_UNINSTALL_PLUGIN') ) exit();
+if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit();
 
 $token = 'woothemes-sensei';
 delete_option( $token . '-settings' );
 delete_option( $token . '-version' );
 delete_option( $token . '_courses_page_id' );
 delete_option( $token . '_user_dashboard_page_id' );
-delete_option( 'skip_install_sensei_pages' ); 
-delete_option( 'sensei_installed' ); 
+delete_option( 'skip_install_sensei_pages' );
+delete_option( 'sensei_installed' );
