f884b8a6eea6128c13bb0a9eb513e0532fee5049
Jeffikus <jeffikus@gmail.com>
2013-05-09 16:08:20 +0200

    {DEV} Gap Fill fronted output fields

--- templates/single-quiz/question_type-gap-fill.php
+++ templates/single-quiz/question_type-gap-fill.php
@@ -10,20 +10,23 @@
  */
 
 global $post, $woothemes_sensei, $current_user;
-// Get Frontend Data
+/// Get Frontend Data
 $user_quizzes = $woothemes_sensei->frontend->data->user_quizzes;
 $question_item = $woothemes_sensei->frontend->data->question_item;
 $question_count = $woothemes_sensei->frontend->data->question_count;
 // Question Meta
 $question_right_answer = get_post_meta( $question_item->ID, '_question_right_answer', true );
-$question_wrong_answers = get_post_meta( $question_item->ID, '_question_wrong_answers', true );
-// Merge right and wrong answers and randomize
-array_push( $question_wrong_answers, $question_right_answer );
-shuffle($question_wrong_answers);
+// Gap Fill data
 $question_text = $question_item->post_title;
+$gapfill_array = explode( '|', $question_right_answer );
+if ( isset( $gapfill_array[0] ) ) { $gapfill_pre = $gapfill_array[0]; } else { $gapfill_pre = ''; }
+if ( isset( $gapfill_array[1] ) ) { $gapfill_gap = $gapfill_array[1]; } else { $gapfill_gap = ''; }
+if ( isset( $gapfill_array[2] ) ) { $gapfill_post = $gapfill_array[2]; } else { $gapfill_post = ''; }
 ?>
 <li>
     <span><?php echo esc_html( stripslashes( $question_text ) ); ?></span>
     <input type="hidden" name="<?php echo esc_attr( 'question_id_' . $question_count ); ?>" value="<?php echo esc_attr( $question_item->ID ); ?>" />
-    TODO - GAP FILL CODE HERE
+    <p class="gapfill-answer">
+    	<span class="gapfill-answer-pre"><?php echo esc_html( $gapfill_pre ); ?></span>&nbsp;<input type="text" id="<?php echo esc_attr( 'question_' . $question_count ); ?>" name="<?php echo esc_attr( 'question_' . $question_count ); ?>" value="" class="gapfill-answer-gap" />&nbsp;<span class="gapfill-answer-post"><?php echo esc_html( $gapfill_post ); ?></span>
+    </p>
 </li>
