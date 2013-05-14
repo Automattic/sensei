a5876a8d0c40aa3999bae7758b52a9ec26d617ca
Jeffikus <jeffikus@gmail.com>
2013-05-14 14:24:21 +0200

    {DEV} removed duplicate functions

--- assets/js/grading-general.js
+++ assets/js/grading-general.js
@@ -4,34 +4,7 @@ jQuery(document).ready( function($) {
 	 * 	1 - Helper Functions.
 	 ***************************************************************************************************/
 
-	/**
-	 * exists checks if selector exists
-	 * @since  1.2.0
-	 * @return boolean
-	 */
-	jQuery.fn.exists = function() {
-		return this.length>0;
-	}
 
-	/**
-	 * JS version of PHP htmlentities.
-	 *
-	 * @since 1.0.8
-	 * @access public
-	 */
- 	jQuery.fn.htmlentities = function( str ) {
- 		return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-	}
-
-	jQuery.fn.ucwords = function( str ) {
-		str = str.toLowerCase();
-		str = str.replace( '-', ' ' );
-		str = str.replace( 'boolean', 'True/False' );
-	    return str.replace(/(^([a-zA-Z\p{M}]))|([ -][a-zA-Z\p{M}])/g,
-	        function($1){
-	            return $1.toUpperCase();
-	        });
-	}
 
 	/***************************************************************************************************
 	 * 	2 - Grading Overview Functions.
