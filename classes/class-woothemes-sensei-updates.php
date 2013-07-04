e9bdf034b54e3f493d0938d9abde4c035d305866
Jeffikus <jeffikus@gmail.com>
2013-07-04 14:17:33 +0200

    First prototype of manual update screen ready for testing.

--- classes/class-woothemes-sensei-updates.php
+++ classes/class-woothemes-sensei-updates.php
@@ -15,8 +15,13 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  * TABLE OF CONTENTS
  *
  * - __construct()
+ * - manual_update_admin_screen()
+ * - manual_update_adminpage_hidden()
  * - update()
  * - assign_role_caps()
+ * - set_default_quiz_grade_type()
+ * - set_default_question_type()
+ * - update_question_answer_data()
  */
 class WooThemes_Sensei_Updates {
 	public $token = 'woothemes-sensei';
@@ -41,13 +46,165 @@ class WooThemes_Sensei_Updates {
 								'1.1.0' => array( 	'auto' 		=> array( 'assign_role_caps' ),
 													'manual' 	=> array()
 												),
-								'1.3.0' => array( 	'auto' 		=> array( 'set_default_quiz_grade_type', 'set_default_question_type', 'update_question_answer_data' ),
+								'1.3.0' => array( 	'auto' 		=> array( 'set_default_quiz_grade_type', 'set_default_question_type' ),
 													'manual' 	=> array( 'update_question_answer_data' )
 												),
 							);
 		$this->legacy = apply_filters( 'sensei_upgrade_functions', $this->legacy, $this->legacy );
 		$this->version = get_option( $this->token . '-version' );
+
+		// Manual Update Screen
+		add_action('admin_menu', array( &$this, 'manual_update_admin_screen' ) );
+
 	} // End __construct()
+
+	/**
+	 * manual_update_admin_screen Adds hidden screen to run manual udpates
+	 * @access public
+	 * @since  1.3.7
+	 * @return void
+	 */
+	public function manual_update_admin_screen() {
+
+		// This WordPress variable is essential: it stores which admin pages are registered to WordPress
+		global $_registered_pages;
+
+		// Get the name of the hook for this plugin
+		// We use "options-general.php" as the parent as we want our page to appear under "options-general.php?page=sensei-manual-update-hidden-page"
+		$hookname = get_plugin_page_hookname('sensei-manual-update-hidden-page', 'options-general.php');
+
+		// Add the callback via the action on $hookname, so the callback function is called when the page "options-general.php?page=sensei-manual-update-hidden-page" is loaded
+		if (!empty($hookname)) {
+			add_action($hookname, array( &$this, 'manual_update_adminpage_hidden' ) );
+		} // End If Statement
+
+		// Add this page to the registered pages
+		$_registered_pages[$hookname] = true;
+
+	} // End manual_update_admin_screen()
+
+	/**
+	 * manual_update_adminpage_hidden html output for hidden manual update screen
+	 * @access public
+	 * @since  1.3.7
+	 * @return void
+	 */
+	public function manual_update_adminpage_hidden() {
+		// Page contents
+		?>
+
+		<div class="wrap">
+
+			<div id="icon-tools" class="icon32"><br></div>	<h2><?php _e( 'Sensei Updates', 'woothemes-sensei' ); ?></h2>
+
+			<?php
+			if ( isset( $_GET['action'] ) && $_GET['action'] == 'update' && isset( $_GET['n'] ) && intval( $_GET['n'] ) >= 0 && ( ( isset( $_POST['checked'][0] ) && '' != $_POST['checked'][0] ) || ( isset( $_GET['functions'] ) && '' != $_GET['functions'] ) ) ) {
+
+				// Setup the data variables
+				$n = intval( $_GET['n'] );
+				$functions_list = '';
+				$done_processing = false;
+
+				// Check for updates to run
+				if ( isset( $_POST['checked'][0] ) && '' != $_POST['checked'][0] ) {
+
+					foreach ( $_POST['checked'] as $key => $value ) {
+
+						// Dynamic function call
+						$done_processing = call_user_func_array( array( $this, $value ), array( false, 1, $n ) );
+
+						// Add to functions list get args
+						if ( '' == $functions_list ) {
+							$functions_list .= $value;
+						} else {
+							$functions_list .= '+' . $value;
+						} // End If Statement
+
+					} // End For Loop
+
+				} // End If Statement
+
+				// Check for updates to run
+				if ( isset( $_GET['functions'] ) && '' != $_GET['functions'] ) {
+
+					// Existing functions from GET variables instead of POST
+					$functions_array = $_GET['functions'];
+
+					foreach ( $functions_array as $key => $value ) {
+
+						// Dynamic function call
+						$done_processing = call_user_func_array( array( $this, $value ), array( false, 1, $n ) );
+
+						// Add to functions list get args
+						if ( '' == $functions_list ) {
+							$functions_list .= $value;
+						} else {
+							$functions_list .= '+' . $value;
+						} // End If Statement
+
+					} // End For Loop
+
+				} // End If Statement
+
+				if ( !$done_processing ) { ?>
+
+					<h3><?php _e( 'Processing Updates.....', 'woothemes-sensei' ); ?></h3>
+
+					<p><?php _e( 'If your browser doesn&#8217;t start loading the next page automatically, click this link:', 'woothemes-sensei' ); ?>&nbsp;&nbsp;<a class="button" href="options-general.php?page=sensei-manual-update-hidden-page&action=update&n=<?php echo ($n + 1) ?>&functions[]=<?php echo $functions_list; ?>"><?php _e( 'Next', 'woothemes-sensei' ); ?></a></p>
+					<script type='text/javascript'>
+					<!--
+					function sensei_nextpage() {
+						location.href = "options-general.php?page=sensei-manual-update-hidden-page&action=update&n=<?php echo ($n + 1) ?>&functions[]=<?php echo $functions_list; ?>";
+					}
+					setTimeout( "sensei_nextpage()", 250 );
+					//-->
+					</script><?php
+
+				} else { ?>
+
+					<p><?php _e( 'Update Completed Successfully!', 'woothemes-sensei' ); ?>&nbsp;&nbsp;<a href="<?php echo admin_url('edit.php?post_type=lesson'); ?>"><?php _e( 'Carry on creating Courses!', 'woothemes-sensei' ); ?></a></p>
+
+				<?php } // End If Statement
+
+			} else { ?>
+
+				<h3><?php _e( 'Updates', 'woothemes-sensei' ); ?></h3>
+				<p><?php _e( 'The following manual updates may be run.', 'woothemes-sensei' ); ?></p>
+
+				<form method="post" action="options-general.php?page=sensei-manual-update-hidden-page&action=update&n=0" name="update-sensei" class="upgrade">
+
+					<table class="widefat" cellspacing="0" id="update-plugins-table">
+
+						<thead>
+							<tr>
+								<th scope="col" class="manage-column check-column"><input type="checkbox" id="plugins-select-all"></th>
+								<th scope="col" class="manage-column"><label for="plugins-select-all"><?php _e( 'Select All', 'woothemes-sensei' ); ?></label></th>
+							</tr>
+						</thead>
+
+						<tfoot>
+							<tr>
+								<th scope="col" class="manage-column check-column"><input type="checkbox" id="plugins-select-all-2"></th>
+								<th scope="col" class="manage-column"><label for="plugins-select-all-2"><?php _e( 'Select All', 'woothemes-sensei' ); ?></label></th>
+							</tr>
+						</tfoot>
+
+						<tbody class="plugins">
+							<tr class="active">
+								<th scope="row" class="check-column"><input type="checkbox" name="checked[]" value="update_question_answer_data"></th>
+								<td><p><strong><?php _e( 'Update Question Answer Data', 'woothemes-sensei' ); ?></strong><br><?php _e( 'Runs through all Learners data and updates the answers from the old quiz types system to the new question types system.', 'woothemes-sensei' ); ?><br><em><?php _e( 'Originally run in v1.3.0', 'woothemes-sensei' ); ?></em></p></td>
+							</tr>
+						</tbody>
+
+					</table>
+
+					<p><input id="update-sensei" class="button" type="submit" value="<?php _e( 'Run Updates', 'woothemes-sensei' ); ?>" name="update"></p>
+
+				</form>
+
+			<?php
+			} // End If Statement
+	}
 
 	/**
 	 * update Calls the functions for updating
@@ -181,7 +338,19 @@ class WooThemes_Sensei_Updates {
 	 * @access public
 	 * @return void
 	 */
-	public function update_question_answer_data( $force = false ) {
+	public function update_question_answer_data( $force = false, $n = 5, $offset = 0 ) {
+
+		// Get Total Number of Updates to run
+		$quiz_count_object = wp_count_posts( 'quiz' );
+		$quiz_count_published = $quiz_count_object->publish;
+
+		// Calculate if this is the last page
+		if ( 0 == $offset ) {
+			$current_page = 1;
+		} else {
+			$current_page = intval( $offset / $n );
+		} // End If Statement
+		$total_pages = intval( $quiz_count_published / $n );
 
 		// Check if update has run
 		$updated = get_option( 'sensei_question_answer_data_update' );
@@ -189,7 +358,8 @@ class WooThemes_Sensei_Updates {
 		if( ! $updated || $force ) {
 
 			$args = array(	'post_type' 		=> 'quiz',
-							'numberposts' 		=> -1,
+							'numberposts' 		=> $n,
+							'offset'			=> $offset,
     						'post_status'		=> 'publish',
 							'suppress_filters' 	=> 0
 							);
@@ -254,8 +424,17 @@ class WooThemes_Sensei_Updates {
 			}
 
 			// Mark update as complete
-			add_option( 'sensei_question_answer_data_update', true );
+			if ( $current_page == $total_pages ) {
+				add_option( 'sensei_question_answer_data_update', true );
+			} // End If Statement
 		}
+
+		if ( $current_page == $total_pages ) {
+			return true;
+		} else {
+			return false;
+		} // End If Statement
+
 	} // End update_question_answer_data
 
 } // End Class
