349455a271b426bebbb5467d7fda283fd85a2833
Hugh Lashbrooke <hugh@woothemes.com>
2013-11-18 14:58:43 +0200

    - Adding <p> tags around lesson buttons Fixes #162

--- classes/class-woothemes-sensei-course.php
+++ classes/class-woothemes-sensei-course.php
@@ -951,7 +951,7 @@ class WooThemes_Sensei_Course {
 							$complete_html .= '<div class="meter green"><span style="width: 100%">100%</span></div>';
 
 							if( $manage ) {
-								$complete_html .= '<a class="button view-results" href="' . $woothemes_sensei->course_results->get_permalink( $course_item->ID ) . '">' . apply_filters( 'sensei_view_results_text', __( 'View results', 'woothemes-sensei' ) ) . '</a>';
+								$complete_html .= '<p><a class="button view-results" href="' . $woothemes_sensei->course_results->get_permalink( $course_item->ID ) . '">' . apply_filters( 'sensei_view_results_text', __( 'View results', 'woothemes-sensei' ) ) . '</a></p>';
 							}
 
 			    		$complete_html .= '</section>';
