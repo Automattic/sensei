9001f7f6528464bfbf482dbe974479c10d8b6646
Hugh Lashbrooke <hugh@woothemes.com>
2013-11-28 15:27:40 +0200

    Deleting course on order refund Fixes #181

--- classes/class-woothemes-sensei.php
+++ classes/class-woothemes-sensei.php
@@ -130,6 +130,7 @@ class WooThemes_Sensei {
 		add_action( 'woocommerce_thankyou' , array( $this, 'sensei_woocommerce_complete_order' ) );
 		add_action( 'woocommerce_order_status_completed' , array( $this, 'sensei_woocommerce_complete_order' ) );
 		add_action( 'woocommerce_order_status_cancelled' , array( $this, 'sensei_woocommerce_cancel_order' ) );
+		add_action( 'woocommerce_order_status_refunded' , array( $this, 'sensei_woocommerce_cancel_order' ) );
 		add_action( 'subscriptions_activated_for_order', array( $this, 'sensei_activate_subscription' ) );
 		// WooCommerce Subscriptions Actions
 		add_action( 'reactivated_subscription', array( $this, 'sensei_woocommerce_reactivate_subscription' ), 10, 2 );
