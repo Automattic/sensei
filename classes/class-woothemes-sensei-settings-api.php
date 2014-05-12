b25194b0ea3122fbbc4a6b9d1d85c2f637796fe4
Hugh Lashbrooke <hugh@woothemes.com>
2014-05-12 11:20:42 +0200

    Adding auto grade answer feedback Fixes #358

--- classes/class-woothemes-sensei-settings-api.php
+++ classes/class-woothemes-sensei-settings-api.php
@@ -844,7 +844,7 @@ class WooThemes_Sensei_Settings_API {
 		global $woothemes_sensei;
 		wp_enqueue_style( $woothemes_sensei->token . '-admin' );
 
-		wp_enqueue_style( 'woothemes-sensei-settings-api', esc_url( $woothemes_sensei->plugin_url . 'assets/css/settings.css' ), '', '1.4.0' );
+		wp_enqueue_style( 'woothemes-sensei-settings-api', esc_url( $woothemes_sensei->plugin_url . 'assets/css/settings.css' ), '', '1.6.0' );
 
 		$this->enqueue_field_styles();
 	} // End enqueue_styles()
