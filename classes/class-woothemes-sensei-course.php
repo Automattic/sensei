91354cec20f798c932eb7b6980d0320a6aee6d41
Dan Johnson <dan@woothemes.com>
2014-06-03 11:25:57 +0100

    Ensures certificate link displays even for courses with no quizzes. Closes #381

--- classes/class-woothemes-sensei-course.php
+++ classes/class-woothemes-sensei-course.php
@@ -1192,9 +1192,13 @@ class WooThemes_Sensei_Course {
 						$complete_html .= '<div class="meter green"><span style="width: 100%">100%</span></div>';
 
 						if( $manage ) {
+							$complete_html .= '<p class="sensei-results-links">';
+							$results_link = '';
 							if( count( $woothemes_sensei->frontend->course->course_quizzes( $course_item->ID ) ) > 0 ) {
-								$complete_html .= '<p class="sensei-results-links"><a class="button view-results" href="' . $woothemes_sensei->course_results->get_permalink( $course_item->ID ) . '">' . apply_filters( 'sensei_view_results_text', __( 'View results', 'woothemes-sensei' ) ) . '</a></p>';
+								$results_link = '<a class="button view-results" href="' . $woothemes_sensei->course_results->get_permalink( $course_item->ID ) . '">' . apply_filters( 'sensei_view_results_text', __( 'View results', 'woothemes-sensei' ) ) . '</a>';
 							}
+							$complete_html .= apply_filters( 'sensei_results_links', $results_link );
+							$complete_html .= '</p>';
 						}
 
 		    		$complete_html .= '</section>';
