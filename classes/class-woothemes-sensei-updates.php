752707ee859966666a845c827ad84940d60ec4dc
Hugh Lashbrooke <hugh@woothemes.com>
2014-08-05 10:50:36 +0200

    Giving Editors both Sensei capabilities & bumping Sensei version Fixes #461

--- classes/class-woothemes-sensei-updates.php
+++ classes/class-woothemes-sensei-updates.php
@@ -62,10 +62,13 @@ class WooThemes_Sensei_Updates {
 																		  'remove_deleted_user_activity' => array( 'title' => 'Remove Sensei activity for deleted users', 'desc' => 'Removes all course, lesson &amp; quiz activity for users that have already been deleted from the database. This will fix incorrect learner counts in the Analysis section.' ) )
 												),
 								'1.6.0' => array( 	'auto' 		=> array( 'add_teacher_role' => array( 'title' => 'Add \'Teacher\' role', 'desc' => 'Adds a \'Teacher\' role to your WordPress site that will allow users to mange the Grading and Analysis pages.' ),
-																		  'add_sensei_caps' => array( 'title' => 'Add administrator capabilities', 'desc' => 'Adds the \'manage_sensei\' and \'manage_sensei_grades\' capabilities to the Administrator role.' ),
+																		  'add_sensei_caps' => array( 'title' => 'Add Administrator capabilities', 'desc' => 'Adds the \'manage_sensei\' and \'manage_sensei_grades\' capabilities to the Administrator role.' ),
 																		  'restructure_question_meta' => array( 'title' => 'Restructure question meta data', 'desc' => 'Restructures the quesiton meta data as it relates to quizzes - this accounts for changes in the data structure in v1.6+. If you \'lost\' questions after the 1.6 upgrade then re-run this update and they will be restored.' ),
 																		  'update_quiz_settings' => array( 'title' => 'Add new quiz settings', 'desc' => 'Adds new settings to quizzes that were previously registered as global settings.' ),
 																		  'reset_lesson_order_meta' => array( 'title' => 'Set default order of lessons', 'desc' => 'Adds data to lessons to ensure that they show up on the \'Order Lessons\' screen - if this update has been run once before then it will reset all lessons to the default order.' ), ),
+													'manual' 	=> array()
+												),
+								'1.7.0' => array( 	'auto' 		=> array( 'add_editor_caps' => array( 'title' => 'Add Editor capabilities', 'desc' => 'Adds the \'manage_sensei_grades\' capability to the Editor role.' ), ),
 													'manual' 	=> array()
 												),
 							);
@@ -819,6 +822,13 @@ class WooThemes_Sensei_Updates {
 			}
 		}
 
+		return true;
+	}
+
+	public function add_editor_caps() {
+		$role = get_role( 'editor' );
+		$role->add_cap( 'manage_sensei' );
+		$role->add_cap( 'manage_sensei_grades' );
 		return true;
 	}
 
