b66f5e01812730f9bbe1a8d8d2b1852cc8d81a16
Dwain Maralack <dwain.maralack@gmail.com>
2014-09-01 01:37:20 +1000

    fixes login logic to check for form post before login failed _GET check

--- templates/user/login-form.php
+++ templates/user/login-form.php
@@ -25,7 +25,7 @@ $current_page_url =  home_url( $wp->request );
 
 <h2><?php _e( 'Login', 'woothemes-sensei' ); ?></h2>
 
-<form method="post" name='sensi-login-form' id="loginform" class="login sensei">
+<form method="post" name="sensi-login-form" id="loginform" class="login sensei">
 
 <?php
 /**
