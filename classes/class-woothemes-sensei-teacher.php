927db42af9aa36888a12ec5f232d2080d71063f7
Dwain Maralack <dwain.maralack@gmail.com>
2014-11-27 19:18:06 +0200

    add the sensei teacher class file #459

---
+++ classes/class-woothemes-sensei-teacher.php
@@ -0,0 +1,28 @@
+git br<?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Sensei Teacher class
+ *
+ * All functionality pertaining to the teacher role.
+ *
+ * @package WordPress
+ * @subpackage Sensei
+ * @category Core
+ * @author WooThemes
+ * @since 1.0.0
+ *
+ * TABLE OF CONTENTS
+ *
+ * - __construct()
+ * - add_teacher_role()
+ * - add_teacher_capabilities()
+ */
+class WooThemes_Sensei_Teacher {
+
+    /**
+     * Teacher class
+     */
+
+} // End Class
+?>
