d2aa3daf2c22cf650f9ad6576b08fff4ec274ee3
Dwain Maralack <dwain.maralack@gmail.com>
2014-11-28 14:25:17 +0200

    added the logic to create the teacher role and to add the capabilities to it #459

--- classes/class-woothemes-sensei-teacher.php
+++ classes/class-woothemes-sensei-teacher.php
@@ -15,14 +15,80 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  * TABLE OF CONTENTS
  *
  * - __construct()
- * - add_teacher_role()
+ * - create_teacher_role()
  * - add_teacher_capabilities()
  */
 class WooThemes_Sensei_Teacher {
 
     /**
-     * Teacher class
+     * $teacher_role
+     *
+     * Keeps a reference to the teacher role object
+     *
+     * @access protected
+     * @since 1.7.0
      */
+    protected $teacher_role;
 
+    /**
+     * WooThemes_Sensei_Teacher::create_teacher_role
+     *
+     * This function checks if the role exist, if not it creates it.
+     * for the teacher role
+     *
+     * @since 1.7.0
+     * @access public
+     * @return void
+     */
+    public function create_role ( ) {
+
+        // check if the role exists
+        $this->teacher_role = get_role( 'teacher' );
+
+        // if the the teacher is not a valid WordPress role create it
+       if ( ! is_a( $this->teacher_role, 'WP_Role' ) ) {
+           // create the role
+           $this->$teacher_role = add_role( 'teacher', __( 'Teacher', 'woothemes-sensei' ) );
+       }
+
+       // add the capabilities before returning
+        $this->add_capabilities();
+
+    }// end create_teacher_role
+
+    /**
+     * WooThemes_Sensei_Teacher::add_capabilities
+     *
+     * @since 1.7.0
+     * @access protected
+     */
+    protected function add_capabilities ( ) {
+        // if this is not a valid WP_Role object exit without adding anything
+        if(  !is_a( $this->teacher_role, 'WP_Role' ) || empty( $this->teacher_role ) ) {
+
+            return;
+        }
+
+        /**
+         * Sensei teachers capabilities array filter
+         *
+         * These capabilities will be applied to the teacher role
+         * @param array $capabilities
+         * keys: (string) $cap_name => (bool) $grant
+         */
+        $caps = apply_filters( 'sensei_teacher_role_capabilities', array(
+            'read' => true,
+            'manage_sensei_grades' => true,
+            'can_edit_courses'=> true,
+            'can_edit_lessons'=> true,
+        ));
+
+        foreach ( $caps as $cap => $grant ) {
+
+            // load the capability on to the teacher role
+            $this->teacher_role->add_cap($cap, $grant);
+
+        } // end for each
+
+    }// end add_cap
 } // End Class
-?>
