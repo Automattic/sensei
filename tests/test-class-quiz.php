78d98abbe42c0a65bf007bc96a9721eb77076c55
Dwain Maralack <dwain.maralack@gmail.com>
2015-02-13 08:05:18 -0600

    update the tests and change the parameter order for the save questions function #618

--- tests/test-class-quiz.php
+++ tests/test-class-quiz.php
@@ -5,6 +5,7 @@ class Sensei_Class_Quiz_Test extends WP_UnitTestCase {
      * setup function
      */
     function setup(){
+        // todo: setup data that will be used for testing
 
     }
 
@@ -34,7 +35,7 @@ class Sensei_Class_Quiz_Test extends WP_UnitTestCase {
                             'The quiz class function `save_user_answers` does not exist ' );
 
         // does this save_user_answers return false for bogus data
-        $this->assertFalse(  $woothemes_sensei->quiz->save_user_answers( -1000, -200, [] ) , 'save_user_answers does not return false for no existent users and lesson ' );
+        $this->assertFalse(  $woothemes_sensei->quiz->save_user_answers( [] ,-1000, -200 ) , 'save_user_answers does not return false for no existent users and lesson ' );
         $this->assertFalse(  $woothemes_sensei->quiz->save_user_answers( '', '' , '' ) , 'save_user_answers does not return false for empty parameters' );
 
 
@@ -47,19 +48,23 @@ class Sensei_Class_Quiz_Test extends WP_UnitTestCase {
             'post_status'    => 'publish',
             'post_type' => 'lesson' );
         $new_lesson_id = wp_insert_post ( $new_lesson_args );
-        $this->assertFalse(  $woothemes_sensei->quiz->save_user_answers( $new_lesson_id , '' , '' ) , 'save_user_answers does not return false for empty user' );
-        $this->assertFalse(  $woothemes_sensei->quiz->save_user_answers( $new_lesson_id , -500 , '' ) , 'save_user_answers does not return false for a non existant user' );
+        $this->assertFalse(  $woothemes_sensei->quiz->save_user_answers( '' , '',$new_lesson_id ) , 'save_user_answers does not return false for empty user' );
+        $this->assertFalse(  $woothemes_sensei->quiz->save_user_answers( '' ,  -500 , $new_lesson_id ) , 'save_user_answers does not return false for a non existant user' );
 
         // Test the answers array
-        $this->assertFalse(  $woothemes_sensei->quiz->save_user_answers( 1, $test_user_id , '' ) , 'save_user_answers does not return false for empty answer array' );
+        $this->assertFalse(  $woothemes_sensei->quiz->save_user_answers( 'Answers Text', $test_user_id,$new_lesson_id ) , 'save_user_answers does not return false if answers is not passed in as an array' );
+        $this->assertFalse(  $woothemes_sensei->quiz->save_user_answers( '' , $test_user_id ,$new_lesson_id ) , 'save_user_answers does not return false for empty answer array' );
         $this->assertFalse(  $woothemes_sensei->quiz->save_user_answers( '', '' , '' ) , 'save_user_answers does not return false incorrectly formatted answers' );
 
 
         //setup the answers to be saved
 
+
         //save the answers
 
-        // check the database to see if it was saved correctly
+        // did the correct data return a valid comment id on the lesson as a result?
+
+        // was the data that was just stored stored correctly ? Check the comment meta on the lesson id
 
     } // end testSaveUserAnswers
 
