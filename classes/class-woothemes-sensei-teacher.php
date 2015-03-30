00190e21dad30f0c5c47c7302f4cdf0b18dbb52a
Hugh Lashbrooke <hugh@woothemes.com>
2015-03-30 11:32:45 +0200

    Forcing all Analysis & Grading queries to filter by author when teacher is logged in Updates #459

--- classes/class-woothemes-sensei-teacher.php
+++ classes/class-woothemes-sensei-teacher.php
@@ -57,6 +57,7 @@ class WooThemes_Sensei_Teacher {
         add_filter( 'pre_get_posts', array( $this, 'course_analysis_teacher_access_limit' ) );
         add_filter( 'wp_count_posts', array( $this, 'list_table_counts' ), 10, 3 );
 
+        add_action( 'pre_get_posts', array( $this, 'filter_queries' ) );
     } // end __constructor()
 
     /**
@@ -92,9 +93,9 @@ class WooThemes_Sensei_Teacher {
      * @access protected
      */
     protected function add_capabilities ( ) {
-        // if this is not a valid WP_Role object exit without adding anything
-        if(  !is_a( $this->teacher_role, 'WP_Role' ) || empty( $this->teacher_role ) ) {
 
+        // if this is not a valid WP_Role object exit without adding anything
+        if(  ! is_a( $this->teacher_role, 'WP_Role' ) || empty( $this->teacher_role ) ) {
             return;
         }
 
@@ -475,6 +476,23 @@ class WooThemes_Sensei_Teacher {
         }
 
         return $counts;
+    }
+
+    public function filter_queries ( $query ) {
+        global $current_user;
+
+        if( ! $this->is_admin_teacher() ) {
+            return;
+        }
+
+        if( isset( $_GET['page'] ) ) {
+            switch( $_GET['page'] ) {
+                case 'sensei_grading':
+                case 'sensei_analysis':
+                    $query->set( 'author', $current_user->ID );
+                break;
+            }
+        }
     }
 
 } // End Class
