f836b44dc52d835c5dc67d29644a93d3ed965d33
Dwain Maralack <dwain.maralack@gmail.com>
2015-04-27 19:11:17 +0200

    update gulp file to included js/admin files as well  #577

--- gulpfile.js
+++ gulpfile.js
@@ -16,28 +16,26 @@ var minifyCSS = require('gulp-minify-css');
 var del = require('del');
 
 var paths = {
-    scripts: ['assets/js/*.js'],
+    scripts: ['assets/js/*.js' ],
+    adminScripts: ['assets/js/admin/*.js'],
     css: ['assets/css/*.css']
 };
 
 gulp.task('clean', function(cb) {
-    del( ['assets/js/*.min.js', 'assets/css/*.min.css'], cb );
+    del( ['assets/js/*.min.js','assets/js/admin/*.min.js', 'assets/css/*.min.css'], cb );
 
 });
 
-gulp.task('default', [ 'clean' ] , function() {
-    gulp.run('css') ;
-    gulp.run('javascript');
-});
+gulp.task('default', [ 'CSS','JS','adminJS' ] );
 
-gulp.task('css', function(){
+gulp.task('CSS',['clean'], function(){
     return gulp.src( paths.css )
         .pipe(minifyCSS({keepBreaks:false}))
         .pipe(rename({ extname: '.min.css' }))
         .pipe( gulp.dest('assets/css') );
 });
 
-gulp.task('javascript', function(){
+gulp.task('JS',['clean'], function(){
      return gulp.src( paths.scripts )
         // This will minify and rename to *.min.js
         .pipe(uglify())
@@ -45,10 +43,10 @@ gulp.task('javascript', function(){
         .pipe( gulp.dest( 'assets/js' ));
 });
 
-gulp.task('watch', function() {
-    // NOTE: this watch recusrively loops when .min changes, find a way to avoid this then
-    // activate the watch again.
-    // Watch .js files
-    //gulp.watch( ['assets/js/*.js' , '!assets/js/*.js' ], ['javascript']);
-   // gulp.watch('assets/css/*.css', ['css']);
+gulp.task('adminJS',['clean'], function(){
+    return gulp.src( paths.adminScripts )
+        // This will minify and rename to *.min.js
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe( gulp.dest( 'assets/js/admin' ));
 });
