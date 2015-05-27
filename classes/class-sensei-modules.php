3da8957263781c4e66b0867a4470c90dccffd1d4
Dwain Maralack <dwain.maralack@gmail.com>
2015-05-27 10:45:19 +0200

    The modules menu items should not be shown to the teachers. Only those who can manage_categories should see thie menu item. Closes #894

--- classes/class-sensei-modules.php
+++ classes/class-sensei-modules.php
@@ -841,7 +841,7 @@ class Sensei_Core_Modules
     public function register_modules_admin_menu_items()
     {
         //add the modules link under the Course main menu
-        add_submenu_page('edit.php?post_type=course', __('Modules', 'woothemes-sensei'), __('Modules', 'woothemes-sensei'), 'edit_lessons', 'edit-tags.php?taxonomy=module','' );
+        add_submenu_page('edit.php?post_type=course', __('Modules', 'woothemes-sensei'), __('Modules', 'woothemes-sensei'), 'manage_categories', 'edit-tags.php?taxonomy=module','' );
 
         // Regsiter new admin page for module ordering
         $hook = add_submenu_page('edit.php?post_type=course', __('Order Modules', 'woothemes-sensei'), __('Order Modules', 'woothemes-sensei'), 'edit_lessons', $this->order_page_slug, array($this, 'module_order_screen'));
