1a0033b4d93afa48a7ccd3d531b9194421289af3
Dwain Maralack <dwain.maralack@gmail.com>
2015-05-28 11:34:41 +0300

    define the wc dependancies as static because this is how it is called

--- inc/class-wc-dependencies.php
+++ inc/class-wc-dependencies.php
@@ -8,7 +8,7 @@ class WC_Dependencies {
 
 	private static $active_plugins;
 
-	function init() {
+	public static function init() {
 
 		self::$active_plugins = (array) get_option( 'active_plugins', array() );
 
@@ -16,7 +16,7 @@ class WC_Dependencies {
 			self::$active_plugins = array_merge( self::$active_plugins, get_site_option( 'active_sitewide_plugins', array() ) );
 	}
 
-	function woocommerce_active_check() {
+    public static function woocommerce_active_check() {
 
 		if ( ! self::$active_plugins ) self::init();
 
