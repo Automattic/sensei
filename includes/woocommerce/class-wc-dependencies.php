070d00236a89cde91e989eb1e2a73909fce708fa
Dwain Maralack <dwain.maralack@gmail.com>
2015-06-18 13:50:36 +0200

    restructure Sensei. Rename classes to includes. Move the inc folder contents  into includes/lib. Change calls to included class to point to the new locations.

--- includes/woocommerce/class-wc-dependencies.php
+++ includes/woocommerce/class-wc-dependencies.php
@@ -26,4 +26,20 @@ class WC_Dependencies {
 
 }
 
+/**
+ * Functions used by plugins
+ */
+if ( ! class_exists( 'WC_Dependencies' ) )
+    require_once 'class-wc-dependencies.php';
+
+/**
+ * WC Detection
+ */
+if ( ! function_exists( 'is_woocommerce_active' ) ) {
+    function is_woocommerce_active() {
+        return WC_Dependencies::woocommerce_active_check();
+    }
+}
+
+
 
