87af89ab73dd76ee3b7d6baa15de342ffed7e21d
Claudio Sanches <contato@claudiosmweb.com>
2015-09-22 16:06:32 -0300

    Created gulp pot task and updated the .pot file

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,14 @@
  * 3) Run gulp to mifiy javascript and css using the 'gulp' command.
  */
 
-var gulp = require( 'gulp' );
-var rename = require( 'gulp-rename' );
-var uglify = require( 'gulp-uglify' );
+var gulp      = require( 'gulp' );
+var rename    = require( 'gulp-rename' );
+var uglify    = require( 'gulp-uglify' );
 var minifyCSS = require( 'gulp-minify-css' );
-var chmod = require( 'gulp-chmod' );
-var del = require( 'del' );
+var chmod     = require( 'gulp-chmod' );
+var del       = require( 'del' );
+var wpPot     = require( 'gulp-wp-pot' );
+var sort      = require( 'gulp-sort' );
 
 var paths = {
 	scripts: ['assets/js/*.js' ],
@@ -44,11 +46,21 @@ gulp.task( 'JS',['clean'], function() {
 		.pipe( gulp.dest( 'assets/js' ));
 });
 
-gulp.task( 'adminJS',['clean'], function(){
+gulp.task( 'adminJS',['clean'], function() {
 	return gulp.src( paths.adminScripts )
 		// This will minify and rename to *.min.js
 		.pipe( uglify() )
 		.pipe( rename({ extname: '.min.js' }) )
 		.pipe( chmod( 644 ) )
 		.pipe( gulp.dest( 'assets/js/admin' ) );
+});
+
+gulp.task( 'pot', function() {
+	return gulp.src( [ '**/**.php', '!node_modules/**'] )
+		.pipe( sort() )
+		.pipe( wpPot({
+			domain: 'woothemes-sensei',
+			bugReport: 'https://www.transifex.com/woothemes/sensei-by-woothemes/'
+		}) )
+		.pipe( gulp.dest( 'lang' ) );
 });
