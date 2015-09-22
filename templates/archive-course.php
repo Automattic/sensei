d055efed41d5efeb2d8f6d3bd77db57b62db831f
Dwain Maralack <dwain.maralack@gmail.com>
2015-09-22 20:11:58 +0200

    move archive before loops outside of the if have posts to ensrue that we can add functionality even if no posts have been found  #743

--- templates/archive-course.php
+++ templates/archive-course.php
@@ -19,6 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <?php get_header(); ?>
 
     <?php
+
         /**
          * sensei_before_main_content hook
          *
@@ -26,6 +27,7 @@ if ( ! defined( 'ABSPATH' ) ) {
          * @hooked sensei_output_content_wrapper - 10 (outputs opening divs for the content)
          */
         do_action('sensei_before_main_content');
+
     ?>
 
     <?php
@@ -39,38 +41,38 @@ if ( ! defined( 'ABSPATH' ) ) {
 
     ?>
 
+    <?php
+
+        /**
+         * action before course archive loop
+         *
+         * @deprecated since 1.9.0 use sensei_loop_course_before instead
+         * @hooked Sensei_Templates::deprecated_archive_hook 80
+         */
+        do_action( 'sensei_archive_before_course_loop' );
+
+    ?>
+
     <?php if ( have_posts() ): ?>
 
-        <?php
-
-            /**
-             * action before course archive loop
-             *
-             * @deprecated since 1.9.0 use sensei_loop_course_before instead
-             * @hooked Sensei_Templates::deprecated_archive_hook 80
-             */
-            do_action( 'sensei_archive_before_course_loop' );
-
-        ?>
-
         <?php Sensei_Templates::get_template( 'loop-course.php' ); ?>
-
-        <?php
-
-            /**
-             * action after course archive  loop
-             *
-             * @deprecated since 1.9.0 use sensei_loop_course_after instead.
-             */
-            do_action( 'sensei_archive_after_course_loop' );
-
-        ?>
 
     <?php else: ?>
 
         <p><?php _e( 'No courses found that match your selection.', 'woothemes-sensei' ); ?></p>
 
     <?php  endif; // End If Statement ?>
+
+    <?php
+
+        /**
+         * action after course archive  loop
+         *
+         * @deprecated since 1.9.0 use sensei_loop_course_after instead.
+         */
+        do_action( 'sensei_archive_after_course_loop' );
+
+    ?>
 
     <?php
 
