6be33fae537bdd1c0620c6b4469d2b3dd4fb30d5
Claudio Sanches <contato@claudiosmweb.com>
2015-09-22 15:49:10 -0300

    Fixed wp coding standards in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,47 +9,46 @@
  * 3) Run gulp to mifiy javascript and css using the 'gulp' command.
  */
 
-var gulp = require('gulp');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var minifyCSS = require('gulp-minify-css');
-var chmod = require('gulp-chmod');
-var del = require('del');
+var gulp = require( 'gulp' );
+var rename = require( 'gulp-rename' );
+var uglify = require( 'gulp-uglify' );
+var minifyCSS = require( 'gulp-minify-css' );
+var chmod = require( 'gulp-chmod' );
+var del = require( 'del' );
 
 var paths = {
-    scripts: ['assets/js/*.js' ],
-    adminScripts: ['assets/js/admin/*.js'],
-    css: ['assets/css/*.css']
+	scripts: ['assets/js/*.js' ],
+	adminScripts: ['assets/js/admin/*.js'],
+	css: ['assets/css/*.css']
 };
 
-gulp.task('clean', function(cb) {
-    del( ['assets/js/*.min.js','assets/js/admin/*.min.js', 'assets/css/*.min.css'], cb );
-
+gulp.task( 'clean', function( cb ) {
+	del( ['assets/js/*.min.js','assets/js/admin/*.min.js', 'assets/css/*.min.css'], cb );
 });
 
-gulp.task('default', [ 'CSS','JS','adminJS' ] );
+gulp.task( 'default', [ 'CSS','JS','adminJS' ] );
 
-gulp.task('CSS',['clean'], function(){
-    return gulp.src( paths.css )
-        .pipe(minifyCSS({keepBreaks:false}))
-        .pipe(rename({ extname: '.min.css' }))
-        .pipe( gulp.dest('assets/css') );
+gulp.task( 'CSS',['clean'], function() {
+	return gulp.src( paths.css )
+		.pipe( minifyCSS({ keepBreaks: false }) )
+		.pipe( rename({ extname: '.min.css' }) )
+		.pipe( gulp.dest( 'assets/css' ) );
 });
 
-gulp.task('JS',['clean'], function(){
-    return gulp.src( paths.scripts )
-        // This will minify and rename to *.min.js
-        .pipe(uglify())
-        .pipe(rename({ extname: '.min.js' }))
-        .pipe(chmod(644))
-        .pipe( gulp.dest( 'assets/js' ));
+gulp.task( 'JS',['clean'], function() {
+	return gulp.src( paths.scripts )
+		// This will minify and rename to *.min.js
+		.pipe( uglify() )
+		.pipe( rename({ extname: '.min.js' }) )
+		.pipe( chmod( 644 ) )
+		.pipe( gulp.dest( 'assets/js' ));
 });
 
-gulp.task('adminJS',['clean'], function(){
-    return gulp.src( paths.adminScripts )
-        // This will minify and rename to *.min.js
-        .pipe(uglify())
-        .pipe(rename({ extname: '.min.js' }))
-        .pipe(chmod(644))
-        .pipe( gulp.dest( 'assets/js/admin' ));
+gulp.task( 'adminJS',['clean'], function(){
+	return gulp.src( paths.adminScripts )
+		// This will minify and rename to *.min.js
+		.pipe( uglify() )
+		.pipe( rename({ extname: '.min.js' }) )
+		.pipe( chmod( 644 ) )
+		.pipe( gulp.dest( 'assets/js/admin' ) );
 });
