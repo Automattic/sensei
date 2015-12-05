e69aed1dbc2f1e870536a54b4e7c156857044f63
Dwain Maralack <dwain.maralack@gmail.com>
2015-12-05 11:32:20 -0800

    introduce a new class Sensei_Student responsible for all learner related functionality

---
+++ includes/class-sensei-student.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Sensei_Student
+ *
+ * The class is responsible for all student specific functionality and helper functions
+ *
+ * @package WordPress
+ * @subpackage Sensei
+ * @category Core
+ * @author Automattic
+ * @since 1.9.0
+ */
+class Sensei_Student{
+
+
+
+}
