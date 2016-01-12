d2b88c4f9a5c2d884fea94c447f75af3dc74f0db
Dwain Maralack <dwain.maralack@gmail.com>
2016-01-12 09:01:28 +0200

    make sure messages do not take over the main Sensei admin menu link. Fixes #1245

--- includes/class-sensei-posttypes.php
+++ includes/class-sensei-posttypes.php
@@ -374,7 +374,7 @@ class Sensei_PostTypes {
 			    'public' => true,
 			    'publicly_queryable' => true,
 			    'show_ui' => true,
-			    'show_in_menu' => 'sensei',
+			    'show_in_menu' => 'admin.php?page=sensei',
 			    'show_in_nav_menus' => true,
 			    'query_var' => true,
 			    'exclude_from_search' => true,
