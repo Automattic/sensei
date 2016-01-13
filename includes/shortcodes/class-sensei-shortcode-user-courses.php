ba27ef4686b33bac7b0242534ee6797bc0481063
Dwain Maralack <dwain.maralack@gmail.com>
2016-01-13 17:20:14 +0200

    show the login form on the sensei_user_courses shortcode when users are logged out closes #1220

--- includes/shortcodes/class-sensei-shortcode-user-courses.php
+++ includes/shortcodes/class-sensei-shortcode-user-courses.php
@@ -64,6 +64,8 @@ class Sensei_Shortcode_User_Courses implements Sensei_Shortcode_Interface {
     public function __construct( $attributes, $content, $shortcode ){
 
         if(!  is_user_logged_in() ) {
+            // show the login form
+            Sensei_Templates::get_template('user/login-form.php');
             return;
         }
 
