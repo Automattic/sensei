60f7c4ca778828d30f3dbd2fa13b9f3d7872549b
Dwain Maralack <dwain.maralack@gmail.com>
2016-02-26 15:52:02 +0200

    hardcode the db settings

--- tests/bin/install.sh
+++ tests/bin/install.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 3 ]; then
-	echo "usage: $0 <db-name> <db-user> <db-pass> [db-host] [wp-version]"
-	exit 1
-fi
+#if [ $# -lt 3 ]; then
+#	echo "usage: $0 <db-name> <db-user> <db-pass> [db-host] [wp-version]"
+#	exit 1
+#fi
 
-DB_NAME=$1
-DB_USER=$2
-DB_PASS=$3
-DB_HOST=${4-localhost}
-WP_VERSION=${5-latest}
+DB_NAME='sensei_test'
+DB_USER='root'
+DB_PASS=''
+DB_HOST='localhost'
+WP_VERSION=4.4.2
 
 WP_TESTS_DIR=${WP_TESTS_DIR-/tmp/wordpress-tests-lib}
 WP_CORE_DIR=${WP_CORE_DIR-/tmp/wordpress/}
