7140115f101f22b4e15d760dd3e5f88cf54d6a6a
Dwain Maralack <dwain.maralack@gmail.com>
2016-04-12 11:04:25 +0200

    Switch next arrows to be after text. Fixes #1243. from Riaan

--- templates/globals/pagination-quiz.php
+++ templates/globals/pagination-quiz.php
@@ -17,6 +17,6 @@ $next_lesson_id = absint( $nav_id_array['next_lesson'] );
 if ( ( 0 < $previous_lesson_id ) || ( 0 < $next_lesson_id ) ) { ?>
     <nav id="post-entries" class="post-entries fix">
         <?php if ( 0 < $previous_lesson_id ) { ?><div class="nav-prev fl"><a href="<?php echo esc_url( get_permalink( $previous_lesson_id ) ); ?>" rel="prev"><span class="meta-nav"></span> <?php echo get_the_title( $previous_lesson_id ); ?></a></div><?php } ?>
-        <?php if ( 0 < $next_lesson_id ) { ?><div class="nav-next fr"><a href="<?php echo esc_url( get_permalink( $next_lesson_id ) ); ?>" rel="prev"><span class="meta-nav"></span> <?php echo get_the_title( $next_lesson_id ); ?></a></div><?php } ?>
+	    <?php if ( 0 < $next_lesson_id ) { ?><div class="nav-next fr"><a href="<?php echo esc_url( get_permalink( $next_lesson_id ) ); ?>" rel="prev"><?php echo get_the_title( $next_lesson_id ); ?> <span class="meta-nav"></span></a></div><?php } ?>
     </nav><!-- #post-entries -->
 <?php } ?>
