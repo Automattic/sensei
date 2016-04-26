0a1014fb5a364c1844ec980d4ddbf40d4e3781d8
Dan Johnson <dan.johnson@automattic.com>
2016-04-26 10:11:59 +0100

    Version bump for 1.9.5

--- woothemes-sensei.php
+++ woothemes-sensei.php
@@ -3,12 +3,12 @@
 Plugin Name: Sensei
 Plugin URI: http://www.woothemes.com/products/sensei/
 Description: A course management plugin that offers the smoothest platform for helping you teach anything.
-Version: 1.9.4
+Version: 1.9.5
 Author: WooThemes
 Author URI: http://www.woothemes.com/
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 Requires at least: 4.1
-Tested up to: 4.2
+Tested up to: 4.5
 Text Domain: woothemes-sensei
 Domain path: /lang/
 */
@@ -50,7 +50,7 @@ Domain path: /lang/
     }
 
 	// set the sensei version number
-	Sensei()->version = '1.9.4';
+	Sensei()->version = '1.9.5';
 
     //backwards compatibility
     global $woothemes_sensei;
