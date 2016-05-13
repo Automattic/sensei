8ea70ddf014afd7e56f1a4c02643c924d48ff5ca
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2016-05-13 12:02:24 +0200

    Inlinde doc for WPML hooks

--- includes/wpml/class-sensei-wpml.php
+++ includes/wpml/class-sensei-wpml.php
@@ -6,10 +6,32 @@ class Sensei_WPML {
 	}
 	
 	public function sensei_before_mail($email_address) {
+		/**
+		* Switch language for email
+		*
+		* Allows WPML to switch current language to one preferred by email recipient. 
+		* WPML checks language set in user preferences and applies it for email 
+		* string localisation
+		* It runs before any email string is obtained and localised
+		*
+		* @since 1.9.6
+		*
+		* @param string  $email_address Recipient's email address 
+		*/
 		do_action('wpml_switch_language_for_email', $email_address);
 	}
 	
 	public function sensei_after_sending_email() {
+		/**
+		* Restore language after sending email
+		*
+		* Allows WPML to switch language to the last one before switching with 
+		* action 'wpml_switch_language_for_email'
+		* It runs just after wp_mail() call
+		* No params
+		*
+		* @since 1.9.6
+		*/
 		do_action('wpml_restore_language_from_email');
 	}
 }
