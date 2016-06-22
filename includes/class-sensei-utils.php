1f2eaeede560ac680feca396f82f390e14d9dd04
Jeffry Evans <jeff@hodegeo.org>
2016-06-22 01:00:28 -0500

    Added $lesson_id and $user_id to filter

--- includes/class-sensei-utils.php
+++ includes/class-sensei-utils.php
@@ -1671,7 +1671,7 @@ class Sensei_Utils {
 			 *
 			 * @param string  	$user_lesson_status	User lesson status
 			 * @param int  		$lesson_id	ID of lesson
-			 * @param in	  	$user_id	ID of user
+			 * @param int	  	$user_id	ID of user
 			 */
 			$user_lesson_status = apply_filters( 'sensei_user_completed_lesson', $user_lesson_status, $lesson_id, $user_id );
 			
