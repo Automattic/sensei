67dded06cdd9d5a5517e8a9ddc2255b8d16c5fad
Nicola Mustone <mustone.nicola@gmail.com>
2016-10-14 09:39:27 +0300

    Remove extra * in code

--- woothemes-sensei.php
+++ woothemes-sensei.php
@@ -27,7 +27,6 @@ Domain path: /lang/
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-*
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
