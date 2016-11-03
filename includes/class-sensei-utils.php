7da4cff4cf85ed5c903c95378dc6fb484efc7aae
Panos Kountanis <panosktn@gmail.com>
2016-11-03 12:04:06 +0200

    Sensei_WC: Don't check cart unless in frontend or doing Ajax
    Otherwise Fatals would occur when trying to access `WC()->cart` methods,
    since it is NULL.
    
    fixes #1622

--- includes/class-sensei-utils.php
+++ includes/class-sensei-utils.php
@@ -2331,6 +2331,25 @@ class Sensei_Utils {
 
         return $merged_array;
     }
+
+	/**
+	 * What type of request is this?
+	 *
+	 * @param  string $type admin, ajax, cron or frontend.
+	 * @return bool
+	 */
+	public static function is_request( $type ) {
+		switch ( $type ) {
+			case 'admin' :
+				return is_admin();
+			case 'ajax' :
+				return defined( 'DOING_AJAX' );
+			case 'cron' :
+				return defined( 'DOING_CRON' );
+			case 'frontend' :
+				return ( ! is_admin() || defined( 'DOING_AJAX' ) ) && ! defined( 'DOING_CRON' );
+		}
+	}
 } // End Class
 
 /**
