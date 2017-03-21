f14af2cbe5109736506aebf81803cc052d200a80
Panos Kountanis <panosktn@gmail.com>
2017-03-21 20:45:19 +0200

    introduce Sensei_Domain_Models_Exception #1741

---
+++ includes/domain-models/class-sensei-domain-models-exception.php
@@ -0,0 +1,14 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+} // Exit if accessed directly
+
+
+/**
+ * Class Sensei_Domain_Models_Exception
+ * @package Domain_Models
+ * @since 1.9.13
+ */
+class Sensei_Domain_Models_Exception extends Exception {
+}
