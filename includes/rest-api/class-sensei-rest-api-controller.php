d470c30f9fb49edf3b8ac01eac1fbe802d310048
Panos Kountanis <panosktn@gmail.com>
2017-03-21 19:21:31 +0200

    introduce REST API Controller refs #1741

---
+++ includes/rest-api/class-sensei-rest-api-controller.php
@@ -0,0 +1,28 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+} // Exit if accessed directly
+
+class Sensei_REST_API_Controller extends WP_REST_Controller {
+    /**
+     * @var Sensei_REST_API_V1
+     */
+    protected $api;
+
+    /**
+     * Sensei_REST_API_Controller constructor.
+     * @param $api Sensei_REST_API_V1
+     */
+    public function __construct( $api ) {
+        $this->api = $api;
+    }
+
+    protected function succeed( $data ) {
+        return new WP_REST_Response( $data, 200 );
+    }
+
+    protected function not_found( $message ) {
+        return new WP_REST_Response( array( 'message' => $message ), 404 );
+    }
+}
