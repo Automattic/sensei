158c5fa0cd780788ac969b6d46699fe6e2ae0679
Panos Kountanis <panosktn@gmail.com>
2017-03-22 17:26:19 +0200

    add typing information and default values to sensei-domain-models-field #1741

--- includes/domain-models/class-sensei-domain-models-field.php
+++ includes/domain-models/class-sensei-domain-models-field.php
@@ -6,16 +6,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Sensei_Domain_Models_Field {
 
-    const FIELD = 'field';
-    const META = 'meta_field';
+    const FIELD   = 'field';
+    const META    = 'meta_field';
     const DERIVED = 'derived_field';
 
+    const STRING_VALUE  = 'string';
+    const INT_VALUE     = 'int';
+    const ARRAY_VALUE   = 'array';
+    const OBJECT_VALUE  = 'object';
+    const BOOLEAN_VALUE = 'boolean';
+    const ANY_VALUE     = 'any';
+
     public $before_return;
+    public $before_output;
     public $map_from;
     public $type;
     public $name;
     public $primary;
     public $required;
+    public $supported_outputs;
+    private $default_value;
+    private $value_type;
 
     private $accepted_field_types = array(
         self::FIELD,
@@ -30,12 +41,21 @@ class Sensei_Domain_Models_Field {
         if ( !isset( $args['type'] ) || !in_array( $args['type'], $this->accepted_field_types ) ) {
             throw new Sensei_Domain_Models_Exception( 'every field should have a type (one of ' . implode( ',', $this->accepted_field_types ) . ')' );
         }
-        $this->name = $args['name'];
-        $this->type = $args['type'];
-        $this->map_from = isset( $args['map_from'] ) ? $args['map_from'] : null;
-        $this->before_return = isset( $args['before_return'] ) ? $args['before_return'] : null;
-        $this->primary = isset( $args['primary'] ) ? (bool)$args['primary'] : false;
-        $this->required = isset( $args['required'] ) ? (bool)$args['required'] : false;
+        $this->name              = $args['name'];
+        $this->type              = $args['type'];
+        $this->map_from          = $this->value_or_default( $args, 'map_from' );
+        $this->before_return     = $this->value_or_default( $args, 'before_return' );
+        $this->primary           = (bool)$this->value_or_default( $args, 'primary', false );
+        $this->required          = (bool)$this->value_or_default( $args, 'required', false );
+        $this->supported_outputs = $this->value_or_default( $args, 'supported_outputs', array( 'json' ) );
+        $this->value_type        = $this->value_or_default( $args, 'value_type', 'any' );
+        $this->default_value     = $this->value_or_default( $args, 'default_value' );
+
+
+    }
+
+    private function value_or_default( $args, $name, $default = null ) {
+        return isset( $args[$name] ) ? $args[$name] : $default;
     }
 
     public function is_meta_field() {
@@ -48,5 +68,53 @@ class Sensei_Domain_Models_Field {
 
     public function is_field() {
         return $this->type === self::FIELD;
+    }
+
+    public function get_name_to_map_from() {
+        if ( isset( $this->map_from ) && !empty( $this->map_from ) ) {
+            return $this->map_from;
+        }
+
+        return $this->name;
+    }
+
+    public function get_default_value() {
+        if ( isset( $this->default_value ) && !empty( $this->default_value ) ) {
+            return ( is_array( $this->default_value ) && is_callable( $this->default_value ) ) ? call_user_func( $this->default_value ) : $this->default_value;
+        }
+
+        if ( self::INT_VALUE === $this->value_type ) {
+            return 0;
+        }
+
+        if ( self::STRING_VALUE === $this->value_type ) {
+            return '';
+        }
+
+        if ( self::ARRAY_VALUE === $this->value_type ) {
+            return array();
+        }
+
+        if ( self::OBJECT_VALUE === $this->value_type ) {
+            return null;
+        }
+
+        return null;
+    }
+
+    public function cast_value( $value ) {
+        if ( self::INT_VALUE === $this->value_type ) {
+            return intval( $value, 10 );
+        }
+
+        if ( self::STRING_VALUE === $this->value_type ) {
+            return '' . $value;
+        }
+
+        if ( self::ARRAY_VALUE === $this->value_type ) {
+            return (array)$value;
+        }
+
+        return $value;
     }
 }
