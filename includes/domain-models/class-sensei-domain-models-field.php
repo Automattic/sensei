e1a65a23f7769dff58f3e32c66768d4f76c466b4
Panos Kountanis <panosktn@gmail.com>
2017-03-22 11:32:43 +0200

    add required and primary domain model field properties #1741

--- includes/domain-models/class-sensei-domain-models-field.php
+++ includes/domain-models/class-sensei-domain-models-field.php
@@ -6,32 +6,36 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Sensei_Domain_Models_Field {
 
-    const TYPE_FIELD = 'field';
+    const FIELD = 'field';
     const META = 'meta_field';
     const DERIVED = 'derived_field';
+
     public $before_return;
     public $map_from;
     public $type;
     public $name;
+    public $primary;
+    public $required;
 
     private $accepted_field_types = array(
-        self::TYPE_FIELD,
+        self::FIELD,
         self::META,
         self::DERIVED
     );
 
     public function __construct( $args ) {
         if ( !isset( $args['name'] ) ) {
-            throw new Exception( 'every field should have a name' );
+            throw new Sensei_Domain_Models_Exception( 'every field should have a name' );
         }
         if ( !isset( $args['type'] ) || !in_array( $args['type'], $this->accepted_field_types ) ) {
-            throw new Exception( 'every field should have a type (one of ' . implode( ',', $this->accepted_field_types ) . ')' );
+            throw new Sensei_Domain_Models_Exception( 'every field should have a type (one of ' . implode( ',', $this->accepted_field_types ) . ')' );
         }
         $this->name = $args['name'];
         $this->type = $args['type'];
         $this->map_from = isset( $args['map_from'] ) ? $args['map_from'] : null;
         $this->before_return = isset( $args['before_return'] ) ? $args['before_return'] : null;
-
+        $this->primary = isset( $args['primary'] ) ? (bool)$args['primary'] : false;
+        $this->required = isset( $args['required'] ) ? (bool)$args['required'] : false;
     }
 
     public function is_meta_field() {
@@ -43,6 +47,6 @@ class Sensei_Domain_Models_Field {
     }
 
     public function is_field() {
-        return $this->type === self::TYPE_FIELD;
+        return $this->type === self::FIELD;
     }
 }
