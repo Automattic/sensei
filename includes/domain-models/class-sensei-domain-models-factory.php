fc02b72ea903feb417be22472f379ab497cb807e
Panos Kountanis <panosktn@gmail.com>
2017-03-23 16:13:56 +0000

    introduce Factory #1741

---
+++ includes/domain-models/class-sensei-domain-models-factory.php
@@ -0,0 +1,91 @@
+<?php
+
+
+class Sensei_Domain_Models_Factory {
+
+
+    /**
+     * @var Sensei_Domain_Models_Registry
+     */
+    private $registry;
+    /**
+     * @var string|object
+     */
+    private $klass;
+
+    function __construct( $klass, $registry ) {
+        $this->registry = $registry;
+        $this->klass = $klass;
+    }
+
+    /**
+     * @param $request
+     * @return Sensei_Domain_Models_Model_Abstract
+     */
+    public function new_from_request( $request )
+    {
+        $fields = $this->registry->get_field_declarations( $this->klass );
+        $field_data = array();
+        foreach ($fields as $field) {
+            if (isset($request[$field->name])) {
+                $field_data[$field->name] = $request[$field->name];
+            } else {
+                $field_data[$field->name] = $field->get_default_value();
+            }
+        }
+
+        return $this->create_object( $field_data );
+    }
+
+    public function all() {
+        $results = array();
+        foreach ($this->get_entities() as $entity) {
+            $results[] = $this->create_object($entity);
+        }
+        return new Sensei_Domain_Models_Model_Collection($results);
+    }
+
+    public function find_one_by_id( $id) {
+        $entity = $this->get_entity( $id );
+        return !empty($entity) ? $this->create_object( $entity ) : null;
+    }
+
+    public function get_field_declarations( $filter_by_type = null ) {
+        return $this->registry->get_field_declarations( $filter_by_type );
+    }
+
+    /**
+     * @param $id unique id
+     * @throws Sensei_Domain_Models_Exception
+     * return object|null
+     */
+    public function get_entity( $id) {
+        return $this->call_fn( 'get_entity', $id);
+    }
+
+    /**
+     * @throws Sensei_Domain_Models_Exception
+     * @return array
+     */
+    public function get_entities() {
+        return $this->call_fn( 'get_entities' );
+    }
+
+    private function call_fn() {
+        $args = func_get_args();
+        $fn_name = array_shift( $args );
+        return call_user_func_array( array( $this->get_domain_model_class( $this->klass ), $fn_name ), $args );
+    }
+
+    public function create_object( $entity) {
+        $klass = $this->get_domain_model_class( $this->klass );
+        return new $klass( $entity );
+    }
+
+    private function get_domain_model_class( $thing ) {
+        if (!is_string( $thing ) ) {
+            $thing = get_class( $thing );
+        }
+        return $thing;
+    }
+}
