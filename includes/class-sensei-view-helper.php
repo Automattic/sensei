83079a4a35ef4576f7724975e7f04c8271888825
Panos Kountanis <panosktn@gmail.com>
2017-03-24 11:08:06 +0000

    refactor question point formatting to use the formatting from the setting #1456

--- includes/class-sensei-view-helper.php
+++ includes/class-sensei-view-helper.php
@@ -11,7 +11,6 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class Sensei_View_Helper {
     public function format_question_points( $points ) {
-        $styles_disabled = (bool)Sensei_Utils::get_setting_as_flag( 'styles_disable', 'sensei_disable_styles' );
         $format = '[%s]';
         if ( isset( Sensei()->settings->settings[ 'quiz_question_points_format' ] ) ) {
             $maybe_format = Sensei()->settings->settings[ 'quiz_question_points_format' ];
@@ -20,6 +19,6 @@ class Sensei_View_Helper {
             }
         }
         $formatted_points = !empty( $format ) ? sprintf( $format, $points ) : $points;
-        return $styles_disabled ? $formatted_points : '<span class="grade">' . $formatted_points . '</span>';
+        return '<span class="grade">' . $formatted_points . '</span>';
     }
 }
