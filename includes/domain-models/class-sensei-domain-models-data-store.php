291a9f0e90db1a08fe9f959987da489681856222
Panos Kountanis <panosktn@gmail.com>
2017-03-29 14:28:27 +0300

    introduce data store interface

---
+++ includes/domain-models/class-sensei-domain-models-data-store.php
@@ -0,0 +1,31 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+} // Exit if accessed directly
+
+/**
+ * Interface Sensei_Domain_Models_Data_Store
+ */
+interface Sensei_Domain_Models_Data_Store {
+
+    /**
+     * @return Sensei_Domain_Models_Model_Collection
+     */
+    public function get_entities();
+
+
+    /**
+     * @param $id the id of the entity
+     * @return Sensei_Domain_Models_Model_Abstract
+     */
+    public function get_entity( $id );
+
+
+    /**
+     * @param $entity Sensei_Domain_Models_Model_Abstract
+     * @param $field_declaration Sensei_Domain_Models_Field_Declaration
+     * @return mixed
+     */
+    public function get_meta_field_value( $entity, $field_declaration );
+}
