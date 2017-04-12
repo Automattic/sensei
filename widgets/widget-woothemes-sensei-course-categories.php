8eecbd815983f39ccac5db28790bfbc52268656f
Thad Roe <git@thadroe.com>
2017-04-12 03:22:53 -0500

    Removed dupe closing p tag in course categories widget admin

--- widgets/widget-woothemes-sensei-course-categories.php
+++ widgets/widget-woothemes-sensei-course-categories.php
@@ -138,7 +138,7 @@ class WooThemes_Sensei_Course_Categories_Widget extends WP_Widget {
 		<!-- Widget Show Hierarchy: Checkbox Input -->
 		<p>
 			<input type="checkbox" class="checkbox" id="<?php echo esc_attr( $this->get_field_id('hierarchical') ); ?>" name="<?php echo esc_attr( $this->get_field_name('hierarchical') ); ?>"<?php checked( $instance['hierarchical'], 'on' ); ?> />
-			<label for="<?php echo esc_attr( $this->get_field_id('hierarchical') ); ?>"><?php _e( 'Show hierarchy', 'woothemes-sensei' ); ?></label></p>
+			<label for="<?php echo esc_attr( $this->get_field_id('hierarchical') ); ?>"><?php _e( 'Show hierarchy', 'woothemes-sensei' ); ?></label>
 		</p>
 <?php
 	} // End form()
