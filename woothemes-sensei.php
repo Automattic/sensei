a1d3f5ef4bd2bf30c2ff813c8a182a1d4f604b89
Boro Sitnikovski <buritomath@gmail.com>
2017-04-12 14:56:45 +0200

    More updates to the header

--- woothemes-sensei.php
+++ woothemes-sensei.php
@@ -1,18 +1,20 @@
 <?php
 /*
-Plugin Name: Sensei
-Plugin URI: http://www.woocommerce.com/products/sensei/
-Description: A course management plugin that offers the smoothest platform for helping you teach anything.
-Version: 1.9.13
-Author: WooThemes
-Author URI: http://www.woocommerce.com/
-License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-Requires at least: 4.1
-Tested up to: 4.7.3
-Text Domain: woothemes-sensei
-Domain path: /lang/
-*/
-/*  Copyright 2013  WooThemes  (email : info@woocommerce.com)
+ * Plugin Name: Sensei
+ * Plugin URI: https://woocommerce.com/products/sensei/
+ * Description: A course management plugin that offers the smoothest platform for helping you teach anything.
+ * Version: 1.9.13
+ * Author: WooCommerce
+ * Author URI: https://woocommerce.com/
+ * License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Requires at least: 4.1
+ * Tested up to: 4.7.3
+ * Text Domain: woothemes-sensei
+ * Domain path: /lang/
+ *
+ * Woo: 152116:bad2a02a063555b7e2bee59924690763
+ */
+/*  Copyright 2013-2017  WooCommerce  (email : info@woocommerce.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as
