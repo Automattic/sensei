d3e91ee4224973536fec1b1b7d7b3dbfba9a984c
Dan Johnson <dan.johnson@automattic.com>
2017-05-11 12:12:42 +0100

    Add wrappers for Twenty Seventeen
    Closes #1664

--- includes/theme-integrations/twentyseventeen.php
+++ includes/theme-integrations/twentyseventeen.php
@@ -11,4 +11,32 @@
  *
  * @since 1.9.10
  */
-Class Sensei_Twentyseventeen extends Sensei__S{ }
+Class Sensei_Twentyseventeen extends Sensei__S{
+
+	/**
+     * Output opening wrappers
+     * @since 1.9.15
+     */
+    public function wrapper_start(){
+    ?>
+
+        <div class="wrap">
+			<div id="primary" class="content-area">
+				<main id="main" class="site-main" role="main">
+
+    <?php }
+
+    /**
+     * Output closing wrappers
+     *
+     * @since 1.9.15
+     */
+    public function wrapper_end(){ ?>
+
+				</main><!-- #main -->
+			</div><!-- #primary -->
+		</div><!-- .wrap -->
+
+	<?php }
+
+}
