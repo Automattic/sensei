00edee17ae10fad547b6a05794f091d6da4aedab
Panos Kountanis <panosktn@gmail.com>
2017-05-24 14:39:41 +0300

    Allow Memberships to Control Course Access
    see https://github.com/Automattic/sensei/issues/1814,
    https://github.com/Automattic/sensei/issues/1813
    
    - Removed the self::is_user_active_member check
    - PHPCS https://github.com/Automattic/sensei/issues/1813
    
    fixes #1813
    fixes #1814

--- includes/class-sensei-wc-memberships.php
+++ includes/class-sensei-wc-memberships.php
@@ -1,5 +1,20 @@
 <?php
+/**
+ * Sensei WooCommerce Memberships Integration
+ *
+ * All functions needed to integrate Sensei and WooCommerce Memberships
+ *
+ * @package Access-Management
+ * @author Automattic
+ */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Class Sensei_WC_Memberships
+ */
 class Sensei_WC_Memberships {
 	const WC_MEMBERSHIPS_PLUGIN_PATH = 'woocommerce-memberships/woocommerce-memberships.php';
 
@@ -7,6 +22,7 @@ class Sensei_WC_Memberships {
 
 	/**
 	 * Load WC Memberships integration hooks if WC Memberships is active
+	 *
 	 * @return void
 	 */
 	public static function load_wc_memberships_integration_hooks() {
@@ -23,6 +39,13 @@ class Sensei_WC_Memberships {
 		add_action( 'wc_memberships_user_membership_saved', array( __CLASS__, 'on_wc_memberships_user_membership_saved' ), 10, 2 );
 	}
 
+	/**
+	 * Is Course Access Restricted.
+	 *
+	 * @param bool $access_restricted Access Restricted.
+	 * @param int  $course_id Course ID.
+	 * @return bool
+	 */
 	public static function is_course_access_restricted( $access_restricted, $course_id ) {
 		if ( false === self::is_wc_memberships_active() ) {
 			return $access_restricted;
@@ -31,23 +54,35 @@ class Sensei_WC_Memberships {
 		return self::is_content_restricted( $course_id );
 	}
 
+	/**
+	 * Is content restricted?
+	 *
+	 * @param int $object_id The object id.
+	 * @return bool
+	 */
 	private static function is_content_restricted( $object_id ) {
 		if ( get_current_user_id() > 0 ) {
-			$access_restricted = !current_user_can( self::WC_MEMBERSHIPS_VIEW_RESTRICTED_POST_CONTENT, $object_id );
+			$access_restricted = ! current_user_can( self::WC_MEMBERSHIPS_VIEW_RESTRICTED_POST_CONTENT, $object_id );
 			return $access_restricted;
 		}
 
 		return wc_memberships_is_post_content_restricted( $object_id );
 	}
 
+	/**
+	 * Add Notice.
+	 *
+	 * @param string $content The content.
+	 * @return string
+	 */
 	public static function add_wc_memberships_notice( $content = '' ) {
 		global $post;
 		if ( false === self::is_wc_memberships_active() ) {
 			return $content;
 		}
 
-		if ( isset( $post->ID ) && !in_array( get_post_type( $post->ID ), array( 'course', 'lesson', 'quiz' ), true ) ||
-			 !self::is_content_restricted( $post->ID ) ) {
+		if ( isset( $post->ID ) && ! in_array( get_post_type( $post->ID ), array( 'course', 'lesson', 'quiz' ), true ) ||
+			 ! self::is_content_restricted( $post->ID ) ) {
 			return $content;
 		}
 		$message = wc_memberships()->get_frontend_instance()->get_content_restricted_message( $post->ID );
@@ -55,20 +90,25 @@ class Sensei_WC_Memberships {
 	}
 
 	/**
+	 * Display Start Course form to members only.
+	 *
 	 * Applied to the `sensei_display_start_course_form` filter to determine
 	 * if the 'start taking this course' form should be displayed for a given course.
 	 * If a course has membership rules, restrict to active logged in members.
 	 *
-	 * @param $course_id the course in question
-	 * @return int|bool the course id or false in case a restriction applies
+	 * @param bool $should_display Should Display.
+	 * @param int  $course_id The course in question.
+	 *
+	 * @return bool|int The course id or false in case a restriction applies.
 	 */
 	public static function display_start_course_form_to_members_only( $should_display, $course_id ) {
 
-		return !self::is_course_access_restricted( $should_display, $course_id );
+		return ! self::is_course_access_restricted( $should_display, $course_id );
 	}
 
 	/**
 	 * Determine if WC Memberships is installed and active
+	 *
 	 * @return bool
 	 */
 	public static function is_wc_memberships_active() {
@@ -76,25 +116,26 @@ class Sensei_WC_Memberships {
 			'WC_Memberships',
 			self::WC_MEMBERSHIPS_PLUGIN_PATH
 		);
-    }
+	}
 
 	/**
 	 * Start courses associated with new membership
 	 * so they show up on "my courses".
 	 *
 	 * Hooked into wc_memberships_user_membership_saved and wc_memberships_user_membership_created
-	 * @param $membership_plan
-	 * @param array $args
+	 *
+	 * @param mixed $membership_plan The Membership Plan.
+	 * @param array $args The args.
 	 */
 	public static function on_wc_memberships_user_membership_saved( $membership_plan, $args = array() ) {
 		$user_membership_id = isset( $args['user_membership_id'] ) ? absint( $args['user_membership_id'] ) : null;
 
-		if ( !$user_membership_id ) {
+		if ( ! $user_membership_id ) {
 			return;
 		}
 
 		$user_membership = wc_memberships_get_user_membership( $user_membership_id );
-		return self::start_courses_associated_with_membership( $user_membership );
+		self::start_courses_associated_with_membership( $user_membership );
 	}
 
 	/**
@@ -103,7 +144,7 @@ class Sensei_WC_Memberships {
 	 *
 	 * Hooked into wc_memberships_user_membership_status_changed
 	 *
-	 * @param WC_Memberships_User_Membership $user_membership the user membership
+	 * @param WC_Memberships_User_Membership $user_membership The user membership.
 	 */
 	public static function start_courses_associated_with_membership( $user_membership ) {
 
@@ -126,11 +167,6 @@ class Sensei_WC_Memberships {
 			return;
 		}
 
-		if ( false === self::is_user_active_member( $user_id, $membership_plan->get_id() ) ) {
-			// User is Inactive so just Bail for now
-			return;
-		}
-
 		$restricted_content = $membership_plan->get_restricted_content();
 		if ( empty( $restricted_content ) ) {
 			return;
@@ -144,9 +180,10 @@ class Sensei_WC_Memberships {
 			$course_id = $maybe_course->ID;
 
 			/**
-			 * filter sensei_wc_memberships_auto_start_course
-			 * determine if we should automatically start users on a specific course that is part of a user membership
-			 * and has not started yet.
+			 * Filter sensei_wc_memberships_auto_start_course
+			 *
+			 * Determine if we should automatically start users on a specific course
+			 * that is part of a user membership and has not started yet.
 			 *
 			 * @param bool $auto_start_courses
 			 * @param WC_Memberships_User_Membership $user_membership the user membership
@@ -162,11 +199,17 @@ class Sensei_WC_Memberships {
 		}
 	}
 
+	/**
+	 * Should we auto start any Courses thie Membership controls access to?
+	 *
+	 * @param WC_Memberships_User_Membership $user_membership User Membership.
+	 * @return bool
+	 */
 	private static function should_auto_start_membership_courses( $user_membership ) {
 		$auto_start_courses = (bool) Sensei()->settings->get( 'sensei_wc_memberships_auto_start_courses' );
 
 		/**
-		 * determine if we should automatically start users on any courses that are part of this user membership;
+		 * Determine if we should automatically start users on any courses that are part of this user membership;
 		 *
 		 * @param bool $auto_start_courses
 		 * @param WC_Memberships_User_Membership $user_membership the user membership
@@ -174,40 +217,13 @@ class Sensei_WC_Memberships {
 		return (bool) apply_filters( 'sensei_wc_memberships_auto_start_courses', $auto_start_courses, $user_membership );
 	}
 
-	public static function is_my_courses_page( $post_id ) {
-		return is_page() && intval( Sensei()->settings->get( 'my_course_page' ) ) === intval( $post_id );
-	}
-
 	/**
-	 * @param int $user_id
-	 * @param int $membership_plan_id
+	 * Is My Courses Page
+	 *
+	 * @param int $post_id Post Id.
 	 * @return bool
 	 */
-	private static function is_user_active_member( $user_id, $membership_plan_id )
-	{
-		if ( true === version_compare(wc_memberships()->get_version(), '1.7.0', '>=') ) {
-			/**
-			 * Sensei WC Memberships "start_courses_associated_with_membership_skip_cache" Filter
-			 *
-			 * Overrides `wc_memberships_is_user_active_member` cache control on WC Memberships >= 1.7.0
-			 * Busts cache when false. We default to false, as this is triggered on membership save/status-change and we don't want stale info
-			 *
-			 * @since 1.9.13
-			 *
-			 * @param bool $use_cache (default false)
-			 * @param int $user_id
-			 * @param int $membership_plan
-			 */
-			$use_cache = (bool)apply_filters(
-				'sensei_wc_memberships_start_courses_associated_with_membership_skip_cache',
-				false,
-				$user_id,
-				$membership_plan_id
-			);
-
-			return wc_memberships_is_user_active_member($user_id, $membership_plan_id, $use_cache);
-		} else {
-			return wc_memberships_is_user_active_member($user_id, $membership_plan_id);
-		}
+	public static function is_my_courses_page( $post_id ) {
+		return is_page() && intval( Sensei()->settings->get( 'my_course_page' ) ) === intval( $post_id );
 	}
 }
