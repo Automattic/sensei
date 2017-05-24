fede79b7346e3a9c894a8e47ae8b8b54fcc226d7
Panos Kountanis <panosktn@gmail.com>
2017-05-24 15:03:31 +0300

    Sensei - WC-Memberships: Restriction support for Lessons and Course videos #1814

--- includes/class-sensei-wc-memberships.php
+++ includes/class-sensei-wc-memberships.php
@@ -37,6 +37,9 @@ class Sensei_WC_Memberships {
 
 		add_action( 'wc_memberships_user_membership_status_changed', array( __CLASS__, 'start_courses_associated_with_membership' ) );
 		add_action( 'wc_memberships_user_membership_saved', array( __CLASS__, 'on_wc_memberships_user_membership_saved' ), 10, 2 );
+		// Adds Memberships restrictions support to Sensei Lessons and Optionally, Course Videos.
+		add_action( 'wp', array( __CLASS__, 'restrict_lesson_details' ) );
+		add_action( 'wp', array( __CLASS__, 'restrict_course_videos' ) );
 	}
 
 	/**
@@ -222,5 +225,68 @@ class Sensei_WC_Memberships {
 	 */
 	public static function is_my_courses_page( $post_id ) {
 		return is_page() && intval( Sensei()->settings->get( 'my_course_page' ) ) === intval( $post_id );
+	}
+
+	/**
+	 * Required: Restrict lesson videos & quiz links until the member has access to the lesson.
+	 * Used to ensure content dripping from Memberships is compatible with Sensei.
+	 *
+	 * This will also remove the "complete lesson" button until the lesson is available.
+	 */
+	function restrict_lesson_details() {
+		global $post;
+
+		// sanity checks.
+		if ( ! function_exists( 'wc_memberships_get_user_access_start_time' ) || ! function_exists( 'Sensei' ) || 'lesson' !== get_post_type( $post ) ) {
+			return;
+		}
+
+		// if access start time isn't set, or is after the current date, remove the video.
+		if ( ! wc_memberships_get_user_access_start_time( get_current_user_id(), 'view', array(
+			'lesson' => $post->ID,
+		) )
+			|| current_time( 'timestamp' ) < wc_memberships_get_user_access_start_time( get_current_user_id(), 'view', array(
+				'lesson' => $post->ID,
+			) ) ) {
+
+			remove_action( 'sensei_single_lesson_content_inside_after',  array( 'Sensei_Lesson', 'footer_quiz_call_to_action' ) );
+			remove_action( 'sensei_single_lesson_content_inside_before', array( 'Sensei_Lesson', 'user_lesson_quiz_status_message' ), 20 );
+
+			remove_action( 'sensei_lesson_video',           array( Sensei()->frontend, 'sensei_lesson_video' ), 10, 1 );
+			remove_action( 'sensei_lesson_meta',            array( Sensei()->frontend, 'sensei_lesson_meta' ), 10 );
+			remove_action( 'sensei_complete_lesson_button', array( Sensei()->frontend, 'sensei_complete_lesson_button' ) );
+		}
+	}
+
+
+	/**
+	 * Optional: Restrict course videos unless the member has access.
+	 * Used if you don't want to show course previews to non-members.
+	 */
+	function restrict_course_videos() {
+		global $post;
+
+		// sanity checks.
+		if ( ! function_exists( 'wc_memberships_get_user_access_start_time' ) || ! function_exists( 'Sensei' ) || 'course' !== get_post_type( $post ) ) {
+			return;
+		}
+
+		$restrict_course_video = (bool) Sensei()->settings->get( 'sensei_wc_memberships_restrict_course_video' );
+
+		if ( ! $restrict_course_video ) {
+			return;
+		}
+
+		// if access start time isn't set, or is after the current date, remove the video.
+		if ( ! wc_memberships_get_user_access_start_time( get_current_user_id(), 'view', array(
+			'course' => $post->ID,
+		) )
+			|| current_time( 'timestamp' ) < wc_memberships_get_user_access_start_time( get_current_user_id(), 'view', array(
+				'course' => $post->ID,
+			) ) ) {
+
+			remove_action( 'sensei_single_course_content_inside_before',  array( 'Sensei_Course', 'the_course_video' ), 40 );
+			remove_action( 'sensei_no_permissions_inside_before_content', array( 'Sensei_Course', 'the_course_video' ), 40 );
+		}
 	}
 }
