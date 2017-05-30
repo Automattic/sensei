4d7b1a72eaf5f8b338383aa26449c48193576137
Panos Kountanis <panosktn@gmail.com>
2017-05-30 19:03:05 +0300

    phpcs includes/class-sensei-notices.php #1817

--- includes/class-sensei-notices.php
+++ includes/class-sensei-notices.php
@@ -1,5 +1,13 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+/**
+ * All functionality pertaining to displaying of various notices on the frontend.
+ *
+ * @package Core
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
  * Sensei Notices Class
@@ -11,28 +19,34 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  *
  * @since 1.6.3
  */
-class Sensei_Notices{
+class Sensei_Notices {
 
 	/**
-	*  @var $notices
-	*/
+	 * Notices.
+	 *
+	 *  @var $notices
+	 */
 	protected $notices;
 
 	/**
+	 * Has Printed.
+	 *
 	 * @var boolean $has_printed
 	 */
 	protected $has_printed;
 
 	/**
+	 * The HTML allowed for message boxes.
+	 *
 	 * @var array The HTML allowed for message boxes.
 	 */
 	protected $allowed_html;
 
 	/**
-	*  constructor
- 	*/
-	public function __construct(){
-		//initialize the notices variable
+	 * Constructor
+	 */
+	public function __construct() {
+		// initialize the notices variable.
 		$this->notices = array();
 		$this->has_printed = false;
 		$this->allowed_html = array(
@@ -51,27 +65,28 @@ class Sensei_Notices{
 			),
 			'a' => array(
 				'href'  => array(),
-				'title' => array()
-			)
+				'title' => array(),
+			),
 		);
 	}
 
 	/**
 	 *  Add a notice to the array of notices for display at a later stage.
 	 *
-	 *
-	 * @param string $content
-	 * @param string $type defaults to alert options( alert, tick , download , info   )
+	 * @param string $content Content.
+	 * @param string $type Defaults to alert options( alert, tick , download , info   ).
 	 *
 	 * @return void
 	 */
-
-	public function add_notice( $content ,  $type = 'alert'   ){
-		// append the new notice
-		$this->notices[] = array('content' => $content , 'type'=> $type );
+	public function add_notice( $content, $type = 'alert' ) {
+		// append the new notice.
+		$this->notices[] = array(
+			'content' => $content,
+			'type' => $type,
+		);
 
 		// if a notice is added after we've printed print it immediately.
-		if( $this->has_printed  ){
+		if ( $this->has_printed ) {
 			$this->maybe_print_notices();
 		}
 
@@ -82,33 +97,31 @@ class Sensei_Notices{
 	 *
 	 * @return void
 	 */
+	public function maybe_print_notices() {
+		if ( count( $this->notices ) > 0 ) {
+			foreach ( $this->notices  as  $notice ) {
 
-	public function maybe_print_notices(){
-		if(  count( $this->notices ) > 0  ){
-			foreach ($this->notices  as  $notice) {
+				$classes = 'sensei-message ' . $notice['type'];
 
-				$classes = 'sensei-message '. $notice['type'];
-				$html = '<div class="'. esc_attr( $classes ) . '">'. wp_kses( $notice['content'], $this->allowed_html ) . '</div>';
-
-				echo $html;
+				echo '<div class="' . esc_attr( $classes ) . '">' . wp_kses( $notice['content'], $this->allowed_html ) . '</div>';
 			}
-			// empty the notice queue to avoid reprinting the same notices
+			// empty the notice queue to avoid reprinting the same notices.
 			$this->clear_notices();
 
 		}
 
-		// set this to print immediately if notices are added after the notices were printed
+		// set this to print immediately if notices are added after the notices were printed.
 		$this->has_printed = true;
 
 	} // end print_notice()
 
 	/**
-	*  Clear all notices
-	*
-	* @return void
-	*/
-	public function clear_notices(){
-		// assign an empty array to clear all existing notices
+	 *  Clear all notices
+	 *
+	 * @return void
+	 */
+	public function clear_notices() {
+		// assign an empty array to clear all existing notices.
 		$this->notices = array();
 	} // end clear_notices()
 
@@ -116,6 +129,7 @@ class Sensei_Notices{
 
 /**
  * Class Woothemes_Sensei_Notices
+ *
  * @ignore only for backward compatibility
  * @since 1.9.0
  */
