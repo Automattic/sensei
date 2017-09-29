260c3a82f87f31a9e1669b962e526eb6a80c9ad9
Panos Kountanis <panosktn@gmail.com>
2017-09-29 14:19:15 +0100

    Do not collapse menu on Messages. Fixes #1638

---
+++ assets/js/admin/message-menu-fix.js
@@ -0,0 +1,7 @@
+jQuery( document ).ready( function () {
+    var pageText = 'edit.php?post_type=sensei_message';
+    var areWeInAdminMessages = document.location.href.includes( pageText );
+    if ( areWeInAdminMessages ) {
+        jQuery( '#toplevel_page_sensei' ).addClass( 'wp-has-submenu wp-has-current-submenu wp-menu-open' );
+    }
+} );
