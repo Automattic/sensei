43850a420b8232ebb714ba3183fdcd67f5def27a
Panos Kountanis <panosktn@gmail.com>
2017-11-06 21:53:52 +0200

    ESLint assets/js/image-selectors.js

--- assets/js/image-selectors.js
+++ assets/js/image-selectors.js
@@ -1,13 +1,13 @@
-jQuery(document).ready( function($) {
+jQuery(document).ready( function() {
 	if ( jQuery( '.radio-images' ).length ) {
-		jQuery( '.radio-images' ).each( function ( i, e ) {
+		jQuery( '.radio-images' ).each( function () {
 			if ( jQuery( this ).is( ':checked' ) ) {
 				jQuery( this ).next( 'img.radio-image-thumb' ).addClass( 'active' );
 			}
 			jQuery( this ).hide();
 		});
 
-		jQuery( '.radio-image-thumb' ).click( function ( e ) {
+		jQuery( '.radio-image-thumb' ).click( function () {
 			jQuery( this ).addClass( 'active' ).siblings( '.active' ).removeClass( 'active' );
 			jQuery( this ).prev( 'input.radio-images' ).trigger( 'click' );
 		});
