714193bcb925413c1cac7f26c0a7a6e3dd58bcdf
Panos Kountanis <panosktn@gmail.com>
2017-11-06 22:11:01 +0200

    ESLint assets/js/user-dashboard.js

--- assets/js/user-dashboard.js
+++ assets/js/user-dashboard.js
@@ -1,4 +1,4 @@
-jQuery(document).ready( function($) {
+jQuery(document).ready( function() {
 
 	/***************************************************************************************************
 	 * 	1 - Helper Functions.
