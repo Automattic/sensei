c6512af0a0ea5113a0174ff6687899c8fcdc691a
Donna Peplinskie <donnapep@gmail.com>
2017-12-15 10:03:54 -0500

    Remove Sensei Welcome screen
    The content on the welcome screen doesn’t offer a lot of valuable
    information, and the What’s New section is stale.

--- includes/class-sensei-autoloader.php
+++ includes/class-sensei-autoloader.php
@@ -127,7 +127,6 @@ class Sensei_Autoloader {
             /**
              * Admin
              */
-            'Sensei_Welcome'            => 'admin/class-sensei-welcome.php' ,
             'Sensei_Learner_Management' => 'admin/class-sensei-learner-management.php' ,
 
             /**
