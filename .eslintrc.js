81cc233abe0d69fe8222f80b5a90d9a582c57b57
Donna Peplinskie <donnapep@gmail.com>
2017-12-21 07:00:06 -0500

    Improve React / JSX linting
    Uses some of the same rules as Gutenberg.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+    "parser": "babel-eslint",
     "env": {
         "browser": true,
         "es6": true
@@ -9,12 +10,29 @@ module.exports = {
         "woo_localized_data": true,
         "wp": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "plugins": [
+        "react"
+    ],
+    "settings": {
+        "react": {
+            "pragma": "wp"
+        }
+    },
     "rules": {
         "indent": [
             "error",
             "tab"
         ],
+        "jsx-quotes": "error",
         "linebreak-style": [
             "error",
             "unix"
@@ -23,6 +41,16 @@ module.exports = {
             "error",
             "single"
         ],
+        "react/display-name": "off",
+        "react/jsx-curly-spacing": [ "error", "always" ],
+        "react/jsx-equals-spacing": "error",
+        "react/jsx-indent": [ "error", "tab" ],
+        "react/jsx-indent-props": [ "error", "tab" ],
+        "react/jsx-key": "error",
+        "react/jsx-tag-spacing": "error",
+        "react/no-children-prop": "off",
+        "react/no-find-dom-node": "warn",
+        "react/prop-types": "off",
         "semi": [
             "error",
             "always"
