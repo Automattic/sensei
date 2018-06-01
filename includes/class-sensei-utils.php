36a6f3ea2f46c3fb43af84270db0d063a3fe9db2
Jake Oehler Morrison <jake.morrison@automattic.com>
2018-06-01 19:21:10 +0100

    Update lesson comment's date on completion

--- includes/class-sensei-utils.php
+++ includes/class-sensei-utils.php
@@ -712,6 +712,7 @@ class Sensei_Utils {
                     $comment = array();
                     $comment['comment_ID'] = $activity_logged;
                     $comment['comment_approved'] = $status;
+                    $comment['comment_date'] = current_time( 'mysql' );
                     wp_update_comment( $comment );
 
                 }
