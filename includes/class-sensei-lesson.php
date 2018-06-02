e119391249a838c65a43ad459dc71ed05b9c98c9
Jake Oehler Morrison <jake.morrison@automattic.com>
2018-06-02 12:36:33 +0100

    Explicitly check for course enrollment before starting lesson
    Prevents auto-enroll when previewing a lesson.

--- includes/class-sensei-lesson.php
+++ includes/class-sensei-lesson.php
@@ -3568,7 +3568,9 @@ class Sensei_Lesson {
 			return;
 		}
 
-		if ( ! sensei_can_user_view_lesson( $lesson_id, $user_id ) ) {
+		$lesson_course_id = get_post_meta( $lesson_id, '_lesson_course', true );
+		$user_taking_course = Sensei_Utils::user_started_course( $lesson_course_id, $user_id );
+		if ( ! $user_taking_course || ! sensei_can_user_view_lesson( $lesson_id, $user_id ) ) {
 			return;
 		}
 
