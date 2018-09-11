5c1df443ed847e104f9aa1867aca381c8aeb6b70
Jake Oehler Morrison <jake.morrison@automattic.com>
2018-09-11 18:08:35 +0100

    Add unsupported theme support for teacher archive pages

---
+++ includes/unsupported-theme-handlers/class-sensei-unsupported-theme-handler-teacher-archive.php
@@ -0,0 +1,105 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Sensei Unsupported Theme Handler for the Teacher Archive Page.
+ *
+ * Handles rendering the teacher archive page for themes that do not declare support for
+ * Sensei.
+ *
+ * @author Automattic
+ *
+ * @since 1.12.0
+ */
+class Sensei_Unsupported_Theme_Handler_Teacher_Archive
+	extends Sensei_Unsupported_Theme_Handler_Page_Imitator
+	implements Sensei_Unsupported_Theme_Handler_Interface {
+
+	/**
+	 * @var WP_User $author
+	 */
+	private $author;
+
+	/**
+	 * We can handle this request if it is for a teacher archive page.
+	 *
+	 * @return bool
+	 */
+	public function can_handle_request() {
+		return is_author()
+				&& Sensei_Teacher::is_a_teacher( get_query_var('author') )
+				&& ! user_can( get_query_var('author'), 'manage_options' );
+	}
+
+	/**
+	 * Set up handling for a teacher archive page.
+	 *
+	 * This is done by manually rendering the content for the page, creating a
+	 * dummy post object, setting its content to the rendered content we generated,
+	 * and then forcing WordPress to render that post.
+	 * Adapted from WooCommerce and bbPress.
+	 *
+	 * @since 1.12.0
+	 */
+	public function handle_request() {
+		$this->author = get_user_by( 'id', get_query_var('author') );
+
+		if ( ! $this->author ) {
+			return;
+		}
+
+		$teacher_post = new WP_Post( (object) array(
+			'post_author'       => $this->author->ID,
+			'post_date'         => $this->author->user_registered,
+			'post_date_gmt'     => $this->author->user_registered,
+			'post_modified'     => $this->author->user_registered,
+			'post_modified_gmt' => $this->author->user_registered,
+			'post_title'        => $this->author->display_name,
+			'post_name'         => $this->author->user_nicename,
+			'post_excerpt'      => '',
+			'post_content'      => '',
+			'post_type'         => 'page',
+		) );
+
+		// Render the teacher archive page and output it as a Page.
+		$content = $this->render_page();
+		$this->output_content_as_page( $content, $teacher_post, array(
+			'post_title' => sanitize_text_field( $teacher_post->post_title ),
+			'post_name'  => $teacher_post->post_name,
+		) );
+	}
+
+	/**
+	 * Return the content for the teacher archive page.
+	 *
+	 * @since 1.12.0
+	 *
+	 * @return string
+	 */
+	private function render_page() {
+		ob_start();
+		add_filter( 'sensei_show_main_header', '__return_false' );
+		add_filter( 'sensei_show_main_footer', '__return_false' );
+		Sensei_Templates::get_template( 'teacher-archive.php' );
+		$content = ob_get_clean();
+
+		return $content;
+	}
+
+	/**
+	 * Prepare the WP query object for the imitated request. The `queried_object` property should
+	 * be the queried author in order for it to show up in the page's `<title>` tag for this author
+	 * query.
+	 *
+	 * @param WP_Query $wp_query
+	 * @param WP_Post  $post_to_copy
+	 * @param array $post_params
+	 */
+	protected function prepare_wp_query( $wp_query, $post_to_copy, $post_params ) {
+		$wp_query->queried_object    = $this->author;
+		$wp_query->queried_object_id = $this->author->ID;
+	}
+
+}
