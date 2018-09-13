a02187c957870aa25c46571948fe79052c491645
Donna Peplinskie <donnapep@gmail.com>
2018-09-13 14:01:37 -0400

    Update comment date when a quiz is failed

--- includes/class-sensei-utils.php
+++ includes/class-sensei-utils.php
@@ -1867,7 +1867,7 @@ class Sensei_Utils {
 					'action'    => 'update', // Update the existing status...
 					'keep_time' => true, // ...but don't change the existing timestamp
 				);
-			if( in_array( $status, array( 'in-progress', 'ungraded', 'graded', 'passed' ), true ) ) {
+			if( in_array( $status, array( 'in-progress', 'ungraded', 'graded', 'passed', 'failed' ), true ) ) {
 				unset( $args['keep_time'] ); // Keep updating what's happened
 			}
 
