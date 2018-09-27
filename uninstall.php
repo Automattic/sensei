c13d127e7468cdb89edc3286a675500c7b31954b
Alex Sanford <alex.sanford1@gmail.com>
2018-09-27 18:09:55 -0300

    Fix some relative require paths

--- uninstall.php
+++ uninstall.php
@@ -15,8 +15,8 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
 }
 
-require 'woothemes-sensei.php';
-require 'includes/class-sensei-data-cleaner.php';
+require dirname( __FILE__ ) . '/woothemes-sensei.php';
+require dirname( __FILE__ ) . '/includes/class-sensei-data-cleaner.php';
 
 // Cleanup all data.
 if ( ! is_multisite() ) {
