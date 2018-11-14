93ac5da9152e42f8b6226db160c6a89621349976
Donna Peplinskie <donnapep@gmail.com>
2018-11-14 06:42:19 -0500

    Escape individual column data array elements

--- includes/class-sensei-analysis-lesson-list-table.php
+++ includes/class-sensei-analysis-lesson-list-table.php
@@ -248,7 +248,13 @@ class Sensei_Analysis_Lesson_List_Table extends WooThemes_Sensei_List_Table {
 										'grade' => $grade,
 									), $item, $this );
 
-		return wp_kses_post( $column_data );
+		$escaped_column_data = array();
+
+		foreach ( $column_data as $key => $data ) {
+			$escaped_column_data[$key] = wp_kses_post( $data );
+		}
+
+		return $escaped_column_data;
 	}
 
 	/**
