0ba6b9ea2f5dad8454ed5261e78688880f2fc5a5
Alex Sanford <alex.sanford1@gmail.com>
2019-01-14 13:27:08 -0400

    Add check for Sensei Compat Plugin
    If we're in the compat plugin, then we know that the WC Paid Courses
    code is activated.

--- includes/class-sensei.php
+++ includes/class-sensei.php
@@ -194,7 +194,9 @@ class Sensei_Main {
 		if ( is_multisite() ) {
 			$active_plugins = array_merge( $active_plugins, get_site_option( 'active_sitewide_plugins', array() ) );
 		}
-		return in_array( 'sensei-wc-paid-courses/sensei-wc-paid-courses.php', $active_plugins ) || array_key_exists( 'sensei-wc-paid-courses/sensei-wc-paid-courses.php', $active_plugins );
+		return in_array( 'sensei-wc-paid-courses/sensei-wc-paid-courses.php', $active_plugins )
+			|| array_key_exists( 'sensei-wc-paid-courses/sensei-wc-paid-courses.php', $active_plugins )
+			|| defined( 'SENSEI_COMPAT_PLUGIN' ) && SENSEI_COMPAT_PLUGIN;
 	}
 
 	/**
