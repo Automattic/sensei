19140bc831835a1f830d7dd69f06eba7175b3204
mostafaabd <tafa777@gmail.com>
2019-02-09 00:52:53 +0200

    fix conflict with wp ultimate recipe plugin 2377

--- includes/class-sensei-course.php
+++ includes/class-sensei-course.php
@@ -52,7 +52,7 @@ class Sensei_Course {
 			add_action( 'add_meta_boxes', array( $this, 'meta_box_setup' ), 20 );
 			add_action( 'save_post', array( $this, 'meta_box_save' ) );
 			// Custom Write Panel Columns
-			add_filter( 'manage_edit-course_columns', array( $this, 'add_column_headings' ), 10, 1 );
+			add_filter( 'manage_edit-course-category_custom_columns', array( $this, 'add_column_headings' ), 10, 1 );
 			add_action( 'manage_posts_custom_column', array( $this, 'add_column_data' ), 10, 2 );
 		} else {
 			$this->my_courses_page = false;
