6ac6e150a432908ce390863e01859fface01a27a
Jake Oehler Morrison <jake.morrison@automattic.com>
2019-02-14 11:47:39 +0000

    Remove deprecated method `Sensei_Main::set_woocommerce_functionality`

--- includes/class-sensei.php
+++ includes/class-sensei.php
@@ -1349,19 +1349,6 @@ class Sensei_Main {
 	} // End sensei_get_woocommerce_product_object()
 
 	/**
-	 * Setup required WooCommerce settings.
-	 *
-	 * @access  public
-	 * @since   1.1.0
-	 * @return  void
-	 */
-	public function set_woocommerce_functionality() {
-
-		_deprecated_function( 'Sensei()->set_woocommerce_functionality', 'Sensei 1.9.0' );
-
-	} // End set_woocommerce_functionality()
-
-	/**
 	 * Disable guest checkout if a course product is in the cart
 	 *
 	 * @deprecated since 1.9.0
