75703bb5217aaa84771c97eb26e5aafbf76b89a9
Alex Sanford <alex.sanford1@gmail.com>
2019-02-20 19:37:41 -0400

    Auto-fill email address

--- includes/email-signup/template.php
+++ includes/email-signup/template.php
@@ -33,7 +33,7 @@
 			</div>
 			<div class="email-input">
 				<div class="mc-field-group">
-					<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Enter your email address">
+					<input type="email" value="<?php echo esc_attr( get_option( 'admin_email', '' ) ); ?>" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Enter your email address">
 				</div>
 				<div class="gdpr-content">
 					<p>We use Mailchimp as our marketing platform. By clicking below to subscribe, you acknowledge that your information will be transferred to Mailchimp for processing. <a href="https://mailchimp.com/legal/" target="_blank">Learn more about Mailchimp's privacy practices here.</a></p>
