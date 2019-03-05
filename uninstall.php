144e8677b54b7a4baf39a3222449e02405e5cb7f
Jake Oehler Morrison <jake.morrison@automattic.com>
2019-03-05 10:30:38 +0000

    Allow uninstall if PHP not met

--- uninstall.php
+++ uninstall.php
@@ -16,6 +16,12 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 }
 
 require dirname( __FILE__ ) . '/sensei.php';
+
+if ( ! function_exists( 'Sensei' ) ) {
+	// We still want people to be able to delete Sensei if they don't meet dependencies.
+	return;
+}
+
 require dirname( __FILE__ ) . '/includes/class-sensei-data-cleaner.php';
 
 // Cleanup all data.
