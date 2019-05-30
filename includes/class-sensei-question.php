62e3698fdd2d144d63c91a0edfa4c0d95ce7e1dc
Patricia Atim <atim@kanzucode.com>
2019-05-30 14:51:16 +0300

    adding custom hook to sensei question class

--- includes/class-sensei-question.php
+++ includes/class-sensei-question.php
@@ -311,7 +311,8 @@ class Sensei_Question {
 			remove_action( 'save_post_question', array( $this, 'save_question' ) );
 
 			// Update question data
-			$question_id = Sensei()->lesson->lesson_save_question( $data, 'question' );
+			$question_id = apply_filters('woosensei_save_question', $data);
+			$question_id = $question_id ? $question_id : $this->lesson_save_question($data);
 
 			// Re-hook same function
 			add_action( 'save_post_question', array( $this, 'save_question' ) );
