53accac68332179bd65b22035ba2bc141dbff39a
Jake Oehler Morrison <jake.morrison@automattic.com>
2019-06-20 13:55:13 +0100

    Rename variable in `uninstall.php` that was conflict with global variable

--- uninstall.php
+++ uninstall.php
@@ -77,8 +77,8 @@ if ( ! is_multisite() ) {
 	$blog_ids         = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
 	$original_blog_id = get_current_blog_id();
 
-	foreach ( $blog_ids as $blog_id ) {
-		switch_to_blog( $blog_id );
+	foreach ( $blog_ids as $current_blog_id ) {
+		switch_to_blog( $current_blog_id );
 
 		// phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable -- $plugin is passed to `uninstall_plugin`
 		if ( is_another_sensei_activated( $plugin ) ) {
