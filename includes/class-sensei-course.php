f34bf4a7aabd3a6d616eb8ee0d07d338c16a0d35
Donna Peplinskie <donnapep@gmail.com>
2019-07-03 18:47:43 -0400

    Remove extraneous whitespace

--- includes/class-sensei-course.php
+++ includes/class-sensei-course.php
@@ -51,7 +51,7 @@ class Sensei_Course {
 			// Metabox functions
 			add_action( 'add_meta_boxes', array( $this, 'meta_box_setup' ), 20 );
 			add_action( 'save_post', array( $this, 'meta_box_save' ) );
-      
+
 			// Custom Write Panel Columns
 			add_filter( 'manage_course_posts_columns', array( $this, 'add_column_headings' ), 10, 1 );
 			add_action( 'manage_course_posts_custom_column', array( $this, 'add_column_data' ), 10, 2 );
