ba6fb648ce2b9d52ea3d42f87d00fae198994b83
Alex Sanford <alex.sanford1@gmail.com>
2019-11-14 12:47:53 +0000

    Set up initial data store for messages block

--- assets/block-editor/messages-block/block.jsx
+++ assets/block-editor/messages-block/block.jsx
@@ -1,6 +1,31 @@
+/**
+ * WordPress dependencies.
+ */
+import { useSelect } from '@wordpress/data';
+import { Placeholder } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 
-function Block( props ) {
-	return 'Client-side rendered!';
+/**
+ * Internal dependencies.
+ */
+import MESSAGES_STORE from '../data/messages-store';
+import Message from './message';
+
+function Block() {
+	const { messages, isFetching } = useSelect( ( select ) => {
+		const store = select( MESSAGES_STORE );
+
+		return {
+			messages: store.getMessages(),
+			isFetching: store.isFetching(),
+		};
+	} );
+
+	if ( isFetching ) {
+		return <Placeholder label={ __( 'Loading Messages...' ) } />;
+	}
+
+	return messages.map( ( message ) => <Message key={ message.id } message={ message } /> );
 }
 
 export default Block;
