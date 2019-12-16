9b5cee12722be4ab447cd048aabd05bae7147077
Renatho De Carli Rosa <renatho@automattic.com>
2019-12-16 13:37:10 -0300

    Refactor duplicate course with lessons tests

--- tests/unit-tests/test-class-admin.php
+++ tests/unit-tests/test-class-admin.php
@@ -14,6 +14,8 @@ class Sensei_Class_Admin_Test extends WP_UnitTestCase {
 	 *
 	 * This function sets up the lessons, quizes and their questions. This function runs before
 	 * every single test in this class
+	 *
+	 * @return void
 	 */
 	public function setup() {
 		parent::setup();
@@ -28,6 +30,8 @@ class Sensei_Class_Admin_Test extends WP_UnitTestCase {
 
 	/**
 	 * Testing the admin class to make sure it is loaded
+	 *
+	 * @return void
 	 */
 	public function testClassInstance() {
 		// test if the class exists
@@ -35,18 +39,15 @@ class Sensei_Class_Admin_Test extends WP_UnitTestCase {
 	} // end testClassInstance
 
 	/**
-	 * Test duplicate courses with lessons.
+	 * Setup function for duplicate course
 	 *
-	 * @covers WooThemes_Sensei_Admin::duplicate_course_with_lessons_action
+	 * This function mocks the redirect method, create and set the user, create a course
+	 * with some lessons and set the needed params.
+	 *
+	 * @param $qty_lessons How many lessons to create.
+	 * @return array       Created course and list of lessons id.
 	 */
-	public function testDuplicateCourseWithLessons() {
-		$qty_lessons = 2;
-
-		$this->assertTrue(
-			method_exists( 'WooThemes_Sensei_Admin', 'duplicate_course_with_lessons_action' ),
-			'The admin class function `duplicate_course_with_lessons_action` does not exist '
-		);
-
+	private function duplicate_course_with_lessons_setup( $qty_lessons ) {
 		// Mock the safe_redirect method
 		Sensei()->admin = $this->getMockBuilder( 'WooThemes_Sensei_Admin' )
 			->setMethods( [ 'safe_redirect' ] )
@@ -65,50 +66,114 @@ class Sensei_Class_Admin_Test extends WP_UnitTestCase {
 		);
 		wp_set_current_user( $admin_user->ID );
 
-		$course_id  = $this->factory->course->create();
-		$lesson_ids = $this->factory->lesson->create_many( $qty_lessons );
-		foreach ( $lesson_ids as $lesson_id ) {
+		// Create one course and add some lessons
+		$course_id   = $this->factory->course->create();
+		$lessons_ids = $this->factory->lesson->create_many( $qty_lessons );
+
+		foreach ( $lessons_ids as $lesson_id ) {
 			add_post_meta( $lesson_id, '_lesson_course', $course_id );
 		}
 
-		// Add the first lesson as prerequisite of the second one
-		add_post_meta( $lesson_ids[1], '_lesson_prerequisite', $lesson_ids[0] );
-
-		// Create nonce to pass in the method
+		// Create nonce
 		$duplicate_nonce = wp_create_nonce( 'duplicate_course_with_lessons_' . $course_id );
 
 		// Set request and get params
 		$_REQUEST['_wpnonce'] = $duplicate_nonce;
 		$_GET['post']         = $course_id;
 
+		return array(
+			'course_id'   => $course_id,
+			'lessons_ids' => $lessons_ids,
+		);
+	}
+
+	/**
+	 * Test duplicate courses with lessons.
+	 *
+	 * @covers WooThemes_Sensei_Admin::duplicate_course_with_lessons_action
+	 *
+	 * @return void
+	 */
+	public function testDuplicateCourseWithLessons() {
+		$this->assertTrue(
+			method_exists( 'WooThemes_Sensei_Admin', 'duplicate_course_with_lessons_action' ),
+			'The admin class function `duplicate_course_with_lessons_action` does not exist '
+		);
+
+		$qty_lessons = 2;
+		$duplication = $this->duplicate_course_with_lessons_setup( $qty_lessons );
+		$course_id   = $duplication['course_id'];
+		$lessons_ids = $duplication['lessons_ids'];
+
 		// Runs the duplication
 		Sensei()->admin->duplicate_course_with_lessons_action();
 
-		// Get duplicated course (all in draft)
-		$course_args   = array(
+		// Get the duplicated course
+		$duplicated_course_args = array(
 			'post_type'   => 'course',
+			'meta_key'    => '_duplicate', // phpcs:ignore Slow query ok.
+			'meta_value'  => $course_id, // phpcs:ignore Slow query ok.
 			'post_status' => [ 'draft' ],
 		);
-		$courses       = get_posts( $course_args );
-		$new_course_id = $courses[0]->ID;
+		$duplicated_courses     = get_posts( $duplicated_course_args );
+		$duplicated_course_id   = $duplicated_courses[0]->ID;
 
-		// Get the lessons of the duplicated post
-		$lesson_args = array(
+		// Get duplicated lessons
+		$duplicated_lesson_args = array(
 			'post_type'   => 'lesson',
 			'meta_key'    => '_lesson_course', // phpcs:ignore Slow query ok.
-			'meta_value'  => $new_course_id, // phpcs:ignore Slow query ok.
+			'meta_value'  => $duplicated_course_id, // phpcs:ignore Slow query ok.
 			'post_status' => [ 'draft' ],
 		);
-		$lessons     = get_posts( $lesson_args );
+		$duplicated_lessons     = get_posts( $duplicated_lesson_args );
 
-		// Lessons assertation
-		$this->assertCount( $qty_lessons, $lessons, 'The number of lessons duplicated should be the same that the original.' );
-
-		// Prerequisite assertation
-		$first_lesson_prerequisite  = get_post_meta( $lessons[0]->ID, '_lesson_prerequisite', true );
-		$second_lesson_prerequisite = get_post_meta( $lessons[1]->ID, '_lesson_prerequisite', true );
-		$this->assertEquals( $first_lesson_prerequisite, '', 'The first lesson prerequisite should be empty.' );
-		$this->assertEquals( $second_lesson_prerequisite, $lessons[0]->ID, 'The second lesson prerequisite should be the first duplicated lesson.' );
+		// Lessons assertion
+		$this->assertCount( $qty_lessons, $duplicated_lessons, 'The number of duplicated lessons should be the same as the original.' );
 	}
 
+	/**
+	 * Test duplicate courses with lessons with prerequisite.
+	 *
+	 * @covers WooThemes_Sensei_Admin::duplicate_course_with_lessons_action
+	 *
+	 * @return void
+	 */
+	public function testDuplicateCourseWithLessonsWithPrerequisite() {
+		$qty_lessons = 2;
+		$duplication = $this->duplicate_course_with_lessons_setup( $qty_lessons );
+		$course_id   = $duplication['course_id'];
+		$lessons_ids = $duplication['lessons_ids'];
+
+		// Add the first lesson as prerequisite of the second one
+		add_post_meta( $lessons_ids[1], '_lesson_prerequisite', $lessons_ids[0] );
+
+		// Runs the duplication
+		Sensei()->admin->duplicate_course_with_lessons_action();
+
+		// Get the duplicated prerequisite lesson
+		$duplicated_prerequisite_lesson_args = array(
+			'post_type'   => 'lesson',
+			'meta_key'    => '_duplicate', // phpcs:ignore Slow query ok.
+			'meta_value'  => $lessons_ids[0], // phpcs:ignore Slow query ok.
+			'post_status' => [ 'draft' ],
+		);
+		$duplicated_prerequisite_lesson      = get_posts( $duplicated_prerequisite_lesson_args );
+		$duplicated_prerequisite_lesson_id   = $duplicated_prerequisite_lesson[0]->ID;
+
+		// Get the duplicated dependent lesson
+		$duplicated_dependent_lesson_args = array(
+			'post_type'   => 'lesson',
+			'meta_key'    => '_duplicate', // phpcs:ignore Slow query ok.
+			'meta_value'  => $lessons_ids[1], // phpcs:ignore Slow query ok.
+			'post_status' => [ 'draft' ],
+		);
+		$duplicated_dependent_lesson      = get_posts( $duplicated_dependent_lesson_args );
+		$duplicated_dependent_lesson_id   = $duplicated_dependent_lesson[0]->ID;
+
+		// Get the prerequisite id
+		$new_prerequisite_id = get_post_meta( $duplicated_dependent_lesson_id, '_lesson_prerequisite', true );
+
+		// Prerequisite assertion
+		$this->assertEquals( $duplicated_prerequisite_lesson_id, $new_prerequisite_id, 'The duplicated dependent should have the duplicated prerequisite as its prerequisite.' );
+	}
 }//end class
