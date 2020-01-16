ec0e0aa60990d94835c21c1c90817a052545a2a5
Jake Oehler Morrison <jake.morrison@automattic.com>
2020-01-16 09:31:20 +0000

    Move jquery-ui-tooltip to dependency of learners-general

--- includes/admin/class-sensei-learner-management.php
+++ includes/admin/class-sensei-learner-management.php
@@ -153,7 +153,7 @@ class Sensei_Learner_Management {
 		wp_enqueue_script(
 			'sensei-learners-general',
 			Sensei()->plugin_url . 'assets/js/learners-general' . $suffix . '.js',
-			array( 'jquery', 'jquery-ui-core', 'jquery-ui-datepicker', 'sensei-core-select2' ),
+			array( 'jquery', 'jquery-ui-core', 'jquery-ui-datepicker', 'jquery-ui-tooltip', 'sensei-core-select2' ),
 			Sensei()->version,
 			true
 		);
@@ -165,8 +165,6 @@ class Sensei_Learner_Management {
 				'inprogress' => __( 'In Progress', 'sensei-lms' ),
 			)
 		);
-
-		wp_enqueue_script( 'jquery-ui-tooltip' );
 
 		$data = array(
 			'remove_generic_confirm'     => __( 'Are you sure you want to remove this user?', 'sensei-lms' ),
