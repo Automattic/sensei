11cc76282acc164a136b3c2a3fdee21ab3324bda
Jake Oehler Morrison <jake.morrison@automattic.com>
2020-01-22 15:04:53 +0000

    Switch button to Take This Course

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -121,7 +121,7 @@ function sensei_start_course_form( $course_id ) {
 
 				<input type="hidden" name="<?php echo esc_attr( 'woothemes_sensei_start_course_noonce' ); ?>" id="<?php echo esc_attr( 'woothemes_sensei_start_course_noonce' ); ?>" value="<?php echo esc_attr( wp_create_nonce( 'woothemes_sensei_start_course_noonce' ) ); ?>" />
 
-				<span><input name="course_start" type="submit" class="course-start" value="<?php esc_html_e( 'Start taking this Course', 'sensei-lms' ); ?>"/></span>
+				<span><input name="course_start" type="submit" class="course-start" value="<?php esc_html_e( 'Take This Course', 'sensei-lms' ); ?>"/></span>
 
 			</form>
 			<?php
