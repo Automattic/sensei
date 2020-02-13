fb5bbff9d4ad85e3758cc72f0552e14908988eea
Jake Oehler Morrison <jake.morrison@automattic.com>
2020-02-13 10:33:03 +0000

    Clean up the learner calculated version

--- includes/class-sensei-data-cleaner.php
+++ includes/class-sensei-data-cleaner.php
@@ -215,6 +215,7 @@ class Sensei_Data_Cleaner {
 	private static $user_meta_keys = array(
 		'^sensei_hide_menu_settings_notice$',
 		'^_module_progress_[0-9]+_[0-9]+$',
+		'^learner_calculated_version$',
 	);
 
 	/**
