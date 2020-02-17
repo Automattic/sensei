4acb5ebaccc38f7f9c8c8eebfd58d8404e5a5807
cjshaw <4930129+cjshawMIT@users.noreply.github.com>
2020-02-17 15:37:33 -0500

    Remove global script include for user-dashboard.js

--- includes/class-sensei-frontend.php
+++ includes/class-sensei-frontend.php
@@ -139,14 +139,6 @@ class Sensei_Frontend {
 
 			$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 
-			if ( isset( Sensei()->settings->settings['my_course_page'] ) ) {
-				if ( is_page( Sensei()->settings->settings['my_course_page'] ) ) {
-					// My Courses tabs script.
-					wp_register_script( Sensei()->token . '-user-dashboard', esc_url( Sensei()->plugin_url . 'assets/js/user-dashboard' . $suffix . '.js' ), array( 'jquery-ui-tabs' ), Sensei()->version, true );
-					wp_enqueue_script( Sensei()->token . '-user-dashboard' );
-				}
-			}
-
 			// Course Archive javascript.
 			if ( is_post_type_archive( 'course' ) ) {
 
