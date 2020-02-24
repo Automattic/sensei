d283881276e2fc1cb305586a16332809463b431d
Peter Kiss <peter@yscik.com>
2020-02-24 19:46:05 +0100

    whitespace whoopsie fix

--- assets/js/admin/lesson-bulk-edit.js
+++ assets/js/admin/lesson-bulk-edit.js
@@ -3,7 +3,7 @@
  */
 
 jQuery( function( $ ) {
-	$( '#the-list' ).on( 'click',  '#bulk-edit #bulk_edit ', function() {
+	$( '#the-list' ).on( 'click', '#bulk-edit #bulk_edit ', function() {
 		// define the bulk edit row
 		var $bulk_row = $( '#bulk-edit' );
 
