4443e70eed026771021b0b2b6f9548a1fe5590b6
JuanchoPestana <juancho.pestana@gmail.com>
2020-02-25 15:02:26 -0600

    Avoid unnecesary get_post_meta() call

--- includes/class-sensei-lesson.php
+++ includes/class-sensei-lesson.php
@@ -696,9 +696,14 @@ class Sensei_Lesson {
 
 		// If course is being changed, set the prerequisite to empty.
 		// phpcs:ignore WordPress.Security.NonceVerification
-		if ( isset( $_POST['lesson_course'] ) && get_post_meta( $post_id, '_lesson_course', true ) !== $_POST['lesson_course'] && '' !== get_post_meta( $post_id, '_lesson_course', true ) && '_lesson_prerequisite' === $meta_key ) {
-				$new_meta_value = '';
+		if ( '_lesson_prerequisite' === $meta_key ) {
+				$lesson_course_meta = get_post_meta( $post_id, '_lesson_course', true );
 		}
+
+		if ( ! empty( $lesson_course_meta ) && isset( $_POST['lesson_course'] ) && $lesson_course_meta !== $_POST['lesson_course'] ) {
+			$new_meta_value = '';
+		}
+
 		// update field with the new value
 		if ( -1 != $new_meta_value ) {
 			return update_post_meta( $post_id, $meta_key, $new_meta_value );
