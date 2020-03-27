0fa23e069dfe9aeeb7c30da7a2af2493f1cc601c
Bhavik Patel <bhavikpate2010@gmail.com>
2020-03-27 15:08:46 +0530

    Update class-sensei-lesson.php
    lesson allow video tags allowing any tags here that wp_kses_post allows

--- includes/class-sensei-lesson.php
+++ includes/class-sensei-lesson.php
@@ -32,7 +32,7 @@ class Sensei_Lesson {
 
 		$this->question_order = '';
 
-		$this->allowed_html = Sensei_Wp_Kses::get_default_wp_kses_allowed_html();
+		$this->allowed_html = array_merge( Sensei_Wp_Kses::get_default_wp_kses_allowed_html(), wp_kses_allowed_html( 'post' ) );
 
 		// Admin actions
 		if ( is_admin() ) {
