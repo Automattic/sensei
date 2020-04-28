25d670be8ad51c2f178c513701a90d8b193627d2
Renatho De Carli Rosa <renatho@automattic.com>
2020-04-28 16:50:38 -0300

    Add missing JSDoc

--- assets/tests-helper/functions.js
+++ assets/tests-helper/functions.js
@@ -1,3 +1,8 @@
+/**
+ * Mock window.location.search value.
+ *
+ * @param {string} search Search string to mock.
+ */
 export const mockSearch = ( search ) => {
 	Object.defineProperty( window, 'location', {
 		value: {
