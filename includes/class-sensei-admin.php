06c497796e1cfbf056d0d6508847823a18bdf410
Peter Kiss <peter@yscik.com>
2020-05-04 17:12:15 +0200

    Fix deprecated create_page stub calling wrong method

--- includes/class-sensei-admin.php
+++ includes/class-sensei-admin.php
@@ -240,8 +240,8 @@ class Sensei_Admin {
 	 */
 	function create_page( $slug, $page_title = '', $page_content = '', $post_parent = 0 ) {
 
-		return Sensei()->onboarding->pages->create_pages( $slug, $page_title, $page_content, $post_parent );
 		_deprecated_function( __METHOD__, '3.1.0', 'Sensei_onboarding\Pages::create_page' );
+		return Sensei()->onboarding->pages->create_page( $slug, $page_title, $page_content, $post_parent );
 
 	}
 
