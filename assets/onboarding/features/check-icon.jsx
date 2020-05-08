25e193aeece240ea92181d088608e3989093f0de
Renatho De Carli Rosa <renatho@automattic.com>
2020-05-08 18:41:47 -0300

    Add status to feature list

---
+++ assets/onboarding/features/check-icon.jsx
@@ -0,0 +1,16 @@
+/**
+ * Check icon component.
+ */
+const CheckIcon = () => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		role="img"
+		aria-hidden="true"
+		focusable="false"
+	>
+		<path d="M9 18.6L3.5 13l1-1L9 16.4l9.5-9.9 1 1z" fill="currentColor" />
+	</svg>
+);
+
+export default CheckIcon;
--- assets/onboarding/features/feature-list.jsx
+++ assets/onboarding/features/feature-list.jsx
@@ -1,4 +1,7 @@
+import { Spinner } from '@wordpress/components';
+
 import FeatureDescription from './feature-description';
+import CheckIcon from './check-icon';
 
 /**
  * Feature list.
@@ -14,20 +17,48 @@ const FeatureList = ( { children } ) => (
  * Feature list item.
  *
  * @param {Object} props
- * @param {string} props.title                        Feature title.
- * @param {string} props.description                  Feature description.
- * @param {string} props.confirmationExtraDescription Extra description that appears only in confirmation modal.
+ * @param {string} [props.status]                       Feature status.
+ * @param {string} props.title                          Feature title.
+ * @param {string} props.description                    Feature description.
+ * @param {string} [props.confirmationExtraDescription] Extra description that appears only in confirmation modal.
  */
-FeatureList.Item = ( { title, description, confirmationExtraDescription } ) => (
+FeatureList.Item = ( {
+	status,
+	title,
+	description,
+	confirmationExtraDescription,
+} ) => (
 	<li className="sensei-onboarding__features-list-item">
-		<h4 className="sensei-onboarding__feature-title">{ title }</h4>
-		<p className="sensei-onboarding__feature-description">
-			<FeatureDescription
-				description={ description }
-				confirmationExtraDescription={ confirmationExtraDescription }
-			/>
-		</p>
+		{ status && (
+			<div className="sensei-onboarding__icon-status">
+				{ status === FeatureList.LOADING_STATUS && <Spinner /> }
+				{ status === FeatureList.ERROR_STATUS && (
+					<i className="sensei-onboarding__circle-icon-wrapper error-icon-wrapper alert-icon" />
+				) }
+				{ status === FeatureList.SUCCESS_STATUS && (
+					<i className="sensei-onboarding__circle-icon-wrapper success-icon-wrapper">
+						<CheckIcon />
+					</i>
+				) }
+			</div>
+		) }
+
+		<div>
+			<h4 className="sensei-onboarding__feature-title">{ title }</h4>
+			<p className="sensei-onboarding__feature-description">
+				<FeatureDescription
+					description={ description }
+					confirmationExtraDescription={
+						confirmationExtraDescription
+					}
+				/>
+			</p>
+		</div>
 	</li>
 );
+
+FeatureList.LOADING_STATUS = 'loading';
+FeatureList.ERROR_STATUS = 'error';
+FeatureList.SUCCESS_STATUS = 'success';
 
 export default FeatureList;
