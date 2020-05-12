0ca91e6c361f009065f8b0553703bce577a3990c
Renatho De Carli Rosa <renatho@automattic.com>
2020-05-12 11:10:23 -0300

    Add installation feedback tests

---
+++ assets/onboarding/features/installation-feedback.test.js
@@ -0,0 +1,94 @@
+import { render, fireEvent } from '@testing-library/react';
+
+import InstallationFeedback from './installation-feedback';
+import FeaturesList from './features-list';
+
+describe( '<InstallationFeedback />', () => {
+	it( 'Should render with loading status', () => {
+		const features = [
+			{
+				id: 'first',
+				title: 'Test',
+				description: 'Test',
+				status: FeaturesList.LOADING_STATUS,
+			},
+			{
+				id: 'second',
+				title: 'Test',
+				description: 'Test',
+				status: FeaturesList.ERROR_STATUS,
+			},
+			{
+				id: 'third',
+				title: 'Test',
+				description: 'Test',
+				status: FeaturesList.SUCCESS_STATUS,
+			},
+		];
+
+		const { queryByText } = render(
+			<InstallationFeedback
+				features={ features }
+				onContinue={ () => {} }
+			/>
+		);
+
+		expect( queryByText( 'Installing…' ) ).toBeTruthy();
+	} );
+
+	it( 'Should render all success', () => {
+		const features = [
+			{
+				id: 'first',
+				title: 'Test',
+				description: 'Test',
+				status: FeaturesList.SUCCESS_STATUS,
+			},
+		];
+
+		const onContinueMock = jest.fn();
+
+		const { container, queryByText } = render(
+			<InstallationFeedback
+				features={ features }
+				onContinue={ onContinueMock }
+			/>
+		);
+
+		expect( container.querySelectorAll( 'button' ).length ).toEqual( 1 );
+
+		fireEvent.click( queryByText( 'Continue' ) );
+		expect( onContinueMock ).toBeCalled();
+	} );
+
+	it( 'Should render errors without loading', () => {
+		const features = [
+			{
+				id: 'first',
+				title: 'Test',
+				description: 'Test',
+				status: FeaturesList.ERROR_STATUS,
+			},
+			{
+				id: 'second',
+				title: 'Test',
+				description: 'Test',
+				status: FeaturesList.SUCCESS_STATUS,
+			},
+		];
+
+		const onContinueMock = jest.fn();
+
+		const { queryByText } = render(
+			<InstallationFeedback
+				features={ features }
+				onContinue={ onContinueMock }
+			/>
+		);
+
+		expect( queryByText( 'Retry' ) ).toBeTruthy();
+
+		fireEvent.click( queryByText( 'Continue' ) );
+		expect( onContinueMock ).toBeCalled();
+	} );
+} );
