c89abecc4bc09318685970acb4049af29ad37875
Peter Kiss <peter@yscik.com>
2020-05-14 18:46:52 +0200

    Add helper for interpolateComponents

---
+++ assets/tests-helper/format-string.js
@@ -0,0 +1,28 @@
+import interpolateComponents from 'interpolate-components';
+
+export const formattingComponents = {
+	em: <em />,
+	strong: <strong />,
+	code: <code />,
+	small: <small />,
+	sub: <sub />,
+	sup: <sup />,
+	br: <br />,
+	p: <p />,
+	del: <del />,
+};
+
+
+/**
+ * Interpolate components and create a node from the given template string.
+ *
+ * @example formatString(' Welcome to {{strong}}Sensei{{/strong}}!')
+ *
+ * @param {string} mixedString Template string.
+ * @param {object} components Replacements.
+ */
+export const formatString = ( mixedString, components = {} ) =>
+	interpolateComponents( {
+		mixedString,
+		components: { ...formattingComponents, ...components },
+	} );
