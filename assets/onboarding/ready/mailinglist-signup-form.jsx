9edfafc2baa1399f786442350938d698676b58a3
Peter Kiss <peter@yscik.com>
2020-05-14 18:21:16 +0200

    Add Mailing list signup form

---
+++ assets/onboarding/ready/mailinglist-signup-form.jsx
@@ -0,0 +1,56 @@
+import { List, Spinner } from '@woocommerce/components';
+import { Button, TextControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { useOnboardingApi } from '../use-onboarding-api.js';
+
+/**
+ *
+ * @returns {*}
+ * @constructor
+ */
+export const MailingListSignupForm = () => {
+	const { data, isBusy } = useOnboardingApi( 'ready' );
+
+	if ( isBusy ) {
+		return (
+			<div style={ { textAlign: 'center' } }>
+				<Spinner />
+			</div>
+		);
+	}
+
+	return (
+		<form
+			action={ data.mc_url }
+			method="post"
+			target="_blank"
+			className="sensei-onboarding__mailinglist-signup-form"
+		>
+			<input
+				type="hidden"
+				name={ `gdpr[${ data.gdpr_field }]` }
+				value="Y"
+			/>
+			<List
+				className="sensei-onboarding__item-list"
+				items={ [
+					{
+						title: '',
+						content: (
+							<TextControl
+								type="email"
+								name="EMAIL"
+								defaultValue={ data.admin_email }
+							/>
+						),
+						after: (
+							<Button isPrimary type="submit">
+								{ __( 'Yes, please!', 'sensei-lms' ) }
+							</Button>
+						),
+					},
+				] }
+			/>
+		</form>
+	);
+};
