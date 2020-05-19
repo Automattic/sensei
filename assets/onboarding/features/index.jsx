2861a743fe6cd26e6fc80a00971a4b9f62a320b7
Renatho De Carli Rosa <renatho@automattic.com>
2020-05-19 11:59:47 -0300

    Update features step to get the extensions

--- assets/onboarding/features/index.jsx
+++ assets/onboarding/features/index.jsx
@@ -1,88 +1,31 @@
 import { useState } from '@wordpress/element';
 import { Card, H } from '@woocommerce/components';
+import { useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 
+import { INSTALLED_STATUS } from './feature-status';
 import { useQueryStringRouter } from '../query-string-router';
 import ConfirmationModal from './confirmation-modal';
 import InstallationFeedback from './installation-feedback';
 import FeaturesSelection from './features-selection';
-import {
-	LOADING_STATUS,
-	ERROR_STATUS,
-	INSTALLED_STATUS,
-} from './feature-status';
 
-// TODO: Make it dynamic.
-const features = [
-	{
-		id: 'paid_courses',
-		title: __( 'WooCommerce Paid Courses', 'sensei-lms' ),
-		price: '$129.00 per year',
-		description: __(
-			'Sell your online courses using the most popular eCommerce plataform on the web - WooCommerce.',
-			'sensei-lms'
-		),
-		confirmationExtraDescription: __(
-			'(The WooCommerce plugin may also be installed and activated for free.)',
-			'sensei-lms'
-		),
-		learnMoreLink:
-			'https://woocommerce.com/products/woocommerce-paid-courses/',
-		status: LOADING_STATUS,
-	},
-	{
-		id: 'course_progress',
-		title: __( 'Course progress', 'sensei-lms' ),
-		description: __(
-			'Enable your students to easily view their progress and pick up where they left off in a course.',
-			'sensei-lms'
-		),
-		learnMoreLink:
-			'https://woocommerce.com/products/sensei-course-progress/',
-		status: INSTALLED_STATUS,
-	},
-	{
-		id: 'certificates',
-		title: __( 'Certificates', 'sensei-lms' ),
-		description: __(
-			'Award your students with a certificate of completion and a sense of accomplishment after finishing a course.',
-			'sensei-lms'
-		),
-		learnMoreLink: 'https://woocommerce.com/products/sensei-certificates/',
-		errorMessage: __(
-			'Error message here, maecenas faucibus mollis interdum tristique euismod.',
-			'sensei-lms'
-		),
-		status: ERROR_STATUS,
-	},
-	{
-		id: 'media_attachments',
-		title: __( 'Media Attachments', 'sensei-lms' ),
-		description: __(
-			'Provide your students with easy access to additional learning materials, from audio files to slideshows and PDFs.',
-			'sensei-lms'
-		),
-		learnMoreLink:
-			'https://woocommerce.com/products/sensei-media-attachments/',
-		status: INSTALLED_STATUS,
-	},
-	{
-		id: 'content_drip',
-		title: __( 'Content Drip', 'sensei-lms' ),
-		price: '$29.00 per year',
-		description: __(
-			'Keep students engaged and improve knowledge retention by setting a delivery schedule for course content.',
-			'sensei-lms'
-		),
-		learnMoreLink: 'https://woocommerce.com/products/sensei-content-drip/',
-		status: LOADING_STATUS,
-	},
-].map( ( feature ) => ( {
-	...feature,
-	title: `${ feature.title } — ${
-		feature.price ? feature.price : __( 'Free', 'sensei-lms' )
-	}`,
-} ) );
+const addPriceToTitle = ( features ) =>
+	features.map( ( feature ) => {
+		let titleComplement;
+
+		if ( feature.status === INSTALLED_STATUS ) {
+			titleComplement = __( 'Installed', 'sensei-lms' );
+		} else {
+			titleComplement = feature.price
+				? `${ feature.price } ${ __( 'per year', 'sensei-lms' ) }`
+				: __( 'Free', 'sensei-lms' );
+		}
+
+		return {
+			...feature,
+			title: `${ feature.title } — ${ titleComplement }`,
+		};
+	} );
 
 /**
  * Features step for setup wizard.
@@ -92,6 +35,15 @@ const Features = () => {
 	const [ feedbackActive, toggleFeedback ] = useState( false );
 	const [ selectedFeatureIds, setSelectedFeatureIds ] = useState( [] );
 	const { goTo } = useQueryStringRouter();
+
+	const { features } = useSelect(
+		( select ) => ( {
+			features: addPriceToTitle(
+				select( 'sensei/setup-wizard' ).getStepData( 'features' )
+			),
+		} ),
+		[]
+	);
 
 	const getSelectedFeatures = () =>
 		features.filter( ( feature ) =>
