400f9dc808447bab9b4bc4a2f14c00ccd966594a
Peter Kiss <peter@yscik.com>
2020-05-21 01:08:53 +0200

    Remove unused import

--- assets/onboarding/features/index.jsx
+++ assets/onboarding/features/index.jsx
@@ -1,7 +1,6 @@
 import { useState } from '@wordpress/element';
 import { Card, H } from '@woocommerce/components';
 import { __ } from '@wordpress/i18n';
-import { useDispatch } from '@wordpress/data';
 import { useQueryStringRouter } from '../query-string-router';
 import ConfirmationModal from './confirmation-modal';
 import InstallationFeedback from './installation-feedback';
