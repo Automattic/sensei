bc3ab2e8facb65354849386fb0ef35a426453b20
Giannis Karagiannis <giannis.karagiannis@automattic.com>
2020-05-20 16:26:39 +0300

    Fix linter issues

--- assets/data-port/stepper/index.jsx
+++ assets/data-port/stepper/index.jsx
@@ -3,27 +3,25 @@ import classnames from 'classnames';
 /**
  * A simple component to display a stepper on data port pages.
  *
- * @param {array} steps The array of the steps.
+ * @param {Array} steps The array of the steps.
  */
 const DataPortStepper = ( { steps } ) => {
-	return(
-		<ol className='sensei-progress-steps'>
-			{
-				steps.map( ( step ) => {
-					let stepClass = classnames( {
-						active: step.isActive,
-						done: step.isComplete,
-					});
+	return (
+		<ol className="sensei-progress-steps">
+			{ steps.map( ( step ) => {
+				const stepClass = classnames( {
+					active: step.isActive,
+					done: step.isComplete,
+				} );
 
-					return (
-						<li className={ stepClass}>
-							{ step.description }
-						</li>
-					);
-				} )
-			}
+				return (
+					<li key={ step.key } className={ stepClass }>
+						{ step.description }
+					</li>
+				);
+			} ) }
 		</ol>
 	);
-}
+};
 
 export default DataPortStepper;
