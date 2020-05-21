05a336140276a838f1d586b9e4323cf48acc6f97
gkaragia <53191348+gkaragia@users.noreply.github.com>
2020-05-21 12:24:50 +0300

    Merge pull request #3107 from Automattic/add/data-port-job-manager

--- tests/framework/data-port/class-sensei-data-port-job-mock.php
+++ tests/framework/data-port/class-sensei-data-port-job-mock.php
@@ -8,24 +8,29 @@
 
 class Sensei_Data_Port_Job_Mock extends Sensei_Data_Port_Job {
 
-	private static $tasks;
+	private $tasks;
 
-	public static function create( $tasks ) {
-		self::$tasks = $tasks;
+	private static $restore_mock;
 
-		return new self( 'test-job' );
-	}
+	public function __construct( $job_id, $args = [], $json = '' ) {
+		parent::__construct( $job_id, $args, $json );
 
-	public function get_name() {
-		return 'test-job';
+		$this->tasks = $args;
 	}
 
 	public function get_tasks() {
-		return self::$tasks;
+		return $this->tasks;
 	}
 
 	public function log( $title, $message, $type, $id ) {
 		$this->add_log_entry( $title, $message, $type, $id );
 	}
 
+	public static function get( $job_id ) {
+		return self::$restore_mock;
+	}
+
+	public static function set_restore_mock( $mock ) {
+		self::$restore_mock = $mock;
+	}
 }
