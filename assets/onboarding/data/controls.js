6608fa0a50be9dc1860fad0056f418b931e25b31
Peter Kiss <peter@yscik.com>
2020-05-22 01:06:03 +0200

    Update frontend event logging when the user changes usage tracking preference

--- assets/onboarding/data/controls.js
+++ assets/onboarding/data/controls.js
@@ -1,6 +1,7 @@
 import apiFetch from '@wordpress/api-fetch';
 
-import { FETCH_FROM_API } from './constants';
+import { FETCH_FROM_API, APPLY_STEP_DATA } from './constants';
+import { logEvent } from '../log-event';
 
 export default {
 	/**
@@ -11,4 +12,12 @@ export default {
 	 * @return {Promise} API fetch promise.
 	 */
 	[ FETCH_FROM_API ]: ( { request } ) => apiFetch( request ),
+	[ APPLY_STEP_DATA ]: ( { step, data } ) => {
+		switch ( step ) {
+			case 'welcome': {
+				logEvent.enable( data.usage_tracking );
+				break;
+			}
+		}
+	},
 };
