9436504158eae522d68844bff74f5948efddcb44
Jake Oehler Morrison <jake.morrison@automattic.com>
2020-05-21 14:56:56 +0100

    REST API: Add base data port and import controller classes

---
+++ includes/rest-api/class-sensei-rest-api-import-controller.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Import REST API Controller.
+ *
+ * @package Sensei\DataPort
+ * @since   3.1.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Sensei Import REST API endpoints.
+ *
+ * @package Sensei
+ * @author  Automattic
+ * @since   3.1.0
+ */
+class Sensei_REST_API_Import_Controller extends Sensei_REST_API_Data_Port_Base {
+	/**
+	 * Routes prefix.
+	 *
+	 * @var string
+	 */
+	protected $rest_base = 'import';
+
+	/**
+	 * Job class name that this controller handles.
+	 *
+	 * @var string
+	 */
+	protected $handler_class = Sensei_Import_Job::class;
+}
