ce47bce78ea29ce58ef2f2c7cfcadf2a4e2cf276
Jake Oehler Morrison <jake.morrison@automattic.com>
2020-05-22 21:06:31 +0100

    Allow files to be tracked in the job

--- includes/data-port/class-sensei-import-job.php
+++ includes/data-port/class-sensei-import-job.php
@@ -30,8 +30,23 @@ class Sensei_Import_Job extends Sensei_Data_Port_Job {
 	 */
 	public function __construct( $job_id, $args = [], $json = '' ) {
 		parent::__construct( $job_id, $args, $json );
+
+		$this->import_task_files();
+
 		// TODO: Generate the tasks from the arguments and/or from the internal state.
-		$this->tasks = [];
+		$this->tasks              = [];
+		$this->tasks['questions'] = $this->initialize_task( Sensei_Import_Questions::class );
+		$this->tasks['lessons']   = $this->initialize_task( Sensei_Import_Lessons::class );
+		$this->tasks['courses']   = $this->initialize_task( Sensei_Import_Courses::class );
+	}
+
+	/**
+	 * Ensure the task files have been included.
+	 */
+	private function import_task_files() {
+		include_once __DIR__ . '/import-tasks/class-sensei-import-questions.php';
+		include_once __DIR__ . '/import-tasks/class-sensei-import-courses.php';
+		include_once __DIR__ . '/import-tasks/class-sensei-import-lessons.php';
 	}
 
 	/**
@@ -43,4 +58,39 @@ class Sensei_Import_Job extends Sensei_Data_Port_Job {
 		return $this->tasks;
 	}
 
+	/**
+	 * Get the configuration for expected files.
+	 *
+	 * @return array {
+	 *    @type array $$component {
+	 *        @type callable $validator  Callback to handle validating the file before save (optional).
+	 *        @type array    $mime_types Expected mime-types for the file.
+	 *    }
+	 * }
+	 */
+	public static function get_file_config() {
+		$files = [];
+
+		$csv_mime_types = [
+			'csv' => 'text/csv',
+			'txt' => 'text/plain',
+		];
+
+		$files['questions'] = [
+			'validator'  => [ Sensei_Import_Questions::class, 'validate_source_file' ],
+			'mime_types' => $csv_mime_types,
+		];
+
+		$files['courses'] = [
+			'validator'  => [ Sensei_Import_Courses::class, 'validate_source_file' ],
+			'mime_types' => $csv_mime_types,
+		];
+
+		$files['lessons'] = [
+			'validator'  => [ Sensei_Import_Lessons::class, 'validate_source_file' ],
+			'mime_types' => $csv_mime_types,
+		];
+
+		return $files;
+	}
 }
