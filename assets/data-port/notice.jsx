cfa7e48e73b63f872209a5c0bae785f2cce200b6
Giannis Karagiannis <giannis.karagiannis@automattic.com>
2020-05-29 18:23:54 +0300

    Move icon to the beginning of the text

--- assets/data-port/notice.jsx
+++ assets/data-port/notice.jsx
@@ -12,13 +12,15 @@ export const Notice = ( { message, isError } ) => {
 
 	return (
 		<div className={ 'sensei-data-port-notice' }>
-			{ isError && (
-				<Dashicon
-					className={ 'sensei-data-port-notice__icon error' }
-					icon={ 'warning' }
-				/>
-			) }
-			<p className={ messageClasses }>{ message }</p>
+			<p className={ messageClasses }>
+				{ isError && (
+					<Dashicon
+						className={ 'sensei-data-port-notice__icon error' }
+						icon={ 'warning' }
+					/>
+				) }
+				{ message }
+			</p>
 		</div>
 	);
 };
