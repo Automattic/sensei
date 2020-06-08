fd04651d1f3dda77d775b258d779589605d0fdca
Jake Oehler Morrison <jake.morrison@automattic.com>
2020-06-08 19:32:58 +0100

    Set import tasks in getter

--- includes/data-port/class-sensei-import-job.php
+++ includes/data-port/class-sensei-import-job.php
@@ -23,28 +23,18 @@ class Sensei_Import_Job extends Sensei_Data_Port_Job {
 	private $tasks;
 
 	/**
-	 * Sensei_Import_Job constructor.
-	 *
-	 * @param string $job_id  The job id.
-	 * @param array  $args    Arguments for the import job.
-	 * @param string $json    JSON string to restore the state from.
-	 */
-	public function __construct( $job_id, $args = [], $json = '' ) {
-		parent::__construct( $job_id, $args, $json );
-
-		// TODO: Generate the tasks from the arguments and/or from the internal state.
-		$this->tasks              = [];
-		$this->tasks['questions'] = $this->initialize_task( Sensei_Import_Questions::class );
-		$this->tasks['lessons']   = $this->initialize_task( Sensei_Import_Lessons::class );
-		$this->tasks['courses']   = $this->initialize_task( Sensei_Import_Courses::class );
-	}
-
-	/**
 	 * Get the tasks of this import job.
 	 *
 	 * @return Sensei_Data_Port_Task_Interface[]
 	 */
 	public function get_tasks() {
+		if ( ! isset( $this->tasks ) ) {
+			$this->tasks              = [];
+			$this->tasks['questions'] = $this->initialize_task( Sensei_Import_Questions::class );
+			$this->tasks['lessons']   = $this->initialize_task( Sensei_Import_Lessons::class );
+			$this->tasks['courses']   = $this->initialize_task( Sensei_Import_Courses::class );
+		}
+
 		return $this->tasks;
 	}
 
