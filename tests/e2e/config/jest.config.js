efb83f7374cf95653c57c26bd3b611f0a7d8aba7
Renatho De Carli Rosa <renatho@automattic.com>
2020-06-08 16:55:34 -0300

    Add screenshots to e2e tests

--- tests/e2e/config/jest.config.js
+++ tests/e2e/config/jest.config.js
@@ -5,4 +5,10 @@ module.exports = {
 	...baseE2Econfig,
 	// Specify the path of your project's E2E tests here.
 	roots: [ path.resolve( __dirname, '../specs' ) ],
+
+	// A list of paths to modules that run some code to configure or set up the testing framework before each test
+	setupFilesAfterEnv: [
+		...baseE2Econfig.setupFilesAfterEnv,
+		path.resolve( __dirname, './jest.setup.js' ),
+	],
 };
