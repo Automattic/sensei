aab18181f14c2723457c30085461f0d9b9a5dd91
Giannis Karagiannis <giannis.karagiannis@automattic.com>
2020-06-09 14:45:41 +0300

    Remove jsdom-sixteen env and add mutation observer shim

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ const baseConfig = require( '@wordpress/scripts/config/jest-unit.config.js' );
 
 module.exports = {
 	...baseConfig,
+	setupFilesAfterEnv: [ './jest.setup.js' ],
 	testPathIgnorePatterns: [
 		'/node_modules/',
 		'<rootDir>/build/',
