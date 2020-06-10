fc18e3df8eb23627531c2bc38e19c238c8ba98b6
Renatho De Carli Rosa <renatho@automattic.com>
2020-06-10 14:41:45 -0300

    Add slack screenshot reporter

--- tests/e2e/config/jest.config.js
+++ tests/e2e/config/jest.config.js
@@ -1,14 +1,18 @@
 const path = require( 'path' );
 const { jestConfig: baseE2Econfig } = require( '@woocommerce/e2e-environment' );
+const { jestConfig: pptrConfig } = require( '@automattic/puppeteer-utils' );
 
 module.exports = {
 	...baseE2Econfig,
-	// Specify the path of your project's E2E tests here.
+	...pptrConfig,
+
+	// Path of your project's E2E tests.
 	roots: [ path.resolve( __dirname, '../specs' ) ],
 
 	// A list of paths to modules that run some code to configure or set up the testing framework before each test
 	setupFilesAfterEnv: [
 		...baseE2Econfig.setupFilesAfterEnv,
+		...pptrConfig.setupFilesAfterEnv,
 		path.resolve( __dirname, './jest.setup.js' ),
 	],
 };
