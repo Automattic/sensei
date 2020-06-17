231f22b18aea4a076b4e59e362ac19c5e9602b9d
Renatho De Carli Rosa <renatho@automattic.com>
2020-06-17 17:55:11 -0300

    Add JSDoc to features observation

--- assets/setup-wizard/features/feature-description-utils.js
+++ assets/setup-wizard/features/feature-description-utils.js
@@ -1,5 +1,17 @@
 import { sprintf, __ } from '@wordpress/i18n';
 
+/**
+ * @typedef  {Object} Feature
+ * @property {string} rawTitle Feature raw title.
+ */
+/**
+ * Get a custom observation for a feature.
+ *
+ * @param {string}    slug             Feature slug.
+ * @param {Feature[]} selectedFeatures Features list.
+ *
+ * @return {string|null} Feature observation.
+ */
 export const getFeatureObservation = ( slug, selectedFeatures ) => {
 	if ( 'woocommerce' !== slug || ! selectedFeatures ) {
 		return null;
