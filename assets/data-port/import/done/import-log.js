5ee1f7ac496db664ae5b6b171d0cf4903edbdd65
Peter Kiss <peter@yscik.com>
2020-06-17 22:57:17 +0200

    Add styling

--- assets/data-port/import/done/import-log.js
+++ assets/data-port/import/done/import-log.js
@@ -1,32 +1,46 @@
 import { Button } from '@wordpress/components';
 import { useState, Fragment } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 
 export const ImportLog = ( { result } ) => {
 	const [ isOpen, setOpen ] = useState( false );
 	const toggle = () => setOpen( ( prevOpen ) => ! prevOpen );
 
+	const postTypeLabels = {
+		course: __( 'Courses', 'sensei-lms' ),
+		lesson: __( 'Lessons', 'sensei-lms' ),
+		question: __( 'Questions', 'sensei-lms' ),
+	};
+
 	return (
 		<>
 			<Button isLink onClick={ toggle }>
-				View Import Log
+				{ __( 'View Import Log', 'sensei-lms' ) }
 			</Button>
-			<div hidden={ ! isOpen }>
-				<table className="form-table">
+			<div hidden={ ! isOpen } className="sensei-import-log-data">
+				<table className="sensei-data-table">
 					{ result
 						.filter( ( { errors } ) => errors.length )
 						.map( ( { type, errors } ) => (
 							<Fragment key={ type }>
 								<thead>
 									<tr>
-										<th> { type } </th>
-										<th>Reason for Failure</th>
+										<th> { postTypeLabels[ type ] } </th>
+										<th>
+											{ __(
+												'Reason for Failure',
+												'sensei-lms'
+											) }
+										</th>
 									</tr>
 								</thead>
 								<tbody>
 									{ errors.map( ( error ) => (
 										<tr key={ error.title }>
 											<td>
-												<code>{ error.title }</code>
+												<span className="sensei-import-log-row__title">
+													{ error.title }
+												</span>
 											</td>
 											<td>{ error.reason }</td>
 										</tr>
--- assets/data-port/import/done/index.js
+++ assets/data-port/import/done/index.js
@@ -1,31 +1,13 @@
 import { __, _n } from '@wordpress/i18n';
 import { Section } from '@woocommerce/components';
-import CheckIcon from '../../../setup-wizard/features/check-icon';
 import { formatString } from '../../../setup-wizard/helpers/format-string';
-import { Button } from '@wordpress/components';
+import { Button, Dashicon } from '@wordpress/components';
 import { ImportLog } from './import-log';
 
 /**
  * Done page of the importer.
  */
-export const DonePage = ( {} ) => {
-	function reset() {}
-
-	const getPostTypeLabel = ( { type, count } ) =>
-		( {
-			course: _n( 'course', 'courses', count, 'sensei-lms' ),
-			lesson: _n( 'lesson', 'lessons', count, 'sensei-lms' ),
-			question: _n( 'question', 'questions', count, 'sensei-lms' ),
-		}[ type ] );
-
-	const getPostTypeLabelWithLink = ( { type, count } ) => {
-		const typeLabel = getPostTypeLabel( { type, count } );
-		return formatString( `{{link}}${ typeLabel }{{/link}}`, {
-			// eslint-disable-next-line jsx-a11y/anchor-has-content
-			link: <a href={ `edit.php?post_type=${ type }` } />,
-		} );
-	};
-
+export const DonePage = ( { restartImport } ) => {
 	const result = [
 		{
 			type: 'course',
@@ -45,6 +27,10 @@ export const DonePage = ( {} ) => {
 					title: 'Lesson 1',
 					reason: 'Invalid title',
 				},
+				{
+					title: 'Lesson 2',
+					reason: 'Invalid everything',
+				},
 			],
 		},
 		{
@@ -54,16 +40,32 @@ export const DonePage = ( {} ) => {
 		},
 	];
 
+	const getPostTypeLabel = ( { type, count } ) =>
+		( {
+			course: _n( 'course', 'courses', count, 'sensei-lms' ),
+			lesson: _n( 'lesson', 'lessons', count, 'sensei-lms' ),
+			question: _n( 'question', 'questions', count, 'sensei-lms' ),
+		}[ type ] );
+
+	const getPostTypeLabelWithLink = ( { type, count } ) => {
+		const typeLabel = getPostTypeLabel( { type, count } );
+		return formatString( `{{link}}${ typeLabel }{{/link}}`, {
+			// eslint-disable-next-line jsx-a11y/anchor-has-content
+			link: <a href={ `edit.php?post_type=${ type }` } />,
+		} );
+	};
+
 	const hasErrors = result.some( ( { errors } ) => errors.length );
 
 	return (
-		<section className="sensei-import-form">
-			<Section
-				className={ 'sensei-import-form__body' }
-				component={ 'section' }
-			>
-				<section className="sensei-import-form__result">
-					<CheckIcon className="sensei-import-form__done" />
+		<section className="sensei-import-form sensei-import-done">
+			<Section className="sensei-import-form__body" component="section">
+				<section className="sensei-import-done__result">
+					<Dashicon
+						icon="yes-alt"
+						size={ 150 }
+						className="sensei-import-done__result-icon"
+					/>
 					<p>
 						{ __(
 							'The following content was successfully imported:',
@@ -84,33 +86,37 @@ export const DonePage = ( {} ) => {
 				</section>
 				{ hasErrors && (
 					<>
-						<p>
-							{ formatString(
-								__(
-									'The following content {{strong}}failed{{/strong}} to import:',
-									'sensei-lms'
-								)
-							) }
-						</p>
-						<ul>
-							{ result
-								.filter( ( { errors } ) => errors.length )
-								.map( ( { type, errors } ) => (
-									<li key={ type }>
-										{ errors.length }{ ' ' }
-										{ getPostTypeLabel( {
-											type,
-											count: errors.length,
-										} ) }
-									</li>
-								) ) }
-						</ul>
-						<ImportLog result={ result } />
+						<section className="sensei-import-done__errors">
+							<p>
+								{ formatString(
+									__(
+										'The following content {{strong}}failed{{/strong}} to import:',
+										'sensei-lms'
+									)
+								) }
+							</p>
+							<ul>
+								{ result
+									.filter( ( { errors } ) => errors.length )
+									.map( ( { type, errors } ) => (
+										<li key={ type }>
+											{ errors.length }{ ' ' }
+											{ getPostTypeLabel( {
+												type,
+												count: errors.length,
+											} ) }
+										</li>
+									) ) }
+							</ul>
+						</section>
+						<section className="sensei-import-done__log">
+							<ImportLog result={ result } />
+						</section>
 					</>
 				) }
 
-				<div className={ 'continue-container' }>
-					<Button isPrimary onClick={ reset }>
+				<div className="continue-container sensei-import-footer">
+					<Button isPrimary onClick={ restartImport }>
 						{ __( 'Import More Content', 'sensei-lms' ) }
 					</Button>
 				</div>
