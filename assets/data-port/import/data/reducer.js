8d431b9e33ee20799ed334aba06483e3c6ec8bfc
Jake Oehler Morrison <jake.morrison@automattic.com>
2020-06-17 22:58:47 +0100

    Introduce data reducer for importer UI

---
+++ assets/data-port/import/data/reducer.js
@@ -0,0 +1,185 @@
+import {
+	START_FETCH_IMPORT_DATA,
+	SUCCESS_FETCH_IMPORT_DATA,
+	ERROR_FETCH_IMPORT_DATA,
+	START_START_IMPORT,
+	SUCCESS_START_IMPORT,
+	ERROR_START_IMPORT,
+	ERROR_UPLOAD_IMPORT_DATA_FILE,
+	START_UPLOAD_IMPORT_DATA_FILE,
+	SUCCESS_UPLOAD_IMPORT_DATA_FILE,
+} from './constants';
+
+import { merge } from 'lodash';
+
+const DEFAULT_STATE = {
+	isFetching: true,
+	fetchError: false,
+	data: {
+		completedSteps: [],
+		upload: {
+			isSubmitting: false,
+			errorMsg: null,
+			levels: {
+				courses: {
+					isUploaded: false,
+					inProgress: false,
+					hasError: false,
+					errorMsg: null,
+					filename: null,
+				},
+				lessons: {
+					isUploaded: false,
+					inProgress: false,
+					hasError: false,
+					errorMsg: null,
+					filename: null,
+				},
+				questions: {
+					isUploaded: false,
+					inProgress: false,
+					hasError: false,
+					errorMsg: null,
+					filename: null,
+				},
+			},
+		},
+		import: {
+			status: '',
+			percentage: 0,
+		},
+	},
+};
+
+function updateLevelState( state, levelKey, attributes ) {
+	const newState = {
+		...state,
+		data: {
+			...state.data,
+			upload: {
+				...state.data.upload,
+				levels: {
+					...state.data.upload.levels,
+				},
+			},
+		},
+	};
+
+	newState.data.upload.levels[ levelKey ] = attributes;
+
+	return newState;
+}
+
+/**
+ * Data importer reducer.
+ *
+ * @param {Object}         state  Current state.
+ * @param {{type: string}} action Action to update the state.
+ *
+ * @return {Object} State updated.
+ */
+export default ( state = DEFAULT_STATE, action ) => {
+	switch ( action.type ) {
+		case START_FETCH_IMPORT_DATA:
+			return {
+				...state,
+				isFetching: true,
+				fetchError: false,
+			};
+
+		case SUCCESS_FETCH_IMPORT_DATA:
+			return {
+				...state,
+				isFetching: false,
+				data: merge( state.data, action.data ),
+			};
+
+		case ERROR_FETCH_IMPORT_DATA:
+			// No need to start a new job until we have our first active upload.
+			const isErrorNoActiveJob =
+				action.error.code === 'sensei_data_port_no_active_job';
+
+			return {
+				...state,
+				isFetching: false,
+				fetchError: isErrorNoActiveJob ? false : action.error,
+			};
+
+		case START_START_IMPORT:
+			return {
+				...state,
+				data: {
+					...state.data,
+					upload: {
+						...state.upload,
+						isSubmitting: true,
+					},
+				},
+			};
+
+		case ERROR_START_IMPORT:
+			return {
+				...state,
+				data: {
+					...state.data,
+					upload: {
+						...state.upload,
+						errorMsg: action.error.message,
+						isSubmitting: false,
+					},
+				},
+			};
+
+		case SUCCESS_START_IMPORT:
+			return {
+				...state,
+				data: {
+					...state.data,
+					completedSteps: action.data.completedSteps,
+					upload: {
+						...state.data.upload,
+						isSubmitting: false,
+					},
+					import: {
+						...state.data.import,
+						...action.data.import,
+					},
+				},
+			};
+
+		case START_UPLOAD_IMPORT_DATA_FILE:
+			return updateLevelState( state, action.level, {
+				isUploaded: false,
+				inProgress: true,
+				hasError: false,
+				errorMsg: null,
+				filename: null,
+			} );
+
+		case SUCCESS_UPLOAD_IMPORT_DATA_FILE:
+			action.data.upload.levels[ action.level ] = {
+				...action.data.upload.levels[ action.level ],
+				inProgress: false,
+				hasError: false,
+				errorMsg: null,
+			};
+
+			return updateLevelState(
+				state,
+				action.level,
+				action.data.upload.levels[ action.level ]
+			);
+
+		case ERROR_UPLOAD_IMPORT_DATA_FILE:
+			return updateLevelState( state, action.level, {
+				isUploaded: false,
+				inProgress: false,
+				hasError: true,
+				errorMsg: action.error.message,
+				filename: null,
+			} );
+
+		default:
+			return state;
+	}
+};
---
+++ assets/data-port/import/data/reducer.test.js
@@ -0,0 +1,141 @@
+import reducer from './reducer';
+import {
+	START_FETCH_IMPORT_DATA,
+	SUCCESS_FETCH_IMPORT_DATA,
+	ERROR_FETCH_IMPORT_DATA,
+	START_START_IMPORT,
+	SUCCESS_START_IMPORT,
+	ERROR_START_IMPORT,
+	ERROR_UPLOAD_IMPORT_DATA_FILE,
+	START_UPLOAD_IMPORT_DATA_FILE,
+	SUCCESS_UPLOAD_IMPORT_DATA_FILE,
+} from './constants';
+
+describe( 'Importer reducer', () => {
+	it( 'Should set isFetching to true on START_FETCH_IMPORT_DATA action', () => {
+		const state = reducer( undefined, {
+			type: START_FETCH_IMPORT_DATA,
+		} );
+
+		expect( state.isFetching ).toBeTruthy();
+	} );
+
+	it( 'Should set isFetching to false and update data on SUCCESS_FETCH_IMPORT_DATA action', () => {
+		const data = {
+			test: 'data',
+		};
+
+		const state = reducer( undefined, {
+			type: SUCCESS_FETCH_IMPORT_DATA,
+			data,
+		} );
+
+		expect( state.isFetching ).toBeFalsy();
+		expect( state.data.test ).toBe( data.test );
+	} );
+
+	it( 'Should set isFetching to false and set fetchError on ERROR_FETCH_IMPORT_DATA action', () => {
+		const error = {
+			code: '',
+			message: 'test',
+		};
+
+		const state = reducer( undefined, {
+			type: ERROR_FETCH_IMPORT_DATA,
+			error,
+		} );
+
+		expect( state.isFetching ).toBeFalsy();
+		expect( state.fetchError ).toBe( error );
+	} );
+
+	it( 'Should set isSubmitting to true in upload step on START_START_IMPORT action', () => {
+		const state = reducer( undefined, {
+			type: START_START_IMPORT,
+		} );
+
+		expect( state.data.upload.isSubmitting ).toBeTruthy();
+	} );
+
+	it( 'Should set isSubmitting to false and errorMsg to the error message on ERROR_START_IMPORT action', () => {
+		const error = {
+			code: '',
+			message: 'test',
+		};
+
+		const state = reducer( undefined, {
+			type: ERROR_START_IMPORT,
+			error,
+		} );
+
+		expect( state.data.upload.isSubmitting ).toBeFalsy();
+		expect( state.data.upload.errorMsg ).toBe( error.message );
+	} );
+
+	it( 'Should set isSubmitting to false and update upload and import state data on SUCCESS_START_IMPORT action', () => {
+		const data = {
+			import: {
+				dinosaur: 'test',
+			},
+		};
+
+		const state = reducer( undefined, {
+			type: SUCCESS_START_IMPORT,
+			data,
+		} );
+
+		expect( state.data.upload.isSubmitting ).toBeFalsy();
+		expect( state.data.import.dinosaur ).toBe( data.import.dinosaur );
+	} );
+
+	it( 'Should set inProgress to true for file level state on START_UPLOAD_IMPORT_DATA_FILE action', () => {
+		const level = 'questions';
+		const state = reducer( undefined, {
+			type: START_UPLOAD_IMPORT_DATA_FILE,
+			level,
+		} );
+
+		expect( state.data.upload.levels[ level ].inProgress ).toBeTruthy();
+	} );
+
+	it( 'Should set inProgress to false and update level state on SUCCESS_UPLOAD_IMPORT_DATA_FILE action', () => {
+		const level = 'questions';
+		const data = {
+			upload: {
+				levels: {
+					questions: {
+						filename: 'test.csv',
+						isUploaded: true,
+					},
+				},
+			},
+		};
+		const state = reducer( undefined, {
+			type: SUCCESS_UPLOAD_IMPORT_DATA_FILE,
+			level,
+			data,
+		} );
+
+		expect( state.data.upload.levels[ level ].inProgress ).toBeFalsy();
+		expect( state.data.upload.levels[ level ].isUploaded ).toBeTruthy();
+	} );
+
+	it( 'Should set inProgress to false, hasError to true, and errorMsg for file level on ERROR_UPLOAD_IMPORT_DATA_FILE action', () => {
+		const level = 'questions';
+		const error = {
+			code: '',
+			message: 'test',
+		};
+
+		const state = reducer( undefined, {
+			type: ERROR_UPLOAD_IMPORT_DATA_FILE,
+			error,
+			level,
+		} );
+
+		expect( state.data.upload.levels[ level ].inProgress ).toBeFalsy();
+		expect( state.data.upload.levels[ level ].errorMsg ).toBe(
+			error.message
+		);
+	} );
+} );
