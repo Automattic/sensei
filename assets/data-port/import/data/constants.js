fc5d196504f33c2479663481074d8d40d4490925
Jake Oehler Morrison <jake.morrison@automattic.com>
2020-06-17 22:52:43 +0100

    Introduce data constants for importer UI

---
+++ assets/data-port/import/data/constants.js
@@ -0,0 +1,31 @@
+/**
+ * Data import constants.
+ */
+export const API_BASE_PATH = '/sensei-internal/v1/import/';
+
+/**
+ * Generic fetch action type constants.
+ */
+export const FETCH_FROM_API = 'FETCH_FROM_API';
+
+/**
+ * Fetch action type constants.
+ */
+export const START_FETCH_IMPORT_DATA = 'START_FETCH_IMPORT_DATA';
+export const SUCCESS_FETCH_IMPORT_DATA = 'SUCCESS_FETCH_IMPORT_DATA';
+export const ERROR_FETCH_IMPORT_DATA = 'ERROR_FETCH_IMPORT_DATA';
+
+/**
+ * Start import job action type constants.
+ */
+export const START_START_IMPORT = 'START_START_IMPORT';
+export const SUCCESS_START_IMPORT = 'SUCCESS_START_IMPORT';
+export const ERROR_START_IMPORT = 'ERROR_START_IMPORT';
+
+/**
+ * Upload file constants.
+ */
+export const START_UPLOAD_IMPORT_DATA_FILE = 'START_UPLOAD_IMPORT_DATA_FILE';
+export const SUCCESS_UPLOAD_IMPORT_DATA_FILE =
+	'SUCCESS_UPLOAD_IMPORT_DATA_FILE';
+export const ERROR_UPLOAD_IMPORT_DATA_FILE = 'ERROR_UPLOAD_IMPORT_DATA_FILE';
