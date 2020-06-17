33eda51c461392799c68ae518f2d2111e4a1d72b
Jake Oehler Morrison <jake.morrison@automattic.com>
2020-06-17 22:59:28 +0100

    Introduce data selectors for importer UI

---
+++ assets/data-port/import/data/selectors.test.js
@@ -0,0 +1,137 @@
+import {
+	getFetchError,
+	getNavigationSteps,
+	getStepData,
+	isCompleteStep,
+	isFetching,
+	isReadyToStart,
+} from './selectors';
+
+describe( 'Importer selectors', () => {
+	it( 'Should get is fetching data', () => {
+		const state = {
+			isFetching: true,
+		};
+
+		expect( isFetching( state ) ).toBeTruthy();
+	} );
+
+	it( 'Should get the fetch error', () => {
+		const error = { code: '', message: 'Error message' };
+		const state = {
+			fetchError: error,
+		};
+
+		expect( getFetchError( state ) ).toEqual( error );
+	} );
+
+	it( 'Should get step data', () => {
+		const stepData = {
+			test: 'dinosaur',
+		};
+
+		const state = {
+			data: {
+				stepA: stepData,
+			},
+		};
+
+		expect( getStepData( state, 'stepA' ) ).toEqual( stepData );
+	} );
+
+	it( 'Should get navigation steps', () => {
+		const state = {
+			data: {
+				completedSteps: [ 'upload' ],
+			},
+		};
+
+		const navSteps = getNavigationSteps( state );
+
+		expect( navSteps[ 0 ].isComplete ).toBeTruthy();
+		expect( navSteps[ 0 ].isNext ).toBeFalsy();
+		expect( navSteps[ 1 ].isComplete ).toBeFalsy();
+		expect( navSteps[ 1 ].isNext ).toBeTruthy();
+		expect( navSteps[ 2 ].isComplete ).toBeFalsy();
+		expect( navSteps[ 2 ].isNext ).toBeFalsy();
+	} );
+
+	it( 'Should return whether step is complete or not', () => {
+		const state = {
+			data: {
+				completedSteps: [ 'welcome' ],
+			},
+		};
+
+		expect( isCompleteStep( state, 'welcome' ) ).toBeTruthy();
+		expect( isCompleteStep( state, 'other' ) ).toBeFalsy();
+	} );
+
+	it( 'Should return not ready to start when no files have been uploaded', () => {
+		const state = {
+			data: {
+				upload: {
+					levels: {
+						courses: {
+							isUploaded: false,
+							inProgress: false,
+							hasError: false,
+							errorMsg: null,
+							filename: null,
+						},
+						lessons: {
+							isUploaded: false,
+							inProgress: false,
+							hasError: false,
+							errorMsg: null,
+							filename: null,
+						},
+						questions: {
+							isUploaded: false,
+							inProgress: false,
+							hasError: false,
+							errorMsg: null,
+							filename: null,
+						},
+					},
+				},
+			},
+		};
+
+		expect( isReadyToStart( state ) ).toBeFalsy();
+	} );
+
+	it( 'Should return as ready to start when no files have been uploaded', () => {
+		const state = {
+			data: {
+				upload: {
+					levels: {
+						courses: {
+							isUploaded: false,
+							inProgress: false,
+							hasError: false,
+							errorMsg: null,
+							filename: null,
+						},
+						lessons: {
+							isUploaded: true,
+							inProgress: false,
+							hasError: false,
+							errorMsg: null,
+							filename: 'test.csv',
+						},
+						questions: {
+							isUploaded: false,
+							inProgress: false,
+							hasError: false,
+							errorMsg: null,
+							filename: null,
+						},
+					},
+				},
+			},
+		};
+
+		expect( isReadyToStart( state ) ).toBeTruthy();
+	} );
+} );
