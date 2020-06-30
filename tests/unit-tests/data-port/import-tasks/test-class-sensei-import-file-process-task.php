9664f47306d9d645ac97335f9de1aacacb533701
Jake Oehler Morrison <jomorrison@gmail.com>
2020-06-30 23:23:02 +0100

    Test post-process tasks are executed

--- tests/unit-tests/data-port/import-tasks/test-class-sensei-import-file-process-task.php
+++ tests/unit-tests/data-port/import-tasks/test-class-sensei-import-file-process-task.php
@@ -24,6 +24,49 @@ class Sensei_Import_File_Process_Task_Tests extends WP_UnitTestCase {
 		require_once SENSEI_TEST_FRAMEWORK_DIR . '/data-port/class-sensei-data-port-job-mock.php';
 	}
 
+
+	/**
+	 * Tests that post process tasks run in batches.
+	 */
+	public function testHandlesPostProcessTasks() {
+		$attachment_id     = wp_insert_attachment( [], SENSEI_TEST_FRAMEWORK_DIR . '/data-port/data-files/test_csv_reader.csv' );
+		$file_process_task = $this->mock_task_method(
+			$this->create_job_stub(
+				[ 'mock-key' => $attachment_id ],
+				[ 'completed-lines' => 10 ]
+			),
+			[ 'process_line', 'handle_something' ]
+		);
+
+		$file_process_task
+			->expects( $this->exactly( 70 ) )
+			->method( 'handle_something' );
+
+		$file_process_task
+			->expects( $this->any() )
+			->method( 'process_line' )
+			->will(
+				$this->returnCallback(
+					function( $line_number, $line ) use ( $file_process_task ) {
+						if ( 12 === $line_number ) {
+							for ( $i = 0; $i < 70; $i++ ) {
+								$file_process_task->add_post_process_task( 'something', [ $i ] );
+							}
+						}
+					}
+				)
+			);
+
+		$file_process_task->run();
+		$this->assertFalse( $file_process_task->is_completed(), 'Lines have been processed, but post-process tasks have not' );
+
+		$file_process_task->run();
+		$this->assertFalse( $file_process_task->is_completed(), 'Post process tasks have not finished' );
+
+		$file_process_task->run();
+		$this->assertTrue( $file_process_task->is_completed(), 'Post process tasks have finished' );
+	}
+
 	/**
 	 * Tests that process_line is called with correct data.
 	 */
@@ -85,9 +128,12 @@ class Sensei_Import_File_Process_Task_Tests extends WP_UnitTestCase {
 	}
 
 	private function mock_task_method( $job, $method ) {
+		if ( ! is_array( $method ) ) {
+			$method = [ $method ];
+		}
 		return $this->getMockBuilder( Sensei_Import_File_Process_Task_Mock::class )
 			->setConstructorArgs( [ $job ] )
-			->setMethods( [ $method ] )
+			->setMethods( $method )
 			->getMock();
 	}
 
