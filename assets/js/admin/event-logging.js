6e55ca444d967b8821cf49ce25aa18e351446c0a
Renatho De Carli Rosa <renatho@automattic.com>
2020-07-02 11:18:23 -0300

    Add import submenu tracking
    Introduces the adminTracking array to track WP core elements
    which we can't add custom code.

--- assets/js/admin/event-logging.js
+++ assets/js/admin/event-logging.js
@@ -1,3 +1,12 @@
+// Used to add tracking to WP core elements, where we can't add custom code.
+const adminTracking = [
+	{
+		selector:
+			'#toplevel_page_sensei a[href="admin.php?page=sensei_import"]',
+		eventName: 'import_click',
+	},
+];
+
 window.sensei_log_event = function( event_name, properties ) {
 	if ( sensei_event_logging.enabled ) {
 		let data = {
@@ -11,9 +20,16 @@ window.sensei_log_event = function( event_name, properties ) {
 
 		jQuery.get( ajaxurl, data );
 	}
-}
+};
 
 jQuery( document ).ready( function( $ ) {
+	adminTracking.forEach( function( tracking ) {
+		$( tracking.selector ).attr(
+			'data-sensei-log-event',
+			tracking.eventName
+		);
+	} );
+
 	$( 'body' ).on( 'click', 'a[data-sensei-log-event]', function( event ) {
 		let sensei_event_name = $( event.target ).data( 'sensei-log-event' );
 		sensei_log_event( sensei_event_name );
