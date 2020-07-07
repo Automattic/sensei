9b7c488881d6c5a9b7d6ed22773658a289c2494f
Renatho De Carli Rosa <renatho@automattic.com>
2020-07-07 15:14:07 -0300

    Add delimiter detection

--- includes/data-port/class-sensei-import-csv-reader.php
+++ includes/data-port/class-sensei-import-csv-reader.php
@@ -60,21 +60,61 @@ class Sensei_Import_CSV_Reader {
 	public function __construct( $csv_file, $completed_lines = 0, $lines_per_batch = 30 ) {
 		$this->file = new SplFileObject( $csv_file );
 		$this->file->setFlags( SplFileObject::READ_CSV );
-
-		/**
-		 * Change the default CSV delimiter.
-		 *
-		 * @since 3.2.0
-		 *
-		 * @param string  $delimiter The CSV file delimiter.
-		 */
-		$this->file->setCsvControl( apply_filters( 'sensei_import_csv_delimiter', ',' ) );
+		$this->set_delimiter();
 
 		$this->file->seek( PHP_INT_MAX );
 		$this->total_lines     = $this->file->key();
 		$this->completed_lines = $completed_lines;
 		$this->is_completed    = $this->completed_lines >= $this->total_lines;
 		$this->lines_per_batch = $lines_per_batch;
+	}
+
+	/**
+	 * Set the delimiter to read the CSV file.
+	 *
+	 * The delimiter detection works testing the delimiter which find more columns.
+	 */
+	private function set_delimiter() {
+		/**
+		 * Filters the default CSV delimiter.
+		 *
+		 * @since 3.2.0
+		 * @since 3.3.0 Updated the default to `false`, so it'll get through the delimiter detection.
+		 * @hook sensei_import_csv_delimiter
+		 *
+		 * @param {string} $delimiter The CSV file delimiter.
+		 */
+		$forced_delimiter = apply_filters( 'sensei_import_csv_delimiter', false );
+
+		if ( $forced_delimiter ) {
+			$this->file->setCsvControl( $forced_delimiter );
+			return;
+		}
+
+		/**
+		 * Filters the CSV delimiter options.
+		 *
+		 * @since 3.3.0
+		 * @hook sensei_import_csv_delimiter_options
+		 *
+		 * @param {string[]} $delimiters The CSV file delimiter options.
+		 */
+		$delimiters         = apply_filters( 'sensei_import_csv_delimiter_options', [ ',', ';', "\t", '|' ] );
+		$max_columns        = 0;
+		$selected_delimiter = '';
+
+		foreach ( $delimiters as $delimiter ) {
+			$this->file->setCsvControl( $delimiter );
+			$this->file->seek( 0 );
+			$columns = count( $this->file->current() );
+
+			if ( $columns > $max_columns ) {
+				$max_columns        = $columns;
+				$selected_delimiter = $delimiter;
+			}
+		}
+
+		$this->file->setCsvControl( $selected_delimiter );
 	}
 
 	/**
