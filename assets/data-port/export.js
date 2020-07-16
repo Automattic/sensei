d3531eefc067b336c921c781abd8706e02da80c4
Peter Kiss <peter@yscik.com>
2020-07-16 12:36:02 +0200

    Add export content type selection page

--- assets/data-port/export.js
+++ assets/data-port/export.js
@@ -2,6 +2,7 @@ import { H } from '@woocommerce/components';
 import { __ } from '@wordpress/i18n';
 import { render } from '@wordpress/element';
 import { useSenseiColorTheme } from '../react-hooks/use-sensei-color-theme';
+import { SelectExportContentPage } from './export/select-export-content-page';
 
 /**
  * Sensei export page.
@@ -24,6 +25,8 @@ const SenseiExportPage = () => {
 						) }
 					</p>
 				</header>
+
+				<SelectExportContentPage />
 			</section>
 		</div>
 	);
---
+++ assets/data-port/export/select-export-content-page.js
@@ -0,0 +1,55 @@
+import { Button, CheckboxControl } from '@wordpress/components';
+import { useReducer } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+const labels = {
+	course: __( 'Courses', 'sensei-lms' ),
+	lesson: __( 'Lessons', 'sensei-lms' ),
+	question: __( 'Questions', 'sensei-lms' ),
+};
+
+export const SelectExportContentPage = ( { onSubmit } ) => {
+	const [ values, setValue ] = useReducer(
+		( state, newValues ) => ( { ...state, ...newValues } ),
+		{
+			course: false,
+			lesson: false,
+			question: false,
+		}
+	);
+	const submit = ( event ) => {
+		event.preventDefault();
+		onSubmit( values );
+	};
+
+	return (
+		<form onSubmit={ submit }>
+			<div className="sensei-data-port-step__body">
+				<p className="sensei-export__select-content__label">
+					{ __(
+						'Which type of content would you like to export?',
+						'sensei-lms'
+					) }
+				</p>
+
+				<div className="sensei-export__select-content__options">
+					{ Object.entries( values ).map( ( [ type, value ] ) => (
+						<CheckboxControl
+							className="sensei-export__select-content__option"
+							key={ type }
+							name={ type }
+							checked={ value }
+							onChange={ ( v ) => setValue( { [ type ]: v } ) }
+							label={ labels[ type ] }
+						/>
+					) ) }
+				</div>
+				<div className="sensei-data-port-step__footer">
+					<Button type="submit" isPrimary>
+						{ __( 'Generate CSV', 'sensei-lms' ) }
+					</Button>
+				</div>
+			</div>
+		</form>
+	);
+};
