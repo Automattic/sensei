5facf507abdd261ab162d79b350ee00c5dd0e621
Peter Kiss <peter@yscik.com>
2020-08-03 18:03:47 +0200

    Update tests

--- tests/unit-tests/data-port/export-tasks/test-class-sensei-export-courses.php
+++ tests/unit-tests/data-port/export-tasks/test-class-sensei-export-courses.php
@@ -70,21 +70,21 @@ class Sensei_Export_Courses_Tests extends WP_UnitTestCase {
 
 		$course = $this->factory->course->create_and_get();
 
-		$terms = [
-			$this->factory->term->create(
-				[
-					'taxonomy' => 'course-category',
-					'name'     => 'Course Category Parent',
-				]
-			),
+		$term =
 			$this->factory->term->create(
 				[
 					'taxonomy' => 'course-category',
 					'name'     => 'Course Category Child',
+					'parent'   => $this->factory->term->create(
+						[
+							'taxonomy' => 'course-category',
+							'name'     => 'Course Category Parent',
+						]
+					),
 				]
-			),
-		];
-		$this->factory->term->add_post_terms( $course->ID, $terms[1], 'course-category', false );
+			);
+
+		$this->factory->term->add_post_terms( $course->ID, $term, 'course-category', false );
 
 		$post   = get_post( $course->ID );
 		$result = $this->export();
@@ -96,13 +96,38 @@ class Sensei_Export_Courses_Tests extends WP_UnitTestCase {
 
 	}
 
-	public function testModulesSerialized() {
+	public function testModulesExported() {
+		$course = $this->factory->course->create_and_get();
+
+		$terms = [
+			$this->factory->term->create(
+				[
+					'taxonomy' => Sensei()->modules->taxonomy,
+					'name'     => 'Module A',
+				]
+			),
+			$this->factory->term->create(
+				[
+					'taxonomy' => Sensei()->modules->taxonomy,
+					'name'     => 'Module B',
+				]
+			),
+		];
+		$this->factory->term->add_post_terms( $course->ID, $terms, Sensei()->modules->taxonomy, false );
+
+		$post   = get_post( $course->ID );
+		$result = $this->export();
+
+		$this->assertEquals(
+			'Module A,Module B',
+			$result[0]['modules']
+		);
 	}
 
 	/**
 	 * Test that course details are exported correctly.
 	 */
-	public function testCourseExported() {
+	public function testCourseContentExported() {
 		$course = $this->factory->course->create_and_get();
 
 		$result = $this->export();
@@ -119,7 +144,65 @@ class Sensei_Export_Courses_Tests extends WP_UnitTestCase {
 		);
 	}
 
+	/**
+	 * Test that course details are exported correctly.
+	 */
+	public function testCourseImageExported() {
+		$course = $this->factory->course->create_and_get();
+
+		$thumbnail_id = $this->factory->attachment->create( [ 'file' => 'localfilename.png' ] );
+		set_post_thumbnail( $course, $thumbnail_id );
+
+		$result = $this->export();
+
+		$this->assertArraySubset(
+			[
+				'image' => wp_get_attachment_image_url( $thumbnail_id ),
+			],
+			$result[0]
+		);
+	}
+
+	public function testCourseTeacherExported() {
+		$teacher = $this->factory->user->create_and_get(
+			[
+				'user_login' => 'sensei',
+				'user_email' => 'testuser@senseilms.com',
+			]
+		);
+		$this->factory->course->create_and_get( [ 'post_author' => $teacher->ID ] );
+
+		$result = $this->export();
+
+		$this->assertArraySubset(
+			[
+				'teacher username' => $teacher->display_name,
+				'teacher email'    => $teacher->user_email,
+
+			],
+			$result[0]
+		);
+	}
+
 	public function testCourseMetaExported() {
+
+		$course = $this->factory->course->create_and_get();
+
+		update_post_meta( $course->ID, '_course_featured', true );
+		update_post_meta( $course->ID, '_course_video_embed', '<iframe>' );
+		update_post_meta( $course->ID, 'disable_notification', false );
+
+		$result = $this->export();
+
+		$this->assertArraySubset(
+			[
+				'featured'      => '1',
+				'video'         => '<iframe>',
+				'notifications' => '1',
+			],
+			$result[0]
+		);
+
 	}
 
 	/**
@@ -129,11 +212,7 @@ class Sensei_Export_Courses_Tests extends WP_UnitTestCase {
 		$course_ids = $this->factory->course->create_many( 2 );
 		add_post_meta( $course_ids[0], '_course_prerequisite', $course_ids[1] );
 
-		$job  = Sensei_Export_Job::create( 'test', 0 );
-		$task = new Sensei_Export_Courses( $job );
-		$task->run();
-
-		$result = self::read_csv( $job->get_file_path( 'course' ) );
+		$result = $this->export();
 
 		$course_1 = self::get_by_id( $result, $course_ids[0] );
 		$this->assertEquals( $course_ids[1], $course_1['prerequisite'] );
@@ -153,7 +232,7 @@ class Sensei_Export_Courses_Tests extends WP_UnitTestCase {
 	 * @return array The line for the course.
 	 */
 	protected static function get_by_id( array $result, $course_id ) {
-		$key = array_search( $course_id, array_column( $result, 'id' ), true );
+		$key = array_search( strval( $course_id ), array_column( $result, 'id' ), true );
 		return $result[ $key ];
 	}
 
