cd89bf73612c22eab81e0d2bd7fac1341d6d30f8
Peter Kiss <peter@yscik.com>
2020-08-03 16:11:55 +0200

    Update property name in tests

--- assets/data-port/export/export-page.test.js
+++ assets/data-port/export/export-page.test.js
@@ -3,7 +3,7 @@ import { ExportPage } from './export-page';
 
 describe( '<ExportPage />', () => {
 	it( 'shows content selection screen if no job is in progress', () => {
-		const { queryByText } = render( <ExportPage state={ null } /> );
+		const { queryByText } = render( <ExportPage job={ null } /> );
 
 		expect(
 			queryByText( 'Which type of content would you like to export?' )
@@ -12,7 +12,7 @@ describe( '<ExportPage />', () => {
 
 	it( 'shows progress screen if there is a job', () => {
 		const { getByRole } = render(
-			<ExportPage state={ { status: 'pending', percent: 0 } } />
+			<ExportPage job={ { status: 'pending', percent: 0 } } />
 		);
 
 		expect( getByRole( 'progressbar' ) ).toBeTruthy();
