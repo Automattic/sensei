e13d679bb5301f300a82842028386d2e67a66404
Peter Kiss <peter@yscik.com>
2020-08-27 18:11:47 +0200

    Update version

--- sensei-lms.php
+++ sensei-lms.php
@@ -88,7 +88,7 @@ if ( ! function_exists( 'Sensei' ) ) {
 	 * @since 1.8.0
 	 */
 	function Sensei() {
-		return Sensei_Main::instance( array( 'version' => '3.4.1' ) );
+		return Sensei_Main::instance( array( 'version' => '3.5.0' ) );
 	}
 }
 
