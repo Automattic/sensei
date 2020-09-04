849f7d2772de66bab46b7323d2d8e9fd3e9f96bc
Renatho De Carli Rosa <renatho@automattic.com>
2020-09-04 15:37:28 -0300

    Add new lesson when pressing enter
    It happens when the user is with the focus in the last lesson from
    a list (in the module or out).

--- assets/blocks/course-outline/lesson-block/edit.js
+++ assets/blocks/course-outline/lesson-block/edit.js
@@ -1,18 +1,40 @@
 import { __ } from '@wordpress/i18n';
+import { createBlock } from '@wordpress/blocks';
+import { useDispatch } from '@wordpress/data';
 
 import SingleLineInput from '../single-line-input';
 
-const Edit = ( { className, attributes: { title }, setAttributes } ) => (
-	<div className={ className }>
-		<SingleLineInput
-			className="wp-block-sensei-lms-course-outline-lesson__input"
-			placeholder={ __( 'Lesson name', 'sensei-lms' ) }
-			value={ title }
-			onChange={ ( value ) => {
-				setAttributes( { title: value } );
-			} }
-		/>
-	</div>
-);
+const Edit = ( {
+	clientId,
+	name,
+	className,
+	attributes: { title },
+	setAttributes,
+	insertBlocksAfter,
+} ) => {
+	const { selectNextBlock } = useDispatch( 'core/block-editor' );
+
+	return (
+		<div className={ className }>
+			<SingleLineInput
+				className="wp-block-sensei-lms-course-outline-lesson__input"
+				placeholder={ __( 'Lesson name', 'sensei-lms' ) }
+				value={ title }
+				onChange={ ( value ) => {
+					setAttributes( { title: value } );
+				} }
+				onKeyUp={ ( e ) => {
+					if ( 13 === e.keyCode ) {
+						selectNextBlock( clientId ).then( ( blocks ) => {
+							if ( ! blocks && 0 < title.length ) {
+								insertBlocksAfter( [ createBlock( name ) ] );
+							}
+						} );
+					}
+				} }
+			/>
+		</div>
+	);
+};
 
 export default Edit;
