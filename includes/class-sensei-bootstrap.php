79ecd88ed5b0e80fde2af679c6e61fd902ca13cc
Peter Kiss <peter@yscik.com>
2020-09-22 13:57:08 +0200

    Add doc comments to bootsrap file

--- includes/class-sensei-bootstrap.php
+++ includes/class-sensei-bootstrap.php
@@ -1,7 +1,12 @@
 <?php
+/**
+ * File containing Sensei_Bootstrap.
+ *
+ * @package Sensei
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit; // Exit if accessed directly.
 }
 
 /**
@@ -11,23 +16,37 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class Sensei_Bootstrap {
 	/**
+	 * Singleton instance.
+	 *
 	 * @var Sensei_Bootstrap
 	 */
 	private static $instance;
 
 	/**
+	 * Autoloader instance.
+	 *
 	 * @var null|Sensei_Autoloader
 	 */
 	private $autoloader = null;
 
 	/**
+	 * Flag to make sure bootstrapping is only run once.
+	 *
 	 * @var bool
 	 */
 	private $is_bootstrapped = false;
 
+	/**
+	 * Sensei_Bootstrap constructor.
+	 */
 	private function __construct() {
 	}
 
+	/**
+	 * Set up Sensei class loaders and includes.
+	 *
+	 * @return $this
+	 */
 	public function bootstrap() {
 		if ( $this->is_bootstrapped ) {
 			return $this;
@@ -39,11 +58,19 @@ class Sensei_Bootstrap {
 		return $this;
 	}
 
+	/**
+	 * Initialize Sensei class autoloader.
+	 */
 	private function init_autoloader() {
 		require_once dirname( __FILE__ ) . '/class-sensei-autoloader.php';
 		$this->autoloader = new Sensei_Autoloader();
 	}
 
+	/**
+	 * Get singleton.
+	 *
+	 * @return Sensei_Bootstrap
+	 */
 	public static function get_instance() {
 		if ( null === self::$instance ) {
 			self::$instance = new self();
@@ -51,6 +78,9 @@ class Sensei_Bootstrap {
 		return self::$instance;
 	}
 
+	/**
+	 * Include global functions and compatibility helpers.
+	 */
 	private function init_must_have_includes() {
 		require_once dirname( __FILE__ ) . '/sensei-functions.php';
 		require_once dirname( __FILE__ ) . '/3rd-party/3rd-party.php';
