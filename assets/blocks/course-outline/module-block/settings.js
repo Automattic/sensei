55c6fa0efa898a599c3d5ce87163b65e24a05b47
Peter Kiss <peter@yscik.com>
2020-09-28 17:26:52 +0200

    Remove unused code

--- assets/blocks/course-outline/module-block/settings.js
+++ assets/blocks/course-outline/module-block/settings.js
@@ -1,6 +1,5 @@
 import { InspectorControls } from '@wordpress/block-editor';
-import { Button, PanelBody } from '@wordpress/components';
-import { useDispatch, useSelect } from '@wordpress/data';
+import { PanelBody } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { ModuleStatusControl } from './module-status-control';
 
@@ -10,44 +9,11 @@ import { ModuleStatusControl } from './module-status-control';
  * @param {Object}   props
  * @param {boolean}  props.isPreviewCompleted    Whether Completed preview is enabled.
  * @param {Function} props.setIsPreviewCompleted Callback to be called when preview is updated.
- * @param {string}   props.clientId
- * @param {string}   props.className
  */
 export function ModuleBlockSettings( {
 	isPreviewCompleted,
 	setIsPreviewCompleted,
-	clientId,
-	className,
 } ) {
-	const getModuleBlocks = useSelect(
-		( select ) => {
-			return () =>
-				select( 'core/block-editor' )
-					.getBlocks( clientId )
-					.filter(
-						( { name } ) =>
-							'sensei-lms/course-outline-module' === name
-					);
-		},
-		[ clientId ]
-	);
-
-	const { updateBlockAttributes } = useDispatch( 'core/block-editor' );
-
-	const blockStyle =
-		( className && className.match( /is-style-(\w+)/ ) ) || 'default';
-
-	const applyStyle = () => {
-		getModuleBlocks().forEach( ( block ) => {
-			updateBlockAttributes( block.clientId, {
-				className: block.className.replace(
-					/(is-style-\w+|$)/,
-					` is-style-${ blockStyle[ 0 ] || 'default' }`
-				),
-			} );
-		} );
-	};
-
 	return (
 		<InspectorControls>
 			<PanelBody
@@ -59,11 +25,6 @@ export function ModuleBlockSettings( {
 					setIsPreviewCompleted={ setIsPreviewCompleted }
 				/>
 			</PanelBody>
-			<div style={ { padding: '16px' } }>
-				<Button isLink onClick={ applyStyle }>
-					{ __( 'Apply style to all modules', 'sensei-lms' ) }
-				</Button>
-			</div>
 		</InspectorControls>
 	);
 }
