54b058a2b9d2dbfb8f90addc430a9dee34190fae
Peter Kiss <peter@yscik.com>
2020-10-01 15:02:57 +0200

    Fix placeholder icon and buttons for Wordpress 5.3

--- assets/blocks/course-outline/course-block/placeholder.js
+++ assets/blocks/course-outline/course-block/placeholder.js
@@ -1,7 +1,7 @@
+import { BlockIcon } from '@wordpress/block-editor';
 import { Button, Placeholder } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import settings from './index';
-
 /**
  * Placeholder for empty Course Outline block.
  *
@@ -10,16 +10,16 @@ import settings from './index';
 export const CourseOutlinePlaceholder = ( { addBlock } ) => (
 	<Placeholder
 		label={ __( 'Course Outline', 'sensei-lms' ) }
-		icon={ settings.icon() }
+		icon={ <BlockIcon icon={ settings.icon } showColors /> }
 		instructions={ __(
 			'Build and display a course outline. A course is made up of modules (optional) and lessons. You can use modules to group related lessons together.',
 			'sensei-lms'
 		) }
 	>
-		<Button isSecondary onClick={ () => addBlock( 'module' ) }>
+		<Button isDefault onClick={ () => addBlock( 'module' ) }>
 			{ __( 'Create a module', 'sensei-lms' ) }
-		</Button>
-		<Button isSecondary onClick={ () => addBlock( 'lesson' ) }>
+		</Button>{ ' ' }
+		<Button isDefault onClick={ () => addBlock( 'lesson' ) }>
 			{ __( 'Create a lesson', 'sensei-lms' ) }
 		</Button>
 	</Placeholder>
