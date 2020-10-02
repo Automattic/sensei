d4e752f2d5e0e7a7094f66dcc9682fdac25b3550
Renatho De Carli Rosa <renatho@automattic.com>
2020-10-02 14:29:38 -0300

    Add focus to blocks when creating through the placeholder

--- assets/blocks/course-outline/course-block/edit.js
+++ assets/blocks/course-outline/course-block/edit.js
@@ -65,7 +65,7 @@ const EditCourseOutlineBlock = ( {
 	if ( isEmpty ) {
 		return (
 			<CourseOutlinePlaceholder
-				addBlock={ ( type ) => setBlocks( [ { type } ] ) }
+				addBlock={ ( type ) => setBlocks( [ { type } ], true ) }
 			/>
 		);
 	}
