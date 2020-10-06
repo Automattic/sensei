1148a2d8a0b99ac4d3cb1674574683cfba70f914
Peter Kiss <peter@yscik.com>
2020-10-06 15:49:43 +0200

    Package svg icons for Wordpress 5.3 compatibility

---
+++ assets/icons/wordpress-icons.js
@@ -0,0 +1,7 @@
+import { Path, SVG } from '@wordpress/components';
+
+export const chevronRight = (
+	<SVG xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+		<Path d="M10.6 6L9.4 7l4.6 5-4.6 5 1.2 1 5.4-6z" />
+	</SVG>
+);
