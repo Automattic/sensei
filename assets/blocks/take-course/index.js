007e1f499fd0cf19d7264b91c8224e740fb2f58c
Peter Kiss <peter@yscik.com>
2020-10-14 23:40:21 +0200

    Update keywords and description

--- assets/blocks/take-course/index.js
+++ assets/blocks/take-course/index.js
@@ -1,3 +1,4 @@
+import { __ } from '@wordpress/i18n';
 import { createButtonBlockType } from '../button';
 
 /**
@@ -7,7 +8,18 @@ export default createButtonBlockType( {
 	tagName: 'button',
 	settings: {
 		name: 'sensei-lms/button-take-course',
-		title: 'Take Course',
+		title: __( 'Take Course', 'sensei-lms' ),
+		description: __(
+			'Allows the learner to start the course. Only displayed to users not already enrolled.',
+			'sensei-lms'
+		),
+		keywords: [
+			__( 'Start', 'sensei-lms' ),
+			__( 'Sign up', 'sensei-lms' ),
+			__( 'Enrol', 'sensei-lms' ),
+			__( 'Enroll', 'sensei-lms' ),
+			__( 'Course', 'sensei-lms' ),
+		],
 		attributes: {
 			text: {
 				default: 'Take Course',
