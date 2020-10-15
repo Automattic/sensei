45725fd1a7e509bdea4d2346936dacb437d23d44
Alex Sanford <alex.sanford1@gmail.com>
2020-10-15 13:01:43 +0000

    Add Edit Lesson link to toolbar

--- assets/blocks/course-outline/lesson-block/settings.js
+++ assets/blocks/course-outline/lesson-block/settings.js
@@ -1,5 +1,11 @@
-import { InspectorControls } from '@wordpress/block-editor';
-import { ExternalLink, FontSizePicker, PanelBody } from '@wordpress/components';
+import { InspectorControls, BlockControls } from '@wordpress/block-editor';
+import {
+	ExternalLink,
+	FontSizePicker,
+	PanelBody,
+	ToolbarButton,
+	ToolbarGroup,
+} from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 
@@ -27,10 +33,50 @@ export const LessonBlockSettings = ( {
 	);
 
 	return (
-		<InspectorControls>
-			{ id && (
-				<PanelBody title={ __( 'Lesson', 'sensei-lms' ) }>
-					<h2>
+		<>
+			<InspectorControls>
+				{ id && (
+					<PanelBody title={ __( 'Lesson', 'sensei-lms' ) }>
+						<h2>
+							<ExternalLink
+								href={ `post.php?post=${ id }&action=edit` }
+								target="lesson"
+								className="wp-block-sensei-lms-course-outline-lesson__edit"
+							>
+								{ __( 'Edit lesson', 'sensei-lms' ) }
+							</ExternalLink>
+						</h2>
+						<p>
+							{ __(
+								'Edit details such as lesson content, prerequisite, quiz settings and more.',
+								'sensei-lms'
+							) }
+						</p>
+					</PanelBody>
+				) }
+				<PanelBody title={ __( 'Typography', 'sensei-lms' ) }>
+					<FontSizePicker
+						fontSizes={ fontSizes }
+						value={ fontSize }
+						onChange={ ( value ) => {
+							setAttributes( { fontSize: value } );
+						} }
+					/>
+				</PanelBody>
+				<PanelBody
+					title={ __( 'Status', 'sensei-lms' ) }
+					initialOpen={ false }
+				>
+					<StatusControl
+						status={ previewStatus }
+						setStatus={ setPreviewStatus }
+						options={ [ Status.NOT_STARTED, Status.COMPLETED ] }
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<BlockControls>
+				<ToolbarGroup>
+					<ToolbarButton>
 						<ExternalLink
 							href={ `post.php?post=${ id }&action=edit` }
 							target="lesson"
@@ -38,34 +84,9 @@ export const LessonBlockSettings = ( {
 						>
 							{ __( 'Edit lesson', 'sensei-lms' ) }
 						</ExternalLink>
-					</h2>
-					<p>
-						{ __(
-							'Edit details such as lesson content, prerequisite, quiz settings and more.',
-							'sensei-lms'
-						) }
-					</p>
-				</PanelBody>
-			) }
-			<PanelBody title={ __( 'Typography', 'sensei-lms' ) }>
-				<FontSizePicker
-					fontSizes={ fontSizes }
-					value={ fontSize }
-					onChange={ ( value ) => {
-						setAttributes( { fontSize: value } );
-					} }
-				/>
-			</PanelBody>
-			<PanelBody
-				title={ __( 'Status', 'sensei-lms' ) }
-				initialOpen={ false }
-			>
-				<StatusControl
-					status={ previewStatus }
-					setStatus={ setPreviewStatus }
-					options={ [ Status.NOT_STARTED, Status.COMPLETED ] }
-				/>
-			</PanelBody>
-		</InspectorControls>
+					</ToolbarButton>
+				</ToolbarGroup>
+			</BlockControls>
+		</>
 	);
 };
