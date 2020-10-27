4ddf949e69f08b8aa0747b0f97eb5546b5424e7a
Peter Kiss <peter@yscik.com>
2020-10-27 13:30:09 +0100

    Send full event if tracking is enabled

--- includes/admin/class-sensei-exit-survey.php
+++ includes/admin/class-sensei-exit-survey.php
@@ -63,7 +63,11 @@ class Sensei_Exit_Survey {
 
 		update_option( 'sensei_exit_survey_data', $feedback );
 
-		Sensei()->usage_tracking->send_anonymous_event( 'plugin_deactivate', $feedback );
+		if ( Sensei()->usage_tracking->is_tracking_enabled() ) {
+			Sensei()->usage_tracking->send_event( 'plugin_deactivate', $feedback );
+		} else {
+			Sensei()->usage_tracking->send_anonymous_event( 'plugin_deactivate', $feedback );
+		}
 	}
 
 }
