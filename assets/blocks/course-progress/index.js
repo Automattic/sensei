b2433bf734dd96acc858d69b49cefea1385d9446
Giannis Karagiannis <giannis.karagiannis@automattic.com>
2020-11-04 17:04:16 +0200

    Add block icons

--- assets/blocks/course-progress/index.js
+++ assets/blocks/course-progress/index.js
@@ -1,4 +1,5 @@
 import { __ } from '@wordpress/i18n';
+import { ProgressIcon as icon } from '../../icons';
 import edit from './edit';
 import metadata from './block';
 
@@ -13,6 +14,7 @@ export default {
 		__( 'Bar', 'sensei-lms' ),
 		__( 'Course', 'sensei-lms' ),
 	],
+	icon,
 	...metadata,
 	edit,
 };
--- assets/icons/index.js
+++ assets/icons/index.js
@@ -1,3 +1,4 @@
 export { CourseIcon } from './course-icon';
 export { LessonIcon } from './lesson-icon';
 export { ModuleIcon } from './module-icon';
+export { ProgressIcon } from './progress-icon';
