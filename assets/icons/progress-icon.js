5d0da29821b9a4f30f5b288a1bcf4aa4e10c1062
Giannis Karagiannis <giannis.karagiannis@automattic.com>
2020-11-05 10:15:58 +0200

    Fix progress icon coloring

--- assets/icons/progress-icon.js
+++ assets/icons/progress-icon.js
@@ -1,27 +1,19 @@
 import { Rect, Path, SVG } from '@wordpress/components';
 
 export const ProgressIcon = () => (
-	<SVG
-		width="24"
-		height="24"
-		viewBox="0 0 24 24"
-		fill="none"
-		xmlns="http://www.w3.org/2000/svg"
-	>
+	<SVG viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 		<Rect
 			x="2.75"
 			y="7.75"
 			width="18.5"
 			height="6.5"
 			rx="3.25"
-			stroke="black"
+			stroke="currentColor"
 			stroke-width="1.5"
 			fill-opacity="0"
 		/>
 		<Path
 			d="M6 7.75 H 16.7 L 10.2 14.25 H 6 C 4.2 14.25 2.75 12.8 2.75 11 C 2.75 9.2 4.2 7.75 6 7.75Z"
-			fill="black"
-			stroke="black"
 			stroke-width="1.5"
 		/>
 	</SVG>
