f7c7a017793ae8e7d15285b0cec3d56a15c10335
Giannis Karagiannis <giannis.karagiannis@automattic.com>
2020-11-09 09:28:19 +0200

    Add curly braces to filter doc

--- includes/blocks/class-sensei-block-take-course.php
+++ includes/blocks/class-sensei-block-take-course.php
@@ -40,7 +40,7 @@ class Sensei_Block_Take_Course {
 		 * @hook sensei_take_course_block_type_args
 		 * @see register_block_type
 		 *
-		 * @param array $args The block arguments as defined by register_block_type.
+		 * @param {array} $args The block arguments as defined by register_block_type.
 		 */
 		$block_args = apply_filters( 'sensei_take_course_block_type_args', [ 'render_callback' => [ $this, 'render_take_course_block' ] ] );
 
