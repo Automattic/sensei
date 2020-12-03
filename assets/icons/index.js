6645a187ab466edf9bb0b6368d592e1ac9a2235c
Giannis Karagiannis <giannis.karagiannis@automattic.com>
2020-12-03 16:21:26 +0200

    Use Sensei logo in the block category

--- assets/icons/index.js
+++ assets/icons/index.js
@@ -2,3 +2,4 @@ export { CourseIcon } from './course-icon';
 export { LessonIcon } from './lesson-icon';
 export { ModuleIcon } from './module-icon';
 export { ProgressIcon } from './progress-icon';
+export { SenseiIcon } from './sensei-icon';
