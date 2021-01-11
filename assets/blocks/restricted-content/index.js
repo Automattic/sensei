309536720da4ba0cacfdbffea4b96f1db1329ad8
Giannis Karagiannis <giannis.karagiannis@automattic.com>
2021-01-11 15:57:20 +0200

    Add restrict content block transforms

--- assets/blocks/restricted-content/index.js
+++ assets/blocks/restricted-content/index.js
@@ -3,6 +3,7 @@ import { Icon } from '@wordpress/components';
 import edit from './edit';
 import save from './save';
 import metadata from './block';
+import { createBlock } from '@wordpress/blocks';
 
 export default {
 	title: __( 'Restricted Content', 'sensei-lms' ),
@@ -23,4 +24,52 @@ export default {
 	edit,
 	save,
 	...metadata,
+	transforms: {
+		from: [
+			{
+				type: 'block',
+				isMultiBlock: true,
+				blocks: [ '*' ],
+				__experimentalConvert: ( blocks ) => {
+					if (
+						blocks.length === 1 &&
+						blocks[ 0 ].name === 'sensei-lms/restricted-content'
+					) {
+						return;
+					}
+
+					// The conversion is done by creating a wrapper block and setting the selected blocks as inner blocks.
+					const wrapperInnerBlocks = blocks.map( ( block ) => {
+						return createBlock(
+							block.name,
+							block.attributes,
+							block.innerBlocks
+						);
+					} );
+
+					const alignments = [ 'wide', 'full' ];
+
+					// Determine the widest setting of all the blocks to be grouped.
+					const widestAlignment = blocks.reduce(
+						( result, block ) => {
+							const { align } = block.attributes;
+							return alignments.indexOf( align ) >
+								alignments.indexOf( result )
+								? align
+								: result;
+						},
+						undefined
+					);
+
+					return createBlock(
+						'sensei-lms/restricted-content',
+						{
+							align: widestAlignment,
+						},
+						wrapperInnerBlocks
+					);
+				},
+			},
+		],
+	},
 };
