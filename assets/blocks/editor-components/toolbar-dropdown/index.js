5ceac2ea4131c6b48a4e88bccc6bbd3b3938dc16
Renatho De Carli Rosa <renatho@automattic.com>
2021-01-18 17:12:21 -0300

    Add toolbar dropdown component

---
+++ assets/blocks/editor-components/toolbar-dropdown/index.js
@@ -0,0 +1,78 @@
+import {
+	Button,
+	Dropdown,
+	MenuGroup,
+	MenuItem,
+	NavigableMenu,
+} from '@wordpress/components';
+import classnames from 'classnames';
+
+/**
+ * @typedef {Object} DropdownOption
+ *
+ * @property {string} label Option label.
+ * @property {string} value Option value.
+ */
+/**
+ * Dropdown for the editor toolbar.
+ *
+ * @param {Object}           props
+ * @param {DropdownOption[]} props.options        Dropdown options.
+ * @param {string}           [props.optionsLabel] Options label.
+ * @param {Object}           props.icon           Icon for the toolbar.
+ * @param {string}           props.value          Current value.
+ * @param {Function}         props.onChange       Change function.
+ */
+const ToolbarDropdown = ( {
+	options,
+	optionsLabel,
+	icon,
+	value,
+	onChange,
+} ) => {
+	const selectedOption = options.find( ( option ) => value === option.value );
+
+	return (
+		<Dropdown
+			renderToggle={ ( { isOpen, onToggle } ) => (
+				<Button
+					onClick={ onToggle }
+					icon={ icon }
+					aria-expanded={ isOpen }
+					aria-haspopup="true"
+				>
+					{ selectedOption.label }
+				</Button>
+			) }
+			renderContent={ ( { onClose } ) => (
+				<NavigableMenu role="menu" stopNavigationEvents>
+					<MenuGroup label={ optionsLabel }>
+						{ options.map( ( option ) => {
+							const isSelected =
+								option.value === selectedOption.value;
+
+							return (
+								<MenuItem
+									key={ option.value }
+									role="menuitemradio"
+									isSelected={ isSelected }
+									className={ classnames( {
+										'sensei-toolbar-dropdown-item-checked': isSelected,
+									} ) }
+									onClick={ () => {
+										onChange( option.value );
+										onClose();
+									} }
+								>
+									{ option.label }
+								</MenuItem>
+							);
+						} ) }
+					</MenuGroup>
+				</NavigableMenu>
+			) }
+		/>
+	);
+};
+
+export default ToolbarDropdown;
