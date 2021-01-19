4aaef08ebe93c8de8a3e726ec39b75e923be5d32
Renatho De Carli Rosa <renatho@automattic.com>
2021-01-19 17:47:57 -0300

    Refactor lesson actions block, splitting parts

---
+++ assets/blocks/lesson-actions/lesson-actions-block/constants.js
@@ -0,0 +1,32 @@
+// The action blocks, ordered.
+export const ACTION_BLOCKS = [
+	'sensei-lms/button-complete-lesson',
+	'sensei-lms/button-next-lesson',
+	'sensei-lms/button-reset-lesson',
+];
+
+export const BLOCKS_DEFAULT_ATTRIBUTES = {
+	'sensei-lms/button-complete-lesson': {
+		inContainer: true,
+		align: 'full',
+	},
+	'sensei-lms/button-next-lesson': {
+		inContainer: true,
+	},
+	'sensei-lms/button-reset-lesson': {
+		inContainer: true,
+	},
+};
+
+export const INNER_BLOCKS_TEMPLATE = ACTION_BLOCKS.map( ( blockName ) => [
+	blockName,
+	{ ...BLOCKS_DEFAULT_ATTRIBUTES[ blockName ] },
+] );
+
+export const PREVIEW_STATE = {
+	completed: [ 'sensei-lms/button-complete-lesson' ],
+	'in-progress': [
+		'sensei-lms/button-next-lesson',
+		'sensei-lms/button-reset-lesson',
+	],
+};
