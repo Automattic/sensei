75863fac1d359a49d1ac9992ed68650d027ed3cc
Giannis Karagiannis <giannis.karagiannis@automattic.com>
2021-01-21 20:30:12 +0200

    Add lesson actions block in backend

---
+++ includes/blocks/class-sensei-lesson-actions-block.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * File containing the Sensei_Lesson_Actions_Block class.
+ *
+ * @package sensei
+ * @since 3.8.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Class Sensei_Lesson_Actions_Block is responsible for rendering the 'Lesson Actions' block.
+ */
+class Sensei_Lesson_Actions_Block {
+
+	/**
+	 * Sensei_Lesson_Actions_Block constructor.
+	 */
+	public function __construct() {
+		Sensei_Blocks::register_sensei_block(
+			'sensei-lms/lesson-actions',
+			[
+				'render_callback' => [ $this, 'render' ],
+			],
+			Sensei()->assets->src_path( 'blocks/lesson-actions/lesson-actions-block' )
+		);
+	}
+
+	/**
+	 * Renders the block.
+	 *
+	 * @param array  $attributes The block attributes.
+	 * @param string $content    The block content.
+	 *
+	 * @return string The block HTML.
+	 */
+	public function render( array $attributes, string $content ) : string {
+		return ! empty( $content ) ? '<div class="sensei-block-wrapper">' . $content . '</div>' : '';
+	}
+}
