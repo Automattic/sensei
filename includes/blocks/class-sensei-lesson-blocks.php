d80a5e7c31734b970e26135cfa0e7e78baba5583
Giannis Karagiannis <giannis.karagiannis@automattic.com>
2021-01-25 15:06:53 +0200

    Remove deprecation of lesson template

--- includes/blocks/class-sensei-lesson-blocks.php
+++ includes/blocks/class-sensei-lesson-blocks.php
@@ -21,7 +21,6 @@ class Sensei_Lesson_Blocks extends Sensei_Blocks_Initializer {
 
 		add_action( 'enqueue_block_assets', [ $this, 'enqueue_block_assets' ] );
 		add_action( 'enqueue_block_editor_assets', [ $this, 'enqueue_block_editor_assets' ] );
-		add_filter( 'sensei_use_sensei_template', [ $this, 'use_single_lesson_template' ] );
 	}
 
 	/**
@@ -58,40 +57,5 @@ class Sensei_Lesson_Blocks extends Sensei_Blocks_Initializer {
 	public function initialize_blocks() {
 		new Sensei_Lesson_Actions_Block();
 		new Sensei_Next_Lesson_Block();
-	}
-
-	/**
-	 * Disable single lesson template if lesson is block based.
-	 *
-	 * @access private
-	 *
-	 * @param bool $enabled
-	 *
-	 * @return bool
-	 */
-	public function use_single_lesson_template( bool $enabled ) : bool {
-		if ( is_single() && 'lesson' === get_post_type() && ! Sensei()->lesson->is_legacy_lesson( get_post() ) ) {
-			add_filter( 'the_content', [ $this, 'hide_lesson_content' ] );
-			return false;
-		}
-
-		return $enabled;
-	}
-
-	/**
-	 * Hides the post content when the user can't access the lesson.
-	 *
-	 * @param string $content The post content.
-	 *
-	 * @access private
-	 *
-	 * @return string The modified content.
-	 */
-	public function hide_lesson_content( string $content ) : string {
-		if ( in_the_loop() && is_main_query() && ! sensei_can_user_view_lesson() ) {
-			return '';
-		}
-
-		return $content;
 	}
 }
