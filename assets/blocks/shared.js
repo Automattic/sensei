43aea4a9645ea4a5bedc96b99c6afc025b500668
Renatho De Carli Rosa <renatho@automattic.com>
2021-01-27 15:09:42 -0300

    Revert restricted content block in the course only for now

--- assets/blocks/shared.js
+++ assets/blocks/shared.js
@@ -3,6 +3,5 @@
  */
 import registerSenseiBlocks from './register-sensei-blocks';
 import ContactTeacherBlock from './contact-teacher-block';
-import RestrictedContentBlock from './restricted-content-block';
 
-registerSenseiBlocks( [ ContactTeacherBlock, RestrictedContentBlock ] );
+registerSenseiBlocks( [ ContactTeacherBlock ] );
--- assets/blocks/single-course.js
+++ assets/blocks/single-course.js
@@ -5,6 +5,7 @@ import registerSenseiBlocks from './register-sensei-blocks';
 import TakeCourseBlock from './take-course-block';
 import CourseProgressBlock from './course-progress-block';
 import { CourseBlock, LessonBlock, ModuleBlock } from './course-outline';
+import RestrictedContentBlock from './restricted-content-block';
 
 registerSenseiBlocks( [
 	CourseBlock,
@@ -12,4 +13,5 @@ registerSenseiBlocks( [
 	LessonBlock,
 	TakeCourseBlock,
 	CourseProgressBlock,
+	RestrictedContentBlock,
 ] );
