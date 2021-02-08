a7cd088487fef775fe28d3d53e3e8db68975a5c5
Peter Kiss <peter@yscik.com>
2021-02-08 18:18:46 +0100

    Update question type texts

--- assets/blocks/quiz/answer-blocks/index.js
+++ assets/blocks/quiz/answer-blocks/index.js
@@ -19,38 +19,41 @@ import SingleLineAnswer from './single-line';
 const questionTypes = {
 	multichoice: {
 		title: __( 'Multiple Choice', 'sensei-lms' ),
-		description: __(
-			'Select one or more answers from a list of choices.',
-			'sensei-lms'
-		),
+		description: __( 'Select from a list of options.', 'sensei-lms' ),
 		edit: () => <div> [Multiple Choice] </div>,
 	},
 	truefalse: {
 		title: __( 'True / False', 'sensei-lms' ),
-		description: __( 'True or false question.', 'sensei-lms' ),
+		description: __(
+			'Select whether a statement is true or false.',
+			'sensei-lms'
+		),
 		edit: () => <div> [True/False] </div>,
 	},
 	gap: {
 		title: __( 'Gap Fill', 'sensei-lms' ),
-		description: __(
-			'Fill in the missing part of a sentence.',
-			'sensei-lms'
-		),
+		description: __( 'Fill in the blank.', 'sensei-lms' ),
 		edit: () => <div> [Gap Fill] </div>,
 	},
 	'single-line': {
 		title: __( 'Single-line', 'sensei-lms' ),
-		description: __( 'Require a written answer.', 'sensei-lms' ),
+		description: __(
+			'Short answer to an open-ended question.',
+			'sensei-lms'
+		),
 		edit: SingleLineAnswer,
 	},
 	'multi-line': {
 		title: __( 'Multi-line', 'sensei-lms' ),
-		description: __( 'Require a written answer.', 'sensei-lms' ),
+		description: __(
+			'Long answer to an open-ended question.',
+			'sensei-lms'
+		),
 		edit: MultiLineAnswer,
 	},
 	'file-upload': {
 		title: __( 'File Upload', 'sensei-lms' ),
-		description: __( 'Require a file to be uploaded.', 'sensei-lms' ),
+		description: __( 'Upload a file or document.', 'sensei-lms' ),
 		edit: () => <div> [File Upload] </div>,
 	},
 };
