52faeedc0687a6a3094bb5efa383f8bc50f815c3
Jake Oehler Morrison <jake.morrison@automattic.com>
2021-02-11 19:13:29 +0000

    Add some additional tests

--- tests/unit-tests/rest-api/test-sensei-rest-api.php
+++ tests/unit-tests/rest-api/test-sensei-rest-api.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Sensei REST API
+ * Sensei REST API tests.
  *
  * @package sensei-lms
  * @since 3.8.1
@@ -34,7 +34,7 @@ class Sensei_REST_API_Tests extends WP_Test_REST_TestCase {
 	}
 
 	/**
-	 * Makes sure lesson content is removed from REST API responses when also hidden on frontend.
+	 * Makes sure lesson content is removed from REST API responses when also hidden on frontend (guests.
 	 */
 	public function testLessonContentHiddenForGuests() {
 		$course_id = $this->factory->course->create();
@@ -45,6 +45,39 @@ class Sensei_REST_API_Tests extends WP_Test_REST_TestCase {
 		$response = $this->server->dispatch( $request );
 		$this->assertEquals( 200, $response->get_status() );
 		$this->assertEquals( '', $response->get_data()['content']['rendered'] );
+	}
 
+	/**
+	 * Makes sure lesson content is removed from REST API responses when also hidden on frontend (non-enrolled).
+	 */
+	public function testLessonContentHiddenForNonEnrolledUsers() {
+		$course_id = $this->factory->course->create();
+		$lesson_id = $this->factory->lesson->create();
+		add_post_meta( $lesson_id, '_lesson_course', $course_id );
+
+		$user_id = $this->factory->user->create();
+		wp_set_current_user( $user_id );
+
+		$request  = new WP_REST_Request( 'GET', '/wp/v2/lessons/' . $lesson_id );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 200, $response->get_status() );
+		$this->assertEquals( '', $response->get_data()['content']['rendered'] );
+	}
+
+	/**
+	 * Makes sure lesson content is shown from REST API responses when the teacher is accessing it.
+	 */
+	public function testLessonContentShownToTeacher() {
+		$user_id = $this->factory->user->create();
+		wp_set_current_user( $user_id );
+
+		$course_id = $this->factory->course->create( [ 'post_author' => $user_id ] );
+		$lesson_id = $this->factory->lesson->create();
+		add_post_meta( $lesson_id, '_lesson_course', $course_id );
+
+		$request  = new WP_REST_Request( 'GET', '/wp/v2/lessons/' . $lesson_id );
+		$response = $this->server->dispatch( $request );
+		$this->assertEquals( 200, $response->get_status() );
+		$this->assertNotEmpty( $response->get_data()['content']['rendered'] );
 	}
 }
