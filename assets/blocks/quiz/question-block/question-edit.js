2d69a879ad12b0bbb5178fd6791d59c134f3599a
Peter Kiss <peter@yscik.com>
2021-02-12 21:21:57 +0100

    Handle enter and backspace in question title, remove rich text formatting

--- assets/blocks/quiz/question-block/question-edit.js
+++ assets/blocks/quiz/question-block/question-edit.js
@@ -1,7 +1,9 @@
 /**
  * WordPress dependencies
  */
-import { BlockControls, InnerBlocks, RichText } from '@wordpress/block-editor';
+import { BlockControls, InnerBlocks } from '@wordpress/block-editor';
+import { useDispatch, select } from '@wordpress/data';
+import { useCallback } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -9,6 +11,7 @@ import { __ } from '@wordpress/i18n';
  */
 import { useBlockIndex } from '../../../shared/blocks/block-index';
 import { useHasSelected } from '../../../shared/helpers/blocks';
+import SingleLineInput from '../../course-outline/single-line-input';
 import types from '../answer-blocks';
 import { QuestionTypeToolbar } from './question-type-toolbar';
 
@@ -27,6 +30,15 @@ const QuestionEdit = ( props ) => {
 		clientId,
 	} = props;
 
+	const { removeBlock, selectBlock } = useDispatch( 'core/block-editor' );
+
+	const selectDescription = useCallback( () => {
+		const innerBlocks = select( 'core/block-editor' ).getBlocks( clientId );
+		if ( innerBlocks.length ) {
+			selectBlock( innerBlocks[ 0 ].clientId );
+		}
+	}, [ clientId, selectBlock ] );
+
 	const index = useBlockIndex( clientId );
 	const AnswerBlock = type && types[ type ];
 
@@ -39,15 +51,17 @@ const QuestionEdit = ( props ) => {
 			}` }
 		>
 			<h2 className="sensei-lms-question-block__index">{ index + 1 }.</h2>
-			<RichText
-				className="sensei-lms-question-block__title"
-				tagName="h2"
-				placeholder={ __( 'Add Question', 'sensei-lms' ) }
-				value={ title }
-				onChange={ ( nextValue ) =>
-					setAttributes( { title: nextValue } )
-				}
-			/>
+			<h2 className="sensei-lms-question-block__title">
+				<SingleLineInput
+					placeholder={ __( 'Add Question', 'sensei-lms' ) }
+					value={ title }
+					onChange={ ( nextValue ) =>
+						setAttributes( { title: nextValue } )
+					}
+					onEnter={ selectDescription }
+					onRemove={ () => removeBlock( clientId ) }
+				/>
+			</h2>
 			<InnerBlocks
 				template={ [
 					[
