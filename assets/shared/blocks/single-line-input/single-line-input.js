7f5108a4b757df869e6ed6c27c91c6afa733f893
Peter Kiss <peter@yscik.com>
2021-02-15 19:03:14 +0100

    Update SingleLineInput comments

--- assets/shared/blocks/single-line-input/single-line-input.js
+++ assets/shared/blocks/single-line-input/single-line-input.js
@@ -14,53 +14,55 @@ import classnames from 'classnames';
  * @param {Function} props.onKeyDown Keydown callback.
  * @param {Function} props.onEnter   Called on Enter.
  * @param {Function} props.onRemove  Called on Backspace when value is empty.
+ * @param {string}   props.value     Input value.
+ * @param {Object}   ref             Input element ref.
  */
-const SingleLineInput = forwardRef(
-	( { onChange, onKeyDown, value, onEnter, onRemove, ...props }, ref ) => {
-		/**
-		 * Handle change.
-		 *
-		 * @param {string} nextValue Change value.
-		 */
-		const handleChange = ( nextValue ) => {
-			onChange( nextValue.replace( /\n/g, '' ) );
-		};
+const SingleLineInput = (
+	{ onChange, onKeyDown, value, onEnter, onRemove, ...props },
+	ref
+) => {
+	/**
+	 * Handle change.
+	 *
+	 * @param {string} nextValue Change value.
+	 */
+	const handleChange = ( nextValue ) => {
+		onChange( nextValue.replace( /\n/g, '' ) );
+	};
 
-		const handleKeyDown = ( e ) => {
-			if ( onKeyDown ) {
-				onKeyDown( e );
-			}
-			switch ( e.keyCode ) {
-				case ENTER:
+	const handleKeyDown = ( e ) => {
+		if ( onKeyDown ) {
+			onKeyDown( e );
+		}
+		switch ( e.keyCode ) {
+			case ENTER:
+				e.preventDefault();
+				if ( onEnter ) {
+					onEnter( e );
+				}
+
+				break;
+			case BACKSPACE:
+				if ( onRemove && ! value?.length ) {
 					e.preventDefault();
-					if ( onEnter ) {
-						onEnter( e );
-					}
+					onRemove();
+				}
+				break;
+		}
+	};
 
-					break;
-				case BACKSPACE:
-					if ( onRemove && ! value?.length ) {
-						e.preventDefault();
-						onRemove();
-					}
-					break;
-			}
-		};
-
-		return (
-			<PlainText
-				ref={ ref }
-				value={ value }
-				onChange={ handleChange }
-				onKeyDown={ handleKeyDown }
-				{ ...props }
-				className={ classnames(
-					'sensei-lms-single-line-input',
-					props.className
-				) }
-			/>
-		);
-	}
-);
-
-export default SingleLineInput;
+	return (
+		<PlainText
+			ref={ ref }
+			value={ value }
+			onChange={ handleChange }
+			onKeyDown={ handleKeyDown }
+			{ ...props }
+			className={ classnames(
+				'sensei-lms-single-line-input',
+				props.className
+			) }
+		/>
+	);
+};
+export default forwardRef( SingleLineInput );
