312d84ce5deb9a33b4470df08f1be552812cb74d
Jake Oehler Morrison <jake.morrison@automattic.com>
2021-02-16 21:49:22 +0000

    Add access checks to analysis views and report downloads

--- includes/class-sensei-analysis.php
+++ includes/class-sensei-analysis.php
@@ -134,6 +134,27 @@ class Sensei_Analysis {
 		if ( isset( $_GET['user_id'] ) ) {
 			$user_id = intval( $_GET['user_id'] );
 		}
+
+		if (
+			$course_id
+			&& (
+				'course' !== get_post_type( $course_id )
+				|| ! current_user_can( get_post_type_object( 'course' )->cap->edit_post, $course_id )
+			)
+		) {
+			wp_die( esc_html__( 'Invalid course', 'sensei-lms' ), 404 );
+		}
+
+		if (
+			$lesson_id
+			&& (
+				'lesson' !== get_post_type( $lesson_id )
+				|| ! current_user_can( get_post_type_object( 'lesson' )->cap->edit_post, $lesson_id )
+			)
+		) {
+			wp_die( esc_html__( 'Invalid lesson', 'sensei-lms' ), 404 );
+		}
+
 		$type = isset( $_GET['view'] ) ? esc_html( $_GET['view'] ) : false;
 
 		if ( 0 < $lesson_id ) {
@@ -593,6 +614,26 @@ class Sensei_Analysis {
 				$user_id = intval( $_GET['user_id'] );
 			}
 			$type = isset( $_GET['view'] ) ? esc_html( $_GET['view'] ) : false;
+
+			if (
+				$course_id
+				&& (
+					'course' !== get_post_type( $course_id )
+					|| ! current_user_can( get_post_type_object( 'course' )->cap->edit_post, $course_id )
+				)
+			) {
+				wp_die( esc_html__( 'Invalid course', 'sensei-lms' ), 404 );
+			}
+
+			if (
+				$lesson_id
+				&& (
+					'lesson' !== get_post_type( $lesson_id )
+					|| ! current_user_can( get_post_type_object( 'lesson' )->cap->edit_post, $lesson_id )
+				)
+			) {
+				wp_die( esc_html__( 'Invalid lesson', 'sensei-lms' ), 404 );
+			}
 
 			// Set up default properties for logging an event.
 			$event_properties = [ 'view' => '' ];
