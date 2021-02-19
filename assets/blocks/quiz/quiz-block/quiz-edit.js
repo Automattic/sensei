b9760977cd7298e82581e0978c73ae5d4fa16228
Renatho De Carli Rosa <renatho@automattic.com>
2021-02-19 15:30:36 -0300

    Add button to add questions from the bank

--- assets/blocks/quiz/quiz-block/quiz-edit.js
+++ assets/blocks/quiz/quiz-block/quiz-edit.js
@@ -2,6 +2,7 @@
  * WordPress dependencies
  */
 import { InnerBlocks } from '@wordpress/block-editor';
+import { Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -38,8 +39,19 @@ const QuizEdit = ( props ) => {
 					isPostTemplate ? [ [ 'sensei-lms/quiz-question', {} ] ] : []
 				}
 				templateInsertUpdatesSelection={ false }
+				renderAppender={ () => (
+					<div className="sensei-lms-quiz-block__appender">
+						<Button type="button" isPrimary isSmall>
+							{ __(
+								'Add questions from the bank',
+								'sensei-lms'
+							) }
+						</Button>
+					</div>
+				) }
 			/>
 			<div className="sensei-lms-quiz-block__separator" />
+
 			<QuizSettings { ...props } />
 		</>
 	);
