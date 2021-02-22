5e831d4930b80f14f5be944b990fb248bb888732
Renatho De Carli Rosa <renatho@automattic.com>
2021-02-22 12:14:21 -0300

    Add questions modal styles

--- assets/blocks/quiz/quiz-block/question-bank-modal.js
+++ assets/blocks/quiz/quiz-block/question-bank-modal.js
@@ -22,60 +22,80 @@ const QuestionBankModal = ( { isOpen, setOpen } ) => {
 
 	return (
 		<Modal
-			title={ __( 'Questions Bank', 'sensei-lms' ) }
+			className="sensei-lms-quiz-block__questions-modal"
+			title={ __( 'Questions', 'sensei-lms' ) }
 			onRequestClose={ () => {
 				setOpen( false );
 			} }
 		>
-			<SelectControl
-				options={ [
-					{ value: '', label: 'Type', disabled: true },
-					{ value: 'T1', label: 'Type 1' },
-					{ value: 'T2', label: 'Type 2' },
-				] }
-				value=""
-				onChange={ () => {} }
-			/>
-			<SelectControl
-				options={ [
-					{ value: '', label: 'Category', disabled: true },
-					{ value: 'C1', label: 'Category 1' },
-					{ value: 'C2', label: 'Category 2' },
-				] }
-				value=""
-				onChange={ () => {} }
-			/>
-			<InputControl
-				placeholder="Search questions"
-				value=""
-				iconRight={ search }
-				onChange={ () => {} }
-			/>
-			<table>
+			<ul className="sensei-lms-quiz-block__questions-modal__filters">
+				<li>
+					<SelectControl
+						options={ [
+							{ value: '', label: 'Type', disabled: true },
+							{ value: 'T1', label: 'Type 1' },
+							{ value: 'T2', label: 'Type 2' },
+						] }
+						value=""
+						onChange={ () => {} }
+					/>
+				</li>
+				<li>
+					<SelectControl
+						options={ [
+							{ value: '', label: 'Category', disabled: true },
+							{ value: 'C1', label: 'Category 1' },
+							{ value: 'C2', label: 'Category 2' },
+						] }
+						value=""
+						onChange={ () => {} }
+					/>
+				</li>
+				<li>
+					<InputControl
+						className="sensei-lms-quiz-block__questions-modal__search-input"
+						placeholder="Search questions"
+						value=""
+						iconRight={ search }
+						onChange={ () => {} }
+					/>
+				</li>
+			</ul>
+
+			<table className="sensei-lms-quiz-block__questions-modal__table">
 				<thead>
 					<tr>
 						<th>
 							<CheckboxControl
+								title={ __(
+									'Toggle all visible questions selection.',
+									'sensei-lms'
+								) }
 								checked={ false }
-								label={ __( 'Question', 'sensei-lms' ) }
 								onChange={ () => {} }
 							/>
 						</th>
+						<th>{ __( 'Question', 'sensei-lms' ) }</th>
 						<th>{ __( 'Type', 'sensei-lms' ) }</th>
 						<th>{ __( 'Category', 'sensei-lms' ) }</th>
 					</tr>
 				</thead>
 				<tbody>
 					<tr>
-						<td>
+						<td className="sensei-lms-quiz-block__questions-modal__question-checkbox">
 							<CheckboxControl
+								id="question-1"
 								checked={ false }
-								label={ __(
+								onChange={ () => {} }
+							/>
+						</td>
+						<td className="sensei-lms-quiz-block__questions-modal__question-title">
+							<label htmlFor="question-1">
+								{ __(
 									'How do you add a course?',
 									'sensei-lms'
 								) }
-								onChange={ () => {} }
-							/>
+							</label>
 						</td>
 						<td>{ __( 'Multiple choice', 'sensei-lms' ) }</td>
 						<td>{ __( 'Sensei LMS', 'sensei-lms' ) }</td>
@@ -83,16 +103,22 @@ const QuestionBankModal = ( { isOpen, setOpen } ) => {
 				</tbody>
 			</table>
 
-			<Button isTertiary>
-				{ __( 'Clear selection', 'sensei-lms' ) }
-			</Button>
-			<Button isPrimary>Add Selected</Button>
+			<ul className="sensei-lms-quiz-block__questions-modal__actions">
+				<li>
+					<Button isTertiary>
+						{ __( 'Clear Selected', 'sensei-lms' ) }
+					</Button>
+				</li>
+				<li>
+					<Button isPrimary>Add Selected</Button>
+				</li>
+			</ul>
 		</Modal>
 	);
 };
 
 QuestionBankModal.Opener = ( { setOpen } ) => (
-	<div className="sensei-lms-quiz-block__opener">
+	<div className="sensei-lms-quiz-block__questions-modal-opener">
 		<Button
 			isPrimary
 			isSmall
