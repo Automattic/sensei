ec76af5fd3fff3f588a636b3e97120dba039c3a7
Jake Oehler Morrison <jake.morrison@automattic.com>
2021-02-25 12:52:35 +0000

    Flatten course structure schema to not use references

--- tests/framework/trait-sensei-rest-api-test-helpers.php
+++ tests/framework/trait-sensei-rest-api-test-helpers.php
@@ -7,8 +7,6 @@
 
 // phpcs:disable WordPress.NamingConventions.ValidFunctionName.MethodNameInvalid -- Using PHPUnit conventions.
 
-use Swaggest\JsonSchema\Schema;
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
@@ -26,26 +24,7 @@ trait Sensei_REST_API_Test_Helpers {
 	 * @param array $result Request response body.
 	 */
 	public function assertMeetsSchema( $schema, $result ) {
-		// We only include `autoload.php` when PHPUnit can be installed, which is on PHP 7.2+.
-		if ( ! class_exists( Schema::class ) ) {
-			$this->markTestSkipped( 'Test requires a higher version of PHP' );
-			return;
-		}
-
-		// Object (key based arrays) should be `stdClass` objects for validation.
-		$normalized_result = json_decode( wp_json_encode( $result ) );
-		$normalized_schema = json_decode( wp_json_encode( $schema ) );
-
-		$schema = Schema::import( $normalized_schema );
-
-		try {
-			$schema->in( $normalized_result );
-		} catch ( \Exception $e ) {
-			// Cheeky way to bail and show error message.
-			$this->assertTrue( false, $e->getMessage() );
-		}
-
 		// If we made it this far, faux-assert always true.
-		$this->assertTrue( true );
+		$this->assertTrue( rest_validate_value_from_schema( $result, $schema ), 'Result does not match schema' );
 	}
 }
