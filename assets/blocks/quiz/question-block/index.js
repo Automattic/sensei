de1453ec95e8db8f3ed74062ce5e475e567558f4
Peter Kiss <peter@yscik.com>
2021-03-01 23:52:48 +0100

    Use block migration for attribute mapping

--- assets/blocks/quiz/question-block/index.js
+++ assets/blocks/quiz/question-block/index.js
@@ -10,6 +10,7 @@ import { __ } from '@wordpress/i18n';
 import edit from './question-edit';
 import metadata from './block.json';
 import icon from '../../../icons/question-icon';
+import { createQuestionBlockAttributes } from './question-block-attributes';
 
 /**
  * Quiz question block definition.
@@ -18,6 +19,35 @@ export default {
 	...metadata,
 	title: __( 'Question', 'sensei-lms' ),
 	icon,
+	deprecated: [
+		{
+			attributes: {
+				grade: { type: 'number' },
+				type: { type: 'string' },
+				title: { type: 'string' },
+				id: { type: 'number' },
+				categories: { type: 'array' },
+				shared: { type: 'boolean' },
+				answer_feedback: { type: 'string' },
+				teacher_notes: { type: 'string' },
+				before: { type: 'string' },
+				after: { type: 'string' },
+				gap: { type: 'array' },
+				options: { type: 'array' },
+				random_order: { type: 'boolean' },
+				answer: { type: 'boolean' },
+			},
+			isEligible: ( attr ) => {
+				return null !== attr.grade;
+			},
+			migrate: ( attr ) => {
+				return createQuestionBlockAttributes( attr );
+			},
+			save: () => {
+				return <InnerBlocks.Content />;
+			},
+		},
+	],
 	usesContext: [ 'sensei-lms/quizId' ],
 	description: __( 'The building block of all quizzes.', 'sensei-lms' ),
 	example: {
