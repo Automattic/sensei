369183cb45f1709da891bbda6b62e41a647f6e66
Donna Peplinskie <donnapep@gmail.com>
2021-03-08 09:41:59 -0500

    Fix resetting the "Number of Questions" quiz setting (#4037)
    * Fix being unable to reset "Number of Questions" quiz setting
    
    * Update includes/rest-api/class-sensei-rest-api-lesson-quiz-controller.php
    
    Co-authored-by: gkaragia <53191348+gkaragia@users.noreply.github.com>
    
    * Set value to empty string when importing invalid _show_questions
    
    * Conditionally set show_questions meta
    
    Co-authored-by: gkaragia <53191348+gkaragia@users.noreply.github.com>

--- includes/rest-api/class-sensei-rest-api-lesson-quiz-controller.php
+++ includes/rest-api/class-sensei-rest-api-lesson-quiz-controller.php
@@ -260,8 +260,8 @@ class Sensei_REST_API_Lesson_Quiz_Controller extends \WP_REST_Controller {
 			$meta_input['_enable_quiz_reset'] = true === $quiz_options['allow_retakes'] ? 'on' : '';
 		}
 
-		if ( isset( $quiz_options['show_questions'] ) ) {
-			$meta_input['_show_questions'] = $quiz_options['show_questions'];
+		if ( array_key_exists( 'show_questions', $quiz_options ) ) {
+			$meta_input['_show_questions'] = empty( $quiz_options['show_questions'] ) ? '' : $quiz_options['show_questions'];
 		}
 
 		if ( isset( $quiz_options['random_question_order'] ) ) {
