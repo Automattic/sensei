54b51ddcab5894f680ad8306e138d68be6eea5ec
Peter Kiss <peter@yscik.com>
2021-03-17 16:05:31 +0100

    Allow parent blocks to provide their block meta

--- assets/shared/blocks/block-metadata.js
+++ assets/shared/blocks/block-metadata.js
@@ -3,6 +3,12 @@
  */
 import { createHigherOrderComponent } from '@wordpress/compose';
 import { dispatch, useDispatch, useSelect } from '@wordpress/data';
+import {
+	createContext,
+	useCallback,
+	useContext,
+	useMemo,
+} from '@wordpress/element';
 /**
  * External dependencies
  */
@@ -53,9 +59,14 @@ const store = {
 /**
  * Block metadata store.
  *
- * @type {string}
+ * @type {string|Object}
  */
 export const BLOCK_META_STORE = createStore( 'sensei/block-metadata', store );
+
+/**
+ * Block metadata context for providing parent block meta to inner blocks.
+ */
+const BlockMetaContext = createContext( {} );
 
 /**
  * Use metadata for the block.
@@ -70,10 +81,43 @@ export const useBlockMeta = ( clientId ) => {
 	);
 	const { setBlockMeta } = useDispatch( BLOCK_META_STORE );
 
-	const setBlockMetaForBlock = ( data ) => setBlockMeta( clientId, data );
+	const contextMeta = useBlockMetaContext();
 
-	return [ blockMeta || {}, setBlockMetaForBlock ];
+	const setBlockMetaForBlock = useCallback(
+		( data ) => setBlockMeta( clientId, data ),
+		[ clientId, setBlockMeta ]
+	);
+
+	const meta = useMemo( () => ( { ...contextMeta, ...blockMeta } ), [
+		blockMeta,
+		contextMeta,
+	] );
+
+	return [ meta, setBlockMetaForBlock ];
 };
+
+/**
+ * Provide this block's meta to its inner blocks.
+ *
+ * The provided parent meta entries will be merged into the inner blocks meta objects.
+ */
+export const withBlockMetaProvider = createHigherOrderComponent( ( Block ) => {
+	return withBlockMeta( ( props ) => {
+		const [ blockMeta ] = useBlockMeta( props.clientId );
+		return (
+			<BlockMetaContext.Provider value={ blockMeta }>
+				<Block { ...props } />
+			</BlockMetaContext.Provider>
+		);
+	} );
+}, 'withBlockMetaProvider' );
+
+/**
+ * Get the parent block's (withBlockMetaProvider) metadata.
+ *
+ * @return {Object} Contextual meta.
+ */
+export const useBlockMetaContext = () => useContext( BlockMetaContext );
 
 /**
  * Attach metadata store to the block.
