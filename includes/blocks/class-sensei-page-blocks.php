4d4ef49eb428c3d0c6350498a1c575821d655e8f
Peter Kiss <peter@yscik.com>
2021-03-30 01:18:22 +0200

    Load styles on frontend

--- includes/blocks/class-sensei-page-blocks.php
+++ includes/blocks/class-sensei-page-blocks.php
@@ -34,7 +34,7 @@ class Sensei_Page_Blocks extends Sensei_Blocks_Initializer {
 	 * @access private
 	 */
 	public function enqueue_block_assets() {
-		Sensei()->assets->enqueue( 'sensei-single-page-blocks', 'blocks/single-page.js', [], true );
+		Sensei()->assets->enqueue( 'sensei-single-page-blocks-style', 'blocks/single-page-style.css' );
 	}
 
 	/**
@@ -43,6 +43,6 @@ class Sensei_Page_Blocks extends Sensei_Blocks_Initializer {
 	 * @access private
 	 */
 	public function enqueue_block_editor_assets() {
-		Sensei()->assets->enqueue( 'sensei-single-page-blocks-style', 'blocks/single-page-style.css' );
+		Sensei()->assets->enqueue( 'sensei-single-page-blocks', 'blocks/single-page.js', [], true );
 	}
 }
