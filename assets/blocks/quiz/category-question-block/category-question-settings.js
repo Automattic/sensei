07194f94319d4326aff5a02a8f449a27776fefcf
Jake Oehler Morrison <jake.morrison@automattic.com>
2021-03-30 22:26:19 +0100

    Remove placeholder text from category question selector

--- assets/blocks/quiz/category-question-block/category-question-settings.js
+++ assets/blocks/quiz/category-question-block/category-question-settings.js
@@ -37,7 +37,7 @@ const CategoryQuestionSettings = ( {
 	const categoryOptions = [
 		{
 			value: '',
-			label: __( 'Category', 'sensei-lms' ),
+			label: '',
 		},
 		...( questionCategories || [] ).map( ( questionCategory ) => ( {
 			value: questionCategory.id,
