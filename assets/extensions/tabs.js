81c532ea6894a95560161274f0a586cd7c29c7c4
Renatho De Carli Rosa <renatho@automattic.com>
2021-04-08 14:12:45 -0300

    Refactor tabs

--- assets/extensions/tabs.js
+++ assets/extensions/tabs.js
@@ -8,63 +8,48 @@ import { __ } from '@wordpress/i18n';
  */
 import { useQueryStringRouter } from '../shared/query-string-router';
 
+const tabs = [
+	{
+		id: 'all',
+		label: __( 'All', 'sensei-lms' ),
+	},
+	{
+		id: 'free',
+		label: __( 'Free', 'sensei-lms' ),
+	},
+	{
+		id: 'third-party',
+		label: __( 'Third party', 'sensei-lms' ),
+	},
+	{
+		id: 'installed',
+		label: __( 'Installed', 'sensei-lms' ),
+	},
+];
+
 const Tabs = () => {
-	const { goTo } = useQueryStringRouter();
+	const { currentRoute, goTo } = useQueryStringRouter();
 
 	return (
 		<nav>
 			<ul className="subsubsub sensei-extensions__tabs">
-				<li>
-					<a
-						href="#all"
-						className="current"
-						aria-current="page"
-						onClick={ ( e ) => {
-							e.preventDefault();
-							goTo( 'all' );
-						} }
-					>
-						{ __( 'All', 'sensei-lms' ) }{ ' ' }
-						<span className="count">(3)</span>
-					</a>
-					|
-				</li>
-				<li>
-					<a
-						href="#free"
-						onClick={ ( e ) => {
-							e.preventDefault();
-							goTo( 'free' );
-						} }
-					>
-						{ __( 'Free', 'sensei-lms' ) }{ ' ' }
-						<span className="count">(3)</span>
-					</a>
-					|
-				</li>
-				<li>
-					<a
-						href="#third-party"
-						onClick={ ( e ) => {
-							e.preventDefault();
-							goTo( 'third-party' );
-						} }
-					>
-						{ __( 'Third party', 'sensei-lms' ) }
-					</a>
-					|
-				</li>
-				<li>
-					<a
-						href="#installed"
-						onClick={ ( e ) => {
-							e.preventDefault();
-							goTo( 'installed' );
-						} }
-					>
-						{ __( 'Installed', 'sensei-lms' ) }
-					</a>
-				</li>
+				{ tabs.map( ( { id, label } ) => (
+					<li key={ id } className="sensei-extensions__tabs__tab">
+						<a
+							href={ `#${ id }` }
+							onClick={ ( e ) => {
+								e.preventDefault();
+								goTo( id );
+							} }
+							{ ...( currentRoute === id && {
+								className: 'current',
+								'aria-current': 'page',
+							} ) }
+						>
+							{ label } <span className="count">(3)</span>
+						</a>
+					</li>
+				) ) }
 			</ul>
 		</nav>
 	);
