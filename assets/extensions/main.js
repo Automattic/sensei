3194f773d9694fd8f1fb53ef820177ec597ff521
Renatho De Carli Rosa <renatho@automattic.com>
2021-04-08 13:54:51 -0300

    Add basic structure for extensions navigation

--- assets/extensions/main.js
+++ assets/extensions/main.js
@@ -5,20 +5,28 @@ import Header from './header';
 import Tabs from './tabs';
 import UpdateNotification from './update-notification';
 import Extensions from './extensions';
+import QueryStringRouter, { Route } from '../shared/query-string-router';
 
 const Main = () => (
 	<main className="sensei-extensions">
 		<div className="sensei-extensions__sections">
-			<div className="sensei-extensions__section --col-12">
-				<Header />
-				<Tabs />
-			</div>
+			<QueryStringRouter paramName="tab" defaultRoute="all">
+				<div className="sensei-extensions__section --col-12">
+					<Header />
+					<Tabs />
+				</div>
 
-			<section className="sensei-extensions__section --col-12">
-				<UpdateNotification />
-			</section>
+				<section className="sensei-extensions__section --col-12">
+					<UpdateNotification />
+				</section>
 
-			<Extensions />
+				<Route route="all">
+					<Extensions />
+				</Route>
+				<Route route="free">Free</Route>
+				<Route route="third-party">Thid party</Route>
+				<Route route="installed">Installed</Route>
+			</QueryStringRouter>
 		</div>
 	</main>
 );
--- assets/extensions/tabs.js
+++ assets/extensions/tabs.js
@@ -3,32 +3,71 @@
  */
 import { __ } from '@wordpress/i18n';
 
-const Tabs = () => (
-	<nav>
-		<ul className="subsubsub sensei-extensions__tabs">
-			<li>
-				<a href="#all" className="current" aria-current="page">
-					{ __( 'All', 'sensei-lms' ) }{ ' ' }
-					<span className="count">(3)</span>
-				</a>
-				|
-			</li>
-			<li>
-				<a href="#free">
-					{ __( 'Free', 'sensei-lms' ) }{ ' ' }
-					<span className="count">(3)</span>
-				</a>
-				|
-			</li>
-			<li>
-				<a href="#third-party">{ __( 'Third party', 'sensei-lms' ) }</a>
-				|
-			</li>
-			<li>
-				<a href="#installed">{ __( 'Installed', 'sensei-lms' ) }</a>
-			</li>
-		</ul>
-	</nav>
-);
+/**
+ * Internal dependencies
+ */
+import { useQueryStringRouter } from '../shared/query-string-router';
+
+const Tabs = () => {
+	const { goTo } = useQueryStringRouter();
+
+	return (
+		<nav>
+			<ul className="subsubsub sensei-extensions__tabs">
+				<li>
+					<a
+						href="#all"
+						className="current"
+						aria-current="page"
+						onClick={ ( e ) => {
+							e.preventDefault();
+							goTo( 'all' );
+						} }
+					>
+						{ __( 'All', 'sensei-lms' ) }{ ' ' }
+						<span className="count">(3)</span>
+					</a>
+					|
+				</li>
+				<li>
+					<a
+						href="#free"
+						onClick={ ( e ) => {
+							e.preventDefault();
+							goTo( 'free' );
+						} }
+					>
+						{ __( 'Free', 'sensei-lms' ) }{ ' ' }
+						<span className="count">(3)</span>
+					</a>
+					|
+				</li>
+				<li>
+					<a
+						href="#third-party"
+						onClick={ ( e ) => {
+							e.preventDefault();
+							goTo( 'third-party' );
+						} }
+					>
+						{ __( 'Third party', 'sensei-lms' ) }
+					</a>
+					|
+				</li>
+				<li>
+					<a
+						href="#installed"
+						onClick={ ( e ) => {
+							e.preventDefault();
+							goTo( 'installed' );
+						} }
+					>
+						{ __( 'Installed', 'sensei-lms' ) }
+					</a>
+				</li>
+			</ul>
+		</nav>
+	);
+};
 
 export default Tabs;
