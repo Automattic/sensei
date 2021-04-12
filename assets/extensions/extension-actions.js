4aa29d2d35c4a52d16c8bc9c860f172352995483
Renatho De Carli Rosa <renatho@automattic.com>
2021-04-12 16:40:40 -0300

    Update extension actions to handle extension object

--- assets/extensions/extension-actions.js
+++ assets/extensions/extension-actions.js
@@ -2,34 +2,69 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { Icon } from '@wordpress/icons';
+
+/**
+ * Internal dependencies
+ */
+import { checked } from '../icons/wordpress-icons';
 
 /**
  * Extension actions component.
  *
  * @param {Object} props             Component props.
+ * @param {string} props.extension   Extension object.
  * @param {string} props.buttonLabel Button label.
- * @param {string} props.detailsLink Details link.
  */
-const ExtensionActions = ( { buttonLabel, detailsLink } ) => (
-	<ul className="sensei-extensions__extension-actions">
-		<li className="sensei-extensions__extension-actions__item">
-			<button className="button button-primary">
-				{ buttonLabel || __( 'Install - $29.99', 'sensei-lms' ) }
-			</button>
-		</li>
-		{ detailsLink && (
+const ExtensionActions = ( { extension = {}, buttonLabel } ) => {
+	let disabledButton = false;
+
+	if ( ! buttonLabel ) {
+		if ( extension.has_update ) {
+			buttonLabel = __( 'Update', 'sensei-lms' );
+		} else if ( extension.version ) {
+			buttonLabel = (
+				<>
+					<Icon
+						className="sensei-extensions__extension-actions__button-icon"
+						icon={ checked }
+						size={ 14 }
+					/>{ ' ' }
+					{ __( 'Installed', 'sensei-lms' ) }
+				</>
+			);
+			disabledButton = true;
+		} else {
+			buttonLabel = `${ __( 'Install', 'sensei-lms' ) } - ${
+				extension.price
+			}`;
+		}
+	}
+
+	return (
+		<ul className="sensei-extensions__extension-actions">
 			<li className="sensei-extensions__extension-actions__item">
-				<a
-					href={ detailsLink }
-					className="sensei-extensions__extension-actions__details-link"
-					target="_blank"
-					rel="noreferrer external"
+				<button
+					className="button button-primary"
+					disabled={ disabledButton }
 				>
-					{ __( 'More details', 'sensei-lms' ) }
-				</a>
+					{ buttonLabel || __( 'Install - $29.99', 'sensei-lms' ) }
+				</button>
 			</li>
-		) }
-	</ul>
-);
+			{ extension.link && (
+				<li className="sensei-extensions__extension-actions__item">
+					<a
+						href={ extension.link }
+						className="sensei-extensions__extension-actions__details-link"
+						target="_blank"
+						rel="noreferrer external"
+					>
+						{ __( 'More details', 'sensei-lms' ) }
+					</a>
+				</li>
+			) }
+		</ul>
+	);
+};
 
 export default ExtensionActions;
