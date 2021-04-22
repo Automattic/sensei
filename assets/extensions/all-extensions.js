842bb6b7619be6bc2cf7996827b7a315c600d8c2
Renatho De Carli Rosa <renatho@automattic.com>
2021-04-22 16:02:34 -0300

    Update extensions page to use dynamic skeleton

--- assets/extensions/all-extensions.js
+++ assets/extensions/all-extensions.js
@@ -1,73 +1,87 @@
 /**
- * WordPress dependencies
+ * External dependencies
  */
-import { __ } from '@wordpress/i18n';
+import { keyBy } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import Card from './card';
 
+// TODO: Get from API.
+const extensionsSkeleton = [
+	{
+		id: 'featured',
+		columns: 12,
+		type: 'featured-list',
+		title: 'Featured',
+		itemSlugs: [
+			'sensei-wc-paid-courses',
+			'sensei-content-drip',
+			'sensei-certificates',
+		],
+	},
+	{
+		id: 'course-creation',
+		columns: 8,
+		type: 'large-list',
+		title: 'Course creation',
+		itemSlugs: [
+			'sensei-course-participants',
+			'sensei-course-progress',
+			'sensei-media-attachments',
+		],
+	},
+	{
+		id: 'learner-engagement',
+		columns: 4,
+		type: 'small-list',
+		title: 'Learner engagement',
+		itemSlugs: [ 'sensei-share-your-grade', 'sensei-post-to-course' ],
+	},
+	{
+		id: 'grid-example',
+		columns: 12,
+		type: 'grid-list',
+		title: 'Grid example',
+		itemSlugs: [
+			'sensei-share-your-grade',
+			'sensei-post-to-course',
+			'sensei-media-attachments',
+			'sensei-course-participants',
+			'sensei-course-progress',
+		],
+	},
+];
+
 const AllExtensions = ( { extensions } ) => {
-	const featuredExtensions = extensions.filter(
-		( extension ) => extension.is_featured
-	);
+	const extensionsBySlug = keyBy( extensions, 'product_slug' );
 
-	return (
-		<>
-			{ featuredExtensions.length > 0 && (
-				<section className="sensei-extensions__section sensei-extensions__grid__col --col-12">
-					<h2 className="sensei-extensions__section__title">
-						{ __( 'Featured', 'sensei-lms' ) }
-					</h2>
-					<ul className="sensei-extensions__section__content sensei-extensions__featured-list">
-						{ featuredExtensions.map( ( extension ) => (
-							<li
-								key={ extension.product_slug }
-								className="sensei-extensions__featured-list__item"
-							>
-								<div className="sensei-extensions__featured-list__card-wrapper">
-									<Card extension={ extension } />
-								</div>
-							</li>
-						) ) }
-					</ul>
-				</section>
-			) }
+	return extensionsSkeleton.map( ( section ) => (
+		<section
+			key={ section.id }
+			className={ `sensei-extensions__section sensei-extensions__grid__col --col-${ section.columns }` }
+		>
+			<h2 className="sensei-extensions__section__title">
+				{ section.title }
+			</h2>
 
-			<section className="sensei-extensions__section sensei-extensions__grid__col --col-8">
-				<h2 className="sensei-extensions__section__title">
-					{ __( 'Course Creation', 'sensei-lms' ) }
-				</h2>
-				<ul className="sensei-extensions__section__content sensei-extensions__large-list">
-					{ extensions.map( ( extension ) => (
-						<li
-							key={ extension.product_slug }
-							className="sensei-extensions__large-list__item"
-						>
-							<Card extension={ extension } />
-						</li>
-					) ) }
-				</ul>
-			</section>
-
-			<section className="sensei-extensions__section sensei-extensions__grid__col --col-4">
-				<h2 className="sensei-extensions__section__title">
-					{ __( 'Learner Engagement', 'sensei-lms' ) }
-				</h2>
-				<ul className="sensei-extensions__section__content sensei-extensions__small-list">
-					{ extensions.map( ( extension ) => (
-						<li
-							key={ extension.product_slug }
-							className="sensei-extensions__small-list__item"
-						>
-							<Card extension={ extension } />
-						</li>
-					) ) }
-				</ul>
-			</section>
-		</>
-	);
+			<ul
+				className={ `sensei-extensions__section__content sensei-extensions__${ section.type }` }
+			>
+				{ section.itemSlugs.map( ( slug ) => (
+					<li
+						key={ extensionsBySlug[ slug ].product_slug }
+						className="sensei-extensions__list-item"
+					>
+						<div className="sensei-extensions__card-wrapper">
+							<Card extension={ extensionsBySlug[ slug ] } />
+						</div>
+					</li>
+				) ) }
+			</ul>
+		</section>
+	) );
 };
 
 export default AllExtensions;
