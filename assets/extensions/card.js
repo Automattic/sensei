bef473a02349eb38d2e1d7c5ced8b4ce28a42745
Peter Kiss <peter@yscik.com>
2021-04-26 23:16:51 +0200

    Update card layout with images & height alignment

--- assets/extensions/card.js
+++ assets/extensions/card.js
@@ -21,6 +21,7 @@ import ExtensionActions from './extension-actions';
  * @param {string}   props.excerpt     Card excerpt (extension excerpt will be used as fallback).
  * @param {string}   props.badgeLabel  Badge label (will check extension update if it's not defined).
  * @param {Object[]} props.customLinks Array with custom links containing the link props.
+ * @param {string}   props.image       Card image.
  * @param {Object}   props.extension   Extension object.
  * @param {Object}   props.htmlProps   Wrapper extra props.
  */
@@ -29,39 +30,52 @@ const Card = ( {
 	excerpt,
 	badgeLabel,
 	customLinks,
+	image,
 	extension,
 	htmlProps,
-} ) => (
-	<article
-		{ ...htmlProps }
-		className={ classnames(
-			'sensei-extensions__card',
-			htmlProps?.className
-		) }
-	>
-		<header className="sensei-extensions__card__header">
-			<h3 className="sensei-extensions__card__title">
-				{ title || extension.title }
-			</h3>
-			{ ( badgeLabel || extension?.[ 'has_update' ] ) && (
-				<small className="sensei-extensions__card__new-badge">
-					{ badgeLabel || __( 'New version', 'sensei-lms' ) }
-				</small>
+} ) => {
+	const backgroundImage =
+		( image && `url(${ image })` ) ||
+		( extension?.image && `url(${ extension.image })` );
+	return (
+		<article
+			{ ...htmlProps }
+			className={ classnames(
+				'sensei-extensions__card',
+				htmlProps?.className
 			) }
-		</header>
-		<div className="sensei-extensions__card__content">
-			<p className="sensei-extensions__card__description">
-				{ excerpt || extension.excerpt }
-			</p>
-
-			{ ( extension || customLinks ) && (
-				<ExtensionActions
-					extension={ extension }
-					customLinks={ customLinks }
-				/>
-			) }
-		</div>
-	</article>
-);
+		>
+			<div
+				className="sensei-extensions__card__image"
+				style={ {
+					backgroundImage,
+				} }
+			/>
+			<div className="sensei-extensions__card__content">
+				<header className="sensei-extensions__card__header">
+					<h3 className="sensei-extensions__card__title">
+						{ title || extension.title }
+					</h3>
+					{ ( badgeLabel || extension?.[ 'has_update' ] ) && (
+						<small className="sensei-extensions__card__new-badge">
+							{ badgeLabel || __( 'New version', 'sensei-lms' ) }
+						</small>
+					) }
+				</header>
+				<div className="sensei-extensions__card__body">
+					<p className="sensei-extensions__card__description">
+						{ excerpt || extension.excerpt }
+					</p>
+					{ ( extension || customLinks ) && (
+						<ExtensionActions
+							extension={ extension }
+							customLinks={ customLinks }
+						/>
+					) }
+				</div>
+			</div>
+		</article>
+	);
+};
 
 export default Card;
