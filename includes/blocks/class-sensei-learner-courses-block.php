54d6028ddc9a06970fd8e9c1c4d58d27bf983586
Donna Peplinskie <donnapep@gmail.com>
2021-07-12 09:29:30 -0400

    Simplify adding class to block on front end

--- includes/blocks/class-sensei-learner-courses-block.php
+++ includes/blocks/class-sensei-learner-courses-block.php
@@ -51,11 +51,7 @@ class Sensei_Learner_Courses_Block {
 		);
 
 		if ( isset( $attributes['options']['layoutView'] ) ) {
-			if ( 'grid' === $attributes['options']['layoutView'] ) {
-				$class .= ' wp-block-sensei-lms-learner-courses--is-grid-view';
-			} elseif ( 'list' === $attributes['options']['layoutView'] ) {
-				$class .= ' wp-block-sensei-lms-learner-courses--is-list-view';
-			}
+			$class .= ' wp-block-sensei-lms-learner-courses--is-' . $attributes['options']['layoutView'] . '-view';
 		}
 
 		return '<div class="wp-block-sensei-lms-learner-courses ' . $class . ' " style="' . esc_attr( $style ) . '">' . $shortcode->render() . '</div>';
