07e9cc6df2c17d8ce36552037eee904f708f256f
Donna Peplinskie <donnapep@gmail.com>
2021-08-11 16:08:06 -0400

    Remove use of unnecessary const

--- assets/blocks/single-page.js
+++ assets/blocks/single-page.js
@@ -2,13 +2,14 @@
  * Internal dependencies
  */
 import registerSenseiBlocks from './register-sensei-blocks';
+import CourseResultsBlock from './course-results-block';
 import LearnerCoursesBlock from './learner-courses-block';
 import LearnerMessagesButtonBlock from './learner-messages-button-block';
-import CourseResultsBlock from './course-results-block';
 import { registerCourseCompletedActionsBlock } from './course-completed-actions';
 
-const blocks = [ LearnerCoursesBlock, LearnerMessagesButtonBlock ];
-
 registerCourseCompletedActionsBlock();
-blocks.push( CourseResultsBlock );
-registerSenseiBlocks( blocks );
+registerSenseiBlocks( [
+	CourseResultsBlock,
+	LearnerCoursesBlock,
+	LearnerMessagesButtonBlock,
+] );
