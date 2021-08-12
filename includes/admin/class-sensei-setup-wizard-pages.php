07aaf3624f55c02aa2060ad4b99dcde9b84060ea
Renatho De Carli Rosa <renatho@automattic.com>
2021-08-12 13:43:27 -0300

    Update contratulations copy

--- includes/admin/class-sensei-setup-wizard-pages.php
+++ includes/admin/class-sensei-setup-wizard-pages.php
@@ -107,7 +107,7 @@ class Sensei_Setup_Wizard_Pages {
 		$blocks[] = serialize_block(
 			[
 				'blockName'    => 'core/paragraph',
-				'innerContent' => [ '<p class="has-text-align-center has-large-font-size">' . __( 'Congratulations on completing the course! 🥳', 'sensei-lms' ) . '</p>' ],
+				'innerContent' => [ '<p class="has-text-align-center has-large-font-size">' . __( 'Congratulations on completing this course! 🥳', 'sensei-lms' ) . '</p>' ],
 				'attrs'        => [
 					'align'    => 'center',
 					'fontSize' => 'large',
