b97f30b8c9b8f291676205ec334facc375846d36
Giannis Karagiannis <giannis.karagiannis@automattic.com>
2021-10-15 11:47:16 +0300

    Add a custom appender in course outline block

---
+++ assets/blocks/course-outline/outline-block/outline-appender.js
@@ -0,0 +1,57 @@
+/**
+ * WordPress dependencies
+ */
+import { createBlock } from '@wordpress/blocks';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { LessonIcon, ModuleIcon } from '../../../icons';
+import TextAppender from '../../../shared/components/text-appender';
+
+/**
+ * Outline block appender for adding a lesson or a module.
+ *
+ * @param {Object} props
+ * @param {string} props.clientId Outline block ID.
+ */
+const OutlineAppender = ( { clientId } ) => {
+	const { insertBlock } = useDispatch( 'core/block-editor' );
+	const internalBlockCount = useSelect(
+		( select ) => select( 'core/block-editor' ).getBlockCount( clientId ),
+		[]
+	);
+
+	const controls = [
+		{
+			title: __( 'Lesson', 'sensei-lms' ),
+			icon: LessonIcon,
+			onClick: () =>
+				insertBlock(
+					createBlock( 'sensei-lms/course-outline-lesson' ),
+					internalBlockCount,
+					clientId,
+					true
+				),
+		},
+		{
+			title: __( 'Module', 'sensei-lms' ),
+			icon: ModuleIcon,
+			onClick: () =>
+				insertBlock(
+					createBlock( 'sensei-lms/course-outline-module' ),
+					internalBlockCount,
+					clientId,
+					true
+				),
+		},
+	];
+
+	const text = __( 'Add Module or Lesson', 'sensei-lms' );
+
+	return <TextAppender controls={ controls } text={ text } label={ text } />;
+};
+
+export default OutlineAppender;
