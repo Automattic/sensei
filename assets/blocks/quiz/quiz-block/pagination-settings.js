2bf1ad945604df53aa80bb1124d2e5c3279b49df
Giannis Karagiannis <giannis.karagiannis@automattic.com>
2021-11-17 15:14:26 +0200

    Move questions input to a separate line

--- assets/blocks/quiz/quiz-block/pagination-settings.js
+++ assets/blocks/quiz/quiz-block/pagination-settings.js
@@ -38,6 +38,7 @@ const PaginationSettings = ( { settings, onChange } ) => {
 	return (
 		<>
 			<PanelBody
+				className="sensei-lms-quiz-block-styling"
 				title={ __( 'Quiz styling', 'sensei-lms' ) }
 				initialOpen={ true }
 			>
@@ -47,52 +48,65 @@ const PaginationSettings = ( { settings, onChange } ) => {
 						'sensei-lms'
 					) }
 				</p>
-				<PanelRow className="sensei-lms-quiz-block-panel__row-title">
-					<h2>{ __( 'Pagination', 'sensei-lms' ) }</h2>
-					<SelectControl
-						label={ __( 'Pagination', 'sensei-lms' ) }
-						hideLabelFromVision
-						value={ paginationNumber !== null ? MULTI : SINGLE }
-						options={ [
-							{
-								label: __( 'Single page', 'sensei-lms' ),
-								value: SINGLE,
-							},
-							{
-								label: __( 'Multi-Page', 'sensei-lms' ),
-								value: MULTI,
-							},
-						] }
-						onChange={ ( value ) => {
-							if ( value === MULTI ) {
+				<PanelRow className="sensei-lms-quiz-block-panel">
+					<h2 className="sensei-lms-quiz-block-panel__row">
+						{ __( 'Pagination', 'sensei-lms' ) }
+					</h2>
+					<div className="sensei-lms-quiz-block-panel__row">
+						<SelectControl
+							label={ __( 'Pagination', 'sensei-lms' ) }
+							className="test"
+							hideLabelFromVision
+							value={ paginationNumber !== null ? MULTI : SINGLE }
+							options={ [
+								{
+									label: __( 'Single page', 'sensei-lms' ),
+									value: SINGLE,
+								},
+								{
+									label: __( 'Multi-Page', 'sensei-lms' ),
+									value: MULTI,
+								},
+							] }
+							onChange={ ( value ) => {
+								if ( value === MULTI ) {
+									onChange( {
+										...settings,
+										paginationNumber: 1,
+									} );
+								} else {
+									onChange( {
+										...settings,
+										paginationNumber: null,
+									} );
+								}
+							} }
+						/>
+					</div>
+					<div className="sensei-lms-quiz-block-panel__row sensei-lms-quiz-block-panel__questions">
+						<NumberControl
+							label={ __( 'Number of Questions', 'sensei-lms' ) }
+							min={ 1 }
+							step={ 1 }
+							hideLabelFromVision
+							suffix={ __( 'Questions', 'sensei-lms' ) }
+							value={ paginationNumber }
+							onChange={ ( value ) =>
 								onChange( {
 									...settings,
-									paginationNumber: 1,
-								} );
-							} else {
-								onChange( {
-									...settings,
-									paginationNumber: null,
-								} );
+									paginationNumber: value,
+								} )
 							}
-						} }
-					/>
-					<NumberControl
-						label={ __( 'Number of Questions', 'sensei-lms' ) }
-						min={ 1 }
-						step={ 1 }
-						hideLabelFromVision
-						suffix={ __( 'Questions', 'sensei-lms' ) }
-						value={ paginationNumber }
-						onChange={ ( value ) =>
-							onChange( { ...settings, paginationNumber: value } )
-						}
-					/>
+						/>
+						<p>{ __( 'per page', 'sensei-lms' ) }</p>
+					</div>
 				</PanelRow>
-				<PanelRow className="sensei-lms-quiz-block-panel__row-title">
-					<h2>{ __( 'Progress Bar', 'sensei-lms' ) }</h2>
+				<PanelRow className="sensei-lms-quiz-block-panel">
+					<h2 className="sensei-lms-quiz-block-panel__row">
+						{ __( 'Progress Bar', 'sensei-lms' ) }
+					</h2>
 					<ToggleControl
-						className="sensei-lms-quiz-block-panel__row-item"
+						className="sensei-lms-quiz-block-panel__row"
 						checked={ showProgressBar }
 						label={ __( 'Show Progress Bar', 'sensei-lms' ) }
 						value={ progressBarRadius }
@@ -100,32 +114,34 @@ const PaginationSettings = ( { settings, onChange } ) => {
 							onChange( { ...settings, showProgressBar: value } )
 						}
 					/>
-					<NumberControl
-						label={ __( 'Radius', 'sensei-lms' ) }
-						min={ 1 }
-						step={ 1 }
-						suffix={ __( 'PX', 'sensei-lms' ) }
-						value={ progressBarRadius }
-						onChange={ ( value ) =>
-							onChange( {
-								...settings,
-								progressBarRadius: value,
-							} )
-						}
-					/>
-					<NumberControl
-						label={ __( 'Height', 'sensei-lms' ) }
-						min={ 1 }
-						step={ 1 }
-						suffix={ __( 'PX', 'sensei-lms' ) }
-						value={ progressBarHeight }
-						onChange={ ( value ) =>
-							onChange( {
-								...settings,
-								progressBarHeight: value,
-							} )
-						}
-					/>
+					<div className="sensei-lms-quiz-block-panel__row sensei-lms-quiz-block-panel__progress-bar">
+						<NumberControl
+							label={ __( 'Radius', 'sensei-lms' ) }
+							min={ 1 }
+							step={ 1 }
+							suffix={ __( 'PX', 'sensei-lms' ) }
+							value={ progressBarRadius }
+							onChange={ ( value ) =>
+								onChange( {
+									...settings,
+									progressBarRadius: value,
+								} )
+							}
+						/>
+						<NumberControl
+							label={ __( 'Height', 'sensei-lms' ) }
+							min={ 1 }
+							step={ 1 }
+							suffix={ __( 'PX', 'sensei-lms' ) }
+							value={ progressBarHeight }
+							onChange={ ( value ) =>
+								onChange( {
+									...settings,
+									progressBarHeight: value,
+								} )
+							}
+						/>
+					</div>
 				</PanelRow>
 			</PanelBody>
 			<PanelColorSettings
