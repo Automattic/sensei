b6c7eaf74ba01bf4e2b6f30b98275bfba9909812
Renatho De Carli Rosa <renatho@automattic.com>
2021-11-30 09:21:11 -0300

    Refactor lesson quiz notice template

--- templates/course-theme/lesson-quiz-notice.php
+++ templates/course-theme/lesson-quiz-notice.php
@@ -11,54 +11,61 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
+/**
+ * Notices map to echo notices HTML.
+ *
+ * @param array $notice
+ */
+function sensei_lesson_quiz_notices_map( $notice ) {
+	?>
+	<div class="sensei-lms-notice sensei-course-theme-lesson-quiz-notice">
+		<div class="sensei-course-theme-lesson-quiz-notice__content">
+			<?php
+			if ( ! empty( $notice['title'] ) ) {
+				?>
+				<h3 class="sensei-course-theme-lesson-quiz-notice__title">
+					<?php
+					echo wp_kses_post( $notice['title'] );
+					?>
+				</h3>
+				<?php
+			}
+			?>
+			<p class="sensei-course-theme-lesson-quiz-notice__text"><?php echo wp_kses_post( $notice['text'] ); ?></p>
+		</div>
+
+		<?php if ( ! empty( $notice['actions'] ) ) { ?>
+			<ul class="sensei-course-theme-lesson-quiz-notice__actions">
+			<?php implode( '', array_map( 'sensei_lesson_quiz_notice_actions_map', $notice['actions'] ) ); ?>
+			</ul>
+		<?php } ?>
+	</div>
+	<?php
+}
+
+/**
+ * Notice actions map to echo the actions.
+ *
+ * @param array $action
+ */
+function sensei_lesson_quiz_notice_actions_map( $action ) {
+	?>
+	<li>
+		<a
+			href="<?php echo esc_url( $action['url'] ); ?>"
+			class="button is-<?php echo esc_attr( $action['style'] ); ?>"
+		>
+			<?php echo wp_kses_post( $action['label'] ); ?>
+		</a>
+	</li>
+	<?php
+}
+
 ?>
 <div>
 	<?php
-	array_map(
-		function( $notice ) {
-			?>
-			<div class="sensei-lms-notice sensei-course-theme-lesson-quiz-notice">
-				<div class="sensei-course-theme-lesson-quiz-notice__content">
-					<?php
-					if ( ! empty( $notice['title'] ) ) {
-						?>
-						<h3 class="sensei-course-theme-lesson-quiz-notice__title">
-							<?php
-							echo wp_kses_post( $notice['title'] );
-							?>
-						</h3>
-						<?php
-					}
-					?>
-					<p class="sensei-course-theme-lesson-quiz-notice__text"><?php echo wp_kses_post( $notice['text'] ); ?></p>
-				</div>
-
-				<?php if ( ! empty( $notice['actions'] ) ) { ?>
-					<ul class="sensei-course-theme-lesson-quiz-notice__actions">
-					<?php
-					array_map(
-						function( $action ) {
-							?>
-							<li>
-								<a
-									href="<?php echo esc_url( $action['url'] ); ?>"
-									class="button is-<?php echo esc_attr( $action['style'] ); ?>"
-								>
-									<?php echo wp_kses_post( $action['label'] ); ?>
-								</a>
-							</li>
-							<?php
-						},
-						$notice['actions']
-					);
-					?>
-					</ul>
-				<?php } ?>
-			</div>
-			<?php
-		},
-		// phpcs:disable VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable -- A template variable
-		$notices
-	);
+	// phpcs:disable VariableAnalysis.CodeAnalysis.VariableAnalysis.UndefinedVariable -- A template variable
+	implode( '', array_map( 'sensei_lesson_quiz_notices_map', $notices ) );
 	?>
 </div>
