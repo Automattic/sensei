7ebab23144e0c77216fa7c449183de55ffed563f
Dmitry Merkushin <merkushin@gmail.com>
2021-12-13 10:05:52 +0300

    Rename meta fields in tests

--- tests/unit-tests/test-class-sensei-course-progression-settings.php
+++ tests/unit-tests/test-class-sensei-course-progression-settings.php
@@ -5,8 +5,8 @@ class Test_Sensei_Course_Progression_Settings extends WP_UnitTestCase {
 		$settings = Sensei_Course_Progression_Settings::instance();
 		$settings->register_post_meta();
 		$keys = get_registered_meta_keys( 'post', 'course' );
-		$this->assertArrayHasKey( '_video_course_autocomplete', $keys, 'Autocomplete key was not found in meta' );
-		$this->assertArrayHasKey( '_video_course_autopause', $keys, 'Autopause key was not found in meta' );
-		$this->assertArrayHasKey( '_video_course_required', $keys, 'Required key was not found in meta' );
+		$this->assertArrayHasKey( 'sensei_course_video_autocomplete', $keys, 'Autocomplete key was not found in meta' );
+		$this->assertArrayHasKey( 'sensei_course_video_autopause', $keys, 'Autopause key was not found in meta' );
+		$this->assertArrayHasKey( 'sensei_course_video_required', $keys, 'Required key was not found in meta' );
 	}
 }
