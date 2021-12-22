56f9edabd712d09d649bb5cc54e086f975ce5cbe
Dmitry Merkushin <merkushin@gmail.com>
2021-12-22 19:53:17 +0300

    Extract onStateChange callback to a separate function

--- assets/js/frontend/course-video/youtube-extension.js
+++ assets/js/frontend/course-video/youtube-extension.js
@@ -5,6 +5,37 @@
 	const firstScriptTag = document.getElementsByTagName( 'script' )[ 0 ];
 	firstScriptTag.parentNode.insertBefore( tag, firstScriptTag );
 
+	function onYouTubePlayerStateChange( event ) {
+		const playerStatus = event.data;
+
+		if (
+			window.sensei.courseVideoSettings.courseVideoRequired &&
+			playerStatus === YT.PlayerState.ENDED
+		) {
+			document.querySelector(
+				'.wp-block-sensei-lms-button-complete-lesson > button'
+			).disabled = false;
+		}
+
+		if (
+			window.sensei.courseVideoSettings.courseVideoAutoComplete &&
+			playerStatus === YT.PlayerState.ENDED
+		) {
+			// submit complete lesson form
+			document
+				.querySelectorAll( '.lesson_button_form' )
+				.forEach( ( form ) => {
+					const action = form.querySelector(
+						'input[name=quiz_action]'
+					).value;
+					if ( action !== 'lesson-complete' ) {
+						return true;
+					}
+					form.submit();
+				} );
+		}
+	}
+
 	window.onYouTubeIframeAPIReady = function () {
 		document
 			.querySelectorAll(
@@ -13,38 +44,7 @@
 			.forEach( ( element ) => {
 				const player = new YT.Player( element, {
 					events: {
-						onStateChange: ( event ) => {
-							const playerStatus = event.data;
-
-							if (
-								window.sensei.courseVideoSettings
-									.courseVideoRequired &&
-								playerStatus === YT.PlayerState.ENDED
-							) {
-								document.querySelector(
-									'.wp-block-sensei-lms-button-complete-lesson > button'
-								).disabled = false;
-							}
-
-							if (
-								window.sensei.courseVideoSettings
-									.courseVideoAutoComplete &&
-								playerStatus === YT.PlayerState.ENDED
-							) {
-								// submit complete lesson form
-								document
-									.querySelectorAll( '.lesson_button_form' )
-									.forEach( ( form ) => {
-										const action = form.querySelector(
-											'input[name=quiz_action]'
-										).value;
-										if ( action !== 'lesson-complete' ) {
-											return true;
-										}
-										form.submit();
-									} );
-							}
-						},
+						onStateChange: onYouTubePlayerStateChange,
 					},
 				} );
 				if ( window.sensei.courseVideoSettings.courseVideoAutoPause ) {
