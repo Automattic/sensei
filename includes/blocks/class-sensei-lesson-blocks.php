a303e2af11f570e4b8c3177584102965cb6bd0a1
Renatho De Carli Rosa <renatho@gmail.com>
2021-12-23 09:28:21 -0300

    Add comments to explain implementation

--- includes/blocks/class-sensei-lesson-blocks.php
+++ includes/blocks/class-sensei-lesson-blocks.php
@@ -77,8 +77,12 @@ class Sensei_Lesson_Blocks extends Sensei_Blocks_Initializer {
 		$post_type_object = get_post_type_object( 'lesson' );
 
 		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-		$lesson_id            = isset( $_GET['post'] ) ? intval( $_GET['post'] ) : 0;
-		$course_id            = Sensei()->lesson->get_course_id( $lesson_id );
+		$lesson_id = isset( $_GET['post'] ) ? intval( $_GET['post'] ) : 0; // WP query is not ready yet.
+		$course_id = Sensei()->lesson->get_course_id( $lesson_id );
+
+		// Notice that for new Lessons, the `lesson_id` will return `0` (post query string not set).
+		// It means the following check will return `false`. It's expected and works well because
+		// new lessons don't have associated courses yet.
 		$sensei_theme_enabled = Sensei_Course_Theme_Option::instance()->has_sensei_theme_enabled( $course_id );
 
 		if ( $sensei_theme_enabled ) {
