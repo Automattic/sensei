98092fc89efc0d5935341827353194f94b9f7884
Nurguly Ashyrov <nurguly.ashyrov@automattic.com>
2022-01-03 17:51:56 +0400

    Add tests for Sensei Course Theme settings.

--- tests/unit-tests/course-theme/test-class-sensei-course-theme.php
+++ tests/unit-tests/course-theme/test-class-sensei-course-theme.php
@@ -15,10 +15,69 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @group course-theme
  */
 class Sensei_Course_Theme_Test extends WP_UnitTestCase {
+
+	use Sensei_Test_Login_Helpers;
+
+	/**
+	 * Sensei Factory helper class - useful to create objects for testing.
+	 *
+	 * @var Sensei_Factory
+	 */
+	private $factory;
+
+	/**
+	 * Instance of `Sensei_Course_Theme_Option` under test.
+	 *
+	 * @var Sensei_Course_Theme_Theme
+	 */
+	private $instance;
+
+	/**
+	 * Setup method. Run first on every test execution.
+	 */
+	public function setup() {
+		parent::setup();
+		$this->factory  = new Sensei_Factory();
+		$this->instance = Sensei_Course_Theme::instance();
+	}
+
 	/**
 	 * Testing the Course Theme class to make sure it is loaded.
 	 */
 	public function testClassInstance() {
 		$this->assertTrue( class_exists( 'Sensei_Course_Theme' ), 'Sensei Course Theme class should exist' );
+	}
+
+	public function testPreviewModeNotAllowedToNonPreviliged() {
+		$course          = $this->factory->course->create_and_get();
+		$GLOBALS['post'] = $course;
+
+		$this->login_as_student();
+
+		$_GET[ Sensei_Course_Theme::PREVIEW_QUERY_VAR ] = (string) $course->ID;
+		$allowed                                        = Sensei_Course_Theme::is_sensei_theme_preview_mode_allowed( $course->ID );
+		$this->assertFalse( $allowed, 'Should not allow preview to students.' );
+	}
+
+	public function testPreviewModeNotAllowedIfNotCoursePage() {
+		$post            = $this->factory->post->create_and_get();
+		$GLOBALS['post'] = $post;
+
+		$this->login_as_admin();
+
+		$allowed = Sensei_Course_Theme::is_sensei_theme_preview_mode_allowed( $post->ID );
+		$this->assertFalse( $allowed, 'Should not allow preview if not a course related page.' );
+	}
+
+	public function testPreviewModeNotAllowedIfPreviewQueryNotCurrentCourse() {
+		$course          = $this->factory->course->create_and_get();
+		$another_course  = $this->factory->course->create_and_get();
+		$GLOBALS['post'] = $course;
+
+		$this->login_as_admin();
+
+		$_GET[ Sensei_Course_Theme::PREVIEW_QUERY_VAR ] = (string) $another_course->ID;
+		$allowed                                        = Sensei_Course_Theme::is_sensei_theme_preview_mode_allowed( $course->ID );
+		$this->assertFalse( $allowed, 'Should not allow preview if preview query id is not current course page.' );
 	}
 }
