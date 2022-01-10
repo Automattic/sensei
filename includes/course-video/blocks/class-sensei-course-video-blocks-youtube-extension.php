49426b3e61b25fdf2ef0270a4d37e65437427f2f
Dmitry Merkushin <merkushin@gmail.com>
2022-01-10 17:37:41 +0300

    Instantiate an instance of self in YouTube extension

--- includes/course-video/blocks/class-sensei-course-video-blocks-youtube-extension.php
+++ includes/course-video/blocks/class-sensei-course-video-blocks-youtube-extension.php
@@ -33,7 +33,7 @@ class Sensei_Course_Video_Blocks_Youtube_Extension extends Sensei_Course_Video_B
 			return self::$instance;
 		}
 
-		self::$instance = new static();
+		self::$instance = new self();
 		return self::$instance;
 	}
 
