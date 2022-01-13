5e5f8407560498ac34b0cbaa33ee981009e7a717
Irma Jakic <irma.jakic@gmail.com>
2022-01-13 12:23:00 +0700

    Write tests for progress bar edit

---
+++ assets/blocks/quiz/quiz-block/quiz-progress-bar-edit.test.js
@@ -0,0 +1,42 @@
+import { render } from '@testing-library/react';
+import QuizProgressBarEdit from './quiz-progress-bar-edit';
+import ProgressBar from '../../../shared/blocks/progress-bar';
+
+describe( 'Testing Quiz Progress Bar Edit', () => {
+	it( 'Test ProgressBar is rendered with expected properties', () => {
+		const pagination = {
+			progressBarColor: 'black',
+			progressBarBackground: 'white',
+			progressBarRadius: 6,
+			progressBarHeight: 12,
+		};
+		const barAttributes = {
+			style: {
+				backgroundColor: 'black',
+			},
+		};
+
+		const barWrapperAttributes = {
+			style: {
+				backgroundColor: 'white',
+				height: 12,
+				borderRadius: 6,
+			},
+		};
+
+		const quizProgressBarEditTest = render(
+			<QuizProgressBarEdit pagination={ pagination } />
+		);
+		const progressBar = render(
+			<ProgressBar
+				lessonsCount={ 10 }
+				completedCount={ 2 }
+				barAttributes={ barAttributes }
+				barWrapperAttributes={ barWrapperAttributes }
+			/>
+		);
+		expect( progressBar.baseElement ).toEqual(
+			quizProgressBarEditTest.baseElement
+		);
+	} );
+} );
