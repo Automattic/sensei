af2b5433f7dba771d8c2a8b296b0b4a9383029a7
Onu Abah <abahonuh@gmail.com>
2022-01-25 18:18:14 +0100

    attempt to fix linting issue

--- includes/class-sensei-lesson.php
+++ includes/class-sensei-lesson.php
@@ -4130,8 +4130,7 @@ class Sensei_Lesson {
 	function save_all_lessons_edit_fields() {
 
 		// verify all the data before attempting to save
-		if ( ! isset( $_POST['security'] ) || ! check_ajax_referer( 'bulk-edit-lessons', 'security' )
-			 || empty( $_POST['post_ids'] ) || ! is_array( $_POST['post_ids'] ) ) {
+		if ( ! isset( $_POST['security'] ) || ! check_ajax_referer( 'bulk-edit-lessons', 'security' ) || empty( $_POST['post_ids'] ) || ! is_array( $_POST['post_ids'] ) ) {
 			die();
 		}
 
