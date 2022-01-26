94bde2dc911cbeebf64cb125aa2303e78af50fbe
Miroslav Mitev <m1r0@users.noreply.github.com>
2022-01-26 22:13:33 +0200

    Use ES6 arrow function in the `course-index.js` file

--- assets/js/admin/course-index.js
+++ assets/js/admin/course-index.js
@@ -4,9 +4,12 @@
 import domReady from '@wordpress/dom-ready';
 
 domReady( () => {
-	jQuery( '.sensei-show-more' ).on( 'click', function ( event ) {
+	jQuery( '.sensei-show-more' ).on( 'click', ( event ) => {
 		event.preventDefault();
 
-		jQuery( this ).addClass( 'hidden' ).siblings().removeClass( 'hidden' );
+		jQuery( event.target )
+			.addClass( 'hidden' )
+			.siblings()
+			.removeClass( 'hidden' );
 	} );
 } );
