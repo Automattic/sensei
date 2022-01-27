836dae5c4422b49c8d37e0dda023f1672470f298
Renatho De Carli Rosa <renatho@gmail.com>
2022-01-27 10:57:58 -0300

    Update debounce import to avoid import lodash

--- assets/course-theme/adminbar-layout.js
+++ assets/course-theme/adminbar-layout.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { debounce } from 'lodash';
+import debounce from 'lodash/debounce';
 
 /**
  * Track how much space the WordPress admin bar takes up at the top of the screen.
