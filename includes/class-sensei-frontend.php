43299cd2aa424d1029bd376c57e1df0f0623bffd
Fernando Jorge Mota <contato@fjorgemota.com>
2022-02-22 11:57:24 -0300

    Fix comments for clarity reasons
    As suggested by Renatho! :D

--- includes/class-sensei-frontend.php
+++ includes/class-sensei-frontend.php
@@ -1239,12 +1239,11 @@ class Sensei_Frontend {
 	public function manually_enrol_learner( $user_id, $course_id ) {
 		$course_enrolment = Sensei_Course_Enrolment::get_course_instance( $course_id );
 
-		// If user is removed, just restore instead.
+		// If user is removed, just restore it.
 		if ( $course_enrolment->is_learner_removed( $user_id ) ) {
 			$course_enrolment->restore_learner( $user_id );
 		}
 
-		// If user isn't still enrolled, enroll manually.
 		$enrolment_manager         = Sensei_Course_Enrolment_Manager::instance();
 		$manual_enrolment_provider = $enrolment_manager->get_manual_enrolment_provider();
 
