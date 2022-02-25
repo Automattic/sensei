03caf181f0e9a94583aded134a7ee2d2221bbc06
Miroslav Mitev <m1r0@users.noreply.github.com>
2022-02-25 22:43:47 +0200

    Refactor the students report tests to improve readability

--- tests/unit-tests/test-class-sensei-analysis-overview-list-table.php
+++ tests/unit-tests/test-class-sensei-analysis-overview-list-table.php
@@ -24,26 +24,15 @@ class Sensei_Analysis_Overview_List_Table_Test extends WP_UnitTestCase {
 	}
 
 	/**
-	 * Tests the last activity logic.
+	 * Tests that the last activity is ignoring "in-progress" lessons.
 	 *
 	 * @covers Sensei_Admin::get_last_activity
 	 */
-	public function testGetLastActivity() {
+	public function testGetLastActivityShouldIgnoreInProgressLessons() {
 		/* Arrange. */
-		$user_id  = $this->factory->user->create();
-		$lesson_1 = $this->factory->lesson->create(
-			[
-				'meta_input' => [
-					'_lesson_course' => $this->factory->course->create(),
-				],
-			]
-		);
-		$lesson_2 = $this->factory->lesson->create(
-			[
-				'meta_input' => [
-					'_lesson_course' => $this->factory->course->create(),
-				],
-			]
+		$user_id   = $this->factory->user->create();
+		$lesson_id = $this->factory->lesson->create(
+			[ 'meta_input' => [ '_lesson_course' => $this->factory->course->create() ] ]
 		);
 
 		$instance = new Sensei_Analysis_Overview_List_Table();
@@ -52,63 +41,87 @@ class Sensei_Analysis_Overview_List_Table_Test extends WP_UnitTestCase {
 
 		/* Act. */
 		// Start lesson 1 (status: in-progress).
-		$lesson_1_activity_comment_id = Sensei_Utils::sensei_start_lesson( $lesson_1, $user_id );
+		Sensei_Utils::sensei_start_lesson( $lesson_id, $user_id );
 
 		/* Assert. */
 		$this->assertEmpty(
 			$method->invoke( $instance, $user_id ),
-			'The last activity should not take into account activities that are in progress.'
+			'The last activity should not take into account lessons that are in progress.'
+		);
+	}
+
+	/**
+	 * Tests that the last activity is based on "completed" lessons.
+	 *
+	 * @covers Sensei_Admin::get_last_activity
+	 * @dataProvider lessonCompleteStatuses
+	 */
+	public function testGetLastActivityShouldBeBasedOnCompletedLessons( $lesson_complete_status ) {
+		/* Arrange. */
+		$user_id   = $this->factory->user->create();
+		$lesson_id = $this->factory->lesson->create(
+			[ 'meta_input' => [ '_lesson_course' => $this->factory->course->create() ] ]
 		);
 
+		$instance = new Sensei_Analysis_Overview_List_Table();
+		$method   = new ReflectionMethod( $instance, 'get_last_activity' );
+		$method->setAccessible( true );
+
 		/* Act. */
-		// Simulate quiz submission.
+		$lesson_activity_comment_id = Sensei_Utils::sensei_start_lesson( $lesson_id, $user_id );
+
+		// Update the lesson status.
 		wp_update_comment(
 			[
-				'comment_ID'       => $lesson_1_activity_comment_id,
-				'comment_approved' => 'ungraded',
+				'comment_ID'       => $lesson_activity_comment_id,
+				'comment_approved' => $lesson_complete_status,
 			]
 		);
 
 		/* Assert. */
 		$this->assertNotEmpty(
 			$method->invoke( $instance, $user_id ),
-			'The last activity should take into account activities with status "ungraded".'
+			'The last activity should take into account lessons with status "' . $lesson_complete_status . '".'
 		);
+	}
+
+	/**
+	 * Lesson statuses that mark the lesson as completed.
+	 *
+	 * @return string[][]
+	 */
+	public function lessonCompleteStatuses(): array {
+		return [
+			[ 'passed' ],
+			[ 'ungraded' ],
+			[ 'failed' ],
+		];
+	}
+
+	/**
+	 * Tests that the last activity should be the more recent one.
+	 *
+	 * @covers Sensei_Admin::get_last_activity
+	 */
+	public function testGetLastActivityShouldReturnTheMoreRecentActivity() {
+		/* Arrange. */
+		$user_id   = $this->factory->user->create();
+		$course_id = $this->factory->course->create();
+		$lesson_1  = $this->factory->lesson->create(
+			[ 'meta_input' => [ '_lesson_course' => $course_id ] ]
+		);
+		$lesson_2  = $this->factory->lesson->create(
+			[ 'meta_input' => [ '_lesson_course' => $course_id ] ]
+		);
+
+		$instance = new Sensei_Analysis_Overview_List_Table();
+		$method   = new ReflectionMethod( $instance, 'get_last_activity' );
+		$method->setAccessible( true );
 
 		/* Act. */
-		// Simulate passing a quiz.
-		wp_update_comment(
-			[
-				'comment_ID'       => $lesson_1_activity_comment_id,
-				'comment_approved' => 'passed',
-			]
-		);
-
-		/* Assert. */
-		$this->assertNotEmpty(
-			$method->invoke( $instance, $user_id ),
-			'The last activity should take into account activities with status "passed".'
-		);
-
-		/* Act. */
-		// Simulate failing a quiz.
-		wp_update_comment(
-			[
-				'comment_ID'       => $lesson_1_activity_comment_id,
-				'comment_approved' => 'failed',
-			]
-		);
-
-		/* Assert. */
-		$this->assertNotEmpty(
-			$method->invoke( $instance, $user_id ),
-			'The last activity should take into account activities with status "failed".'
-		);
-
-		/* Act. */
-		// Complete lesson 1 and update its date.
+		// Complete lesson 1 and update its activity date.
 		$lesson_1_activity_comment_id = Sensei_Utils::sensei_start_lesson( $lesson_1, $user_id, true );
-		$lesson_1_activity_timestamp  = strtotime( '-7 days' );
+		$lesson_1_activity_timestamp  = strtotime( '-3 days' );
 		wp_update_comment(
 			[
 				'comment_ID'   => $lesson_1_activity_comment_id,
@@ -116,21 +129,9 @@ class Sensei_Analysis_Overview_List_Table_Test extends WP_UnitTestCase {
 			]
 		);
 
-		/* Assert. */
-		$this->assertEquals(
-			wp_date(
-				get_option( 'date_format' ),
-				$lesson_1_activity_timestamp,
-				new DateTimeZone( 'GMT' )
-			),
-			$method->invoke( $instance, $user_id ),
-			'The last activity date format or timezone is invalid.'
-		);
-
-		/* Act. */
-		// Complete lesson 2 and update its date.
+		// Complete lesson 2 and update its activity date.
 		$lesson_2_activity_comment_id = Sensei_Utils::sensei_start_lesson( $lesson_2, $user_id, true );
-		$lesson_2_activity_timestamp  = strtotime( '-1 day' );
+		$lesson_2_activity_timestamp  = strtotime( '-2 day' );
 		wp_update_comment(
 			[
 				'comment_ID'   => $lesson_2_activity_comment_id,
@@ -140,10 +141,82 @@ class Sensei_Analysis_Overview_List_Table_Test extends WP_UnitTestCase {
 
 		/* Assert. */
 		$this->assertEquals(
-			'1 day ago',
+			'2 days ago',
 			$method->invoke( $instance, $user_id ),
-			'The last activity should be the more recent activity or the date format is invalid and should be in human-readable form.'
+			'The last activity should be the more recent activity.'
 		);
 
+		/* Act. */
+		// Update lesson 1 activity date.
+		$lesson_1_activity_timestamp = strtotime( '-1 days' );
+		wp_update_comment(
+			[
+				'comment_ID'   => $lesson_1_activity_comment_id,
+				'comment_date' => gmdate( 'Y-m-d H:i:s', $lesson_1_activity_timestamp ),
+			]
+		);
+
+		/* Assert. */
+		$this->assertEquals(
+			'1 day ago',
+			$method->invoke( $instance, $user_id ),
+			'The last activity should be the more recent activity.'
+		);
+	}
+
+	/**
+	 * Tests that the last activity date format is human-readable when less than a week.
+	 *
+	 * @covers Sensei_Admin::get_last_activity
+	 */
+	public function testGetLastActivityShouldUseHumanReadableTimeFormatIfLessThanAWeek() {
+		/* Arrange. */
+		$user_id   = $this->factory->user->create();
+		$lesson_id = $this->factory->lesson->create(
+			[ 'meta_input' => [ '_lesson_course' => $this->factory->course->create() ] ]
+		);
+
+		$instance = new Sensei_Analysis_Overview_List_Table();
+		$method   = new ReflectionMethod( $instance, 'get_last_activity' );
+		$method->setAccessible( true );
+
+		/* Act. */
+		// Complete lesson and update its activity date.
+		$lesson_activity_comment_id = Sensei_Utils::sensei_start_lesson( $lesson_id, $user_id, true );
+		$lesson_activity_timestamp  = strtotime( '-7 days' );
+		wp_update_comment(
+			[
+				'comment_ID'   => $lesson_activity_comment_id,
+				'comment_date' => gmdate( 'Y-m-d H:i:s', $lesson_activity_timestamp ),
+			]
+		);
+
+		/* Assert. */
+		$this->assertEquals(
+			wp_date(
+				get_option( 'date_format' ),
+				$lesson_activity_timestamp,
+				new DateTimeZone( 'GMT' )
+			),
+			$method->invoke( $instance, $user_id ),
+			'The last activity date format or timezone is invalid.'
+		);
+
+		/* Act. */
+		// Update the lesson's activity date.
+		$lesson_activity_timestamp = strtotime( '-1 day' );
+		wp_update_comment(
+			[
+				'comment_ID'   => $lesson_activity_comment_id,
+				'comment_date' => gmdate( 'Y-m-d H:i:s', $lesson_activity_timestamp ),
+			]
+		);
+
+		/* Assert. */
+		$this->assertEquals(
+			'1 day ago',
+			$method->invoke( $instance, $user_id ),
+			'The last activity date format should be in human-readable form.'
+		);
 	}
 }
