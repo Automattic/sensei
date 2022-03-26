f2e6df2eb68f19e4fb10b21d4391510d14aec98c
Dmitry Merkushin <merkushin@gmail.com>
2022-03-26 18:45:28 +0300

    Add test with last activity

---
+++ tests/unit-tests/reports/overview/list-table/test-class-sensei-reports-overview-list-table-courses.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Tests for Sensei_Reports_Overview_List_Table_Courses class.
+ *
+ * @covers Sensei_Reports_Overview_List_Table_Courses
+ */
+class Sensei_Reports_Overview_List_Table_Courses_Test extends WP_UnitTestCase {
+
+}
