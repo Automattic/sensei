bfdd17f2c86a1067a93550f390023974c8186e13
Dmitry Merkushin <merkushin@gmail.com>
2022-03-29 13:11:57 +0300

    Fix default order value

--- includes/reports/overview/list-table/class-sensei-reports-overview-list-table-abstract.php
+++ includes/reports/overview/list-table/class-sensei-reports-overview-list-table-abstract.php
@@ -86,7 +86,7 @@ abstract class Sensei_Reports_Overview_List_Table_Abstract extends Sensei_List_T
 
 		// Handle order.
 		// phpcs:ignore WordPress.Security.NonceVerification.Recommended -- No action, nonce is not required.
-		$order = sanitize_key( wp_unslash( $_GET['order'] ?? '' ) );
+		$order = sanitize_key( wp_unslash( $_GET['order'] ?? 'ASC' ) );
 		$order = ( 'ASC' === strtoupper( $order ) ) ? 'ASC' : 'DESC';
 
 		$per_page = $this->get_items_per_page( 'sensei_comments_per_page' );
@@ -146,7 +146,7 @@ abstract class Sensei_Reports_Overview_List_Table_Abstract extends Sensei_List_T
 
 		// Handle order.
 		// phpcs:ignore WordPress.Security.NonceVerification.Recommended -- No action, nonce is not required.
-		$order = sanitize_key( wp_unslash( $_GET['order'] ?? '' ) );
+		$order = sanitize_key( wp_unslash( $_GET['order'] ?? 'ASC' ) );
 		$order = ( 'ASC' === strtoupper( $order ) ) ? 'ASC' : 'DESC';
 
 		$args = array(
