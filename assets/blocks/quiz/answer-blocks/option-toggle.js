24fe3fb23d09acc962c9f6953f4c2db943ddd018
Renatho De Carli Rosa <renatho@gmail.com>
2022-04-08 16:06:30 -0300

    Extends OptionToggle to be used with input

--- assets/blocks/quiz/answer-blocks/option-toggle.js
+++ assets/blocks/quiz/answer-blocks/option-toggle.js
@@ -8,6 +8,15 @@ import classnames from 'classnames';
  */
 import { check } from '@wordpress/icons';
 
+/**
+ * Toggle component using divs.
+ *
+ * @param {Object}  props            Component props.
+ * @param {string}  props.className  Class name for the wrapper.
+ * @param {boolean} props.isChecked  Whether it's checked.
+ * @param {boolean} props.isCheckbox Whether it's a checkbox.
+ * @param {Object}  props.children   Component children.
+ */
 export const OptionToggle = ( {
 	className,
 	isChecked,
@@ -28,8 +37,24 @@ export const OptionToggle = ( {
 				{ 'is-checked': isChecked, 'is-checkbox': isCheckbox }
 			) }
 		>
-			{ isChecked && isCheckbox && check }
+			{ isCheckbox && check }
 		</div>
 		{ children }
 	</div>
+);
+
+/**
+ * Toggle component using input.
+ *
+ * @param {Object} props      Component props.
+ * @param {string} props.type Input toggle type (`checkbox` or `radio`).
+ */
+export const InputToggle = ( props ) => (
+	<>
+		<input
+			className="sensei-lms-question-block__option-toggle-input"
+			{ ...props }
+		/>
+		<OptionToggle isCheckbox={ props.type === 'checkbox' } />
+	</>
 );
