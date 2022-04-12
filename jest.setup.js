811c48859114fe0f0479e66c96878c5b21762802
Gabriel Caires <gabriel.caires@automattic.com>
2022-04-12 18:43:30 -0300

    test unfetch

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,11 @@
+/**
+ * External dependencies
+ */
 import MutationObserver from '@sheerun/mutationobserver-shim';
-
+/**
+ * WordPress dependencies
+ */
+import '@wordpress/jest-preset-default/scripts/setup-globals';
+import 'unfetch/polyfill';
+import '@testing-library/jest-dom';
 window.MutationObserver = MutationObserver;
