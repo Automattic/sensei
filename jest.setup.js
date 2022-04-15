e27c65133a2694c008fbe0571e10f8ad336f9010
Gabriel Caires <gabriel.caires@automattic.com>
2022-04-15 14:55:25 -0300

    Clean nock before run every test

--- jest.setup.js
+++ jest.setup.js
@@ -2,13 +2,14 @@
  * External dependencies
  */
 import MutationObserver from '@sheerun/mutationobserver-shim';
+import nock from 'nock';
+import 'whatwg-fetch';
+
 /**
  * WordPress dependencies
  */
 import '@wordpress/jest-preset-default/scripts/setup-globals';
 import '@testing-library/jest-dom';
-import 'whatwg-fetch';
-import nock from 'nock';
 
 window.MutationObserver = MutationObserver;
 
