f6b119cddb08d3ed81a487d4d684a8c486d838f4
Imran Hossain <imran.hossain@automattic.com>
2022-04-18 16:52:07 +0600

    js test fix

--- jest.setup.js
+++ jest.setup.js
@@ -2,9 +2,11 @@
  * External dependencies
  */
 import MutationObserver from '@sheerun/mutationobserver-shim';
+import nock from 'nock';
 /**
  * WordPress dependencies
  */
 import '@wordpress/jest-preset-default/scripts/setup-globals';
 import '@testing-library/jest-dom';
 window.MutationObserver = MutationObserver;
+beforeAll( () => nock.cleanAll() );
