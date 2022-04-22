29dfcebc708bef741cd08f5352c45ad49c3fa0f1
Irma Jakic <irma.jakic@gmail.com>
2022-04-22 09:36:26 +0200

    Add remove_enrolment in selector in student bulk action button tests

--- assets/admin/students/student-bulk-action-button/index.test.js
+++ assets/admin/students/student-bulk-action-button/index.test.js
@@ -32,7 +32,7 @@ describe( '<StudentBulkActionButton />', () => {
 		setupSelector( [
 			{ value: 'enrol_restore_enrolment', selected: true },
 			{ value: 'remove_progress' },
-			{ value: 'remove_progress' },
+			{ value: 'remove_enrolment' },
 		] );
 		render( <StudentBulkActionButton /> );
 
@@ -52,7 +52,7 @@ describe( '<StudentBulkActionButton />', () => {
 		setupSelector( [
 			{ value: 'enrol_restore_enrolment' },
 			{ value: 'remove_progress', selected: true },
-			{ value: 'remove_progress' },
+			{ value: 'remove_enrolment' },
 		] );
 		render( <StudentBulkActionButton /> );
 
