fc2ab39c7eccd2dcf8517113e19675141377fe8f
Gabriel Caires <gabriel.caires@automattic.com>
2022-04-25 19:57:41 -0300

    Use list instead of html to display the tests names properly

--- playwright.config.js
+++ playwright.config.js
@@ -27,7 +27,7 @@ const config = {
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: process.env.CI ? 'github' : 'html',
+	reporter: process.env.CI ? 'github' : 'list',
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
