99ecb6ecb8948ca8fe9805ae54a0686ea13dab00
Miroslav Mitev <m1r0@users.noreply.github.com>
2022-05-16 14:37:34 +0300

    Update the version placeholder

--- includes/reports/overview/data-provider/class-sensei-reports-overview-data-provider-students.php
+++ includes/reports/overview/data-provider/class-sensei-reports-overview-data-provider-students.php
@@ -89,7 +89,7 @@ class Sensei_Reports_Overview_Data_Provider_Students implements Sensei_Reports_O
 	/**
 	 * Filter the users to ones enrolled in a course.
 	 *
-	 * @since  x.x.x
+	 * @since  4.4.1
 	 * @access private
 	 *
 	 * @param WP_User_Query $query The user query.
