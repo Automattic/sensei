59619029062e7560118dff3a8105d63d71e5c638
Miroslav Mitev <m1r0@users.noreply.github.com>
2022-05-16 14:44:24 +0300

    Update the copyright date

--- sensei-lms.php
+++ sensei-lms.php
@@ -15,7 +15,7 @@
  */
 
 /**
- * Copyright 2013-2020 Automattic
+ * Copyright 2013-2022 Automattic
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2, as
