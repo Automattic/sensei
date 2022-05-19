cb3685e876e3bbf9f1a7cee5604046f513c4a3b4
Aarón Fas <aaron.fas@automattic.com>
2022-05-19 17:47:07 +0200

    Fix test and use correct `expect` method.

--- assets/admin/editor-wizard/wizard.test.js
+++ assets/admin/editor-wizard/wizard.test.js
@@ -8,7 +8,6 @@ import '@testing-library/jest-dom';
  * Internal dependencies
  */
 import Wizard from './wizard';
-import { expect } from '@playwright/test';
 
 const SOME_DUMMY_CONTENT = 'SOME_DUMMY_CONTENT';
 const SOME_DUMMY_ACTIONS = 'SOME_DUMMY_ACTIONS';
@@ -23,7 +22,7 @@ describe( '<Wizard />', () => {
 
 		const { queryByText } = render( <Wizard steps={ [ DummyStep ] } /> );
 
-		expect( queryByText( 'SOMETHING_NOT_EXISTING' ) ).toBeTruthy();
+		expect( queryByText( SOME_DUMMY_CONTENT ) ).toBeTruthy();
 		expect( queryByText( SOME_DUMMY_ACTIONS ) ).toBeTruthy();
 	} );
 
