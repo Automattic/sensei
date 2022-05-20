0d62336977dccfcc865b165feaf4c4cab33dc498
Alex Sanford <alex.sanford1@gmail.com>
2022-05-20 11:21:22 -0300

    Add lesson toolbar button to save course and open lesson

---
+++ assets/blocks/course-outline/lesson-block/lesson-edit-toolbar.js
@@ -0,0 +1,91 @@
+/**
+ * WordPress dependencies
+ */
+import {
+	Button,
+	ExternalLink,
+	Spinner,
+	Toolbar,
+	ToolbarItem,
+} from '@wordpress/components';
+import { dispatch, useSelect } from '@wordpress/data';
+import { useEffect, useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { COURSE_STORE } from '../course-outline-store';
+
+const getLessonURL = ( lessonId ) => `post.php?post=${ lessonId }&action=edit`;
+
+/**
+ * Link to edit the Lesson in a new tab.
+ *
+ * @param {Object} props          Component props.
+ * @param {number} props.lessonId The lesson ID.
+ */
+export const EditLessonLink = ( { lessonId } ) => (
+	<ExternalLink
+		href={ getLessonURL( lessonId ) }
+		target="lesson"
+		className="wp-block-sensei-lms-course-outline-lesson__edit"
+	>
+		{ __( 'Edit lesson', 'sensei-lms' ) }
+	</ExternalLink>
+);
+
+/**
+ * Toolbar section for the link to edit a lesson.
+ *
+ * @param {Object} props          Component props.
+ * @param {number} props.lessonId The lesson ID.
+ */
+const LessonEditToolbar = ( { lessonId } ) => {
+	// Determine whether we are currently saving.
+	const { isSavingPost, isSavingMetaBoxes, isSavingStructure } = useSelect(
+		( select ) => ( {
+			isSavingPost: select( 'core/editor' ).isSavingPost(),
+			isSavingMetaBoxes: select( 'core/edit-post' ).isSavingMetaBoxes(),
+			isSavingStructure: select( COURSE_STORE ).getIsSavingStructure(),
+		} )
+	);
+
+	// Should we open the lesson in a new tab after save?
+	const [ shouldEditLesson, setShouldEditLesson ] = useState( false );
+
+	// Open the lesson in a new tab if needed.
+	useEffect( () => {
+		if ( shouldEditLesson && lessonId ) {
+			setShouldEditLesson( false );
+			window.open( getLessonURL( lessonId ), '_blank' );
+		}
+	}, [ shouldEditLesson, lessonId, isSavingStructure ] );
+
+	// Component for the "Save and edit lesson" button.
+	const savePostLink = (
+		<ToolbarItem
+			as={ Button }
+			onClick={ () => {
+				setShouldEditLesson( true );
+				dispatch( 'core/editor' ).savePost();
+			} }
+		>
+			{ __( 'Save and edit lesson', 'sensei-lms' ) }
+		</ToolbarItem>
+	);
+
+	// Spinner.
+	const savingPostIndicator = <ToolbarItem as={ Spinner } />;
+
+	let toolbarItem = savePostLink;
+	if ( lessonId ) {
+		toolbarItem = <EditLessonLink lessonId={ lessonId } />;
+	} else if ( isSavingPost || isSavingStructure || isSavingMetaBoxes ) {
+		toolbarItem = savingPostIndicator;
+	}
+
+	return <Toolbar className="components-button">{ toolbarItem }</Toolbar>;
+};
+
+export default LessonEditToolbar;
