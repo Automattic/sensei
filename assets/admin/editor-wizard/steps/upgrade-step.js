48539b69cd9cfbe962e0f32709e1be77c86ccc2e
Fernando Jorge Mota <contato@fjorgemota.com>
2022-05-20 22:37:52 -0300

    Start working on the Paid Course upsell step

--- assets/admin/editor-wizard/steps/upgrade-step.js
+++ assets/admin/editor-wizard/steps/upgrade-step.js
@@ -1,25 +1,52 @@
 /**
+ * WordPress dependencies
+ */
+import { Button } from '@wordpress/components';
+
+/**
  * Upgrade step during course creation wizard.
  */
 const UpgradeStep = () => {
-	// TODO Implement this.
 	return (
-		<div>
-			<div>PENDING TO IMPLEMENT</div>
+		<div className="components-modal-columns">
+			<div className="components-modal-columns__description">
+				<p>
+					Do you want to sell this course? This requires Sensei Pro
+					which also unlocks many useful features.
+				</p>
+				<h2>$149 USD</h2>
+				<span className="label">per year, 1 site</span>
+				<ul>
+					<li>WooCommerce integration</li>
+					<li>Schedule &apos;drip&apos; content</li>
+					<li>Set expiration date of courses</li>
+					<li>Quiz timer</li>
+					<li>Flashcards, Image Hotspots, and CHecklists</li>
+					<li>1 year of updates & support</li>
+				</ul>
+			</div>
+			<div className="components-modal-columns__image">
+				Image container
+			</div>
 		</div>
 	);
 };
 
-UpgradeStep.Title = 'Upgrade Step';
+UpgradeStep.Title = 'Sell with Sensei Pro';
 
 UpgradeStep.Actions = ( { goToNextStep } ) => {
-	// TODO Implement this.
+	const upgrade = () => {
+		window.open( '', 'sensei-pricing', 'noreferrer' );
+		goToNextStep();
+	};
 	return (
 		<div>
-			<button onClick={ goToNextStep }>Next</button>
-			<a href="https://senseilms.com" target="_blank" rel="noreferrer">
-				Upgrade
-			</a>
+			<Button isTertiary onClick={ goToNextStep }>
+				Continue with Sensei Free
+			</Button>
+			<Button isPrimary onClick={ upgrade } target="_blank">
+				Get Sensei Pro
+			</Button>
 		</div>
 	);
 };
