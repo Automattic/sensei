1fca99eff79058cf6cc2eef48c39478662e638c6
Fernando Jorge Mota <contato@fjorgemota.com>
2022-05-26 17:33:08 -0300

    Add Lesson Details Step

--- assets/admin/editor-wizard/steps/lesson-details-step.js
+++ assets/admin/editor-wizard/steps/lesson-details-step.js
@@ -1,24 +1,61 @@
 /**
- * Internal dependencies
+ * WordPress dependencies
  */
-import senseiProUpsell from '../../../images/sensei-pro-upsell.png';
+import { __ } from '@wordpress/i18n';
+import { useDispatch } from '@wordpress/data';
+import { store as editorStore } from '@wordpress/editor';
+import { Button } from '@wordpress/components';
 
 /**
- * Initial step for course creation wizard.
+ * Internal dependencies
  */
-const LessonDetailsStep = () => {
-	// TODO Implement this.
+import LimitedTextControl from '../../../blocks/editor-components/limited-text-control';
+import detailsStepImage from '../../../images/details-step.png';
+
+/**
+ * Initial step for lesson creation wizard.
+ *
+ * @param {Object}   props
+ * @param {Object}   props.data
+ * @param {Function} props.setData
+ */
+const LessonDetailsStep = ( { data: wizardData, setData: setWizardData } ) => {
+	const { editPost } = useDispatch( editorStore );
+
+	const updateLessonTitle = ( title ) => {
+		setWizardData( { ...wizardData, lessonTitle: title } );
+		editPost( {
+			title,
+		} );
+	};
+
 	return (
 		<div className="sensei-editor-wizard-modal__columns">
 			<div className="sensei-editor-wizard-modal__content">
-				<h1>Lesson Details Step</h1>
-				<div>PENDING TO IMPLEMENT</div>
+				<h1>{ __( 'Create your lesson', 'sensei-lms' ) }</h1>
+				<div className="sensei-editor-wizard-step__description">
+					{ __(
+						'It is best to keep your Lesson Title short because it will show in your course outline and navigation. You can easily change both later.',
+						'sensei-lms'
+					) }
+				</div>
+				<div className="sensei-editor-wizard-step__form">
+					<LimitedTextControl
+						label={ __( 'Lesson Title', 'sensei-lms' ) }
+						value={ wizardData.lessonTitle ?? '' }
+						onChange={ updateLessonTitle }
+						maxLength={ 40 }
+					/>
+				</div>
 			</div>
 			<div className="sensei-editor-wizard-modal__illustration">
 				<img
-					src={ window.sensei.pluginUrl + senseiProUpsell }
-					alt="PENDING TO IMPLEMENT, BUT HERE TO SHOW IT WORKING"
+					src={ window.sensei.pluginUrl + detailsStepImage }
 					className="sensei-editor-wizard-modal__illustration-image"
+					alt={ __(
+						'Illustration of lesson sample with some placeholders.',
+						'sensei-lms'
+					) }
 				/>
 			</div>
 		</div>
@@ -28,7 +65,9 @@ const LessonDetailsStep = () => {
 LessonDetailsStep.Actions = ( { goToNextStep } ) => {
 	return (
 		<div>
-			<button onClick={ goToNextStep }>Next</button>
+			<Button isPrimary onClick={ goToNextStep }>
+				{ __( 'Continue', 'sensei-lms' ) }
+			</Button>
 		</div>
 	);
 };
