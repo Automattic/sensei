322cb1c499021e13f0d134f835970e4f4f90d3c1
Gabriel Caires <gabriel.caires@automattic.com>
2022-05-27 18:40:26 -0300

    Fix test warning
    Error Description:
    "Error: Not implemented: navigation (except hash changes)"

--- assets/setup-wizard/ready/index.test.js
+++ assets/setup-wizard/ready/index.test.js
@@ -21,218 +21,221 @@ const stepData = {
 };
 
 // Mock features data.
-jest.mock( '../data/use-setup-wizard-step', () => ( {
+jest.mock('../data/use-setup-wizard-step', () => ({
 	useSetupWizardStep: jest.fn(),
-} ) );
+}));
 
 // Mock apiFetch.
-jest.mock( '@wordpress/api-fetch', () => jest.fn() );
+jest.mock('@wordpress/api-fetch', () => jest.fn());
 
 // Mock features data.
-const mockStepData = ( mockData = {} ) => {
-	useSetupWizardStep.mockReturnValue( {
+const mockStepData = (mockData = {}) => {
+	useSetupWizardStep.mockReturnValue({
 		stepData,
 		isComplete: false,
 		submitStep: () => {},
 		...mockData,
-	} );
+	});
 };
 
-describe( '<Ready />', () => {
-	beforeEach( () => {
+describe('<Ready />', () => {
+	beforeEach(() => {
 		window.sensei_log_event = jest.fn();
 		delete window.location;
 		window.location = { assign: jest.fn() };
 
 		mockStepData();
-	} );
+	});
 
-	afterEach( () => {
+	afterEach(() => {
 		delete window.sensei_log_event;
-	} );
+	});
 
-	it( 'Should have a sign-up form pointing to the mailing list provider', () => {
-		const { container } = render( <Ready /> );
+	it('Should have a sign-up form pointing to the mailing list provider', () => {
+		const { container } = render(<Ready />);
 
-		const form = container.querySelector( 'form' );
-		expect( form.getAttribute( 'action' ) ).toEqual( stepData.mc_url );
-	} );
+		const form = container.querySelector('form');
+		expect(form.getAttribute('action')).toEqual(stepData.mc_url);
+	});
 
-	it( 'Should have the admin e-mail pre-filled in the sign-up form', () => {
-		const { container } = render( <Ready /> );
+	it('Should have the admin e-mail pre-filled in the sign-up form', () => {
+		const { container } = render(<Ready />);
 
-		const form = container.querySelector( 'form' );
-		expect( form.querySelector( 'input[type=email]' ).value ).toEqual(
+		const form = container.querySelector('form');
+		expect(form.querySelector('input[type=email]').value).toEqual(
 			stepData.admin_email
 		);
-	} );
+	});
 
-	it( 'Should have a create course button', () => {
-		const { queryByText } = render( <Ready /> );
+	it('Should have a create course button', () => {
+		const { queryByText } = render(<Ready />);
 
+		expect(queryByText('Create a course').getAttribute('href')).toEqual(
+			'post-new.php?post_type=course'
+		);
+	});
+
+	it('Should have an import content button', () => {
+		const { queryByText } = render(<Ready />);
 		expect(
-			queryByText( 'Create a course' ).getAttribute( 'href' )
-		).toEqual( 'post-new.php?post_type=course' );
-	} );
-
-	it( 'Should have an import content button', () => {
-		const { queryByText } = render( <Ready /> );
-		expect(
-			queryByText( 'Import content', {
+			queryByText('Import content', {
 				selector: 'a',
-			} ).getAttribute( 'href' )
+			}).getAttribute('href')
 		).toEqual(
 			'edit.php?post_type=course&page=sensei-tools&tool=import-content'
 		);
-	} );
+	});
 
-	it( 'Should have a create your first course link.', () => {
-		const { queryByText } = render( <Ready /> );
-
-		expect(
-			queryByText( /create your first course/ ).getAttribute( 'href' )
-		).toEqual( 'https://senseilms.com/lesson/courses/' );
-	} );
-
-	it( 'Should have a install sample course button', () => {
-		const { queryByText } = render( <Ready /> );
+	it('Should have a create your first course link.', () => {
+		const { queryByText } = render(<Ready />);
 
 		expect(
-			queryByText( 'Install a sample course', { selector: 'button' } )
+			queryByText(/create your first course/).getAttribute('href')
+		).toEqual('https://senseilms.com/lesson/courses/');
+	});
+
+	it('Should have a install sample course button', () => {
+		const { queryByText } = render(<Ready />);
+
+		expect(
+			queryByText('Install a sample course', { selector: 'button' })
 		).toBeTruthy();
-	} );
+	});
 
-	it( 'Should log event when clicking to join mailing list', () => {
-		const { queryByText } = render( <Ready /> );
-		const button = queryByText( 'Yes, please!' );
+	it('Should log event when clicking to join mailing list', () => {
+		const { queryByText } = render(<Ready />);
+		const button = queryByText('Yes, please!');
 
 		// Temporarily set button type to "button" to prevent form submission.
-		button.setAttribute( 'type', 'button' );
-		fireEvent.click( button );
-		button.setAttribute( 'type', 'submit' );
+		button.setAttribute('type', 'button');
+		fireEvent.click(button);
+		button.setAttribute('type', 'submit');
 
-		expect( window.sensei_log_event ).toHaveBeenCalledWith(
+		expect(window.sensei_log_event).toHaveBeenCalledWith(
 			'setup_wizard_ready_mailing_list',
 			undefined
 		);
-	} );
+	});
 
-	it( 'Should log event when clicking "Create a Course" button', () => {
-		const { queryByText } = render( <Ready /> );
+	it('Should log event when clicking "Create a Course" button', () => {
+		const { queryByText } = render(<Ready />);
 
-		const element = queryByText( 'Create a course', { selector: 'a' } );
+		const element = queryByText('Create a course', { selector: 'a' });
 
-		element.setAttribute( 'href', '#' ); // Temporarily rewrite the `href` attribute to avoid warnings
+		element.setAttribute('href', '#'); // Temporarily rewrite the `href` attribute to avoid warnings
 
-		fireEvent.click( element );
+		fireEvent.click(element);
 
-		expect( window.sensei_log_event ).toHaveBeenCalledWith(
+		expect(window.sensei_log_event).toHaveBeenCalledWith(
 			'setup_wizard_ready_create_course',
 			undefined
 		);
-	} );
+	});
 
-	it( 'Should log event when clicking to import content button', async () => {
-		const { queryByText } = render( <Ready /> );
+	it('Should log event when clicking to import content button', async () => {
+		const { queryByText } = render(<Ready />);
 
-		const element = queryByText( 'Import content', { selector: 'a' } );
+		const element = queryByText('Import content', { selector: 'a' });
 
-		element.setAttribute( 'href', '#' ); // Temporarily rewrite the `href` attribute to avoid warnings
-		fireEvent.click( element );
+		element.setAttribute('href', '#'); // Temporarily rewrite the `href` attribute to avoid warnings
+		fireEvent.click(element);
 
-		expect( window.sensei_log_event ).toHaveBeenCalledWith(
+		expect(window.sensei_log_event).toHaveBeenCalledWith(
 			'setup_wizard_ready_import',
 			undefined
 		);
-	} );
+	});
 
-	it( 'Should log event when clicking to install sample course', async () => {
-		const fetchPromise = Promise.resolve( { id: 1 } );
-		apiFetch.mockImplementation( () => fetchPromise );
+	it('Should log event when clicking to install sample course', async () => {
+		const fetchPromise = Promise.resolve({ id: 1 });
+		apiFetch.mockImplementation(() => fetchPromise);
 
-		const { queryByText } = render( <Ready /> );
+		const { queryByText } = render(<Ready />);
 
 		fireEvent.click(
-			queryByText( 'Install a sample course', { selector: 'button' } )
+			queryByText('Install a sample course', { selector: 'button' })
 		);
 
-		expect( window.sensei_log_event ).toHaveBeenCalledWith(
+		expect(window.sensei_log_event).toHaveBeenCalledWith(
 			'setup_wizard_ready_install_course',
 			undefined
 		);
 
-		await act( () => fetchPromise );
-	} );
+		await act(() => fetchPromise);
+	});
 
-	it( 'Should log event when clicking to create the first course', () => {
-		const { queryByText } = render( <Ready /> );
+	it('Should log event when clicking to create the first course', () => {
+		const { queryByText } = render(<Ready />);
 
-		fireEvent.click( queryByText( 'create your first course.' ) );
+		fireEvent.click(queryByText('create your first course.'));
 
-		expect( window.sensei_log_event ).toHaveBeenCalledWith(
+		expect(window.sensei_log_event).toHaveBeenCalledWith(
 			'setup_wizard_ready_learn_more',
 			undefined
 		);
-	} );
+	});
 
-	it( 'Should log event when clicking to exit', () => {
-		const { queryByText } = render( <Ready /> );
+	it('Should log event when clicking to exit', () => {
+		const { queryByText } = render(<Ready />);
 
-		fireEvent.click( queryByText( 'Exit to Courses' ) );
+		const element = queryByText('Exit to Courses', { selector: 'a' });
 
-		expect( window.sensei_log_event ).toHaveBeenCalledWith(
+		element.setAttribute('href', '#'); // Temporarily rewrite the `href` attribute to avoid warnings
+		fireEvent.click(element);
+
+		expect(window.sensei_log_event).toHaveBeenCalledWith(
 			'setup_wizard_ready_exit',
 			undefined
 		);
-	} );
+	});
 
-	it( 'Should submit the ready step', () => {
+	it('Should submit the ready step', () => {
 		const submitMock = jest.fn();
 
-		mockStepData( {
+		mockStepData({
 			isComplete: false,
 			submitStep: submitMock,
-		} );
+		});
 
-		render( <Ready /> );
+		render(<Ready />);
 
-		expect( submitMock ).toBeCalled();
-	} );
+		expect(submitMock).toBeCalled();
+	});
 
-	it( 'Should not submit the ready step when it is already complete', () => {
+	it('Should not submit the ready step when it is already complete', () => {
 		const submitMock = jest.fn();
 
-		mockStepData( {
+		mockStepData({
 			isComplete: true,
 			submitStep: submitMock,
-		} );
+		});
 
-		render( <Ready /> );
+		render(<Ready />);
 
-		expect( submitMock ).not.toBeCalled();
-	} );
+		expect(submitMock).not.toBeCalled();
+	});
 
-	it( 'Should run sample installation', async () => {
+	it('Should run sample installation', async () => {
 		window.sensei_setup_wizard = { nonce: '123' };
 
-		apiFetch.mockResolvedValueOnce( { id: 1 } );
-		apiFetch.mockResolvedValueOnce( {
+		apiFetch.mockResolvedValueOnce({ id: 1 });
+		apiFetch.mockResolvedValueOnce({
 			status: { status: 'pending' },
-		} );
-		apiFetch.mockResolvedValueOnce( {
+		});
+		apiFetch.mockResolvedValueOnce({
 			status: { status: 'completed' },
-		} );
+		});
 
-		const { queryByText } = render( <Ready /> );
+		const { queryByText } = render(<Ready />);
 
 		fireEvent.click(
-			queryByText( 'Install a sample course', { selector: 'button' } )
+			queryByText('Install a sample course', { selector: 'button' })
 		);
 
-		await waitFor( () => {
-			expect( window.location.assign ).toBeCalledWith(
+		await waitFor(() => {
+			expect(window.location.assign).toBeCalledWith(
 				'?redirect_imported_sample=1&job_id=1&nonce=123'
 			);
-		} );
-	} );
-} );
+		});
+	});
+});
