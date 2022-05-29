8fd7b1043021a23f19e94f205a0e658ec7a24812
Dmitry Merkushin <merkushin@gmail.com>
2022-05-29 18:41:40 +0300

    Test Sensei_View_Helper

---
+++ tests/unit-tests/test-class-sensei-view-helper.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * Test Sensei_View_Helper class.
+ *
+ * @covers Sensei_View_Helper
+ */
+class Sensei_View_Helper_Test extends WP_UnitTestCase {
+
+	/**
+	 * Test the format_question_points method.
+	 *
+	 * @param string $points
+	 * @param string $settings
+	 * @param string $expected
+	 *
+	 * @dataProvider providerFormatQuestionPoints_PointsGiven_ReturnsMatchingResult
+	 */
+	public function testFormatQuestionPoints_PointsGiven_ReturnsMatchingResult( string $points, array $settings, string $expected ) {
+		/* Arrange */
+		Sensei()->settings->settings = $settings;
+
+		/* Act */
+		$helper = new Sensei_View_Helper();
+		$actual = $helper->format_question_points( $points );
+
+		/* Assert */
+		self::assertSame( $expected, $actual );
+	}
+
+	public function providerFormatQuestionPoints_PointsGiven_ReturnsMatchingResult(): array {
+		return [
+			'format: none'             => [
+				'1',
+				'settings' => [
+					'quiz_question_points_format' => 'none',
+				],
+				'<span class="grade"></span>',
+			],
+			'format: number (default)' => [
+				'1',
+				[
+					'quiz_question_points_format' => 'number',
+				],
+				'<span class="grade">1</span>',
+			],
+			'format: brackets'         => [
+				'1',
+				[
+					'quiz_question_points_format' => 'brackets',
+				],
+				'<span class="grade">[1]</span>',
+			],
+			'format: text'             => [
+				'1',
+				[
+					'quiz_question_points_format' => 'text',
+				],
+				'<span class="grade">Points: 1</span>',
+			],
+			'format: full'             => [
+				'1',
+				[
+					'quiz_question_points_format' => 'full',
+				],
+				'<span class="grade">[Points: 1]</span>',
+			],
+		];
+	}
+}
