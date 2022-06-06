12175a1f786e9406dc94c4510d345dfbe4c16983
Renatho De Carli Rosa <renatho@gmail.com>
2022-06-06 12:14:52 -0300

    Add some tests to the open state wizard

--- assets/admin/editor-wizard/helpers.test.js
+++ assets/admin/editor-wizard/helpers.test.js
@@ -1,7 +1,12 @@
 /**
+ * External dependencies
+ */
+import { render, fireEvent, waitFor, act } from '@testing-library/react';
+
+/**
  * Internal dependencies
  */
-import { replacePlaceholders } from './helpers';
+import { replacePlaceholders, useWizardOpenState } from './helpers';
 
 describe( 'replacePlaceholders', () => {
 	const replaces = {
@@ -68,5 +73,63 @@ describe( 'replacePlaceholders', () => {
 		const newBlocks = replacePlaceholders( blocks, replaces );
 
 		expect( newBlocks ).toEqual( expectedBlocks );
+	} );
+} );
+
+describe( 'useWizardOpenState', () => {
+	const TestComponent = () => {
+		const [ open, setDone ] = useWizardOpenState();
+		return (
+			<div>
+				{ open ? 'open' : 'closed' }
+				<button onClick={ () => setDone( true ) }>done</button>
+			</div>
+		);
+	};
+
+	beforeAll( () => {
+		jest.useFakeTimers();
+	} );
+
+	it( 'Should start open when no other modal is open', () => {
+		const { queryByText } = render( <TestComponent /> );
+
+		// Initializes initial state.
+		act( () => {
+			jest.runOnlyPendingTimers();
+		} );
+
+		expect( queryByText( 'open' ) ).toBeTruthy();
+	} );
+
+	it( 'Should open when other modals get closed', async () => {
+		document.body.classList.add( 'modal-open' );
+
+		const { queryByText } = render( <TestComponent /> );
+
+		// Initializes initial state.
+		act( () => {
+			jest.runOnlyPendingTimers();
+		} );
+
+		expect( queryByText( 'closed' ) ).toBeTruthy();
+
+		document.body.classList.remove( 'modal-open' );
+		await waitFor( () => {
+			expect( queryByText( 'open' ) ).toBeTruthy();
+		} );
+	} );
+
+	it( 'Should be closed when wizard is done', async () => {
+		const { queryByText } = render( <TestComponent /> );
+
+		fireEvent.click( queryByText( 'done' ) );
+
+		// Initializes initial state.
+		act( () => {
+			jest.runOnlyPendingTimers();
+		} );
+
+		expect( queryByText( 'closed' ) ).toBeTruthy();
 	} );
 } );
