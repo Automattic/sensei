36893738c1abb3a0d3005d166c3066cfb990b4aa
Aarón Fas <aaron.fas@automattic.com>
2022-06-09 13:26:09 +0200

    Add Default lesson pattern.

---
+++ includes/block-patterns/lesson/templates/default.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Default lesson pattern content.
+ *
+ * @package sensei-lms
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?>
+<!-- wp:paragraph {"placeholder":"<?php esc_html_e( 'Write lesson content...', 'sensei-lms' ); ?>"} -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:sensei-lms/lesson-actions -->
+<div class="wp-block-sensei-lms-lesson-actions"><div class="sensei-buttons-container"><!-- wp:sensei-lms/button-view-quiz {"inContainer":true} -->
+<div class="wp-block-sensei-lms-button-view-quiz is-style-default sensei-buttons-container__button-block wp-block-sensei-lms-button-view-quiz__wrapper"><div class="wp-block-sensei-lms-button-view-quiz is-style-default wp-block-sensei-button wp-block-button has-text-align-left"><button class="wp-block-button__link"><?php esc_html_e( 'View Quiz', 'sensei-lms' ); ?></button></div></div>
+<!-- /wp:sensei-lms/button-view-quiz -->
+
+<!-- wp:sensei-lms/button-complete-lesson {"inContainer":true} -->
+<div class="wp-block-sensei-lms-button-complete-lesson is-style-default sensei-buttons-container__button-block wp-block-sensei-lms-button-complete-lesson__wrapper"><div class="wp-block-sensei-lms-button-complete-lesson is-style-default wp-block-sensei-button wp-block-button has-text-align-left"><button class="wp-block-button__link sensei-stop-double-submission"><?php esc_html_e( 'Complete Lesson', 'sensei-lms' ); ?></button></div></div>
+<!-- /wp:sensei-lms/button-complete-lesson -->
+
+<!-- wp:sensei-lms/button-next-lesson {"inContainer":true} -->
+<div class="wp-block-sensei-lms-button-next-lesson is-style-default sensei-buttons-container__button-block wp-block-sensei-lms-button-next-lesson__wrapper"><div class="wp-block-sensei-lms-button-next-lesson is-style-default wp-block-sensei-button wp-block-button has-text-align-left"><button class="wp-block-button__link"><?php esc_html_e( 'Next Lesson', 'sensei-lms' ); ?></button></div></div>
+<!-- /wp:sensei-lms/button-next-lesson -->
+
+<!-- wp:sensei-lms/button-reset-lesson {"inContainer":true} -->
+<div class="wp-block-sensei-lms-button-reset-lesson is-style-outline sensei-buttons-container__button-block wp-block-sensei-lms-button-reset-lesson__wrapper"><div class="wp-block-sensei-lms-button-reset-lesson is-style-outline wp-block-sensei-button wp-block-button has-text-align-left"><button class="wp-block-button__link sensei-stop-double-submission"><?php esc_html_e( 'Reset Lesson', 'sensei-lms' ); ?></button></div></div>
+<!-- /wp:sensei-lms/button-reset-lesson --></div></div>
+<!-- /wp:sensei-lms/lesson-actions -->
