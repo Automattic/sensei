bb06fe4630ba3248373f5c764f312f29c49abf43
Aarón Fas <aaron.fas@automattic.com>
2022-06-09 13:28:41 +0200

    Remove Quiz block from Discussions, Files and Video lesson patterns.

--- includes/block-patterns/lesson/templates/video-lesson.php
+++ includes/block-patterns/lesson/templates/video-lesson.php
@@ -36,26 +36,3 @@ if ( ! defined( 'ABSPATH' ) ) {
 <!-- /wp:sensei-lms/button-reset-lesson --></div></div>
 <!-- /wp:sensei-lms/lesson-actions -->
 
-<!-- wp:sensei-lms/quiz {"options":{"passRequired":false,"quizPassmark":0,"autoGrade":false,"allowRetakes":false,"showQuestions":null,"randomQuestionOrder":false,"failedIndicateIncorrect":true,"failedShowCorrectAnswers":true,"failedShowAnswerFeedback":true,"buttonTextColor":null,"buttonBackgroundColor":null,"pagination":{"paginationNumber":null,"showProgressBar":false,"progressBarRadius":6,"progressBarHeight":12,"progressBarColor":null,"progressBarBackground":null},"enableQuizTimer":"","timerValue":null}} -->
-<!-- wp:sensei-lms/quiz-question -->
-<!-- wp:sensei-lms/question-description -->
-<!-- wp:paragraph {"placeholder":"<?php esc_html_e( 'Add question description or type / to choose a block.', 'sensei-lms' ); ?>"} -->
-<p></p>
-<!-- /wp:paragraph -->
-<!-- /wp:sensei-lms/question-description -->
-
-<!-- wp:sensei-lms/question-answers /-->
-
-<!-- wp:sensei-lms/quiz-question-feedback-correct -->
-<!-- wp:paragraph {"placeholder":"<?php esc_html_e( 'Show a message when the question is answered correctly. Type / to choose a block.', 'sensei-lms' ); ?>"} -->
-<p></p>
-<!-- /wp:paragraph -->
-<!-- /wp:sensei-lms/quiz-question-feedback-correct -->
-
-<!-- wp:sensei-lms/quiz-question-feedback-incorrect -->
-<!-- wp:paragraph {"placeholder":"<?php esc_html_e( 'Show a message when the question is answered incorrectly. Type / to choose a block.', 'sensei-lms' ); ?>"} -->
-<p></p>
-<!-- /wp:paragraph -->
-<!-- /wp:sensei-lms/quiz-question-feedback-incorrect -->
-<!-- /wp:sensei-lms/quiz-question -->
-<!-- /wp:sensei-lms/quiz -->
