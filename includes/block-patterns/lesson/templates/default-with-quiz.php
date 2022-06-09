35f2658a1c46b7d38809b124e20546fcf6a58d8c
Aarón Fas <aaron.fas@automattic.com>
2022-06-09 15:44:25 +0200

    Move lesson actions block to bottom for Default with Quiz pattern.

--- includes/block-patterns/lesson/templates/default-with-quiz.php
+++ includes/block-patterns/lesson/templates/default-with-quiz.php
@@ -14,24 +14,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 <p></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:sensei-lms/lesson-actions -->
-<div class="wp-block-sensei-lms-lesson-actions"><div class="sensei-buttons-container"><!-- wp:sensei-lms/button-view-quiz {"inContainer":true} -->
-<div class="wp-block-sensei-lms-button-view-quiz is-style-default sensei-buttons-container__button-block wp-block-sensei-lms-button-view-quiz__wrapper"><div class="wp-block-sensei-lms-button-view-quiz is-style-default wp-block-sensei-button wp-block-button has-text-align-left"><button class="wp-block-button__link"><?php esc_html_e( 'View Quiz', 'sensei-lms' ); ?></button></div></div>
-<!-- /wp:sensei-lms/button-view-quiz -->
-
-<!-- wp:sensei-lms/button-complete-lesson {"inContainer":true} -->
-<div class="wp-block-sensei-lms-button-complete-lesson is-style-default sensei-buttons-container__button-block wp-block-sensei-lms-button-complete-lesson__wrapper"><div class="wp-block-sensei-lms-button-complete-lesson is-style-default wp-block-sensei-button wp-block-button has-text-align-left"><button class="wp-block-button__link sensei-stop-double-submission"><?php esc_html_e( 'Complete Lesson', 'sensei-lms' ); ?></button></div></div>
-<!-- /wp:sensei-lms/button-complete-lesson -->
-
-<!-- wp:sensei-lms/button-next-lesson {"inContainer":true} -->
-<div class="wp-block-sensei-lms-button-next-lesson is-style-default sensei-buttons-container__button-block wp-block-sensei-lms-button-next-lesson__wrapper"><div class="wp-block-sensei-lms-button-next-lesson is-style-default wp-block-sensei-button wp-block-button has-text-align-left"><button class="wp-block-button__link"><?php esc_html_e( 'Next Lesson', 'sensei-lms' ); ?></button></div></div>
-<!-- /wp:sensei-lms/button-next-lesson -->
-
-<!-- wp:sensei-lms/button-reset-lesson {"inContainer":true} -->
-<div class="wp-block-sensei-lms-button-reset-lesson is-style-outline sensei-buttons-container__button-block wp-block-sensei-lms-button-reset-lesson__wrapper"><div class="wp-block-sensei-lms-button-reset-lesson is-style-outline wp-block-sensei-button wp-block-button has-text-align-left"><button class="wp-block-button__link sensei-stop-double-submission"><?php esc_html_e( 'Reset Lesson', 'sensei-lms' ); ?></button></div></div>
-<!-- /wp:sensei-lms/button-reset-lesson --></div></div>
-<!-- /wp:sensei-lms/lesson-actions -->
-
 <!-- wp:sensei-lms/quiz {"options":{"passRequired":false,"quizPassmark":0,"autoGrade":false,"allowRetakes":false,"showQuestions":null,"randomQuestionOrder":false,"failedIndicateIncorrect":true,"failedShowCorrectAnswers":true,"failedShowAnswerFeedback":true,"buttonTextColor":null,"buttonBackgroundColor":null,"pagination":{"paginationNumber":null,"showProgressBar":false,"progressBarRadius":6,"progressBarHeight":12,"progressBarColor":null,"progressBarBackground":null},"enableQuizTimer":"","timerValue":null}} -->
 <!-- wp:sensei-lms/quiz-question {"id":1630,"title":"<?php esc_html_e( 'Question 1', 'sensei-lms' ); ?>","answer":{"answers":[{"label":"<?php esc_html_e( 'Yes', 'sensei-lms' ); ?>","correct":true},{"label":"<?php esc_html_e( 'No', 'sensei-lms' ); ?>","correct":false}]},"options":{"grade":1,"answerFeedback":null,"randomOrder":false}} -->
 
@@ -56,3 +38,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 <!-- /wp:sensei-lms/quiz-question-feedback-incorrect -->
 <!-- /wp:sensei-lms/quiz-question -->
 <!-- /wp:sensei-lms/quiz -->
+
+<!-- wp:sensei-lms/lesson-actions -->
+<div class="wp-block-sensei-lms-lesson-actions"><div class="sensei-buttons-container"><!-- wp:sensei-lms/button-view-quiz {"inContainer":true} -->
+		<div class="wp-block-sensei-lms-button-view-quiz is-style-default sensei-buttons-container__button-block wp-block-sensei-lms-button-view-quiz__wrapper"><div class="wp-block-sensei-lms-button-view-quiz is-style-default wp-block-sensei-button wp-block-button has-text-align-left"><button class="wp-block-button__link"><?php esc_html_e( 'View Quiz', 'sensei-lms' ); ?></button></div></div>
+		<!-- /wp:sensei-lms/button-view-quiz -->
+
+		<!-- wp:sensei-lms/button-complete-lesson {"inContainer":true} -->
+		<div class="wp-block-sensei-lms-button-complete-lesson is-style-default sensei-buttons-container__button-block wp-block-sensei-lms-button-complete-lesson__wrapper"><div class="wp-block-sensei-lms-button-complete-lesson is-style-default wp-block-sensei-button wp-block-button has-text-align-left"><button class="wp-block-button__link sensei-stop-double-submission"><?php esc_html_e( 'Complete Lesson', 'sensei-lms' ); ?></button></div></div>
+		<!-- /wp:sensei-lms/button-complete-lesson -->
+
+		<!-- wp:sensei-lms/button-next-lesson {"inContainer":true} -->
+		<div class="wp-block-sensei-lms-button-next-lesson is-style-default sensei-buttons-container__button-block wp-block-sensei-lms-button-next-lesson__wrapper"><div class="wp-block-sensei-lms-button-next-lesson is-style-default wp-block-sensei-button wp-block-button has-text-align-left"><button class="wp-block-button__link"><?php esc_html_e( 'Next Lesson', 'sensei-lms' ); ?></button></div></div>
+		<!-- /wp:sensei-lms/button-next-lesson -->
+
+		<!-- wp:sensei-lms/button-reset-lesson {"inContainer":true} -->
+		<div class="wp-block-sensei-lms-button-reset-lesson is-style-outline sensei-buttons-container__button-block wp-block-sensei-lms-button-reset-lesson__wrapper"><div class="wp-block-sensei-lms-button-reset-lesson is-style-outline wp-block-sensei-button wp-block-button has-text-align-left"><button class="wp-block-button__link sensei-stop-double-submission"><?php esc_html_e( 'Reset Lesson', 'sensei-lms' ); ?></button></div></div>
+		<!-- /wp:sensei-lms/button-reset-lesson --></div></div>
+<!-- /wp:sensei-lms/lesson-actions -->
