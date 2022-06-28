0eba3c1752df11550159d9587e7e98501fd3ed57
Aarón Fas <aaron.fas@automattic.com>
2022-06-28 07:49:11 +0200

    Disable caching for enroled courses query.
    Caching also makes terms cache to be calculated and saved, which is a
    heavy task when a high amount of users are enroled.

--- includes/class-sensei-learner.php
+++ includes/class-sensei-learner.php
@@ -377,6 +377,8 @@ class Sensei_Learner {
 			'order'       => $order,
 			'orderby'     => $orderby,
 			'tax_query'   => [], // phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_tax_query -- Just empty to set array.
+			'lazy_load_term_meta' => false,
+			'cache_results' => false,
 		];
 
 		$query_args   = array_merge( $default_args, $base_query_args );
