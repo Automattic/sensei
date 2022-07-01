c5a8eb365bfeea862fe7b52aac486ee32947bb9d
Renatho De Carli Rosa <renatho@gmail.com>
2022-07-01 15:43:07 -0300

    Refactor to avoid running functions when serverStructure and questions are not ready

--- assets/blocks/quiz/quiz-store.js
+++ assets/blocks/quiz/quiz-store.js
@@ -118,29 +118,38 @@ registerStructureStore( {
 			snakeCase
 		);
 
+		const lesson = select( 'core/editor' ).getCurrentPost();
+		const serverStructure = select( QUIZ_STORE ).getServerStructure();
+
 		const questionBlocks = select( 'core/block-editor' ).getBlocks(
 			clientId
 		);
 
-		const questionBlockAttributes = parseQuestionBlocks( questionBlocks );
+		let questions = [];
 
-		const serverQuestionsById = keyBy(
-			select( QUIZ_STORE ).getServerStructure().questions,
-			'id'
-		);
+		if ( 0 < questionBlocks.length && serverStructure ) {
+			const questionBlockAttributes = parseQuestionBlocks(
+				questionBlocks
+			);
 
-		const lesson = select( 'core/editor' ).getCurrentPost();
+			const serverQuestionsById = keyBy(
+				serverStructure.questions,
+				'id'
+			);
+
+			questions = questionBlockAttributes.map( ( question ) =>
+				// Avoid overriding non-editable question.
+				false === question.editable
+					? serverQuestionsById[ question.id ]
+					: omit( question, READ_ONLY_ATTRIBUTES )
+			);
+		}
 
 		return {
 			lesson_status: lesson?.status,
 			lesson_title: lesson?.title,
 			options,
-			questions: questionBlockAttributes.map( ( question ) =>
-				// Avoid overriding non-editable question.
-				false === question.editable
-					? serverQuestionsById[ question.id ]
-					: omit( question, READ_ONLY_ATTRIBUTES )
-			),
+			questions,
 		};
 	},
 
