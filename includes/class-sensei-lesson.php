bc9074bb7a65c9e161f14b9f1799ad02562c5083
Fernando Jorge Mota <contato@fjorgemota.com>
2022-07-04 11:26:22 -0300

    Make sure that the lesson IDs array has only unique items

--- includes/class-sensei-lesson.php
+++ includes/class-sensei-lesson.php
@@ -3834,6 +3834,7 @@ class Sensei_Lesson {
 	public function get_course_ids( $lesson_ids ) {
 		global $wpdb;
 
+		$lesson_ids = array_unique( $lesson_ids );
 		sort( $lesson_ids, SORT_NUMERIC );
 
 		$cache_key     = 'lesson/get-course-ids/' . md5( implode( ',', $lesson_ids ) );
