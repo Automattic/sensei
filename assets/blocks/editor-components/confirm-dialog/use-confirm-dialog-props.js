743e5fd0f48a71ca18fa94b1c3b45be82f21ef96
Fernando Jorge Mota <contato@fjorgemota.com>
2022-07-15 17:13:32 -0300

    Add hook useConfirmDialogProps to help using ConfirmDialog component appropriately

---
+++ assets/blocks/editor-components/confirm-dialog/use-confirm-dialog-props.js
@@ -0,0 +1,34 @@
+/**
+ * WordPress dependencies
+ */
+import { useState } from '@wordpress/element';
+
+/**
+ * Hook that returns the props for the component ConfirmDialog, with an additional async function that mimics the
+ * synchronous native confirm() API. Loosely inspired by react-confirm HOC.
+ *
+ * @see https://github.com/haradakunihiko/react-confirm
+ * @return {Array} The first item is the props to pass to ConfirmDialog, the second one is the async function to call to
+ * 					trigger ConfirmDialog.
+ */
+export const useConfirmDialogProps = () => {
+	const [ props, setProps ] = useState( { isOpen: false } );
+	const confirm = ( text, title ) => {
+		return new Promise( ( resolve ) => {
+			const callback = ( value ) => () => {
+				resolve( value );
+				setProps( { isOpen: false } );
+			};
+			setProps( {
+				isOpen: true,
+				children: text,
+				title,
+				onConfirm: callback( true ),
+				onCancel: callback( false ),
+			} );
+		} );
+	};
+	return [ props, confirm ];
+};
+
+export default useConfirmDialogProps;
