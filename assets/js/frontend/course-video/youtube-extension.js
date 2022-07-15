167987856dee6a2157ceae5190f9443d5fd74f62
Renatho De Carli Rosa <renatho@gmail.com>
2022-07-15 17:33:57 -0300

    Update to support multiple instances of the same player

--- assets/js/frontend/course-video/youtube-extension.js
+++ assets/js/frontend/course-video/youtube-extension.js
@@ -10,25 +10,32 @@ import { registerVideo } from './video-blocks-manager';
  */
 const initYouTubePlayer = ( iframe ) => {
 	let onVideoEnd = () => {};
-	const player = new YT.Player( iframe, {
-		events: {
-			onStateChange: ( event ) => {
-				const playerStatus = event.data;
-				if ( playerStatus === YT.PlayerState.ENDED ) {
-					onVideoEnd();
-				}
+
+	const player = YT.get( iframe.id ) || new YT.Player( iframe );
+
+	const onReady = () => {
+		registerVideo( {
+			pauseVideo: player.pauseVideo.bind( player ),
+			registerVideoEndHandler: ( cb ) => {
+				onVideoEnd = cb;
 			},
-			onReady: () => {
-				registerVideo( {
-					pauseVideo: player.pauseVideo.bind( player ),
-					registerVideoEndHandler: ( cb ) => {
-						onVideoEnd = cb;
-					},
-					url: player.getVideoUrl(),
-					blockElement: iframe.closest( 'figure' ),
-				} );
-			},
-		},
+			url: player.getVideoUrl(),
+			blockElement: iframe.closest( 'figure' ),
+		} );
+	};
+
+	if ( player.getDuration ) {
+		// Just in case it's called after the player is ready.
+		onReady();
+	} else {
+		player.addEventListener( 'onReady', onReady );
+	}
+
+	player.addEventListener( 'onStateChange', ( event ) => {
+		const playerStatus = event.data;
+		if ( playerStatus === YT.PlayerState.ENDED ) {
+			onVideoEnd();
+		}
 	} );
 };
 
