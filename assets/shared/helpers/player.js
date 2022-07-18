15f3b46d1f30330093b229544ee2b4f216042212
Renatho De Carli Rosa <renatho@gmail.com>
2022-07-18 20:16:11 -0300

    Add documentation and improve promises

--- assets/shared/helpers/player.js
+++ assets/shared/helpers/player.js
@@ -5,6 +5,13 @@ const VIMEO_TYPE = 'vimeo';
 
 const players = {
 	[ VIDEO_TYPE ]: {
+		/**
+		 * Initialize the player.
+		 *
+		 * @param {HTMLVideoElement} element The player element.
+		 *
+		 * @return {Promise<HTMLVideoElement>} The video player through a promise.
+		 */
 		initializePlayer: ( element ) => {
 			// Return that it's ready when it can get the video duration.
 			if ( ! isNaN( element.duration ) ) {
@@ -17,20 +24,80 @@ const players = {
 				} );
 			} );
 		},
+
+		/**
+		 * Get the video duration.
+		 *
+		 * @param {HTMLVideoElement} player The player element.
+		 *
+		 * @return {Promise<number>} The duration of the video in seconds through a promise.
+		 */
 		getDuration: ( player ) => Promise.resolve( player.duration ),
+
+		/**
+		 * Set the video to a current time.
+		 *
+		 * @param {HTMLVideoElement} player  The player element.
+		 * @param {number}           seconds The video time in seconds to set.
+		 *
+		 * @return {Promise} A promise that resolves if the video was set to a current time successfully.
+		 */
 		setCurrentTime: ( player, seconds ) => {
-			player.currentTime = seconds;
+			try {
+				player.currentTime = seconds;
+				return Promise.resolve();
+			} catch ( e ) {
+				return Promise.reject( e );
+			}
 		},
-		play: ( player ) => {
-			player.play();
-		},
+
+		/**
+		 * Play the video.
+		 *
+		 * @param {HTMLVideoElement} player The player element.
+		 *
+		 * @return {Promise} The native promise from the video play function.
+		 */
+		play: ( player ) => player.play(),
+
+		/**
+		 * Pause the video.
+		 *
+		 * @param {HTMLVideoElement} player The player element.
+		 *
+		 * @return {Promise} A promise that resolves if the video was paused successfully.
+		 */
 		pause: ( player ) => {
-			player.pause();
+			try {
+				player.pause();
+				return Promise.resolve();
+			} catch ( e ) {
+				return Promise.reject( e );
+			}
 		},
 	},
 	[ VIDEOPRESS_TYPE ]: {
+		/**
+		 * The embed pattern to check if it's the respective type.
+		 */
 		embedPattern: /(videopress|video\.wordpress)\.com\/.+/i,
+
+		/**
+		 * Initialize the player.
+		 *
+		 * @param {HTMLIFrameElement} element The player element.
+		 *
+		 * @return {Promise<HTMLIFrameElement>} The video player through a promise.
+		 */
 		initializePlayer: ( element ) => Promise.resolve( element ),
+
+		/**
+		 * Get the video duration.
+		 *
+		 * @param {HTMLIFrameElement} player The player element.
+		 *
+		 * @return {Promise<number>} The duration of the video in seconds through a promise.
+		 */
 		getDuration: ( player ) =>
 			new Promise( ( resolve ) =>
 				// eslint-disable-next-line @wordpress/no-global-event-listener -- Not in a React context.
@@ -46,30 +113,81 @@ const players = {
 					resolve( event.data.durationMs / 1000 );
 				} )
 			),
+
+		/**
+		 * Set the video to a current time.
+		 *
+		 * @param {HTMLIFrameElement} player  The player element.
+		 * @param {number}            seconds The video time in seconds to set.
+		 *
+		 * @return {Promise} A promise that resolves if the video was set to a current time successfully.
+		 */
 		setCurrentTime: ( player, seconds ) => {
-			player.contentWindow.postMessage(
-				{
-					event: 'videopress_action_set_currenttime',
-					currentTime: seconds,
-				},
-				'*'
-			);
+			try {
+				player.contentWindow.postMessage(
+					{
+						event: 'videopress_action_set_currenttime',
+						currentTime: seconds,
+					},
+					'*'
+				);
+				return Promise.resolve();
+			} catch ( e ) {
+				return Promise.reject( e );
+			}
 		},
+
+		/**
+		 * Play the video.
+		 *
+		 * @param {HTMLIFrameElement} player The player element.
+		 *
+		 * @return {Promise} A promise that resolves if the video was played successfully.
+		 */
 		play: ( player ) => {
-			player.contentWindow.postMessage(
-				{ event: 'videopress_action_play' },
-				'*'
-			);
+			try {
+				player.contentWindow.postMessage(
+					{ event: 'videopress_action_play' },
+					'*'
+				);
+				return Promise.resolve();
+			} catch ( e ) {
+				return Promise.reject( e );
+			}
 		},
+
+		/**
+		 * Pause the video.
+		 *
+		 * @param {HTMLIFrameElement} player The player element.
+		 *
+		 * @return {Promise} A promise that resolves if the video was paused successfully.
+		 */
 		pause: ( player ) => {
-			player.contentWindow.postMessage(
-				{ event: 'videopress_action_pause' },
-				'*'
-			);
+			try {
+				player.contentWindow.postMessage(
+					{ event: 'videopress_action_pause' },
+					'*'
+				);
+				return Promise.resolve();
+			} catch ( e ) {
+				return Promise.reject( e );
+			}
 		},
 	},
 	[ YOUTUBE_TYPE ]: {
+		/**
+		 * The embed pattern to check if it's the respective type.
+		 */
 		embedPattern: /(youtu\.be|youtube\.com)\/.+/i,
+
+		/**
+		 * Initialize the player.
+		 *
+		 * @param {HTMLIFrameElement} element The player element.
+		 *
+		 * @return {Object} The YouTube player instance through a promise.
+		 */
 		initializePlayer: ( element ) =>
 			new Promise( ( resolve ) => {
 				window.senseiYouTubeIframeAPIReady.then( () => {
@@ -89,35 +207,118 @@ const players = {
 					}
 				} );
 			} ),
-		getDuration: ( player ) => player.getDuration(),
+
+		/**
+		 * Get the video duration.
+		 *
+		 * @param {Object} player The YouTube player instance.
+		 *
+		 * @return {Promise<number>} The duration of the video in seconds through a promise.
+		 */
+		getDuration: ( player ) => Promise.resolve( player.getDuration() ),
+
+		/**
+		 * Set the video to a current time.
+		 *
+		 * @param {Object} player  The YouTube player instance.
+		 * @param {number} seconds The video time in seconds to set.
+		 *
+		 * @return {Promise} A resolved promise.
+		 */
 		setCurrentTime: ( player, seconds ) => {
 			player.seekTo( seconds );
+			return Promise.resolve();
 		},
+
+		/**
+		 * Play the video.
+		 *
+		 * @param {Object} player The YouTube player instance.
+		 *
+		 * @return {Promise} A resolved promise.
+		 */
 		play: ( player ) => {
 			player.playVideo();
+			return Promise.resolve();
 		},
+
+		/**
+		 * Pause the video.
+		 *
+		 * @param {Object} player The YouTube player instance.
+		 *
+		 * @return {Promise} A resolved promise.
+		 */
 		pause: ( player ) => {
 			player.pauseVideo();
+			return Promise.resolve();
 		},
 	},
 	[ VIMEO_TYPE ]: {
+		/**
+		 * The embed pattern to check if it's the respective type.
+		 */
 		embedPattern: /vimeo\.com\/.+/i,
+
+		/**
+		 * Initialize the player.
+		 *
+		 * @param {HTMLIFrameElement} element The player element.
+		 *
+		 * @return {Object} The Vimeo player instance through a promise.
+		 */
 		initializePlayer: ( element ) =>
 			Promise.resolve( new window.Vimeo.Player( element ) ),
+
+		/**
+		 * Get the video duration.
+		 *
+		 * @param {Object} player The Vimeo player instance.
+		 *
+		 * @return {Promise<number>} The duration of the video in seconds through a promise
+		 *                           (original return from Vimeo API).
+		 */
 		getDuration: ( player ) => player.getDuration(),
-		setCurrentTime: ( player, seconds ) => {
-			player.setCurrentTime( seconds );
-		},
-		play: ( player ) => {
-			player.play();
-		},
-		pause: ( player ) => {
-			player.pause();
-		},
+
+		/**
+		 * Set the video to a current time.
+		 *
+		 * @param {Object} player  The Vimeo player instance.
+		 * @param {number} seconds The video time in seconds to set.
+		 *
+		 * @return {Promise} A promise that resolves if the video was set to a current time successfully.
+		 *                   (original return from Vimeo API).
+		 */
+		setCurrentTime: ( player, seconds ) => player.setCurrentTime( seconds ),
+
+		/**
+		 * Play the video.
+		 *
+		 * @param {Object} player The Vimeo player instance.
+		 *
+		 * @return {Promise} A promise that resolves if the video was played successfully.
+		 *                   (original return from Vimeo API).
+		 */
+		play: ( player ) => player.play(),
+
+		/**
+		 * Pause the video.
+		 *
+		 * @param {Object} player The Vimeo player instance.
+		 *
+		 * @return {Promise} A promise that resolves if the video was paused successfully.
+		 *                   (original return from Vimeo API).
+		 */
+		pause: ( player ) => player.pause(),
 	},
 };
 
 class Player {
+	/**
+	 * Player constructor.
+	 *
+	 * @param {HTMLVideoElement|HTMLIFrameElement} element The player element.
+	 */
 	constructor( element ) {
 		this.playerPromise = null;
 		this.type = null;
@@ -131,6 +332,11 @@ class Player {
 		}
 	}
 
+	/**
+	 * Set the player type.
+	 *
+	 * @throws Will throw an error if the video type is not found.
+	 */
 	setType() {
 		if ( this.element instanceof window.HTMLVideoElement ) {
 			this.type = VIDEO_TYPE;
@@ -146,35 +352,62 @@ class Player {
 		}
 	}
 
+	/**
+	 * Get the video player.
+	 *
+	 * @return {Promise<Object|HTMLVideoElement|HTMLIFrameElement>} The video player through a promise.
+	 */
 	getPlayer() {
 		if ( ! this.playerPromise ) {
 			this.playerPromise =
 				players[ this.type ]?.initializePlayer( this.element ) ||
 				// A promise that never resolves if it doesn't exist.
-				new Promise( () => {} );
+				Promise.reject( new Error( 'Failed getting the player' ) );
 		}
 
 		return this.playerPromise;
 	}
 
+	/**
+	 * Get the video duration.
+	 *
+	 * @return {Promise<number>} The duration of the video in seconds through a promise.
+	 */
 	getDuration() {
 		return this.getPlayer().then( ( player ) =>
 			players[ this.type ].getDuration( player )
 		);
 	}
 
+	/**
+	 * Set the video to a current time.
+	 *
+	 * @param {number} seconds The video time in seconds to set.
+	 *
+	 * @return {Promise} A promise that resolves if the video was set to a current time successfully.
+	 */
 	setCurrentTime( seconds ) {
 		return this.getPlayer().then( ( player ) =>
 			players[ this.type ].setCurrentTime( player, seconds )
 		);
 	}
 
+	/**
+	 * Play the video.
+	 *
+	 * @return {Promise} A promise that resolves if the video was played successfully.
+	 */
 	play() {
 		return this.getPlayer().then( ( player ) =>
 			players[ this.type ].play( player )
 		);
 	}
 
+	/**
+	 * Pause the video.
+	 *
+	 * @return {Promise} A promise that resolves if the video was paused successfully.
+	 */
 	pause() {
 		return this.getPlayer().then( ( player ) =>
 			players[ this.type ].pause( player )
