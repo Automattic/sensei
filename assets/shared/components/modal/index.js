f0f957f118a26b2d4d140a5f3cc8ed497f01eddf
Aarón Fas <aaron.fas@automattic.com>
2022-07-19 15:23:52 +0200

    Add Modal reusable component.
    - Implementation based on the same concept of sensei-modal used in
      Contact Teacher.
    
    -

---
+++ assets/shared/components/modal/index.js
@@ -0,0 +1,46 @@
+/**
+ * WordPress dependencies
+ */
+import { Icon, close as closeIcon } from '@wordpress/icons';
+
+/**
+ * External dependencies
+ */
+import classNames from 'classnames';
+
+const Modal = ( { isOpen, setIsOpen, children } ) => {
+	const close = ( event ) => {
+		setIsOpen( false );
+		event.preventDefault();
+	};
+
+	return (
+		<div
+			className={ classNames( 'sensei-modal', {
+				'sensei-modal--open': isOpen,
+			} ) }
+		>
+			<button
+				className="sensei-modal__overlay"
+				aria-label="Close"
+				onClick={ close }
+			/>
+			<div className="sensei-modal__wrapper">
+				<div className="sensei-modal__header">
+					<div className="sensei-modal__title">
+						This is the title.
+					</div>
+					<button
+						className="sensei-modal__close-button"
+						onClick={ close }
+					>
+						<Icon icon={ closeIcon } />
+					</button>
+				</div>
+				<div className="sensei-modal__content">{ children }</div>
+			</div>
+		</div>
+	);
+};
+
+export default Modal;
