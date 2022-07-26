ccb8fe13027ee0bad8bf1491f3b903c620405b0b
Aarón Fas <aaron.fas@automattic.com>
2022-07-26 10:56:16 +0200

    Add support to modal close upon ESC key pressed.

--- assets/shared/components/modal/index.js
+++ assets/shared/components/modal/index.js
@@ -2,22 +2,19 @@
  * WordPress dependencies
  */
 import { Icon, close as closeIcon } from '@wordpress/icons';
+import { useEffect } from '@wordpress/element';
 
 /**
  * Modal component.
  *
  * @param {Object}   props
- * @param {boolean}  props.isOpen      Whether the modal should be open or not.
  * @param {Function} props.handleClose Callback to run when trying to close the modal.
  * @param {string}   props.title       The title for the modal. Empty by default.
  * @param {Object}   props.children    The content of the modal.
  * @return {JSX.Element} The modal component or null (if not open).
  */
-const Modal = ( { isOpen, handleClose, title = '', children } ) => {
-	// Return null if the modal is not open.
-	if ( ! isOpen ) {
-		return null;
-	}
+const Modal = ( { handleClose, title = '', children } ) => {
+	useRunOnEscape( handleClose );
 
 	return (
 		<div className={ 'sensei-modal' }>
@@ -42,4 +39,22 @@ const Modal = ( { isOpen, handleClose, title = '', children } ) => {
 	);
 };
 
+const useRunOnEscape = ( handleClose ) => {
+	useEffect( () => {
+		const handleEsc = ( event ) => {
+			if ( event.keyCode === 27 ) {
+				handleClose( event );
+			}
+		};
+		// Attach close event on Escape key.
+		// eslint-disable-next-line @wordpress/no-global-event-listener
+		window.addEventListener( 'keydown', handleEsc );
+
+		return () => {
+			// Detach from keydown on component unmounting.
+			// eslint-disable-next-line @wordpress/no-global-event-listener
+			window.removeEventListener( 'keydown', handleEsc );
+		};
+	}, [ handleClose ] );
+};
 export default Modal;
