295694cfa8fb7a7098549c56c967430f00eb39b3
Imran Hossain <imranh920@gmail.com>
2022-08-06 05:55:48 +0600

    Add course list block variation in page editor

--- assets/blocks/single-page.js
+++ assets/blocks/single-page.js
@@ -6,8 +6,12 @@ import CourseResultsBlock from './course-results-block';
 import LearnerCoursesBlock from './learner-courses-block';
 import LearnerMessagesButtonBlock from './learner-messages-button-block';
 import { registerCourseCompletedActionsBlock } from './course-completed-actions';
+import { registerCourseListBlock } from './course-list-block';
 
 registerCourseCompletedActionsBlock();
+
+registerCourseListBlock();
+
 registerSenseiBlocks( [
 	CourseResultsBlock,
 	LearnerCoursesBlock,
