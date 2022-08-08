86c06b71d6d43a2c08890d7037cb9ef92604e274
Dmitry Merkushin <merkushin@gmail.com>
2022-08-08 16:57:05 +0300

    Don't use typed properties syntax from PHP 7.4

--- includes/student-progress/repository/class-sensei-course-progress-repository-aggregate.php
+++ includes/student-progress/repository/class-sensei-course-progress-repository-aggregate.php
@@ -2,16 +2,17 @@
 
 class Sensei_Course_Progress_Repository_Aggregate implements Sensei_Course_Progress_Repository_Interface {
 
-	private Sensei_Course_Progress_Repository_Interface $repository_tables;
-	private Sensei_Course_Progress_Repository_Interface $repository_comments;
-	private bool $use_tables;
+	private $repository_tables;
+	private $repository_comments;
+	private $use_tables;
 	public function __construct(
 		Sensei_Course_Progress_Repository_Tables $tables,
 		Sensei_Course_Progress_Repository_Comments $comments,
 		bool $use_tables = true
 	) {
-		$this->repository_tables = $tables;
+		$this->repository_tables   = $tables;
 		$this->repository_comments = $comments;
+		$this->use_tables          = $use_tables;
 	}
 
 	public function create( int $course_id, int $user_id ): Sensei_Course_Progress_Interface {
@@ -19,23 +20,23 @@ class Sensei_Course_Progress_Repository_Aggregate implements Sensei_Course_Progr
 		return new Sensei_Course_Progress( $course_id, $user_id );
 	}
 	public function get( int $course_id, int $user_id ): ?Sensei_Course_Progress_Interface {
-		if ($this->use_tables) {
-			return $this->repository_tables->get($course_id, $user_id);
+		if ( $this->use_tables ) {
+			return $this->repository_tables->get( $course_id, $user_id );
 		} else {
-			return $this->repository_comments->get($course_id, $user_id);
+			return $this->repository_comments->get( $course_id, $user_id );
 		}
 	}
 	public function has( int $course_id, int $user_id ): bool {
-		if ($this->use_tables) {
-			return $this->repository_tables->has($course_id, $user_id);
+		if ( $this->use_tables ) {
+			return $this->repository_tables->has( $course_id, $user_id );
 		} else {
-			return $this->repository_comments->has($course_id, $user_id);
+			return $this->repository_comments->has( $course_id, $user_id );
 		}
 	}
 	public function save( Sensei_Course_Progress_Interface $course_progress ): void {
-		$this->repository_comments->save($course_progress);
-		if ($this->use_tables) {
-			$this->repository_tables->save($course_progress);
+		$this->repository_comments->save( $course_progress );
+		if ( $this->use_tables ) {
+			$this->repository_tables->save( $course_progress );
 		}
 	}
 }
