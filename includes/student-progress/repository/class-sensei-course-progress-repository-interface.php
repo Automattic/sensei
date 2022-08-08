9618263b76fc1cea0cf2238f04334d422eec959a
Dmitry Merkushin <merkushin@gmail.com>
2022-08-08 16:54:47 +0300

    Fix course progress repository interface and add aggregate example

--- includes/student-progress/repository/class-sensei-course-progress-repository-interface.php
+++ includes/student-progress/repository/class-sensei-course-progress-repository-interface.php
@@ -1,8 +1,8 @@
 <?php
 
-interface Sensei_Course_Progress_Interface {
+interface Sensei_Course_Progress_Repository_Interface {
 	public function create( int $course_id, int $user_id ): Sensei_Course_Progress_Interface;
-	public function get( int $course_id, int $user_id ): Sensei_Course_Progress_Interface;
+	public function get( int $course_id, int $user_id ): ?Sensei_Course_Progress_Interface;
 	public function has( int $course_id, int $user_id ): bool;
 	public function save( Sensei_Course_Progress_Interface $course_progress ): void;
 }
