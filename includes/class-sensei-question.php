2cb14a36a3f92a9eb17d891c7da9365f0b31d752
Peter Kiss <peter@yscik.com>
2022-08-10 11:00:25 +0200

    Add classic editor support for questions

--- includes/class-sensei-question.php
+++ includes/class-sensei-question.php
@@ -225,8 +225,23 @@ class Sensei_Question {
 			if ( ! Sensei()->quiz->is_block_based_editor_enabled() ) {
 				add_meta_box( 'multiple-question-lessons-panel', __( 'Quizzes', 'sensei-lms' ), array( $this, 'question_lessons_panel' ), 'multiple_question', 'side', 'default' );
 				add_meta_box( 'question-edit-panel', $metabox_title, array( $this, 'question_edit_panel' ), 'question', 'normal', 'high' );
+				add_filter( 'sensei_scripts_allowed_post_types', [ $this, 'load_lesson_edit_script' ] );
 			}
 		}
+	}
+
+	/**
+	 * Also load the lesson metabox scripts for the question post type when using the legacy editor.
+	 *
+	 * @access private
+	 *
+	 * @param array $post_types
+	 *
+	 * @return array
+	 */
+	public function load_lesson_edit_script( $post_types ) {
+		$post_types[] = 'question';
+		return $post_types;
 	}
 
 	public function question_edit_panel() {
--- includes/class-sensei-quiz.php
+++ includes/class-sensei-quiz.php
@@ -76,13 +76,13 @@ class Sensei_Quiz {
 	 * @return bool
 	 */
 	public function is_block_based_editor_enabled() {
-		if ( ! function_exists( 'is_plugin_active' ) ) {
-			include_once ABSPATH . 'wp-admin/includes/plugin.php';
-		}
+
+		$current_screen = function_exists('get_current_screen') ? get_current_screen() : null;
 
 		// If custom question types have been registered, or the Classic Editor plugin is activated,
 		// disable the block based quiz editor for now.
-		$is_block_based_editor_enabled = ! has_filter( 'sensei_question_types' ) && ! is_plugin_active( 'classic-editor/classic-editor.php' );
+		$is_block_based_editor_enabled = ! has_filter( 'sensei_question_types' ) && $current_screen && $current_screen->is_block_editor();
+
 
 		/**
 		 * Filter to change whether the block based editor should be used instead of the legacy
