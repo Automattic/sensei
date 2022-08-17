c40e02fd3efed35c8cfe1fc53db85076b4fb0f4d
Imran Hossain <imranh920@gmail.com>
2022-08-17 06:21:02 +0600

    Remove the globally available blocks from pages and courses

--- assets/blocks/single-page.js
+++ assets/blocks/single-page.js
@@ -2,13 +2,8 @@
  * Internal dependencies
  */
 import registerSenseiBlocks from './register-sensei-blocks';
-import CourseProgressBlock from './course-progress-block';
-import { ContinueCourse, CourseActions } from './course-actions-block';
-import CourseResultsBlock from './course-results-block';
 import LearnerCoursesBlock from './learner-courses-block';
 import LearnerMessagesButtonBlock from './learner-messages-button-block';
-import TakeCourseBlock from './take-course-block';
-import ViewResultsBlock from './view-results-block';
 import { registerCourseCompletedActionsBlock } from './course-completed-actions';
 import { registerCourseListBlock } from './course-list-block';
 
@@ -16,13 +11,4 @@ registerCourseCompletedActionsBlock();
 
 registerCourseListBlock();
 
-registerSenseiBlocks( [
-	ContinueCourse,
-	CourseActions,
-	CourseProgressBlock,
-	CourseResultsBlock,
-	LearnerCoursesBlock,
-	LearnerMessagesButtonBlock,
-	TakeCourseBlock,
-	ViewResultsBlock,
-] );
+registerSenseiBlocks( [ LearnerCoursesBlock, LearnerMessagesButtonBlock ] );
