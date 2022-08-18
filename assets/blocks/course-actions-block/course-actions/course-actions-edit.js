5f5f0ccb894c356427d0a89961c549c55b7e2481
Donna Peplinskie <donnapep@gmail.com>
2022-08-18 13:31:19 -0400

    Show editor warning for incorrect usage of course actions block

--- assets/blocks/course-actions-block/course-actions/course-actions-edit.js
+++ assets/blocks/course-actions-block/course-actions/course-actions-edit.js
@@ -4,6 +4,11 @@
 import { __ } from '@wordpress/i18n';
 import { InnerBlocks } from '@wordpress/block-editor';
 
+/**
+ * Internal dependencies
+ */
+import InvalidUsageError from '../../../shared/components/invalid-usage';
+
 const innerBlocksTemplate = [
 	[
 		'sensei-lms/button-take-course',
@@ -25,18 +30,28 @@ const innerBlocksTemplate = [
  * @param {Object} props
  * @param {Object} props.className Block className.
  */
-const CourseActionsEdit = ( { className } ) => (
-	<div className={ className }>
-		<InnerBlocks
-			allowedBlocks={ [
-				'sensei-lms/button-take-course',
-				'sensei-lms/button-continue-course',
-				'sensei-lms/button-view-results',
-			] }
-			template={ innerBlocksTemplate }
-			templateLock="all"
-		/>
-	</div>
-);
+const CourseActionsEdit = ( { className, context: { postType } } ) => {
+	if ( 'course' !== postType ) {
+		return (
+			<InvalidUsageError
+				message={ __( 'The Course Actions block can only be used inside the Course List block.', 'sensei-lms' ) }
+			/>
+		);
+	}
+
+	return (
+		<div className={ className }>
+			<InnerBlocks
+				allowedBlocks={ [
+					'sensei-lms/button-take-course',
+					'sensei-lms/button-continue-course',
+					'sensei-lms/button-view-results',
+				] }
+				template={ innerBlocksTemplate }
+				templateLock="all"
+			/>
+		</div>
+	);
+}
 
 export default CourseActionsEdit;
