eef49cc9c49579f835f5e794185a5d832aa862be
Gabriel Caires <gabriel.caires@automattic.com>
2022-09-06 14:53:31 -0300

    Add tests to the continue course block

--- tests/unit-tests/blocks/test-class-sensei-continue-course-block.php
+++ tests/unit-tests/blocks/test-class-sensei-continue-course-block.php
@@ -54,46 +54,81 @@ class Sensei_Continue_Course_Block_Test extends WP_UnitTestCase {
 	 *
 	 * @covers Sensei_Continue_Course_Block::render
 	 */
-	public function testRender_NotEnrolled_ReturnsEmptyString() {
-		$this->login_as_student();
+	 public function testRender_NotEnrolled_ReturnsEmptyString() {
+	 	$this->login_as_student();
 
-		$result = $this->block->render( [], self::CONTENT );
+	 	$result = $this->block->render( [], self::CONTENT );
 
-		$this->assertEmpty( $result );
-	}
+	 	$this->assertEmpty( $result );
+	 }
 
-	/**
-	 * Doesn't render the block if the student is enrolled and has already completed the course.
-	 *
-	 * @covers Sensei_Continue_Course_Block::render
-	 */
-	public function testRender_EnrolledAndCourseCompleted_ReturnsEmptyString() {
-		$user_id = $this->factory->user->create();
-		$this->manuallyEnrolStudentInCourse( $user_id, $this->course->ID );
-		Sensei_Utils::update_course_status( $user_id, $this->course->ID, 'complete' );
+	 /**
+	  * Doesn't render the block if the student is enrolled and has already completed the course.
+	  *
+	  * @covers Sensei_Continue_Course_Block::render
+	  */
+	 public function testRender_EnrolledAndCourseCompleted_ReturnsEmptyString() {
+	 	$user_id = $this->factory->user->create();
+	 	$this->manuallyEnrolStudentInCourse( $user_id, $this->course->ID );
+	 	Sensei_Utils::update_course_status( $user_id, $this->course->ID, 'complete' );
+	 	$this->login_as( $user_id );
+
+	 	$result = $this->block->render( [], self::CONTENT );
+
+	 	$this->assertEmpty( $result );
+	 }
+
+	 /**
+	  * Renders the block and links to the course page if the student is enrolled but has not completed the course.
+	  *
+	  * @covers Sensei_Continue_Course_Block::render
+	  */
+	 public function testRender_EnrolledAndCourseNotCompleted_ReturnsModifiedBlockContent() {
+	 	$user_id = $this->factory->user->create();
+	 	$this->manuallyEnrolStudentInCourse( $user_id, $this->course->ID );
+	 	$this->login_as( $user_id );
+
+	 	$result = $this->block->render( [], self::CONTENT );
+
+	 	$this->assertRegExp( '|<a href="http://example.org/\?course=continue-course-block".*>Continue</a>|', $result );
+	 }
+
+	 public function testRender_EnrolledAndStartedLesson_ReturnsModifiedBlockContentWithLessonUrl() {
+	 	/* Arrange */
+	 	$user_id           = $this->factory->user->create();
+	 	$course_lesson_ids = $this->factory->lesson->create_many( 2, [ 'meta_input' => [ '_lesson_course' => $this->course->ID ] ] );
+
+	 	$this->login_as( $user_id );
+
+	 	$this->manuallyEnrolStudentInCourse( $user_id, $this->course->ID );
+	 	Sensei_Utils::user_start_lesson( $user_id, $course_lesson_ids[0] );
+
+	 	/* Act */
+	 	$result = $this->block->render( [], self::CONTENT );
+
+	 	/* Assert */
+	 	$lesson_title = get_post( $course_lesson_ids[0] )->post_name;
+	 	$this->assertRegExp( '|<a href="http://example.org/\?lesson=' . $lesson_title . '".*>Continue</a>|', $result );
+	 }
+
+
+	public function testRender_WhenTheCourseDoesntHaveALesson_ReturnsLinkToTheCourse() {
+		/* Arrange */
+		$user_id           = $this->factory->user->create();
+
 		$this->login_as( $user_id );
 
-		$result = $this->block->render( [], self::CONTENT );
-
-		$this->assertEmpty( $result );
-	}
-
-	/**
-	 * Renders the block and links to the course page if the student is enrolled but has not completed the course.
-	 *
-	 * @covers Sensei_Continue_Course_Block::render
-	 */
-	public function testRender_EnrolledAndCourseNotCompleted_ReturnsModifiedBlockContent() {
-		$user_id = $this->factory->user->create();
 		$this->manuallyEnrolStudentInCourse( $user_id, $this->course->ID );
-		$this->login_as( $user_id );
 
+		/* Act */
 		$result = $this->block->render( [], self::CONTENT );
 
-		$this->assertRegExp( '|<a href="http://example.org/\?course=continue-course-block".*>Continue</a>|', $result );
+		/* Assert */
+		$course_title = $this->course->post_name;
+		$this->assertRegExp( '|<a href="http://example.org/\?course=' . $course_title . '".*>Continue</a>|', $result );
 	}
 
-	public function testRender_EnrolledAndStartedLesson_ReturnsModifiedBlockContentWithLessonUrl() {
+	public function testRender_WhenTheStudentDoesntHaveStartedALesson_ReturnsLinkToFirstLesson() {
 		/* Arrange */
 		$user_id           = $this->factory->user->create();
 		$course_lesson_ids = $this->factory->lesson->create_many( 2, [ 'meta_input' => [ '_lesson_course' => $this->course->ID ] ] );
@@ -101,13 +136,13 @@ class Sensei_Continue_Course_Block_Test extends WP_UnitTestCase {
 		$this->login_as( $user_id );
 
 		$this->manuallyEnrolStudentInCourse( $user_id, $this->course->ID );
-		Sensei_Utils::user_start_lesson( $user_id, $course_lesson_ids[0] );
+		Sensei_Utils::user_start_lesson( $user_id, $course_lesson_ids[0], true );
 
 		/* Act */
 		$result = $this->block->render( [], self::CONTENT );
 
 		/* Assert */
-		$lesson_title = get_post( $course_lesson_ids[0] )->post_name;
+		$lesson_title = get_post( $course_lesson_ids[1] )->post_name;
 		$this->assertRegExp( '|<a href="http://example.org/\?lesson=' . $lesson_title . '".*>Continue</a>|', $result );
 	}
 }
