a55789205be6bc46cda22acd97373492942a18f4
Irma Jakic <irma.jakic@gmail.com>
2022-09-09 11:11:26 +0200

    Add JSDoc to Featured Label component

--- assets/blocks/course-list-block/featured-label/index.js
+++ assets/blocks/course-list-block/featured-label/index.js
@@ -4,6 +4,13 @@
 import { __ } from '@wordpress/i18n';
 import { useEntityProp } from '@wordpress/core-data';
 
+/**
+ * Featured label wrapper component.
+ *
+ * @param {Object} props
+ * @param {string} props.postId   Course id.
+ * @param {Object} props.children Child component to be wrapped in Feature Label component.
+ */
 const FeaturedLabel = ( { postId, children } ) => {
 	const [ meta ] = useEntityProp( 'postType', 'course', 'meta', postId );
 	const [ media ] = useEntityProp(
