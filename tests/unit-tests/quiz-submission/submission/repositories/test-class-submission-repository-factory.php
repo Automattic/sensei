1c50b9290c0dadf2f7e2fef4d4d26a23c370fadf
Miroslav Mitev <m1r0@users.noreply.github.com>
2022-09-10 00:47:55 +0300

    Add tests for the submission repository factory

---
+++ tests/unit-tests/quiz-submission/submission/repositories/test-class-submission-repository-factory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace SenseiTest\Quiz_Submission\Submission\Repositories;
+
+use Sensei\Quiz_Submission\Submission\Repositories\Submission_Repository_Interface;
+use Sensei\Quiz_Submission\Submission\Repositories\Submission_Repository_Factory;
+
+/**
+ * Class Submission_Repository_Factory_Test
+ *
+ * @covers \Sensei\Quiz_Submission\Submission\Repositories\Submission_Repository_Factory
+ */
+class Submission_Repository_Factory_Test extends \WP_UnitTestCase {
+
+	public function testCreate_WhenCalled_ReturnsSubmissionRepository(): void {
+		/* Arrange. */
+		$factory = new Submission_Repository_Factory();
+
+		/* Act. */
+		$actual = $factory->create();
+
+		/* Assert. */
+		self::assertInstanceOf( Submission_Repository_Interface::class, $actual );
+	}
+
+}
