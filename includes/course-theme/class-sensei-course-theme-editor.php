23bf66dd1a4efe79f44d62be4f0a10b61cfa5a3b
Peter Kiss <peter@yscik.com>
2022-09-14 19:03:54 +0200

    Add Divi workarounds

--- includes/course-theme/class-sensei-course-theme-editor.php
+++ includes/course-theme/class-sensei-course-theme-editor.php
@@ -174,6 +174,7 @@ class Sensei_Course_Theme_Editor {
 		if ( $this->lesson_has_learning_mode() || $this->is_site_editor() ) {
 			Sensei()->assets->enqueue( Sensei_Course_Theme::THEME_NAME . '-blocks', 'course-theme/blocks/index.js', [ 'sensei-shared-blocks' ] );
 			Sensei()->assets->enqueue_style( 'sensei-shared-blocks-editor-style' );
+			Sensei()->assets->enqueue_style( 'sensei-learning-mode-editor' );
 			Sensei_Course_Theme::instance()->enqueue_fonts();
 
 			if ( Sensei_Course_Theme_Option::should_override_theme() ) {
