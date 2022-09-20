cd9d3089458d9ba73796565f613d09719c883c80
Fernando Jorge Mota <contato@fjorgemota.com>
2022-09-20 17:22:34 -0300

    Move ad for Sensei Pro to below the GetHelp section

--- assets/home/main.js
+++ assets/home/main.js
@@ -6,7 +6,6 @@ import { __ } from '@wordpress/i18n';
 import { useSelect } from '@wordpress/data';
 import { EditorNotices } from '@wordpress/editor';
 import { RawHTML } from '@wordpress/element';
-import { applyFilters } from '@wordpress/hooks';
 
 /**
  * Internal dependencies
@@ -54,34 +53,10 @@ const Main = () => {
 		return <div>{ __( 'No extensions found.', 'sensei-lms' ) }</div>;
 	}
 
-	/**
-	 * Filters the featured product display.
-	 *
-	 * @since 4.1.0
-	 *
-	 * @param {boolean} hideFeaturedProduct Whether to hide the extensions featured product.
-	 */
-	const hideFeaturedProduct = applyFilters(
-		'senseiExtensionsFeaturedProductHide',
-		false
-	);
-
-	/**
-	 * Filters the featured product component.
-	 *
-	 * @param {Object} FeaturedProduct Component.
-	 */
-	const FeaturedProduct = applyFilters(
-		'senseiExtensionsFeaturedProduct',
-		FeaturedProductSenseiPro
-	);
-
 	return (
 		<>
 			<Grid as="main" className="sensei-home">
 				<Col className="sensei-home__section" cols={ 12 }>
-					{ ! hideFeaturedProduct && <FeaturedProduct /> }
-
 					<Header />
 
 					{ error !== null && (
@@ -102,9 +77,15 @@ const Main = () => {
 				<Col cols={ 6 }>
 					<GetHelp />
 				</Col>
+
+				<Col cols={ 12 }>
+					<FeaturedProductSenseiPro />
+				</Col>
+
 				<Col cols={ 6 }>
 					<SenseiGuides />
 				</Col>
+
 				<Col cols={ 6 }>
 					<LatestNews />
 				</Col>
