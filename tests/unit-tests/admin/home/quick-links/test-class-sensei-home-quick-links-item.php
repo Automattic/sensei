2ef8b1fdf7ababbea8cb2e8801d3976aec6b4b7b
Aarón Fas <aaron.fas@automattic.com>
2022-09-26 17:16:07 +0200

    Fix typos in tests.

--- tests/unit-tests/admin/home/quick-links/test-class-sensei-home-quick-links-item.php
+++ tests/unit-tests/admin/home/quick-links/test-class-sensei-home-quick-links-item.php
@@ -14,17 +14,16 @@
 class Sensei_Home_Quick_Links_Item_Test extends WP_UnitTestCase {
 
 	/**
-	/**
 	 * Test item properties are correctly set.
 	 */
 	public function testItemKeepsCorrectTitleAndUrl() {
 		$the_title = 'Some random title';
 		$the_url   = 'Some random url';
 
-		$category = new Sensei_Home_Quick_Links_Item( $the_title, $the_url );
+		$item = new Sensei_Home_Quick_Links_Item( $the_title, $the_url );
 
-		$this->assertEquals( $the_title, $category->get_title() );
-		$this->assertEquals( $the_url, $category->get_url() );
+		$this->assertEquals( $the_title, $item->get_title() );
+		$this->assertEquals( $the_url, $item->get_url() );
 	}
 
 }
