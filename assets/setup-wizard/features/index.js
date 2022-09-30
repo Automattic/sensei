71088d7e75d6a8bfad2102c113febfe63913eb4e
Renatho De Carli Rosa <renatho@gmail.com>
2022-09-30 17:33:29 -0300

    Add dynamic actions

--- assets/setup-wizard/features/index.js
+++ assets/setup-wizard/features/index.js
@@ -1,12 +1,58 @@
 /**
  * WordPress dependencies
  */
+import { useMemo } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import apiFetch from '@wordpress/api-fetch';
 
 /**
  * Internal dependencies
  */
 import { useSetupWizardStep } from '../data/use-setup-wizard-step';
+import useActionsNavigator, {
+	actionMinimumTimer,
+} from './use-actions-navigator';
+
+const featureLabels = {
+	woocommerce: __( 'Adding WooCommerce', 'sensei-lms' ),
+	'sensei-certificates': __( 'Adding certificates', 'sensei-lms' ),
+};
+
+const senseiHomePath = '/wp-admin/admin.php?page=sensei-home';
+
+/**
+ * Get actions for the features to be installed.
+ *
+ * @param {Object}   stepData          The features step data.
+ * @param {string[]} stepData.selected Selected features to be installed.
+ * @param {Object[]} stepData.options  Features available to install.
+ *
+ * @return {Object} Actions to install the selected features.
+ */
+const getFeatureActions = ( { selected, options } ) => {
+	// Filter not activated features.
+	const featuresToInstall = selected.filter( ( slug ) =>
+		options.some(
+			( option ) => option.product_slug === slug && ! option.is_activated
+		)
+	);
+
+	return featuresToInstall.map( ( slug ) => ( {
+		label: featureLabels[ slug ],
+		action: () =>
+			new Promise( ( resolve ) => {
+				apiFetch( {
+					path: '/sensei-internal/v1/sensei-extensions/install',
+					method: 'POST',
+					data: {
+						plugin: slug,
+					},
+				} ).then( () => {
+					resolve();
+				} );
+			} ),
+	} ) );
+};
 
 /**
  * Features step for Setup Wizard.
@@ -14,7 +60,28 @@ import { useSetupWizardStep } from '../data/use-setup-wizard-step';
 const Features = () => {
 	const { stepData } = useSetupWizardStep( 'features' );
 
-	const percentage = 30;
+	// Create list of actions.
+	const actions = useMemo(
+		() => [
+			{
+				label: __( 'Preparing your tailored experience', 'sensei-lms' ),
+			},
+			...getFeatureActions( stepData ),
+			{
+				label: __( 'Taking you to your new Sensei Home', 'sensei-lms' ),
+				action: () =>
+					new Promise( ( resolve ) => {
+						setTimeout( () => {
+							window.location.href = senseiHomePath;
+							resolve();
+						}, actionMinimumTimer );
+					} ),
+			},
+		],
+		[ stepData ]
+	);
+
+	const { percentage, label } = useActionsNavigator( actions );
 
 	return (
 		<div className="sensei-setup-wizard__features-step">
@@ -23,7 +90,9 @@ const Features = () => {
 				role="status"
 				aria-live="polite"
 			>
-				{ __( 'Preparing your tailored experience', 'sensei-lms' ) }
+				<div className="sensei-setup-wizard__fade-in" key={ label }>
+					{ label }
+				</div>
 			</div>
 
 			<div className="sensei-setup-wizard__features-progress-bar">
@@ -37,11 +106,6 @@ const Features = () => {
 					className="sensei-setup-wizard__features-progress-bar-filled"
 					style={ { width: `${ percentage }%` } }
 				/>
-			</div>
-
-			<div>
-				To be installed or activated (if not yet):{ ' ' }
-				{ stepData.selected.join( ', ' ) }
 			</div>
 		</div>
 	);
---
+++ assets/setup-wizard/features/use-actions-navigator.js
@@ -0,0 +1,55 @@
+/**
+ * WordPress dependencies
+ */
+import { useEffect, useState } from '@wordpress/element';
+
+// Minimum timer for the actions, to make sure the user will have time to read the texts.
+export const actionMinimumTimer = 1500;
+
+/**
+ * Function that simulates an action, returning a promise with the minimum time.
+ *
+ * @return {Promise} Promise that resolves after a minimum timer.
+ */
+const minimumTimerPromise = () =>
+	new Promise( ( resolve ) => {
+		setTimeout( () => {
+			resolve();
+		}, actionMinimumTimer );
+	} );
+
+/**
+ * Actions navigator hook.
+ *
+ * @param {Array} actions
+ *
+ * @return {{percentage: number, label: string}} Current action data.
+ */
+const useActionsNavigator = ( actions ) => {
+	const [ currentAction, setCurrentAction ] = useState();
+
+	// Navigate through the actions.
+	useEffect( () => {
+		if ( undefined === currentAction ) {
+			setCurrentAction( 0 );
+			return;
+		}
+
+		// Run action.
+		Promise.all( [
+			minimumTimerPromise(),
+			actions[ currentAction ]?.action?.(),
+		] ).then( () => {
+			if ( currentAction + 1 < actions.length ) {
+				setCurrentAction( ( prev ) => prev + 1 );
+			}
+		} );
+	}, [ currentAction, actions ] );
+
+	return {
+		percentage: ( ( currentAction + 1 ) / actions.length ) * 100 || 0,
+		label: actions[ currentAction ]?.label,
+	};
+};
+
+export default useActionsNavigator;
