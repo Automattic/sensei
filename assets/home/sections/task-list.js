8116939e944c77fb16da7d896a9beae6faa73b8d
Renatho De Carli Rosa <renatho@gmail.com>
2022-10-07 18:59:35 -0300

    Add tasks progress bar

--- assets/home/sections/task-list.js
+++ assets/home/sections/task-list.js
@@ -3,12 +3,14 @@
  */
 import Section from '../section';
 import Tasks from '../tasks/tasks';
+import Progress from '../tasks/progress';
 
 /**
  * Task List section component.
  */
 const TaskList = () => (
 	<Section>
+		<Progress totalTasks={ 10 } completedTasks={ 5 } />
 		<Tasks />
 	</Section>
 );
