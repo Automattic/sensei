a47efe9512ed53b891c51b0de91cd4b612616032
Aarón Fas <aaron.fas@automattic.com>
2022-10-10 12:26:09 +0200

    Add extensions provider for backend implementation.

--- includes/rest-api/class-sensei-rest-api-internal.php
+++ includes/rest-api/class-sensei-rest-api-internal.php
@@ -85,6 +85,7 @@ class Sensei_REST_API_Internal {
 		$this->tasks_provider       = new Sensei_Home_Tasks_Provider();
 		$this->news_provider        = new Sensei_Home_News_Provider( $remote_data_api );
 		$this->guides_provider      = new Sensei_Home_Guides_Provider( $remote_data_api );
+		$this->extensions_provider  = new Sensei_Home_Extensions_Provider( $remote_data_api );
 
 		add_action( 'rest_api_init', [ $this, 'register' ] );
 	}
@@ -112,7 +113,8 @@ class Sensei_REST_API_Internal {
 				$this->promo_provider,
 				$this->tasks_provider,
 				$this->news_provider,
-				$this->guides_provider
+				$this->guides_provider,
+				$this->extensions_provider
 			),
 		];
 
