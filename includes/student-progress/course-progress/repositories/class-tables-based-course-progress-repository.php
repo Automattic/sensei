edf16e1ecd221d12e2a4b8d3772376c627be4d2e
Dmitry Merkushin <merkushin@gmail.com>
2022-10-10 20:24:46 +0800

    Add table based repositories for course progress, lesson progress and quiz progress

---
+++ includes/student-progress/course-progress/repositories/class-tables-based-course-progress-repository.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * File containing the class \Sensei\Student_Progress\Course_Progress\Repositories\Tables_Based_Course_Progress_Repository.
+ *
+ * @package sensei
+ */
+
+namespace Sensei\Student_Progress\Course_Progress\Repositories;
+
+use DateTimeImmutable;
+use Sensei\Student_Progress\Course_Progress\Models\Course_Progress;
+use wpdb;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Class Tables_Based_Course_Progress_Repository
+ *
+ * @since $$next-version$$
+ */
+class Tables_Based_Course_Progress_Repository implements Course_Progress_Repository_Interface {
+	/**
+	 * WordPress database object.
+	 *
+	 * @var wpdb
+	 */
+	private $wpdb;
+
+	/**
+	 * Tables_Based_Course_Progress_Repository constructor.
+	 *
+	 * @param wpdb $wpdb WordPress database object.
+	 */
+	public function __construct( wpdb $wpdb ) {
+		$this->wpdb = $wpdb;
+	}
+
+	/**
+	 * Creates a new course progress.
+	 *
+	 * @param int $course_id The course ID.
+	 * @param int $user_id The user ID.
+	 * @return Course_Progress The course progress.
+	 */
+	public function create( int $course_id, int $user_id ): Course_Progress {
+		$current_datetime = current_datetime();
+		$this->wpdb->insert(
+			$this->wpdb->prefix . 'sensei_lms_progress',
+			[
+				'post_id'        => $course_id,
+				'user_id'        => $user_id,
+				'parent_post_id' => null,
+				'type'           => 'course',
+				'status'         => Course_Progress::STATUS_IN_PROGRESS,
+				'started_at'     => $current_datetime->getTimestamp(),
+				'completed_at'   => null,
+				'created_at'     => $current_datetime->getTimestamp(),
+				'updated_at'     => $current_datetime->getTimestamp(),
+			],
+			[
+				'%d',
+				'%d',
+				null,
+				'%s',
+				'%s',
+				'%d',
+				null,
+				'%d',
+				'%d',
+			]
+		);
+		$id = (int) $this->wpdb->insert_id;
+
+		return new Course_Progress(
+			$id,
+			$course_id,
+			$user_id,
+			$current_datetime,
+			Course_Progress::STATUS_IN_PROGRESS,
+			$current_datetime,
+			null,
+			$current_datetime
+		);
+	}
+
+	/**
+	 * Gets a course progress.
+	 *
+	 * @param int $course_id The course ID.
+	 * @param int $user_id The user ID.
+	 * @return Course_Progress|null The course progress or null if it does not exist.
+	 */
+	public function get( int $course_id, int $user_id ): ?Course_Progress {
+		$table_name = $this->wpdb->prefix . 'sensei_lms_progress';
+		$query      = $this->wpdb->prepare(
+			'SELECT * FROM ' . $table_name . ' WHERE post_id = %d AND user_id = %d AND type = %s',
+			$course_id,
+			$user_id,
+			'course'
+		);
+
+		$row = $this->wpdb->get_row( $query );
+		if ( ! $row ) {
+			return null;
+		}
+
+		return new Course_Progress(
+			(int) $row->id,
+			(int) $row->post_id,
+			(int) $row->user_id,
+			new DateTimeImmutable( "@{$row->created_at}", wp_timezone() ),
+			$row->status,
+			new DateTimeImmutable( "@{$row->started_at}", wp_timezone() ),
+			$row->completed_at ? new DateTimeImmutable( "@{$row->completed_at}", wp_timezone() ) : null,
+			new DateTimeImmutable( "@{$row->updated_at}", wp_timezone() )
+		);
+	}
+
+	/**
+	 * Checks if a course progress exists.
+	 *
+	 * @param int $course_id The course ID.
+	 * @param int $user_id The user ID.
+	 * @return bool Whether the course progress exists.
+	 */
+	public function has( int $course_id, int $user_id ): bool {
+		$table_name = $this->wpdb->prefix . 'sensei_lms_progress';
+		$query      = $this->wpdb->prepare(
+			'SELECT COUNT(*) FROM ' . $table_name . ' WHERE post_id = %d AND user_id = %d AND type = %s',
+			$course_id,
+			$user_id,
+			'course'
+		);
+
+		$count = (int) $this->wpdb->get_var( $query );
+
+		return $count > 0;
+	}
+
+	/**
+	 * Save course progress.
+	 *
+	 * @param Course_Progress $course_progress The course progress.
+	 */
+	public function save( Course_Progress $course_progress ): void {
+
+		$this->wpdb->update(
+			$this->wpdb->prefix . 'sensei_lms_progress',
+			[
+				'status'       => $course_progress->get_status(),
+				'started_at'   => $course_progress->get_started_at()->getTimestamp(),
+				'completed_at' => $course_progress->get_completed_at() ? $course_progress->get_completed_at()->getTimestamp() : null,
+				'updated_at'   => current_datetime()->getTimestamp(),
+			],
+			[
+				'id' => $course_progress->get_id(),
+			],
+			[
+				'%s',
+				'%d',
+				$course_progress->get_completed_at() ? '%d' : null,
+				'%d',
+			],
+			[
+				'%d',
+			]
+		);
+	}
+}
---
+++ tests/unit-tests/student-progress/lesson-progress/repositories/test-class-tables-based-lesson-progress-repository.php
@@ -0,0 +1,266 @@
+<?php
+
+namespace SenseiTest\Student_Progress\Lesson_Progress\Repositories;
+
+use Sensei\Student_Progress\Lesson_Progress\Models\Lesson_Progress;
+use Sensei\Student_Progress\Lesson_Progress\Repositories\Tables_Based_Lesson_Progress_Repository;
+use Sensei_Course;/**
+ * Tests for Tables_Based_Lesson_Progress_Repository.
+				   *
+																								  * @covers \Sensei\Student_Progress\Lesson_Progress\Repositories\Tables_Based_Lesson_Progress_Repository
+ */
+class Tables_Based_Lesson_Progress_Repository_Test extends \WP_UnitTestCase {
+	public function testCreate_ParamsGiven_InsertsToWpdb(): void {
+		/* Arrange. */
+		$wpdb       = $this->createMock( \wpdb::class );
+		$repository = new Tables_Based_Lesson_Progress_Repository( $wpdb );
+
+		/* Expect & Act. */
+		$wpdb
+			->expects( $this->once() )
+			->method( 'insert' )
+			->with(
+				'sensei_lms_progress',
+				$this->callback(
+					function( $array ) {
+						if ( ! isset( $array['post_id'] ) || $array['post_id'] !== 1 ) {
+							return false;
+						}
+						if ( ! isset( $array['user_id'] ) || $array['user_id'] !== 2 ) {
+							return false;
+						}
+						if ( ! array_key_exists( 'parent_post_id', $array ) || ! is_null( $array['parent_post_id'] ) ) {
+							return false;
+						}
+						if ( ! isset( $array['type'] ) || $array['type'] !== 'lesson' ) {
+							return false;
+						}
+						if ( ! isset( $array['status'] ) || $array['status'] !== 'in-progress' ) {
+							return false;
+						}
+						return true;
+					}
+				),
+				[
+					'%d',
+					'%d',
+					null,
+					'%s',
+					'%s',
+					'%d',
+					null,
+					'%d',
+					'%d',
+				]
+			);
+		$repository->create( 1, 2 );
+	}
+
+	public function testCreate_ParamsGiven_ReturnsMatchingCourseProgress(): void {
+		/* Arrange. */
+		$wpdb            = $this->createMock( \wpdb::class );
+		$wpdb->insert_id = 3;
+		$repository      = new Tables_Based_Lesson_Progress_Repository( $wpdb );
+
+		/* Act. */
+		$progress = $repository->create( 1, 2 );
+
+		/* Assert. */
+		$expected = [
+			'id'        => 3,
+			'lesson_id' => 1,
+			'user_id'   => 2,
+			'status'    => 'in-progress',
+		];
+		self::assertSame( $expected, $this->export_progress( $progress ) );
+	}
+
+	public function testGet_NotFound_ReturnsNull(): void {
+		/* Arrange. */
+		$wpdb = $this->createMock( \wpdb::class );
+		$wpdb
+			->method( 'get_row' )
+			->willReturn( null );
+		$repository = new Tables_Based_Lesson_Progress_Repository( $wpdb );
+
+		/* Act. */
+		$progress = $repository->get( 1, 2 );
+
+		/* Assert. */
+		self::assertNull( $progress );
+	}
+
+	public function testGet_ProgressFound_ReturnsMatchingProgress(): void {
+		/* Arrange. */
+		$wpdb = $this->createMock( \wpdb::class );
+		$wpdb
+			->method( 'get_row' )
+			->willReturn(
+				(object) [
+					'id'             => 3,
+					'post_id'        => 1,
+					'user_id'        => 2,
+					'parent_post_id' => null,
+					'type'           => 'lesson',
+					'status'         => 'in-progress',
+					'created_at'     => ( new \DateTimeImmutable( '2022-01-01 00:00:00', wp_timezone() ) )->getTimestamp(),
+					'updated_at'     => ( new \DateTimeImmutable( '2022-01-02 00:00:00', wp_timezone() ) )->getTimestamp(),
+					'started_at'     => ( new \DateTimeImmutable( '2022-01-03 00:00:00', wp_timezone() ) )->getTimestamp(),
+					'completed_at'   => ( new \DateTimeImmutable( '2022-01-04 00:00:00', wp_timezone() ) )->getTimestamp(),
+				]
+			);
+		$repository = new Tables_Based_Lesson_Progress_Repository( $wpdb );
+
+		/* Act. */
+		$progress = $repository->get( 1, 2 );
+
+		/* Assert. */
+		$expected = [
+			'id'           => 3,
+			'lesson_id'    => 1,
+			'user_id'      => 2,
+			'status'       => 'in-progress',
+			'created_at'   => '2022-01-01 00:00:00',
+			'updated_at'   => '2022-01-02 00:00:00',
+			'started_at'   => '2022-01-03 00:00:00',
+			'completed_at' => '2022-01-04 00:00:00',
+		];
+		self::assertSame( $expected, $this->export_progress_with_dates( $progress ) );
+	}
+
+	public function testHas_NotFound_ReturnsFalse(): void {
+		/* Arrange. */
+		$wpdb = $this->createMock( \wpdb::class );
+		$wpdb
+			->method( 'get_var' )
+			->willReturn( 0 );
+		$repository = new Tables_Based_Lesson_Progress_Repository( $wpdb );
+
+		/* Act. */
+		$has = $repository->has( 1, 2 );
+
+		/* Assert. */
+		self::assertFalse( $has );
+	}
+
+	public function testHas_ProgressFound_ReturnsFalse(): void {
+		/* Arrange. */
+		$wpdb = $this->createMock( \wpdb::class );
+		$wpdb
+			->method( 'get_var' )
+			->willReturn( 2 );
+		$repository = new Tables_Based_Lesson_Progress_Repository( $wpdb );
+
+		/* Act. */
+		$has = $repository->has( 1, 2 );
+
+		/* Assert. */
+		self::assertTrue( $has );
+	}
+
+	public function testSave_ProgressGiven_CallsWpdbUpdate(): void {
+		/* Arrange. */
+		$wpdb       = $this->createMock( \wpdb::class );
+		$progress   = new Lesson_Progress(
+			1,
+			2,
+			3,
+			'complete',
+			new \DateTimeImmutable( '@1', wp_timezone() ),
+			new \DateTimeImmutable( '@2', wp_timezone() ),
+			new \DateTimeImmutable( '@3', wp_timezone() ),
+			new \DateTimeImmutable( '@4', wp_timezone() )
+		);
+		$repository = new Tables_Based_Lesson_Progress_Repository( $wpdb );
+
+		/* Expect & Act. */
+		$wpdb
+			->expects( self::once() )
+			->method( 'update' )
+			->with(
+				'sensei_lms_progress',
+				$this->callback(
+					function ( $data ) {
+						if ( ! isset( $data['status'] ) || 'complete' !== $data['status'] ) {
+							return false;
+						}
+						if ( ! isset( $data['started_at'] ) || 1 !== $data['started_at'] ) {
+							return false;
+						}
+						if ( ! isset( $data['completed_at'] ) || 2 !== $data['completed_at'] ) {
+							return false;
+						}
+						return true;
+					}
+				),
+				[
+					'id' => 1,
+				],
+				[
+					'%s',
+					'%d',
+					'%d',
+					'%d',
+				],
+				[
+					'%d',
+				]
+			);
+		$repository->save( $progress );
+	}
+
+	public function testCount_ParamsGiven_ReturnsMatchingValue(): void {
+		/* Arrange. */
+		$course = $this->createMock( Sensei_Course::class );
+		$course->method( 'course_lessons' )->with( 1, 'publish', 'ids' )->willReturn( [ 2, 3, 4 ] );
+
+		$initial_course  = Sensei()->course;
+		Sensei()->course = $course;
+
+		$wpdb = $this->createMock( \wpdb::class );
+		$wpdb
+			->method( 'prepare' )
+			->with(
+				'SELECT COUNT(*) FROM sensei_lms_progress WHERE post_id IN (2,3,4) AND user_id = %d AND type = %s',
+				2,
+				'lesson'
+			)
+			->willReturn( 'query' );
+		$wpdb
+			->method( 'get_var' )
+			->with( 'query' )
+			->willReturn( 5 );
+
+		$repository = new Tables_Based_Lesson_Progress_Repository( $wpdb );
+
+		/* Act. */
+		$actual = $repository->count( 1, 2 );
+
+		/* Assert. */
+		self::assertSame( 5, $actual );
+
+		Sensei()->course = $initial_course;
+	}
+
+	private function export_progress( Lesson_Progress $progress ): array {
+		return [
+			'id'        => $progress->get_id(),
+			'lesson_id' => $progress->get_lesson_id(),
+			'user_id'   => $progress->get_user_id(),
+			'status'    => $progress->get_status(),
+		];
+	}
+
+	private function export_progress_with_dates( ?Lesson_Progress $progress ): array {
+		return [
+			'id'           => $progress->get_id(),
+			'lesson_id'    => $progress->get_lesson_id(),
+			'user_id'      => $progress->get_user_id(),
+			'status'       => $progress->get_status(),
+			'created_at'   => $progress->get_created_at()->format( 'Y-m-d H:i:s' ),
+			'updated_at'   => $progress->get_updated_at()->format( 'Y-m-d H:i:s' ),
+			'started_at'   => $progress->get_started_at()->format( 'Y-m-d H:i:s' ),
+			'completed_at' => $progress->get_completed_at()->format( 'Y-m-d H:i:s' ),
+		];
+	}
+}
