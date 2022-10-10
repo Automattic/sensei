73b1cdcf60860f7dd1113ad38c29d39210da1b9c
Renatho De Carli Rosa <renatho@gmail.com>
2022-10-10 16:20:37 -0300

    Add first course styles and components

--- assets/home/sections/task-list.js
+++ assets/home/sections/task-list.js
@@ -4,6 +4,7 @@
 import Section from '../section';
 import Tasks from '../tasks/tasks';
 import Progress from '../tasks/progress';
+import FirstCourse from '../tasks/first-course';
 
 /**
  * Task List section component.
@@ -12,6 +13,12 @@ const TaskList = () => (
 	<Section>
 		<Progress totalTasks={ 10 } completedTasks={ 5 } />
 		<Tasks />
+		<FirstCourse
+			siteTitle="Learn Photography"
+			courseTitle="Architectural Photography"
+			siteLogo="https://techcrunch.com/wp-content/uploads/2022/06/Leica-on-black.jpeg?w=1390&crop=1"
+			featuredImage="https://techcrunch.com/wp-content/uploads/2022/06/Leica-on-black.jpeg?w=1390&crop=1"
+		/>
 	</Section>
 );
 
