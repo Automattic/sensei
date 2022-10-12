9b613d777f1dbf20a3f93ae1df2dd00ef0f12790
Aarón Fas <aaron.fas@automattic.com>
2022-10-12 14:34:10 +0200

    Load extensions data from property and not from Store.

--- assets/home/sections/extensions.js
+++ assets/home/sections/extensions.js
@@ -9,53 +9,32 @@ import { __ } from '@wordpress/i18n';
 import { Col, Grid } from '../grid';
 import Card from '../card';
 import Section from '../section';
-import { useSelect } from '@wordpress/data';
-import { EXTENSIONS_STORE } from '../../extensions/store';
-import { Spinner } from '@wordpress/components';
 
 /**
  * Extensions section component.
+ *
+ * @param {Object}   props
+ * @param {Object[]} props.extensions
  */
-const Extensions = () => {
-	const { extensions, isExtensionsLoading } = useSelect( ( select ) => {
-		const store = select( EXTENSIONS_STORE );
-
-		return {
-			isExtensionsLoading: ! store.hasFinishedResolution(
-				'getExtensions'
-			),
-			extensions: store
-				.getExtensions()
-				.filter(
-					( extension ) => extension.product_slug !== 'sensei-pro'
-				),
-		};
-	}, [] );
-
-	if ( isExtensionsLoading ) {
-		return (
-			<div className="sensei-home__loader">
-				<Spinner />
-			</div>
-		);
-	}
-
-	if ( 0 === extensions.length ) {
+const Extensions = ( { extensions } ) => {
+	if ( extensions === undefined || extensions.length === 0 ) {
 		return null;
 	}
 
 	return (
 		<Section title={ __( 'Extensions', 'sensei-lms' ) }>
 			<Grid>
-				{ extensions.map( ( extension ) => (
-					<Col
-						key={ extension.product_slug }
-						className="sensei-extensions__card-wrapper"
-						cols={ 4 }
-					>
-						<Card { ...extension } />
-					</Col>
-				) ) }
+				{ extensions.map( ( extension ) => {
+					return (
+						<Col
+							key={ extension.product_slug }
+							className="sensei-extensions__card-wrapper"
+							cols={ 4 }
+						>
+							<Card { ...extension } />
+						</Col>
+					);
+				} ) }
 			</Grid>
 		</Section>
 	);
