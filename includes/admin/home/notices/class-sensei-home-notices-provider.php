7c9f874775b0553011ecccb0beed3774a7a45df5
Jake Oehler Morrison <jake.morrison@automattic.com>
2022-10-12 11:03:07 +0100

    Add ABSPATH checks where missing

--- includes/admin/home/notices/class-sensei-home-notices-provider.php
+++ includes/admin/home/notices/class-sensei-home-notices-provider.php
@@ -6,6 +6,10 @@
  * @since   $$next-version$$
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
 /**
  * Class that gathers and produces the local notices for Sensei Home.
  */
