2e5bfe9a39ec8f271959542753f4580fb12e10f4
Aarón Fas <aaron.fas@automattic.com>
2022-10-13 08:55:43 +0200

    Add installation information to `Sensei_Home_Extensions_Provider`.
    - Logic has been copied from
      `Sensei_Extensions->add_installed_extensions_properties`.
    - `Sensei_Extension` deprecation has been considered but we still need
      `Sensei_REST_API_Extensions_Controller` to manage actions like
      installing/updating the plugins.

--- includes/admin/home/class-sensei-home-extensions-provider.php
+++ includes/admin/home/class-sensei-home-extensions-provider.php
@@ -40,11 +40,43 @@ class Sensei_Home_Extensions_Provider {
 	 * @return array
 	 */
 	public function get() {
+		// Retrieve plugins list from remote.
+		$data           = $this->api->fetch( HOUR_IN_SECONDS, true );
+		$remote_plugins = $data['plugins'] ?? [];
 
-		$data    = $this->api->fetch( HOUR_IN_SECONDS, true );
-		$plugins = $data['plugins'] ?? [];
+		// Retrieve installed plugins information.
+		require_once ABSPATH . 'wp-admin/includes/plugin.php';
+		$installed_plugins = get_plugins();
 
-		$extensions = array_map( [ $this, 'map_plugin' ], $plugins );
+		$extensions = array_map(
+			function ( $remote_plugin ) use ( $installed_plugins ) {
+
+				$plugin_file    = $remote_plugin['plugin_file'] ?? null;
+				$remote_version = $remote_plugin['version'] ?? null;
+
+				$is_installed      = isset( $installed_plugins[ $plugin_file ] );
+				$installed_version = $is_installed ? $installed_plugins[ $plugin_file ]['Version'] : null;
+
+				return [
+					// Remote plugin data.
+					'title'             => $remote_plugin['title'],
+					'product_slug'      => $remote_plugin['product_slug'],
+					'excerpt'           => $remote_plugin['excerpt'] ?? null,
+					'image'             => $remote_plugin['image_large'] ?? null,
+					'price'             => $remote_plugin['price'] ?? null,
+					'link'              => $remote_plugin['link'] ?? null,
+					'version'           => $remote_version,
+					'plugin_file'       => $plugin_file,
+					// Installed status information.
+					'is_installed'      => $is_installed,
+					'is_activated'      => $is_installed && is_plugin_active( $plugin_file ),
+					'installed_version' => $installed_version,
+					'has_update'        => $is_installed && $remote_version && version_compare( $remote_version, $installed_version, '>' ),
+
+				];
+			},
+			$remote_plugins
+		);
 
 		/**
 		 * Filters the list of extensions that will be later displayed in the Sensei Home page.
@@ -57,8 +89,14 @@ class Sensei_Home_Extensions_Provider {
 		 * @property {string} product_slug The extension product slug.
 		 * @property {string} description The extension description.
 		 * @property {string} image URL to be used as extension image.
-		 * @property {float} price The extension price.
+		 * @property {float}  price The extension price.
 		 * @property {string} link URL to get more information about the extension.
+		 * @property {string} version The latest version number.
+		 * @property {string} plugin_file The main plugin file path.
+		 * @property {bool}   is_installed Whether the extension is installed or not.
+		 * @property {bool}   is_activated Whether the extension is activated or not.
+		 * @property {string} installed_version Optional. The installed version.
+		 * @property {bool}   has_update Optional. For installed extensions whether the update has an update available or not.
 		 *
 		 * @param {Extension[]} $extensions
 		 *
@@ -67,23 +105,34 @@ class Sensei_Home_Extensions_Provider {
 		return apply_filters( 'sensei_home_extensions', $extensions );
 	}
 
-
 	/**
-	 * Transform remote plugin data into extension format.
+	 * Given a list of extensions and the currently installed plugins returns the former extended with some properties about installation status.
 	 *
-	 * @param array $remote_plugin_data Plugin information on remote.
+	 * @param array[] $extensions The list of extensions.
+	 * @param array[] $installed_plugins The plugins currently installed.
 	 * @return array
 	 */
-	private function map_plugin( $remote_plugin_data ) {
+	private function add_installed_properties( $extensions, $installed_plugins ) {
 
-		return [
-			'title'        => $remote_plugin_data['title'],
-			'product_slug' => $remote_plugin_data['product_slug'],
-			'excerpt'      => $remote_plugin_data['excerpt'] ?? null,
-			'image'        => $remote_plugin_data['image_large'] ?? null,
-			'price'        => $remote_plugin_data['price'] ?? null,
-			'link'         => $remote_plugin_data['link'] ?? null,
-		];
+		$result = [];
+
+		foreach ( $extensions as $extension ) {
+			$plugin_file = $extension['plugin_file'];
+
+			$is_installed      = isset( $installed_plugins[ $plugin_file ] );
+			$installed_version = $is_installed ? $installed_plugins[ $plugin_file ]['Version'] : null;
+
+			$result[] = array_merge(
+				$extension,
+				[
+					'is_installed'      => $is_installed,
+					'is_activated'      => $is_installed && is_plugin_active( $plugin_file ),
+					'installed_version' => $installed_version,
+					'has_update'        => $is_installed && isset( $extension['version'] ) && version_compare( $extension['version'], $installed_version, '>' ),
+				]
+			);
+		}
+
+		return $result;
 	}
-
 }
