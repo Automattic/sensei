12f979fcf57ceff2de8e90fcfd52f1ef7e6ca73e
Renatho De Carli Rosa <renatho@gmail.com>
2022-10-13 15:27:44 -0300

    Add tasks completion

--- assets/home/tasks-section/index.js
+++ assets/home/tasks-section/index.js
@@ -1,4 +1,11 @@
 /**
+ * WordPress dependencies
+ */
+import { Notice } from '@wordpress/components';
+import { useEffect, useState } from '@wordpress/element';
+import apiFetch from '@wordpress/api-fetch';
+
+/**
  * Internal dependencies
  */
 import Section from '../section';
@@ -6,6 +13,43 @@ import Progress from './progress';
 import Tasks from './tasks';
 import FirstCourse from './first-course';
 import Ready from './ready';
+
+/**
+ * Hook to update the state to ready when all tasks are completed.
+ *
+ * @param {boolean} isServerCompleted   Whether tasks were already completed in the server.
+ * @param {number}  totalTasks          Number of tasks.
+ * @param {number}  totalCompletedTasks Number of completed tasks.
+ */
+const useReadyState = (
+	isServerCompleted,
+	totalTasks,
+	totalCompletedTasks
+) => {
+	const [ ready, setReady ] = useState( isServerCompleted );
+	const [ readyError, setReadyError ] = useState( false );
+
+	useEffect( () => {
+		if ( ! ready && totalCompletedTasks === totalTasks ) {
+			setReadyError( false );
+			apiFetch( {
+				path: '/sensei-internal/v1/home/tasks/complete',
+				method: 'POST',
+			} )
+				.then( () => {
+					setReady( true );
+				} )
+				.catch( ( err ) => {
+					setReadyError( err );
+				} );
+		}
+	}, [ totalTasks, totalCompletedTasks, ready ] );
+
+	return {
+		ready,
+		readyError,
+	};
+};
 
 /**
  * Tasks section component.
@@ -19,32 +63,50 @@ const TasksSection = ( { data } ) => {
 	const sortedItems = items.sort( ( a, b ) => a.priority - b.priority );
 	const completedItems = sortedItems.filter( ( i ) => i.done );
 
+	const { ready, readyError } = useReadyState(
+		data.is_completed,
+		items.length,
+		completedItems.length
+	);
+
+	let content;
+
+	if ( readyError ) {
+		content = (
+			<Notice status="error" isDismissible={ false }>
+				{ readyError.message }
+			</Notice>
+		);
+	} else if ( ready ) {
+		content = <Ready />;
+	} else {
+		content = (
+			<>
+				<div className="sensei-home-tasks-section__content">
+					<Progress
+						totalTasks={ items.length }
+						completedTasks={ completedItems.length }
+					/>
+					<Tasks items={ sortedItems } />
+				</div>
+				<div className="sensei-home-tasks-section__first-course">
+					<FirstCourse
+						siteTitle="Learn Photography"
+						courseTitle="Architectural Photography"
+						siteLogo="https://techcrunch.com/wp-content/uploads/2022/06/Leica-on-black.jpeg?w=1390&crop=1"
+						featuredImage="https://techcrunch.com/wp-content/uploads/2022/06/Leica-on-black.jpeg?w=1390&crop=1"
+					/>
+				</div>
+			</>
+		);
+	}
+
 	return (
 		<Section
 			className="sensei-home-tasks-section"
 			insideClassName="sensei-home-tasks-section__inside"
 		>
-			{ data.is_completed ? (
-				<Ready />
-			) : (
-				<>
-					<div className="sensei-home-tasks-section__content">
-						<Progress
-							totalTasks={ items.length }
-							completedTasks={ completedItems.length }
-						/>
-						<Tasks items={ sortedItems } />
-					</div>
-					<div className="sensei-home-tasks-section__first-course">
-						<FirstCourse
-							siteTitle="Learn Photography"
-							courseTitle="Architectural Photography"
-							siteLogo="https://techcrunch.com/wp-content/uploads/2022/06/Leica-on-black.jpeg?w=1390&crop=1"
-							featuredImage="https://techcrunch.com/wp-content/uploads/2022/06/Leica-on-black.jpeg?w=1390&crop=1"
-						/>
-					</div>
-				</>
-			) }
+			{ content }
 		</Section>
 	);
 };
--- assets/home/tasks-section/index.test.js
+++ assets/home/tasks-section/index.test.js
@@ -1,7 +1,8 @@
 /**
  * External dependencies
  */
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
+import nock from 'nock';
 
 /**
  * Internal dependencies
@@ -42,5 +43,56 @@ describe( '<TasksSection />', () => {
 				'Your new course is ready to meet its students! Share it with the world.'
 			)
 		).toBeTruthy();
+	} );
+
+	it( 'Should post to the server that all tasks are completed', async () => {
+		const data = {
+			is_completed: false,
+			items: [
+				{ id: '1', title: 'Task 1', done: true },
+				{ id: '2', title: 'Task 2', done: true },
+			],
+		};
+
+		nock( 'http://localhost' )
+			.post( '/sensei-internal/v1/home/tasks/complete' )
+			.query( {
+				_locale: 'user',
+			} )
+			.reply( 200, {} );
+
+		const { queryByText } = render( <TasksSection data={ data } /> );
+
+		await waitFor( () => {
+			expect(
+				queryByText(
+					'Your new course is ready to meet its students! Share it with the world.'
+				)
+			).toBeTruthy();
+		} );
+	} );
+
+	it( 'Should display error when request to complete fails', async () => {
+		const data = {
+			is_completed: false,
+			items: [
+				{ id: '1', title: 'Task 1', done: true },
+				{ id: '2', title: 'Task 2', done: true },
+			],
+		};
+		const errorMessage = 'Error message';
+
+		nock( 'http://localhost' )
+			.post( '/sensei-internal/v1/home/tasks/complete' )
+			.query( {
+				_locale: 'user',
+			} )
+			.reply( 400, { message: errorMessage } );
+
+		const { queryByText } = render( <TasksSection data={ data } /> );
+
+		await waitFor( () => {
+			expect( queryByText( errorMessage ) ).toBeTruthy();
+		} );
 	} );
 } );
