c08eeb98e78434005263bbf1302330b0585024b2
Mikey Arce <mikeyarce@gmail.com>
2022-10-16 16:11:55 -0700

    Add AJAX test for get_course_list

---
+++ tests/unit-tests/admin/test-class-sensei-learners-admin-bulk-actions-ajax.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * AJAX tests for Sensei.
+ *
+ * @group ajax-calls
+ */
+class Sensei_Learners_Admin_Bulk_Actions_View_AJAX_Test extends WP_Ajax_UnitTestCase {
+	use Sensei_Course_Enrolment_Test_Helpers;
+
+	/**
+	 * Gets the manual enrolment manager.
+	 *
+	 * @return false|Sensei_Course_Manual_Enrolment_Provider
+	 */
+	private function getManualEnrolmentProvider() {
+		return Sensei_Course_Enrolment_Manager::instance()->get_manual_enrolment_provider();
+	}
+
+	/**
+	 * Test the functionality of displaying additional courses from the Students page "More" button using the get_course_list action.
+	 */
+	public function testSingleRow_ItemGiven_ReturnsMatchingCourses() {
+		$this->factory = new Sensei_Factory();
+
+		// Generate 12 courses
+		$this->factory->generate_many_courses( 12 );
+		$courses = $this->factory->get_courses();
+
+		// Generate 2 Students
+		$users = $this->factory->user->create_many( 2, array( 'role' => 'subscriber' ) );
+
+		// Enroll users into courses
+		foreach ( $users as $user ) {
+			foreach ( $courses as $course ) {
+				$provider = $this->getManualEnrolmentProvider();
+				$provider->enrol_learner( $user, $course );
+			}
+		}
+
+		$this->_setRole( 'administrator' );
+		$_POST['nonce']   = wp_create_nonce( 'get_course_list' );
+		$_POST['user_id'] = 2;
+
+		try {
+			$this->_handleAjax( 'get_course_list' );
+		} catch ( \WPAjaxDieContinueException $e ) {
+		}
+		$response = json_decode( $this->_last_response );
+
+		$this->assertTrue( isset( $e ) );
+		$this->assertInternalType( 'object', $response );
+		$this->assertObjectHasAttribute( 'success', $response );
+		$this->assertTrue( $response->success );
+		$this->assertCount( 9, $response->data );
+
+		foreach ( $response->data as $item ) {
+			$this->assertContains( '<a href="http://example.org/wp-admin/edit.php?post_type=course', $item );
+		}
+	}
+}
