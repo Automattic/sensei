11e668081be7a216cc0f5bc79d3b45e5ae034586
Fernando Jorge Mota <contato@fjorgemota.com>
2022-10-17 20:11:31 -0300

    Fix linting error on the data cleaner

--- includes/class-sensei-data-cleaner.php
+++ includes/class-sensei-data-cleaner.php
@@ -89,7 +89,7 @@ class Sensei_Data_Cleaner {
 		'sensei_course_theme_query_var_flushed',
 		'sensei_settings_sections_visited',
 		'sensei_home_tasks_list_is_completed',
-		'sensei_home_task_visited_woocommerce'
+		'sensei_home_task_visited_woocommerce',
 	);
 
 	/**
