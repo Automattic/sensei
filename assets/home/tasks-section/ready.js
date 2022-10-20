0cc972a9166939f563dc55d2eeba081c7223b652
Renatho De Carli Rosa <renatho@gmail.com>
2022-10-20 14:56:51 -0300

    Fix tumblr variable name
    Co-authored-by: Fernando Jorge Mota <fjorgemota@users.noreply.github.com>

--- assets/home/tasks-section/ready.js
+++ assets/home/tasks-section/ready.js
@@ -37,7 +37,7 @@ const Ready = ( { coursePermalink, onDismiss } ) => {
 	// Prepare social media links.
 	const shareLink = encodeURIComponent( coursePermalink );
 	const facebookLink = `https://www.facebook.com/sharer/sharer.php?u=${ shareLink }`;
-	const tumblerLink = sprintf(
+	const tumblrLink = sprintf(
 		// translators: %1$s placeholder is the share link, %2$s placeholder is the share link.
 		'https://www.tumblr.com/widgets/share/tool?posttype=link&caption=%1$s&content=%2$s&canonicalUrl=%2$s',
 		__( 'My new course is ready!', 'sensei-lms' ),
@@ -101,7 +101,7 @@ const Ready = ( { coursePermalink, onDismiss } ) => {
 				<li>
 					<a
 						className="sensei-home-ready__social-link"
-						href={ tumblerLink }
+						href={ tumblrLink }
 						target="_blank"
 						rel="noreferrer"
 					>
