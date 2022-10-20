741664627ad289920d8d0d8efc9dc99a3f395823
Jake Oehler Morrison <jake.morrison@automattic.com>
2022-10-20 13:33:31 +0100

    Set promo banner default based on ability to manage Sensei

--- includes/admin/home/promo-banner/class-sensei-home-promo-banner-provider.php
+++ includes/admin/home/promo-banner/class-sensei-home-promo-banner-provider.php
@@ -17,6 +17,10 @@ class Sensei_Home_Promo_Banner_Provider {
 	 * @return array
 	 */
 	public function get(): array {
+		$default_show_banner = true;
+		if ( ! current_user_can( 'manage_sensei' ) ) {
+			$default_show_banner = false;
+		}
 
 		return [
 			/**
@@ -29,7 +33,7 @@ class Sensei_Home_Promo_Banner_Provider {
 			 *
 			 * @return {bool}
 			 */
-			'is_visible' => apply_filters( 'sensei_home_promo_banner_show', true ),
+			'is_visible' => apply_filters( 'sensei_home_promo_banner_show', $default_show_banner ),
 		];
 	}
 }
