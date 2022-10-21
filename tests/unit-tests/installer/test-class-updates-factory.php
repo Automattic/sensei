24b37259a1aa7e33a5a84dd36a6bf8f4faa3683b
Dmitry Merkushin <merkushin@gmail.com>
2022-10-22 01:52:11 +0800

    Update installer corresponding to the latest updates

--- tests/unit-tests/installer/test-class-updates-factory.php
+++ tests/unit-tests/installer/test-class-updates-factory.php
@@ -16,7 +16,7 @@ class Updates_Factory_Test extends \WP_UnitTestCase {
 		$factory = new Updates_Factory();
 
 		/* Act. */
-		$updates = $factory->create( '1.2.3', SENSEI_LMS_VERSION );
+		$updates = $factory->create( '1.2.3', SENSEI_LMS_VERSION, true );
 
 		/* Assert. */
 		$this->assertInstanceOf( Sensei_Updates::class, $updates );
