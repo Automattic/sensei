c214d0c8e280f565d5cc58e960c4ffe9fdb57fba
Fernando Jorge Mota <contato@fjorgemota.com>
2022-10-21 09:37:23 -0300

    Add component to render notices

---
+++ assets/home/notices.js
@@ -0,0 +1,131 @@
+/**
+ * WordPress dependencies
+ */
+import { RawHTML } from '@wordpress/element';
+
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
+ * Component to render an action of a given notice.
+ *
+ * @param {Object} props        Component props.
+ * @param {Object} props.action The action to render.
+ */
+const NoticeAction = ( { action } ) => {
+	if ( ! action || ! action.label || ! action.url ) {
+		return null;
+	}
+
+	const buttonClass = action.primary ? 'button-primary' : 'button-secondary';
+	return (
+		<a
+			href={ action.url }
+			target={ action.target ?? '_self' }
+			rel="noopener noreferrer"
+			className={ classnames( 'button', buttonClass ) }
+		>
+			{ action.label }
+		</a>
+	);
+};
+
+/**
+ * Component to render a list of actions for a notice based on an array of actions, or nothing if there's no actions.
+ *
+ * @param {Object}   props         Component props.
+ * @param {Object[]} props.actions The list of actions to render.
+ */
+const NoticeActions = ( { actions } ) => {
+	if ( ! actions || ! Array.isArray( actions ) ) {
+		return null;
+	}
+	return (
+		<div className="sensei-notice__actions">
+			{ actions.map( ( action ) => (
+				<NoticeAction key={ action.url } action={ action } />
+			) ) }
+		</div>
+	);
+};
+
+/**
+ * Renders the heaidng of a given notice, wrapped with a proper CSS class, or null if there's no heading.
+ *
+ * @param {Object} props         Component props.
+ * @param {string} props.heading The heading to render.
+ */
+const NoticeHeading = ( { heading } ) => {
+	if ( ! heading ) {
+		return null;
+	}
+	return <div className="sensei-notice__heading">{ heading }</div>;
+};
+
+/**
+ * Component that renders a single notice.
+ *
+ * @param {Object} props              Component props.
+ * @param {string} props.noticeId     The notice ID.
+ * @param {Object} props.notice       The notice data.
+ * @param {string} props.dismissNonce The nonce to dismiss notices.
+ */
+const Notice = ( { noticeId, notice, dismissNonce } ) => {
+	let noticeClass = '';
+	if ( !! notice.style ) {
+		noticeClass = 'sensei-notice-' + notice.style;
+	}
+
+	const isDismissible = notice.dismissible && dismissNonce;
+
+	const containerProps = {
+		className: classnames( 'notice', 'sensei-notice', noticeClass, {
+			'is-dismissible': isDismissible,
+		} ),
+	};
+
+	if ( isDismissible ) {
+		containerProps[ 'data-dismiss-action' ] = 'sensei_dismiss_notice';
+		containerProps[ 'data-dismiss-notice' ] = noticeId;
+		containerProps[ 'data-dismiss-nonce' ] = dismissNonce;
+	}
+
+	return (
+		<div { ...containerProps }>
+			<div className="sensei-notice__wrapper">
+				<div className="sensei-notice__content">
+					<NoticeHeading heading={ notice.heading } />
+					<RawHTML>{ notice.message }</RawHTML>
+				</div>
+			</div>
+
+			<NoticeActions info_link actions={ notice.actions } />
+		</div>
+	);
+};
+
+/**
+ * Component to show a list of notices.
+ *
+ * @param {Object} props              Component props.
+ * @param {Object} props.notices      Mapping of notice IDs => notice object.
+ * @param {string} props.dismissNonce A nonce to be able to dismiss things.
+ */
+const Notices = ( { notices, dismissNonce } ) => {
+	return (
+		<>
+			{ Object.entries( notices ).map( ( [ noticeId, notice ] ) => (
+				<Notice
+					key={ noticeId }
+					notice={ notice }
+					noticeId={ noticeId }
+					dismissNonce={ dismissNonce }
+				/>
+			) ) }
+		</>
+	);
+};
+
+export default Notices;
