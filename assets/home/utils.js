628695046bd8dbf9072175a44ee3cea5887f6165
Aarón Fas <aaron.fas@automattic.com>
2022-10-24 20:07:59 +0200

    Add try/catch to addUtms util method.

--- assets/home/utils.js
+++ assets/home/utils.js
@@ -5,11 +5,15 @@
  * @return {string} The url with UTMs added.
  */
 export const addUtms = ( url ) => {
-	const parsed = new URL( url );
-	if ( parsed.hostname === 'senseilms.com' ) {
-		parsed.searchParams.set( 'utm_source', 'plugin_sensei' );
-		parsed.searchParams.set( 'utm_medium', 'upsell' );
-		parsed.searchParams.set( 'utm_campaign', 'sensei_home' );
+	try {
+		const parsed = new URL( url );
+		if ( parsed.hostname === 'senseilms.com' ) {
+			parsed.searchParams.set( 'utm_source', 'plugin_sensei' );
+			parsed.searchParams.set( 'utm_medium', 'upsell' );
+			parsed.searchParams.set( 'utm_campaign', 'sensei_home' );
+		}
+		return parsed.toString();
+	} catch {
+		return url;
 	}
-	return parsed.toString();
 };
