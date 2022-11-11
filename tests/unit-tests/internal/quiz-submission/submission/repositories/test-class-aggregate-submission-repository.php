f038ccc740bb174a7127c85a789186930926581e
Miroslav Mitev <m1r0@users.noreply.github.com>
2022-11-11 13:58:33 +0200

    Convert quiz submission dates to UTC when saving to tables

--- tests/unit-tests/internal/quiz-submission/submission/repositories/test-class-aggregate-submission-repository.php
+++ tests/unit-tests/internal/quiz-submission/submission/repositories/test-class-aggregate-submission-repository.php
@@ -2,6 +2,8 @@
 
 namespace SenseiTest\Internal\Quiz_Submission\Submission\Repositories;
 
+use DateTimeImmutable;
+use DateTimeZone;
 use Sensei\Internal\Quiz_Submission\Submission\Models\Submission;
 use Sensei\Internal\Quiz_Submission\Submission\Repositories\Aggregate_Submission_Repository;
 use Sensei\Internal\Quiz_Submission\Submission\Repositories\Comments_Based_Submission_Repository;
@@ -203,7 +205,7 @@ class Aggregate_Submission_Repository_Test extends \WP_UnitTestCase {
 
 	public function testSave_UseTablesOnAndSubmissionNotFound_DoesntCallTablesBasedRepository(): void {
 		/* Arrange. */
-		$progress       = $this->create_submission();
+		$submission     = $this->create_submission();
 		$comments_based = $this->createMock( Comments_Based_Submission_Repository::class );
 		$tables_based   = $this->createMock( Tables_Based_Submission_Repository::class );
 		$tables_based
@@ -218,7 +220,45 @@ class Aggregate_Submission_Repository_Test extends \WP_UnitTestCase {
 			->expects( $this->never() )
 			->method( 'save' );
 
-		$repository->save( $progress );
+		$repository->save( $submission );
+	}
+
+	public function testSave_WhenSubmissionWithNonUTCDatesGiven_ConvertsDatesToUTC() {
+		/* Arrange. */
+		$comments_based = $this->createMock( Comments_Based_Submission_Repository::class );
+		$tables_based   = $this->createMock( Tables_Based_Submission_Repository::class );
+		$submission     = new Submission(
+			1,
+			2,
+			3,
+			12.34,
+			new DateTimeImmutable( 'now', new DateTimeZone( 'US/Central' ) ),
+			new DateTimeImmutable( 'now', new DateTimeZone( 'US/Central' ) )
+		);
+
+		$tables_based
+			->method( 'get' )
+			->with( 2, 3 )
+			->willReturn( $submission );
+
+		$repository = new Aggregate_Submission_Repository( $comments_based, $tables_based, true );
+
+		/* Expect & Act. */
+		$tables_based
+			->expects( $this->once() )
+			->method( 'save' )
+			->with(
+				$this->callback(
+					function ( Submission $submission_to_save ) {
+						$this->assertSame( '+00:00', $submission_to_save->get_created_at()->getTimezone()->getName() );
+						$this->assertSame( '+00:00', $submission_to_save->get_updated_at()->getTimezone()->getName() );
+
+						return true;
+					}
+				)
+			);
+
+		$repository->save( $submission );
 	}
 
 	public function testDelete_UseTablesOff_DoesntCallTablesBasedRepository(): void {
