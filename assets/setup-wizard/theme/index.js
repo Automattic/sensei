376080f0838109c65722a1e4ad7dd83c286a9555
Renatho De Carli Rosa <renatho@gmail.com>
2022-11-22 18:03:30 -0300

    Add theme image

--- assets/setup-wizard/theme/index.js
+++ assets/setup-wizard/theme/index.js
@@ -8,6 +8,7 @@ import { __ } from '@wordpress/i18n';
  */
 import { useQueryStringRouter } from '../../shared/query-string-router';
 import { H } from '../../shared/components/section';
+import onboardingThemeImage from '../../images/onboarding-theme.png';
 
 /**
  * Theme step for Setup Wizard.
@@ -20,48 +21,61 @@ const Theme = () => {
 	};
 
 	return (
-		<div className="sensei-setup-wizard__content sensei-setup-wizard__content--large">
-			<div className="sensei-setup-wizard__title">
-				<H className="sensei-setup-wizard__step-title">
-					{ __( 'Get new Sensei theme', 'sensei-lms' ) }
-				</H>
-				<p>
-					{ __(
-						"The new Sensei theme it's build from ground up with Learning Mode in mind to optimize your full site so that everything works smootly together.",
-						'sensei-lms'
-					) }
-				</p>
-			</div>
-
-			<div className="sensei-setup-wizard__actions sensei-setup-wizard__actions--full-width">
-				<div className="sensei-setup-wizard__theme-actions">
-					<button
-						className="sensei-setup-wizard__button sensei-setup-wizard__button--primary"
-						onClick={ goToNextStep }
-					>
-						{ __( 'Install the new Sensei theme', 'sensei-lms' ) }
-					</button>
-
-					<button
-						className="sensei-setup-wizard__button sensei-setup-wizard__button--secondary sensei-setup-wizard__button--only-medium"
-						onClick={ goToNextStep }
-					>
-						{ __( 'Explore the theme', 'sensei-lms' ) }
-					</button>
+		<>
+			<div className="sensei-setup-wizard__content sensei-setup-wizard__content--large">
+				<div className="sensei-setup-wizard__title">
+					<H className="sensei-setup-wizard__step-title">
+						{ __( 'Get new Sensei theme', 'sensei-lms' ) }
+					</H>
+					<p>
+						{ __(
+							"The new Sensei theme it's build from ground up with Learning Mode in mind to optimize your full site so that everything works smootly together.",
+							'sensei-lms'
+						) }
+					</p>
 				</div>
 
-				<div className="sensei-setup-wizard__action-skip">
-					<button
-						className="sensei-setup-wizard__button sensei-setup-wizard__button--link"
-						onClick={ goToNextStep }
-					>
-						{ __( 'Skip theme selection', 'sensei-lms' ) }
-					</button>
+				<div className="sensei-setup-wizard__actions sensei-setup-wizard__actions--full-width">
+					<div className="sensei-setup-wizard__theme-actions">
+						<button
+							className="sensei-setup-wizard__button sensei-setup-wizard__button--primary"
+							onClick={ goToNextStep }
+						>
+							{ __(
+								'Install the new Sensei theme',
+								'sensei-lms'
+							) }
+						</button>
+
+						<button
+							className="sensei-setup-wizard__button sensei-setup-wizard__button--secondary sensei-setup-wizard__button--only-medium"
+							onClick={ goToNextStep }
+						>
+							{ __( 'Explore the theme', 'sensei-lms' ) }
+						</button>
+					</div>
+
+					<div className="sensei-setup-wizard__action-skip">
+						<button
+							className="sensei-setup-wizard__button sensei-setup-wizard__button--link"
+							onClick={ goToNextStep }
+						>
+							{ __( 'Skip theme selection', 'sensei-lms' ) }
+						</button>
+					</div>
 				</div>
 			</div>
 
-			<div>Lorem ipsum</div>
-		</div>
+			<div className="sensei-setup-wizard-theme">
+				<div className="sensei-setup-wizard-theme__image-wrapper">
+					<img
+						src={ window.sensei.pluginUrl + onboardingThemeImage }
+						alt={ __( 'Sensei theme illustration', 'sensei-lms' ) }
+						className="sensei-setup-wizard-theme__image"
+					/>
+				</div>
+			</div>
+		</>
 	);
 };
 
