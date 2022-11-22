07869bcb691613c6143702150863b17961ff7c37
Aarón Fas <aaron.fas@automattic.com>
2022-11-22 17:07:19 +0100

    Fix code style.

--- includes/class-sensei-grading-main.php
+++ includes/class-sensei-grading-main.php
@@ -257,7 +257,7 @@ class Sensei_Grading_Main extends Sensei_List_Table {
 
 		$title = Sensei_Learner::get_full_name( $item->user_id );
 
-		$quiz_id = Sensei()->lesson->lesson_quizzes( $item->comment_post_ID, 'any' );
+		$quiz_id   = Sensei()->lesson->lesson_quizzes( $item->comment_post_ID, 'any' );
 		$quiz_link = add_query_arg(
 			array(
 				'page'    => $this->page_slug,
