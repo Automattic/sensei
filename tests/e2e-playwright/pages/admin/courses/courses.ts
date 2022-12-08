e0b20594bf19ebf9b3718784ed7a412cd7f79a9c
Dmitry Merkushin <merkushin@gmail.com>
2022-12-08 22:05:33 +0800

    Add courses and dashboard pages for e2e tests

---
+++ tests/e2e-playwright/pages/admin/courses/courses.ts
@@ -0,0 +1,30 @@
+const PostType = require('../post-type');
+
+class WizardModal {
+	constructor( locator: Locator ) {
+		this.wizard = locator;
+		this.form = this.wizard.locator( '.sensei-editor-wizard-step__form' );
+		this.input = this.form.locator( 'input' ).first();
+		this.textArea = this.form.locator( 'textarea' ).first();
+		this.continueButton = this.wizard.locator( 'button:has-text("Continue")' );
+		this.continueWithFreeButton = this.wizard.locator( 'button:has-text("Continue with Sensei Free")' );
+		this.startWithDefaultLayoutButton = this.wizard.locator( 'button:has-text("Start with default layout")' );
+	}
+}
+
+class CoursesPage extends PostType {
+  constructor( page: Page ) {
+	super( page, 'course' );
+
+	const wizardLocator = page.locator( '.sensei-editor-wizard' );
+	this.wizardModal = new WizardModal( wizardLocator );
+
+	this.createCourseButton = page.locator( 'a.page-title-action[href$="post-new.php?post_type=course"]:has-text("New Course")' );
+
+	this.publishButton = page.locator( '[aria-label="Editor top bar"] >> text=Publish' );
+	this.confirmPublishButton = page.locator( '[aria-label="Editor publish"] >> text=Publish' ).first();
+	this.viewPreviewLink = page.locator( 'a:has-text("View Preview")' ).first();
+  }
+}
+
+module.exports = CoursesPage;
---
+++ tests/e2e-playwright/pages/admin/dashboard/dashboard.ts
@@ -0,0 +1,26 @@
+const PATH = '/wp-admin/';
+
+class DashboardPage {
+	private readonly page: Page;
+
+	constructor( page: Page ) {
+		this.page = page;
+		this.mainMenu = page.locator( '#adminmenu' );
+		this.senseiMenuItem = this.mainMenu.locator( 'a[href$="admin.php?page=sensei"]:has-text("Sensei LMS")' );
+		this.coursesMenuItem = this.mainMenu.locator( 'a[href$="edit.php?post_type=course"]:has-text("Courses")' );
+	}
+
+	async goTo() {
+		await this.page.goto( PATH );
+	}
+
+	async getSenseiMenuItem() {
+		return this.senseiMenuItem;
+	}
+
+	async getCoursesMenuItem() {
+		return this.coursesMenuItem;
+	}
+}
+
+module.exports = DashboardPage;
