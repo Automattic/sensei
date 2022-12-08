badbf6edb656ad087d45ca3f73b0034c92ab6323
Imran Hossain <imranh920@gmail.com>
2022-12-08 23:44:55 +0600

    Add Guest Student role to the newly created user

--- includes/class-sensei-guest-user.php
+++ includes/class-sensei-guest-user.php
@@ -1,10 +1,19 @@
 <?php
+/**
+ * Guest User
+ *
+ * Handles operations related to allowing guest users take a course.
+ *
+ * @package Sensei\Frontend
+ * @since 1.3.0
+ */
+
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit; // Exit if accessed directly.
 }
 
 /**
- * Functions related to allowing guest users take a course.
+ * Sensei Guest User Class.
  *
  * @author Automattic
  *
@@ -14,11 +23,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Sensei_Guest_User {
 
 	/**
+	 * Keeps a reference to the Guest Student role object
+	 *
+	 * @access protected
+	 * @since $$next-version$$
+	 *
+	 * @var string
+	 */
+	protected $guest_student_role = 'guest_student';
+
+	/**
 	 * Sensei_Guest_User constructor.
 	 *
 	 * @since $$next-version$$
 	 */
-	function __construct() {
+	public function __construct() {
+
+		// Check if the Guest Student role exists.
+		$guest_role = get_role( $this->guest_student_role );
+
+		// If Guest Student is not a valid WordPress role create it.
+		if ( ! is_a( $guest_role, 'WP_Role' ) ) {
+			// Create the role.
+			add_role( $this->guest_student_role, __( 'Guest Student', 'sensei-lms' ) );
+		}
+
 		add_action( 'wp', array( $this, 'sensei_create_guest_user_for_open_course' ), 9 );
 	}
 
@@ -30,19 +59,24 @@ class Sensei_Guest_User {
 	public function sensei_create_guest_user_for_open_course() {
 		global $post;
 
-		// Create guest user for open course.
+		// Conditionally create guest user for open course.
 		if (
 			is_singular( 'course' )
 			&& isset( $_POST['course_start'] )
-			&& wp_verify_nonce( $_POST['woothemes_sensei_start_course_noonce'], 'woothemes_sensei_start_course_noonce' )
+			&& isset( $_POST['woothemes_sensei_start_course_noonce'] )
+			&& wp_verify_nonce( wp_unslash( $_POST['woothemes_sensei_start_course_noonce'] ), 'woothemes_sensei_start_course_noonce' ) // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized -- Don't modify the nonce.
 			&& ! post_password_required( $post->ID )
 			&& ! is_user_logged_in()
 			&& get_post_meta( $post->ID, 'open_access', true )
 		) {
-			error_log('wtf1');
-			$user_name = 'guest_user_'. rand( 10000000, 99999999 ) . '_' . get_user_count();
-			$user_id = wp_create_user( $user_name, $user_name, $user_name . '@senseiguest.senseiguest' );
-			error_log( $user_id );
+			$user_name = 'guest_user_' . wp_rand( 10000000, 99999999 ) . '_' . get_user_count();
+			$user_id   = wp_create_user( $user_name, $user_name, $user_name . '@senseiguest.senseiguest' );
+
+			// Set the role of the Guest Student.
+			$user = new WP_User( $user_id );
+			$user->set_role( $this->guest_student_role );
+
+			// Log the newly created user in.
 			wp_set_current_user( $user_id );
 			wp_set_auth_cookie( $user_id, true );
 		}
