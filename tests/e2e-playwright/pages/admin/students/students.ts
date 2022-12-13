73c084391bf2051fc533d3178989b1f9f8edd990
Gabriel Caires <gabriel.caires@automattic.com>
2022-12-13 16:54:44 -0300

    Convert student page module

--- tests/e2e-playwright/pages/admin/students/students.ts
+++ tests/e2e-playwright/pages/admin/students/students.ts
@@ -1,8 +1,12 @@
+import type { Locator, Page, Response } from '@playwright/test';
+
 const PATH = '/wp-admin/admin.php?page=sensei_learners';
 
 class StudentsModalFragment {
-	constructor( locator ) {
-		this.base = locator;
+	addToCourseButton: Locator;
+	removeFromCourseButton: Locator;
+
+	constructor( private base: Locator ) {
 		this.addToCourseButton = this.base.locator(
 			'button:has-text("Add to Course")'
 		);
@@ -11,14 +15,16 @@ class StudentsModalFragment {
 			'button:has-text("Remove from Course")'
 		);
 	}
-	async selectCourse( courseName ) {
+	async selectCourse( courseName: string ) {
 		this.base.locator( `label:has-text("${ courseName }")` ).check();
 	}
 }
 
-class StudentsPage {
-	constructor( page ) {
-		this.page = page;
+export default class StudentsPage {
+	modal: StudentsModalFragment;
+	enrolledCoursesColumn: Locator;
+
+	constructor( private page: Page ) {
 		this.modal = new StudentsModalFragment(
 			page.locator( '[role=dialog]' )
 		);
@@ -28,16 +34,19 @@ class StudentsPage {
 	}
 
 	// Migrate to role selector as soon it is available.
-	async getRowByStudent( studentName ) {
+	async getRowByStudent( studentName: string ): Promise< Locator > {
 		return this.page.locator(
 			`tr:has([data-user-name="${ studentName }"])`
 		);
 	}
-	async goTo() {
+	async goTo(): Promise< Response > {
 		return this.page.goto( PATH );
 	}
 
-	async openStudentAction( studentName, action ) {
+	async openStudentAction(
+		studentName: string,
+		action: string
+	): Promise< void > {
 		await this.page
 			.locator( `[data-user-name="${ studentName }"]` )
 			.locator( `button[aria-label="Select an action"]` )
@@ -46,5 +55,3 @@ class StudentsPage {
 		return this.page.locator( `button:has-text("${ action }")` ).click();
 	}
 }
-
-module.exports = StudentsPage;
