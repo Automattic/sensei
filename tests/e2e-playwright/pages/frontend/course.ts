5f08fe3b2c22ea20e3c622a4a254b0f00f38d56c
Gabriel Caires <gabriel.caires@automattic.com>
2022-12-19 19:10:16 -0300

    Use student to test the lesson rendering

--- tests/e2e-playwright/pages/frontend/course.ts
+++ tests/e2e-playwright/pages/frontend/course.ts
@@ -4,8 +4,11 @@
 import { Page } from '@playwright/test';
 
 import type { Locator } from '@playwright/test';
+import { getContextByRole } from '@e2e/helpers/context';
 export class CoursePage {
-	constructor( private page: Page, private link?: string ) {}
+	constructor( private page: Page, private link?: string ) {
+		page.context().storageState( { path: getContextByRole( 'student' ) } );
+	}
 
 	async goTo(): Promise< void > {
 		this.page.goto( this.link );
--- tests/e2e-playwright/specs/admin/teacher/lessons/lesson-creation.spec.ts
+++ tests/e2e-playwright/specs/admin/teacher/lessons/lesson-creation.spec.ts
@@ -7,12 +7,14 @@ import { expect } from '@playwright/test';
 import LessonList from '@e2e/pages/admin/lessons';
 import { test } from './fixture';
 import { CoursePage as FrontEndCoursePage } from '@e2e/pages/frontend/course';
+import { getContextByRole } from '@e2e/helpers/context';
 
 const { describe } = test;
 
 describe.serial( 'Create Default Lesson', () => {
 	test( 'creates a lesson for a course', async ( {
 		page,
+		browser,
 		approvedCourse: course,
 	} ) => {
 		const lessonList = new LessonList( page );
@@ -31,10 +33,14 @@ describe.serial( 'Create Default Lesson', () => {
 
 		await lessonEdit.publish();
 
-		const coursePage = new FrontEndCoursePage( page, course.link );
+		const coursePage = new FrontEndCoursePage(
+			await browser.newPage( {
+				storageState: getContextByRole( 'student' ),
+			} ),
+			course.link
+		);
 		await coursePage.goTo();
 		await coursePage.takeCourse.click();
-
 		await expect( page.getByText( 'Test Lesson One' ) ).toBeVisible();
 		await expect( page.getByText( 'Test Lesson Content' ) ).toBeVisible();
 	} );
