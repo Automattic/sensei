eea3a403314700defcbcdc34d7d919bcb2b52caf
Renatho De Carli Rosa <renatho@gmail.com>
2023-01-05 14:54:51 -0300

    Add extra check to make sure the request is coming from the admin

--- includes/3rd-party/divi.php
+++ includes/3rd-party/divi.php
@@ -11,7 +11,13 @@
  */
 function sensei_fix_divi_yoast_conflict() {
 	if ( isset( $_GET['et_block_layout_preview'] ) && '1' === $_GET['et_block_layout_preview'] ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-		remove_action( 'plugins_loaded', 'wpseo_init', 14 );
+		$admin_url        = get_admin_url();
+		$admin_url_length = strlen( $admin_url );
+
+		// Check if the request is coming from the admin.
+		if ( isset( $_SERVER['HTTP_REFERER'] ) && substr( esc_url_raw( wp_unslash( $_SERVER['HTTP_REFERER'] ) ), 0, $admin_url_length ) === $admin_url ) {
+			remove_action( 'plugins_loaded', 'wpseo_init', 14 );
+		}
 	}
 }
 add_action( 'plugins_loaded', 'sensei_fix_divi_yoast_conflict', 13 );
