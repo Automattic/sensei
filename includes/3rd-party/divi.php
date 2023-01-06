86d4422ec55e7a6e0d9c6378e2e04b6bb1fe43e4
Renatho De Carli Rosa <renatho@gmail.com>
2023-01-05 23:40:58 -0300

    Add extra check to remove action only on lessons with learning mode enabled

--- includes/3rd-party/divi.php
+++ includes/3rd-party/divi.php
@@ -12,9 +12,13 @@
 function sensei_fix_divi_yoast_conflict() {
 	if (
 		isset( $_GET['et_block_layout_preview'] ) && '1' === $_GET['et_block_layout_preview'] // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		&& 'lesson' === get_post_type()
 		&& current_user_can( 'edit_post', get_the_id() )
 	) {
-		remove_all_actions( 'wpseo_head' );
+		$course_id = Sensei_Utils::get_current_course();
+		if ( Sensei_Course_Theme_Option::has_learning_mode_enabled( $course_id ) ) {
+			remove_all_actions( 'wpseo_head' );
+		}
 	}
 }
 add_action( 'wp', 'sensei_fix_divi_yoast_conflict', 13 );
