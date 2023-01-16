e834d1025757c111ef153b32929bb4b07f665ece
Dmitry Merkushin <merkushin@gmail.com>
2023-01-16 21:43:33 +0700

    Fix an issue after merging conflicts

--- includes/internal/student-progress/quiz-progress/repositories/class-comments-based-quiz-progress-repository.php
+++ includes/internal/student-progress/quiz-progress/repositories/class-comments-based-quiz-progress-repository.php
@@ -143,55 +143,6 @@ class Comments_Based_Quiz_Progress_Repository implements Quiz_Progress_Repositor
 	public function delete( Quiz_Progress $quiz_progress ): void {
 		Sensei_Utils::sensei_delete_quiz_answers( $quiz_progress->get_quiz_id(), $quiz_progress->get_user_id() );
 	}
- 
-	/**
-	 * Delete all quiz progress for a given quiz.
-	 *
-	 * @internal
-	 *
-	 * @param int $quiz_id Quiz identifier.
-	 */
-	public function delete_for_quiz( int $quiz_id ): void {
-		$lesson_id = Sensei()->quiz->get_lesson_id( $quiz_id );
-
-		$activity_args = [
-			'post_id' => $lesson_id,
-			'type'    => 'sensei_lesson_status',
-		];
-		$comments      = Sensei_Utils::sensei_check_for_activity( $activity_args, true );
-		foreach ( $comments as $comment ) {
-			$this->delete_grade_and_answers( $comment->comment_ID );
-			Sensei_Utils::sensei_delete_quiz_answers( $quiz_id, $comment->user_id );
-		}
-	}
-
-	/**
-	 * Delete all quiz grades and answers for a user.
-	 *
-	 * @internal
-	 *
-	 * @param int $user_id User identifier.
-	 */
-	public function delete_for_user( int $user_id ): void {
-		$activity_args = [
-			'user_id' => $user_id,
-			'type'    => 'sensei_lesson_status',
-		];
-		$comments      = Sensei_Utils::sensei_check_for_activity( $activity_args, true );
-		foreach ( $comments as $comment ) {
-			$this->delete_grade_and_answers( $comment->comment_ID );
-		}
-	}
-
-	/**
-	 * Delete the quiz grade and answers.
-	 *
-	 * @param int $comment_id Comment identifier.
-	 */
-	private function delete_grade_and_answers( $comment_id ): void {
-		delete_comment_meta( $comment_id, 'quiz_answers' );
-		delete_comment_meta( $comment_id, 'grade' );
-	}
 
 	/**
 	 * Delete all quiz progress for a given quiz.
