c5c9e87d014cdd6a2ea7e92cd16bde8ac30841e3
Imran Hossain <imran.hossain@automattic.com>
2023-01-18 22:07:23 +0600

    Render archive page content inside archive template

--- templates/archive-course.php
+++ templates/archive-course.php
@@ -31,26 +31,62 @@
 		?>
 	</div>
 
-	<?php if ( have_posts() ) : ?>
+	<?php
 
-		<?php sensei_load_template( 'loop-course.php' ); ?>
+	$sensei_settings_course_page = get_post( Sensei()->settings->get( 'course_page' ) );
 
-	<?php else : ?>
+	if (
+		is_a( $sensei_settings_course_page, 'WP_Post' ) &&
+		! Sensei()->post_types->has_old_shortcodes( $sensei_settings_course_page->post_content ) &&
+		! empty( $sensei_settings_course_page->post_content ) &&
+		has_block( 'core/query', $sensei_settings_course_page->post_content )
+	) {
+		echo wp_kses(
+			do_blocks( $sensei_settings_course_page->post_content ),
+			array_merge(
+				wp_kses_allowed_html( 'post' ),
+				[
+					'option' => [
+						'selected' => [],
+						'value'    => [],
+					],
+					'select' => [
+						'class'          => [],
+						'id'             => [],
+						'name'           => [],
+						'data-param-key' => [],
+					],
+					'form'   => [
+						'action' => [],
+						'method' => [],
+					],
+					'input'  => [
+						'type'  => [],
+						'name'  => [],
+						'value' => [],
+					],
+				]
+			)
+		);
+
+		remove_all_actions( 'sensei_pagination' );
+	} elseif ( have_posts() ) {
+		sensei_load_template( 'loop-course.php' );
+	} else {
+		?>
 
 		<p><?php esc_html_e( 'No courses found that match your selection.', 'sensei-lms' ); ?></p>
 
-	<?php endif; ?>
+		<?php
+	}
 
-	<?php
+	/**
+	 * This action runs after including the course archive loop. This hook fires within the archive-course.php
+	 * It fires even if the current archive has no posts.
+	 *
+	 * @since 1.9.0
+	 */
+	do_action( 'sensei_archive_after_course_loop' );
 
-		/**
-		 * This action runs after including the course archive loop. This hook fires within the archive-course.php
-		 * It fires even if the current archive has no posts.
-		 *
-		 * @since 1.9.0
-		 */
-		do_action( 'sensei_archive_after_course_loop' );
-
-	?>
-
-<?php get_sensei_footer(); ?>
+	get_sensei_footer();
+?>
