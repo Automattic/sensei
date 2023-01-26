4930b9fd0e61c72d941df475cbdc8ac929757195
onubrooks <abahonuh@gmail.com>
2023-01-26 17:00:06 +0100

    create skeletal class where the integration happens

---
+++ includes/class-sensei-mailpoet.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * File containing the class Sensei_MailPoet.
+ *
+ * @package sensei
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( \MailPoet\API\API::class ) ) {
+	exit;
+}
+
+/**
+ * MailPoet integration class.
+ *
+ * Handles the integration with the MailPoet plugin,
+ * creates a list for each course and group, adds enrolled students.
+ *
+ *
+ * @package Core
+ * @since $$next-version$$
+ */
+class Sensei_MailPoet {
+
+	/**
+	 * MailPoet handle.
+	 *
+	 * @var object
+	 */
+	private $mailpoet_api = null;
+
+	/**
+	 * Constructor
+	 *
+	 * @since 1.9.0
+	 */
+	public function __construct() {
+		if ( class_exists( \MailPoet\API\API::class ) ) {
+			$this->mailpoet_api = \MailPoet\API\API::MP( 'v1' );
+			if ( $this->mailpoet_api->isSetupComplete() ) {
+				 // Todo: maybe exit here?
+			}
+		}
+	}
+
+	/**
+	 * Create a list on MailPoet for a course/group.
+	 *
+	 */
+	public function create_list() {
+	}
+
+	/**
+	 * Get all groups and courses in Sensei.
+	 *
+	 */
+	public function get_sensei_courses_groups() {
+	}
+
+	/**
+	 * Get all lists in MailPoet. Separates them into groups and courses.
+	 *
+	 */
+	public function get_mailpoet_lists() {
+	}
+
+	/**
+	 * Compares MailPoet lists with courses and groups on Sensei site.
+	 *
+	 */
+	public function compare_courses_groups() {
+	}
+
+	/**
+	 * Sync MailPoet lists with Sensei site courses and grooups.
+	 *
+	 */
+	public static function sync_lists() {
+	}
+
+	/**
+	 * Sync MailPoet list students with Sensei site courses and groups students.
+	 *
+	 */
+	public static function sync_enrolles() {
+	}
+}
