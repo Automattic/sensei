7ba579a4394e493627853e2f36b6a5a7e43026a3
Gabriel Caires <gabriel.caires@automattic.com>
2023-02-10 14:30:02 +0000

    Fix lint issues

--- tests/unit-tests/internal/emails/test-class-email-blocks.php
+++ tests/unit-tests/internal/emails/test-class-email-blocks.php
@@ -28,27 +28,27 @@ class Email_Blocks_Test extends \WP_UnitTestCase {
 
 	public function testSetAllowedBlocks_WhenCalledWithANonEmailPostType_ReturnDefaultBlocks() {
 		/* Arrange. */
-		$blocks = new Email_Blocks();
-		$default_block = ['core/a', 'core/b', 'core/c'];
-		$block_editor_context = new \WP_Block_Editor_Context(['post' => new WP_Post((object) array('post_type' => 'any_post_type'))]);
+		$blocks               = new Email_Blocks();
+		$default_block        = [ 'core/a', 'core/b', 'core/c' ];
+		$block_editor_context = new \WP_Block_Editor_Context( [ 'post' => new WP_Post( (object) array( 'post_type' => 'any_post_type' ) ) ] );
 
 		/* Act. */
-		$allowed_blocks =  $blocks->set_allowed_blocks( $default_block, $block_editor_context);
+		$allowed_blocks = $blocks->set_allowed_blocks( $default_block, $block_editor_context );
 
 		/* Assert. */
-		self::assertSame( $allowed_blocks, $default_block);
+		self::assertSame( $allowed_blocks, $default_block );
 	}
 
 	public function testSetAllowedBlocks_WhenCalledWithEmailPostType_ReturnAllowedBlocks() {
 		/* Arrange. */
-		$blocks = new Email_Blocks();
-		$default_block = ['core/block-a', 'core/block-b', 'core/block-c'];
-		$block_editor_context = new \WP_Block_Editor_Context(['post' => new WP_Post((object) array('post_type' => Email_Post_Type::POST_TYPE ))]);
+		$blocks               = new Email_Blocks();
+		$default_block        = [ 'core/block-a', 'core/block-b', 'core/block-c' ];
+		$block_editor_context = new \WP_Block_Editor_Context( [ 'post' => new WP_Post( (object) array( 'post_type' => Email_Post_Type::POST_TYPE ) ) ] );
 
 		/* Act. */
-		$allowed_blocks = $blocks->set_allowed_blocks( $default_block, $block_editor_context);
+		$allowed_blocks = $blocks->set_allowed_blocks( $default_block, $block_editor_context );
 
 		/* Assert. */
-		self::assertSame( $allowed_blocks, Email_Blocks::ALLOWED_BLOCKS);
+		self::assertSame( $allowed_blocks, Email_Blocks::ALLOWED_BLOCKS );
 	}
 }
