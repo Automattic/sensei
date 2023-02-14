789a32c03073434e0839dcd8c0c4cb64ed92be30
Imran Hossain <imranh920@gmail.com>
2023-02-15 02:38:00 +0600

    Test email generator class

---
+++ tests/unit-tests/internal/emails/test-class-email-generator.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace SenseiTest\Internal\Emails;
+
+use Sensei\Internal\Emails\Email_Generator;
+use Sensei_Factory;
+
+/**
+ * Tests for Sensei\Internal\Emails\Email_Generator class.
+ *
+ * @covers \Sensei\Internal\Emails\Email_Generator
+ */
+class Email_Generator_Test extends \WP_UnitTestCase {
+
+	/**
+	 * Factory for creating test data.
+	 *
+	 * @var Sensei_Factory
+	 */
+	protected $factory;
+
+	public function setUp(): void {
+		parent::setUp();
+
+		$this->factory = new Sensei_Factory();
+	}
+
+	public function testGenerateEmail_WhenCalledByStudentStartCourseEvent_CallsEmailSendingActionWithRightData() {
+		/* Arrange. */
+		$student_id = $this->factory->user->create(
+			[
+				'display_name' => 'Test Student',
+			]
+		);
+		$teacher_id = $this->factory->user->create(
+			[
+				'user_email' => 'test@a.com',
+			]
+		);
+		$course     = $this->factory->course->create_and_get(
+			[
+				'post_title'  => 'Test Course',
+				'post_author' => $teacher_id,
+			]
+		);
+		( new Email_Generator() )->init();
+
+		$email_data = [
+			'name' => '',
+			'data' => null,
+		];
+
+		add_action(
+			'sensei_send_html_email',
+			function ( $email_name, $replacements ) use ( &$email_data ) {
+				$email_data['name'] = $email_name;
+				$email_data['data'] = $replacements;
+			},
+			10,
+			2
+		);
+
+		/* Act. */
+		do_action( 'sensei_user_course_start', $student_id, $course->ID );
+
+		/* Assert. */
+		self::assertEquals( 'student_started_course_to_teacher', $email_data['name'] );
+		self::assertArrayHasKey( 'test@a.com', $email_data['data'] );
+		self::assertEquals( 'Test Student', $email_data['data']['test@a.com']['student:displayname'] );
+		self::assertEquals( 'Test Course', $email_data['data']['test@a.com']['course.name'] );
+	}
+}
