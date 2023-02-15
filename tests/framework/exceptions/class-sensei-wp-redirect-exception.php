699acba5644b30b7e62b4c37ef68289334aedadd
Miroslav Mitev <m1r0@users.noreply.github.com>
2023-02-16 01:48:16 +0200

    Add test `wp_redirect` helper trait

---
+++ tests/framework/exceptions/class-sensei-wp-redirect-exception.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * File containing the Sensei_WP_Redirect_Exception.
+ *
+ * @package sensei-tests
+ */
+
+/**
+ * Exception for cases of wp_redirect().
+ */
+class Sensei_WP_Redirect_Exception extends Exception {}
