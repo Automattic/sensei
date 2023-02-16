a8ea182e28018f2536a78bc55c1fcc5383a8107a
Imran Hossain <imranh920@gmail.com>
2023-02-16 19:20:15 +0600

    Add tests for block settings remover frontend

--- assets/blocks/email-editor.js
+++ assets/blocks/email-editor.js
@@ -3,35 +3,40 @@
  */
 import { addFilter } from '@wordpress/hooks';
 
-addFilter(
-	'blocks.registerBlockType',
-	'sensei-lms/email-blocks',
-	removeIrrelevantSettings
-);
+export default function handleEmailBlocksEditor() {
+	addFilter(
+		'blocks.registerBlockType',
+		'sensei-lms/email-blocks',
+		removeIrrelevantSettings,
+		10
+	);
 
-/**
- * Update the blocks to remove extra settings for emails.
- *
- * @param {Object} settings Block settings.
- */
-function removeIrrelevantSettings( settings ) {
-	if (
-		settings &&
-		settings.supports &&
-		settings.supports.typography &&
-		settings.supports.typography.__experimentalFontFamily
-	) {
-		settings = {
-			...settings,
-			supports: {
-				...settings.supports,
-				typography: {
-					...settings.supports.typography,
-					__experimentalFontFamily: false,
+	/**
+	 * Update the blocks to remove extra settings when used in email editor.
+	 *
+	 * @param {Object} settings Block settings.
+	 */
+	function removeIrrelevantSettings( settings ) {
+		if (
+			settings &&
+			settings.supports &&
+			settings.supports.typography &&
+			settings.supports.typography.__experimentalFontFamily
+		) {
+			settings = {
+				...settings,
+				supports: {
+					...settings.supports,
+					typography: {
+						...settings.supports.typography,
+						__experimentalFontFamily: false,
+					},
 				},
-			},
-		};
-	}
+			};
+		}
 
-	return settings;
+		return settings;
+	}
 }
+
+handleEmailBlocksEditor();
