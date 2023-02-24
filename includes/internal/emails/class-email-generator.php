c92f280dc6bc0f4ca3c75e99677ae383282d41c1
Imran Hossain <imranh920@gmail.com>
2023-02-25 00:29:21 +0600

    Add email generation for course complete event

--- includes/internal/emails/class-email-generator.php
+++ includes/internal/emails/class-email-generator.php
@@ -7,8 +7,6 @@
 
 namespace Sensei\Internal\Emails;
 
-use function Sensei_WC_Paid_Courses\E2e_Test_Data\Helpers\create_post;
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
@@ -27,6 +25,7 @@ class Email_Generator {
 	 */
 	public function init(): void {
 		add_action( 'sensei_user_course_start', [ $this, 'student_started_course_mail_to_teacher' ], 10, 2 );
+		add_action( 'sensei_course_status_updated', [ $this, 'student_completed_course_mail_to_teacher' ], 10, 3 );
 	}
 
 	/**
@@ -58,6 +57,58 @@ class Email_Generator {
 				$recipient => [
 					'student:displayname' => $student->display_name,
 					'course.name'         => $course->post_title,
+				],
+			]
+		);
+	}
+
+	/**
+	 * Send email to teacher when a student completes a course.
+	 *
+	 * @param string $status      The status.
+	 * @param int    $student_id  The learner ID.
+	 * @param int    $course_id   The course ID.
+	 *
+	 * @access private
+	 */
+	public function student_completed_course_mail_to_teacher( $status = 'in-progress', $student_id = 0, $course_id = 0 ) {
+
+		if ( 'complete' !== $status || ! \Sensei_Course::is_user_enrolled( $course_id, $student_id ) ) {
+			return;
+		}
+
+		$email_type = 'student_completes_course';
+		$student    = new \WP_User( $student_id );
+		$teacher_id = get_post_field( 'post_author', $course_id, 'raw' );
+		$teacher    = new \WP_User( $teacher_id );
+		$recipient  = stripslashes( $teacher->user_email );
+		$grade      = __( 'N/A', 'sensei-lms' );
+		$lesson_ids = \Sensei()->course->course_lessons( $course_id, 'any', 'ids' );
+		$manage_url = esc_url(
+			add_query_arg(
+				array(
+					'page'      => 'sensei_learners',
+					'course_id' => $course_id,
+					'view'      => 'learners',
+				),
+				admin_url( 'admin.php' )
+			)
+		);
+
+		if ( ! empty( $lesson_ids ) && \Sensei()->course->course_quizzes( $course_id, true ) ) {
+			$grade = \Sensei_Utils::sensei_course_user_grade( $course_id, $student_id ) . '%';
+		}
+
+		$this->send_email_action(
+			$email_type,
+			[
+				$recipient => [
+					'student:id'          => $student_id,
+					'student:displayname' => $student->display_name,
+					'course:id'           => $course_id,
+					'course:name'         => get_the_title( $course_id ),
+					'grade:percentage'    => $grade,
+					'manage.students'     => $manage_url, // Emogrifier breaks the URL if : is used.
 				],
 			]
 		);
