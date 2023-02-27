a914ad47df72f7c41903660a93062bbd863206b6
Imran Hossain <imranh920@gmail.com>
2023-02-27 07:19:12 +0600

    Add test for disabling legacy email

--- tests/unit-tests/internal/emails/test-class-email-customization.php
+++ tests/unit-tests/internal/emails/test-class-email-customization.php
@@ -21,4 +21,44 @@ class Email_Customization_Test extends \WP_UnitTestCase {
 		/* Assert. */
 		$this->assertInstanceOf( Email_Customization::class, $result );
 	}
+
+	public function testInstace_WhenInitiated_AddsHookForRemovingLegacyEmail() {
+		/* Arrange. */
+		$settings = $this->createMock( Sensei_Settings::class );
+		$instance = Email_Customization::instance( $settings );
+
+		/* Act. */
+		$instance->init();
+
+		/* Assert. */
+		$this->assertEquals( 10, has_action( 'init', [ $instance, 'disable_legacy_emails' ] ) );
+	}
+
+	/**
+	 * Tests that the legacy email hooks are removed when the disable_legacy_emails function is called.
+	 *
+	 * @param string $action_name The name of the action.
+	 * @param string $function_name The name of the function.
+	 *
+	 * @dataProvider legacyHooksDataProvider
+	 */
+	public function testDisableLegacy_WhenCalled_RemovesLegacyEmailHooks( $action_name, $function_name ) {
+		/* Arrange. */
+		$settings        = $this->createMock( Sensei_Settings::class );
+		$instance        = Email_Customization::instance( $settings );
+		$priority_before = has_action( $action_name, [ \Sensei()->emails, $function_name ] );
+
+		/* Act. */
+		$instance->disable_legacy_emails();
+
+		/* Assert. */
+		$priority_after = has_action( $action_name, [ \Sensei()->emails, $function_name ] );
+		$this->assertNotEquals( $priority_before, $priority_after );
+	}
+
+	public function legacyHooksDataProvider() {
+		return [
+			'student_completes_course' => [ 'sensei_course_status_updated', 'teacher_completed_course' ],
+		];
+	}
 }
