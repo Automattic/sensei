cac398d4b65b40b9c60fc48f6a6da22f70071bc4
Imran Hossain <imranh920@gmail.com>
2023-03-01 09:09:06 +0600

    Remove existing legacy email

--- includes/internal/emails/class-email-customization.php
+++ includes/internal/emails/class-email-customization.php
@@ -158,5 +158,6 @@ class Email_Customization {
 		remove_action( 'sensei_course_status_updated', [ \Sensei()->emails, 'teacher_completed_course' ] );
 		remove_action( 'sensei_user_course_start', [ \Sensei()->emails, 'teacher_started_course' ] );
 		remove_action( 'sensei_user_quiz_submitted', [ \Sensei()->emails, 'teacher_quiz_submitted' ] );
+		remove_action( 'sensei_course_status_updated', [ \Sensei()->emails, 'learner_completed_course' ] );
 	}
 }
