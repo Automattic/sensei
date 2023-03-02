4147ae55b6e29e2bcdecfc37be8295ec97518c0b
Imran Hossain <imranh920@gmail.com>
2023-03-02 12:38:14 +0600

    Test disabling legacy email for quiz grade

--- tests/unit-tests/internal/emails/test-class-email-customization.php
+++ tests/unit-tests/internal/emails/test-class-email-customization.php
@@ -64,6 +64,7 @@ class Email_Customization_Test extends \WP_UnitTestCase {
 			'student_quiz_submitted'   => [ 'sensei_user_quiz_submitted', 'teacher_quiz_submitted', \Sensei()->emails ],
 			'course_completed'         => [ 'sensei_course_status_updated', 'learner_completed_course', \Sensei()->emails ],
 			'teacher_course_assigned'  => [ 'sensei_course_new_teacher_assigned', 'teacher_course_assigned_notification', \Sensei()->teacher ],
+			'quiz_graded'              => [ 'sensei_user_quiz_grade', 'learner_graded_quiz', \Sensei()->emails ],
 		];
 	}
 }
