02f24e4c81c29251e3bd2a034cd795f55f9bcb7a
Imran Hossain <imranh920@gmail.com>
2023-03-03 14:10:40 +0600

    Add teacher message reply generator in autoloader

--- includes/class-sensei-autoloader.php
+++ includes/class-sensei-autoloader.php
@@ -309,6 +309,7 @@ class Sensei_Autoloader {
 			\Sensei\Internal\Emails\Student_Submits_Quiz::class => 'internal/emails/generators/class-student-submits-quiz.php',
 			\Sensei\Internal\Emails\Course_Completed::class => 'internal/emails/generators/class-course-completed.php',
 			\Sensei\Internal\Emails\New_Course_Assigned::class => 'internal/emails/generators/class-new-course-assigned.php',
+			\Sensei\Internal\Emails\Teacher_Message_Reply::class => 'internal/emails/generators/class-teacher-message-reply.php',
 		);
 	}
 
