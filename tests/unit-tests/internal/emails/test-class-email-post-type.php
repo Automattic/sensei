a086758b4d2808fe262db10ca61a501f3a3e589e
Imran Hossain <imranh920@gmail.com>
2023-03-14 14:39:42 +0600

    Test that user can delete if hook is not attached

--- tests/unit-tests/internal/emails/test-class-email-post-type.php
+++ tests/unit-tests/internal/emails/test-class-email-post-type.php
@@ -138,4 +138,23 @@ class Email_Post_Type_Test extends \WP_UnitTestCase {
 		/* Assert. */
 		$this->assertFalse( current_user_can( 'delete_post', $email_id ) );
 	}
+
+	public function testUserCap_WhenCalled_AllowsDeletingUnlessHookIsAttached() {
+		/* Arrange. */
+		$this->login_as_admin();
+		$email_post_type = new Email_Post_Type();
+		$email_post_type->register_post_type();
+		$email_id = $this->factory->post->create(
+			[
+				'post_type'   => 'sensei_email',
+				'post_status' => 'publish',
+			]
+		);
+
+		/* Act. */
+		remove_action( 'map_meta_cap', [ $email_post_type, 'map_meta_cap' ], 10 );
+
+		/* Assert. */
+		$this->assertTrue( current_user_can( 'delete_post', $email_id ) );
+	}
 }
