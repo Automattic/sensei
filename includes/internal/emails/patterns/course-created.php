540a66099516f10774cb92d8160ac02e4a8ec183
Imran Hossain <imranh920@gmail.com>
2023-03-22 13:07:27 +0600

    Update course created mail for mobile

--- includes/internal/emails/patterns/course-created.php
+++ includes/internal/emails/patterns/course-created.php
@@ -10,11 +10,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"40px","right":"56px","bottom":"56px","left":"56px"}},"color":{"background":"#ffffff"}}} -->
-<div class="wp-block-group has-background" style="background-color:#ffffff;padding-top:40px;padding-right:56px;padding-bottom:56px;padding-left:56px"><!-- wp:post-title {"style":{"typography":{"textTransform":"none","fontStyle":"normal","fontWeight":"700","fontSize":"40px"},"color":{"text":"#101517"},"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} /-->
 
-	<!-- wp:group {"style":{"color":{"background":"#f6f7f7"},"spacing":{"padding":{"top":"32px","right":"32px","bottom":"32px","left":"32px"},"margin":{"top":"48px"}}}} -->
-	<div class="wp-block-group has-background" style="background-color:#f6f7f7;margin-top:48px;padding-top:32px;padding-right:32px;padding-bottom:32px;padding-left:32px"><!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"},"spacing":{"margin":{"top":"0px","bottom":"0px"}},"color":{"text":"#101517"}}} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"40px","right":"56px","bottom":"56px","left":"56px"}},"color":{"background":"#ffffff"}},"className":"email-notification__content"} -->
+<div class="wp-block-group email-notification__content has-background" style="background-color:#ffffff;padding-top:40px;padding-right:56px;padding-bottom:56px;padding-left:56px"><!-- wp:post-title {"style":{"typography":{"textTransform":"capitalize","fontStyle":"normal","fontWeight":"700","fontSize":"40px"},"color":{"text":"#101517"},"spacing":{"margin":{"bottom":"48px"}}},"className":"email-notification__title"} /-->
+
+	<!-- wp:group {"style":{"color":{"background":"#f6f7f7"},"spacing":{"padding":{"top":"32px","right":"32px","bottom":"53px","left":"32px"},"margin":{"top":"48px","bottom":"0px"},"blockGap":"0px"}}} -->
+	<div class="wp-block-group has-background" style="background-color:#f6f7f7;margin-top:48px;margin-bottom:0px;padding-top:32px;padding-right:32px;padding-bottom:53px;padding-left:32px"><!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"},"spacing":{"margin":{"top":"0px","bottom":"0px"}},"color":{"text":"#101517"}}} -->
 		<p class="has-text-color" style="color:#101517;font-size:16px;margin-top:0px;margin-bottom:0px"><strong>Course Name</strong></p>
 		<!-- /wp:paragraph -->
 
@@ -31,8 +32,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<!-- /wp:paragraph -->
 
 		<!-- wp:buttons {"style":{"spacing":{"margin":{"top":"40px"}}}} -->
-		<div class="wp-block-buttons" style="margin-top:40px"><!-- wp:button {"style":{"spacing":{"padding":{"top":"16px","bottom":"16px","left":"20px","right":"20px"}},"color":{"background":"#101517","text":"#ffffff"},"typography":{"fontSize":"16px"},"border":{"radius":"0px"}}} -->
-			<div class="wp-block-button has-custom-font-size" style="font-size:16px"><a class="wp-block-button__link has-text-color has-background" href="[manage:course]" style="border-radius:0px;background-color:#101517;color:#ffffff;padding-top:16px;padding-right:20px;padding-bottom:16px;padding-left:20px">Manage course</a></div>
+		<div class="wp-block-buttons" style="margin-top:40px"><!-- wp:button {"style":{"color":{"text":"#fefefe","background":"#101517"},"typography":{"fontSize":"16px"}},"className":"has-custom-font-size"} -->
+			<div class="wp-block-button has-custom-font-size" style="font-size:16px"><a class="wp-block-button__link has-text-color has-background" href="[manage:course]" style="color:#fefefe;background-color:#101517">Manage course</a></div>
 			<!-- /wp:button -->
 		</div>
 		<!-- /wp:buttons -->
