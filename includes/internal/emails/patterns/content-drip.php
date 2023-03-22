fce0bdea00a55241df30b866e986957ecc0cfba0
Imran Hossain <imranh920@gmail.com>
2023-03-22 14:18:27 +0600

    Update update email group and buttons

--- includes/internal/emails/patterns/content-drip.php
+++ includes/internal/emails/patterns/content-drip.php
@@ -11,8 +11,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"40px","right":"56px","bottom":"56px","left":"56px"}},"color":{"background":"#ffffff"}},"className":"email-notification__content"} -->
-<div class="wp-block-group email-notification__content has-background" style="background-color:#ffffff;padding-top:40px;padding-right:56px;padding-bottom:56px;padding-left:56px"><!-- wp:post-title {"style":{"typography":{"textTransform":"capitalize","fontStyle":"normal","fontWeight":"700","fontSize":"40px"},"color":{"text":"#101517"},"spacing":{"margin":{"bottom":"48px"}}},"className":"email-notification__title"} /-->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"40px","right":"56px","left":"56px","bottom":"48px"}},"color":{"background":"#ffffff"}},"className":"email-notification__content"} -->
+<div class="wp-block-group email-notification__content has-background" style="background-color:#ffffff;padding-top:40px;padding-right:56px;padding-bottom:48px;padding-left:56px"><!-- wp:post-title {"style":{"typography":{"textTransform":"capitalize","fontStyle":"normal","fontWeight":"700","fontSize":"40px","lineHeight":1.2},"color":{"text":"#101517"},"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} /-->
 
 	<!-- wp:group {"style":{"color":{"background":"#f6f7f7"},"spacing":{"padding":{"top":"32px","right":"32px","bottom":"53px","left":"32px"},"margin":{"top":"48px","bottom":"0px"},"blockGap":"0px"}}} -->
 	<div class="wp-block-group has-background" style="background-color:#f6f7f7;margin-top:48px;margin-bottom:0px;padding-top:32px;padding-right:32px;padding-bottom:53px;padding-left:32px"><!-- wp:paragraph {"style":{"typography":{"fontSize":"32px"},"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}},"color":{"text":"#101517"}}} -->
@@ -27,8 +27,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<p class="has-text-color" style="color:#101517;font-size:16px;margin-top:0;margin-bottom:0">[lesson:name]</p>
 		<!-- /wp:paragraph -->
 
-		<!-- wp:buttons {"style":{"spacing":{"margin":{"top":"40px","bottom":"20px"}}}} -->
-		<div class="wp-block-buttons" style="margin-top:40px;margin-bottom:20px"><!-- wp:button {"style":{"color":{"background":"#020202","text":"#fefefe"},"typography":{"fontSize":"16px"}},"className":"has-custom-font-size"} -->
+		<!-- wp:buttons {"style":{"spacing":{"margin":{"top":"40px"}}}} -->
+		<div class="wp-block-buttons" style="margin-top:40px"><!-- wp:button {"style":{"color":{"background":"#020202","text":"#fefefe"},"typography":{"fontSize":"16px"}},"className":"has-custom-font-size"} -->
 			<div class="wp-block-button has-custom-font-size" style="font-size:16px"><a class="wp-block-button__link has-text-color has-background" href="[lesson:url]" style="color:#fefefe;background-color:#020202">View Lesson</a></div>
 			<!-- /wp:button --></div>
 		<!-- /wp:buttons --></div>
--- includes/internal/emails/patterns/course-created.php
+++ includes/internal/emails/patterns/course-created.php
@@ -11,8 +11,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"40px","right":"56px","bottom":"56px","left":"56px"}},"color":{"background":"#ffffff"}},"className":"email-notification__content"} -->
-<div class="wp-block-group email-notification__content has-background" style="background-color:#ffffff;padding-top:40px;padding-right:56px;padding-bottom:56px;padding-left:56px"><!-- wp:post-title {"style":{"typography":{"textTransform":"capitalize","fontStyle":"normal","fontWeight":"700","fontSize":"40px"},"color":{"text":"#101517"},"spacing":{"margin":{"bottom":"48px"}}},"className":"email-notification__title"} /-->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"40px","right":"56px","left":"56px","bottom":"48px"}},"color":{"background":"#ffffff"}},"className":"email-notification__content"} -->
+<div class="wp-block-group email-notification__content has-background" style="background-color:#ffffff;padding-top:40px;padding-right:56px;padding-bottom:48px;padding-left:56px"><!-- wp:post-title {"style":{"typography":{"textTransform":"capitalize","fontStyle":"normal","fontWeight":"700","fontSize":"40px","lineHeight":1.2},"color":{"text":"#101517"},"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} /-->
 
 	<!-- wp:group {"style":{"color":{"background":"#f6f7f7"},"spacing":{"padding":{"top":"32px","right":"32px","bottom":"53px","left":"32px"},"margin":{"top":"48px","bottom":"0px"},"blockGap":"0px"}}} -->
 	<div class="wp-block-group has-background" style="background-color:#f6f7f7;margin-top:48px;margin-bottom:0px;padding-top:32px;padding-right:32px;padding-bottom:53px;padding-left:32px"><!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"},"spacing":{"margin":{"top":"0px","bottom":"0px"}},"color":{"text":"#101517"}}} -->
