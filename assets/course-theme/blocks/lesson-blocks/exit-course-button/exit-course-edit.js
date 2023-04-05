f7751ba91be0a01fd06955a1d9100a5e5c4b79ac
Gabriel Caires <gabriel.caires@automattic.com>
2023-04-05 19:28:37 +0100

    Enable course link to use the link color

---
+++ assets/course-theme/blocks/lesson-blocks/exit-course-button/exit-course-edit.js
@@ -0,0 +1,19 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { useBlockProps } from '@wordpress/block-editor';
+
+export const ExitCourseEdit = () => {
+	const blockProps = useBlockProps();
+
+	return (
+		<a
+			{ ...blockProps }
+			href="#exit-course-button-pseudo-link"
+			onClick={ ( event ) => event.preventDefault() }
+		>
+			{ __( 'Exit Course', 'sensei-lms' ) }
+		</a>
+	);
+};
