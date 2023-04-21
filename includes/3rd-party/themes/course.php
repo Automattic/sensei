4eb587dcdc29dd5540c1e2fb2cdcc469d8574e65
Gabriel Caires <gabriel.caires@automattic.com>
2023-04-21 14:50:57 +0100

    Load learning mode style by theme

---
+++ includes/3rd-party/themes/course.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Adds additional compatibility with Automattic/course theme.
+ *
+ * @package 3rd-Party
+ */
+
+
+/**
+ * Load learning mode is enabled and the current theme is the automattic/course.
+ */
+
+function sensei_disable_learning_mode_style_for_course_theme() {
+	add_filter( 'internal_course_theme_use_learning_mode_style_from_sensei', '__return_true'  );
+}
+
+function sensei_load_learning_mode_style_for_course_theme() {
+	$course_id = Sensei_Utils::get_current_course();
+	$is_course_theme =  'course' === wp_get_theme()->get_template();
+
+	if( empty( $course_id ) || ! $is_course_theme ) {
+		return false;
+	}
+
+
+	if ( Sensei_Course_Theme_Option::has_learning_mode_enabled( $course_id )  ) {
+		Sensei()->assets->enqueue( 'course-learning-mode', 'css/3rd-party/themes/course/learning-mode.css');
+	}
+}
+
+add_action( 'wp', 'sensei_disable_learning_mode_style_for_course_theme' );
+add_action( 'wp_enqueue_scripts', 'sensei_load_learning_mode_style_for_course_theme' );
