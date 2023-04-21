2ffb06b718e19af6031a266c4ddb50729732d7be
Jake Oehler Morrison <s+github@jom.im>
2023-04-22 00:05:41 +0100

    One more missing docblock

--- includes/3rd-party/yoast.php
+++ includes/3rd-party/yoast.php
@@ -3,9 +3,19 @@
  * Adds additional compatibility with Yoast.
  *
  * @package 3rd-Party
- * @deprecated $$next-version$$
+ * @since $$next-version$$
  */
 
+/**
+ * Overrides the shortcodes that Yoast loads.
+ *
+ * @since $$next-version$$
+ *
+ * @param bool   $is_excluded Whether the post type is excluded.
+ * @param string $post_type The post type.
+ *
+ * @return bool Whether the post type is excluded.
+ */
 function sensei_wordpress_seo_exclude_some_sensei_cpts( $is_excluded, $post_type ) {
 	if ( in_array( $post_type, Sensei_PostTypes::SITEMAPS_EXCLUDED_PUBLIC_POST_TYPES, true ) ) {
 		return true;
