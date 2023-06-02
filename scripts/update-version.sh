3138c6c28b00d22782c8a0a364c877e1adfdfad8
Dmitry Merkushin <merkushin@gmail.com>
2023-06-02 13:55:40 +0400

    Update Sensei version constant

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -19,6 +19,11 @@ CURRENT_DIR=$(pwd)
 # Update version in sensei-lms.php
 sed -E -i'' "s/\* Version: [0-9]+\.[0-9]+\.[0-9]+/\* Version: $VERSION/" "$CURRENT_DIR/sensei-lms.php"
 
+# Find constant and replace the version there:
+#	define( 'SENSEI_LMS_VERSION', '4.15.0' ); // WRCS: DEFINED_VERSION.
+sed -E -i'' "s/'SENSEI_LMS_VERSION', '[0-9]+\.[0-9]+\.[0-9]+/'SENSEI_LMS_VERSION', '$VERSION/" "$CURRENT_DIR/sensei-lms.php"
+
+
 # Update first occurrence of version in package.json & package-lock.json
 sed -i'' "s/^  \"version\": \"[0-9]*\.[0-9]*\.[0-9]*\"/  \"version\": \"$VERSION\"/g" "$CURRENT_DIR/package.json"
 sed -i'' "s/^  \"version\": \"[0-9]*\.[0-9]*\.[0-9]*\"/  \"version\": \"$VERSION\"/g" "$CURRENT_DIR/package-lock.json"
