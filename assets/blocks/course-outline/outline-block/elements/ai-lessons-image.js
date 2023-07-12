9b931d05fea742ec2f9b91b818f66d2fdf474a5e
Gabriel Caires <gabriel.caires@automattic.com>
2023-07-12 16:54:16 +0100

    Refactor and fix layout alignment issues

---
+++ assets/blocks/course-outline/outline-block/elements/ai-lessons-image.js
@@ -0,0 +1,26 @@
+const AiLessonsImage = () => (
+	<div className="ai-course-outline-image">
+		<div className="ai-course-outline-image__item">
+			<div
+				style={ { width: '78%' } }
+				className="ai-course-outline-image__content"
+			/>
+		</div>
+
+		<div className="ai-course-outline-image__item">
+			<div
+				style={ { width: '47%' } }
+				className="ai-course-outline-image__content"
+			/>
+		</div>
+
+		<div className="ai-course-outline-image__item">
+			<div
+				style={ { width: '25%' } }
+				className="ai-course-outline-image__content"
+			/>
+		</div>
+	</div>
+);
+
+export default AiLessonsImage;
