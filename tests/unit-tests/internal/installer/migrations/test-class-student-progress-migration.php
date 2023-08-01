c6e18007ee3b9d7a7bee581a96526d0f2918e87c
Dmitry Merkushin <merkushin@gmail.com>
2023-08-01 03:56:11 +0300

    Fix linter issues

--- tests/unit-tests/internal/installer/migrations/test-class-student-progress-migration.php
+++ tests/unit-tests/internal/installer/migrations/test-class-student-progress-migration.php
@@ -13,6 +13,8 @@ use Sensei_Factory;
 class Student_Progress_Migration_Test extends \WP_UnitTestCase {
 
 	/**
+	 * Migration instance.
+	 *
 	 * @var \Sensei\Internal\Installer\Migrations\Student_Progress_Migration
 	 */
 	private $migration;
@@ -26,6 +28,7 @@ class Student_Progress_Migration_Test extends \WP_UnitTestCase {
 		$this->factory   = new Sensei_Factory();
 
 		global $wpdb;
+		// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
 		$wpdb->query( 'TRUNCATE TABLE ' . $wpdb->prefix . 'sensei_lms_progress' );
 	}
 
@@ -50,7 +53,7 @@ class Student_Progress_Migration_Test extends \WP_UnitTestCase {
 		$expected = 0;
 
 		/* Act. */
-		$actual = $this->migration->run( $dry_run = false );
+		$actual = $this->migration->run( $dry_run = false ); // phpcs:ignore Squiz.PHP.DisallowMultipleAssignments.Found
 
 		/* Assert. */
 		$this->assertEquals( $expected, $actual );
@@ -59,7 +62,12 @@ class Student_Progress_Migration_Test extends \WP_UnitTestCase {
 	public function testRun_CommentsExist_ReturnsMatchingNumberOfInserts(): void {
 		/* Arrange. */
 		$course_id = $this->factory->course->create( array( 'post_title' => 'Course 1' ) );
-		$lesson_id = $this->factory->lesson->create( array( 'post_title' => 'Lesson 1', 'post_parent' => $course_id ) );
+		$lesson_id = $this->factory->lesson->create(
+			array(
+				'post_title'  => 'Lesson 1',
+				'post_parent' => $course_id,
+			)
+		);
 
 		\Sensei_Utils::start_user_on_course( 1, $course_id );
 		\Sensei_Utils::user_start_lesson( 1, $lesson_id, true );
@@ -67,10 +75,11 @@ class Student_Progress_Migration_Test extends \WP_UnitTestCase {
 		update_option( 'sensei_migrated_progress_last_comment_id', 0 );
 
 		/* Act. */
-		$this->migration->run( $dry_run = false );
+		$this->migration->run( $dry_run = false ); // phpcs:ignore Squiz.PHP.DisallowMultipleAssignments.Found
 
 		/* Assert. */
 		global $wpdb;
+		// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
 		$actual_rows = $wpdb->get_var( "SELECT COUNT(*) FROM {$wpdb->prefix}sensei_lms_progress" );
 		$this->assertEquals( 2, $actual_rows );
 	}
