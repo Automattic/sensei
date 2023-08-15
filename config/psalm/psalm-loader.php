efa92673e414c1b53186fc853e7c40036b2c9c7a
Dmitry Merkushin <merkushin@gmail.com>
2023-08-15 16:03:39 +0200

    Fix linter issues

--- config/psalm/psalm-loader.php
+++ config/psalm/psalm-loader.php
@@ -1,8 +1,14 @@
 <?php
+/**
+ * File containing the psalm loader.
+ *
+ * @package sensei
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedConstantFound
+	define( 'ABSPATH', __DIR__ . '/../../' );
+}
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../vendor/php-stubs/wordpress-stubs/wordpress-stubs.php';
-
-if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/../../' );
-}
