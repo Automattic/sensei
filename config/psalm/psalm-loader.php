aa63fbc182f52fddf7f46b741de2273bcba50aa1
Dmitry Merkushin <merkushin@gmail.com>
2023-08-15 14:27:41 +0200

    Add Psalm

---
+++ config/psalm/psalm-loader.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/php-stubs/wordpress-stubs/wordpress-stubs.php';
+
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/../../' );
+}
