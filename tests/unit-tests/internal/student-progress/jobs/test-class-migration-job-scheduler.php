393fc4825a5a785e26063b0d4cab4d6036f56442
Miroslav Mitev <m1r0@users.noreply.github.com>
2023-08-21 22:46:39 +0300

    Add tests for the `Migration_Job_Scheduler` class

--- tests/unit-tests/internal/student-progress/jobs/test-class-migration-job-scheduler.php
+++ tests/unit-tests/internal/student-progress/jobs/test-class-migration-job-scheduler.php
@@ -2,6 +2,8 @@
 
 namespace SenseiTest\Internal\Student_Progress\Jobs;
 
+use Sensei\Internal\Action_Scheduler\Action_Scheduler;
+use Sensei\Internal\Student_Progress\Jobs\Migration_Job;
 use Sensei\Internal\Student_Progress\Jobs\Migration_Job_Scheduler;
 
 /**
@@ -10,5 +12,148 @@ use Sensei\Internal\Student_Progress\Jobs\Migration_Job_Scheduler;
  * @covers \Sensei\Internal\Student_Progress\Jobs\Migration_Job_Scheduler
  */
 class Migration_Job_Scheduler_Test extends \WP_UnitTestCase {
+	public function testInit_Always_AddsMigrationJobHook() {
+		/* Arrange. */
+		$action_scheduler = $this->createMock( Action_Scheduler::class );
+		$migration_job    = $this->createMock( Migration_Job::class );
+		$job_scheduler    = new Migration_Job_Scheduler( $action_scheduler, $migration_job );
 
+		$migration_job->method( 'get_job_name' )
+			->willReturn( 'foo' );
+
+		/* Act. */
+		$job_scheduler->init();
+
+		/* Assert. */
+		$this->assertSame(
+			10,
+			has_action( 'sensei_lms_jobs_foo', [ $job_scheduler, 'run_job' ] )
+		);
+	}
+
+	public function testSchedule_Always_SchedulesAction() {
+		/* Arrange. */
+		$action_scheduler = $this->createMock( Action_Scheduler::class );
+		$migration_job    = $this->createMock( Migration_Job::class );
+		$job_scheduler    = new Migration_Job_Scheduler( $action_scheduler, $migration_job );
+
+		$migration_job->method( 'get_job_name' )
+			->willReturn( 'foo' );
+
+		/* Assert. */
+		$action_scheduler
+			->expects( $this->once() )
+			->method( 'schedule_single_action' )
+			->with( 'sensei_lms_jobs_foo', [], false );
+
+		/* Act. */
+		$job_scheduler->schedule();
+	}
+
+	public function testRunJob_WhenFirstRun_UpdatesStartedOption() {
+		/* Arrange. */
+		$action_scheduler = $this->createMock( Action_Scheduler::class );
+		$migration_job    = $this->createMock( Migration_Job::class );
+		$job_scheduler    = new Migration_Job_Scheduler( $action_scheduler, $migration_job );
+
+		/* Act. */
+		$job_scheduler->run_job();
+
+		/* Assert. */
+		$this->assertIsFloat(
+			get_option( Migration_Job_Scheduler::STARTED_OPTION_NAME )
+		);
+	}
+
+	public function testRunJob_Always_RunsJob() {
+		/* Arrange. */
+		$action_scheduler = $this->createMock( Action_Scheduler::class );
+		$migration_job    = $this->createMock( Migration_Job::class );
+		$job_scheduler    = new Migration_Job_Scheduler( $action_scheduler, $migration_job );
+
+		/* Assert. */
+		$migration_job
+			->expects( $this->once() )
+			->method( 'run' );
+
+		/* Act. */
+		$job_scheduler->run_job();
+	}
+
+	public function testRunJob_WhenJobHasErrors_UpdatesErrorsOption() {
+		/* Arrange. */
+		$action_scheduler = $this->createMock( Action_Scheduler::class );
+		$migration_job    = $this->createMock( Migration_Job::class );
+		$job_scheduler    = new Migration_Job_Scheduler( $action_scheduler, $migration_job );
+
+		$migration_job->method( 'get_errors' )
+			->willReturn( [ 'error 1', 'error 2' ] );
+
+		/* Act. */
+		$job_scheduler->run_job();
+
+		/* Assert. */
+		$this->assertSame(
+			[ 'error 1', 'error 2' ],
+			get_option( Migration_Job_Scheduler::ERRORS_OPTION_NAME )
+		);
+	}
+
+	public function testRunJob_WhenJobIsComplete_UpdatesCompletedOption() {
+		/* Arrange. */
+		$action_scheduler = $this->createMock( Action_Scheduler::class );
+		$migration_job    = $this->createMock( Migration_Job::class );
+		$job_scheduler    = new Migration_Job_Scheduler( $action_scheduler, $migration_job );
+
+		$migration_job->method( 'is_complete' )
+			->willReturn( true );
+
+		/* Act. */
+		$job_scheduler->run_job();
+
+		/* Assert. */
+		$this->assertIsFloat(
+			get_option( Migration_Job_Scheduler::COMPLETED_OPTION_NAME )
+		);
+	}
+
+	public function testRunJob_WhenJobIsComplete_DoesntScheduleAction() {
+		/* Arrange. */
+		$action_scheduler = $this->createMock( Action_Scheduler::class );
+		$migration_job    = $this->createMock( Migration_Job::class );
+		$job_scheduler    = new Migration_Job_Scheduler( $action_scheduler, $migration_job );
+
+		$migration_job->method( 'is_complete' )
+			->willReturn( true );
+		$migration_job->method( 'get_job_name' )
+			->willReturn( 'foo' );
+
+		/* Assert. */
+		$action_scheduler
+			->expects( $this->never() )
+			->method( 'schedule_single_action' )
+			->with( 'sensei_lms_jobs_foo', [], false );
+
+		/* Act. */
+		$job_scheduler->run_job();
+	}
+
+	public function testRunJob_WhenJobIsNotComplete_SchedulesAction() {
+		/* Arrange. */
+		$action_scheduler = $this->createMock( Action_Scheduler::class );
+		$migration_job    = $this->createMock( Migration_Job::class );
+		$job_scheduler    = new Migration_Job_Scheduler( $action_scheduler, $migration_job );
+
+		$migration_job->method( 'get_job_name' )
+			->willReturn( 'foo' );
+
+		/* Assert. */
+		$action_scheduler
+			->expects( $this->once() )
+			->method( 'schedule_single_action' )
+			->with( 'sensei_lms_jobs_foo', [], false );
+
+		/* Act. */
+		$job_scheduler->run_job();
+	}
 }
