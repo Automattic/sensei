d9465cda7d250329455372f1aea778125081530c
Imran Hossain <imranh920@gmail.com>
2023-08-22 07:56:10 +0600

    Use block props to enable v2 features

--- assets/blocks/quiz/question-description-block/question-description.js
+++ assets/blocks/quiz/question-description-block/question-description.js
@@ -1,13 +1,9 @@
 /**
  * WordPress dependencies
  */
-import { InnerBlocks } from '@wordpress/block-editor';
+import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 import { compose } from '@wordpress/compose';
 import { __ } from '@wordpress/i18n';
-/**
- * External dependencies
- */
-import classnames from 'classnames';
 
 /**
  * Internal dependencies
@@ -18,10 +14,12 @@ import { withBlockMeta } from '../../../shared/blocks/block-metadata';
  * Question Description control.
  */
 const QuestionDescription = () => {
+	const blockProps = useBlockProps( {
+		className: 'sensei-lms-question-description-block',
+	} );
+
 	return (
-		<div
-			className={ classnames( 'sensei-lms-question-description-block' ) }
-		>
+		<div { ...blockProps }>
 			<InnerBlocks
 				template={ [
 					[
