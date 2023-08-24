5c100e98c2da441841021fcc20b302f23d2593b5
Imran Hossain <imranh920@gmail.com>
2023-08-24 18:14:07 +0600

    Remove label from multi line placeholder answer

--- assets/blocks/quiz/answer-blocks/multi-line.js
+++ assets/blocks/quiz/answer-blocks/multi-line.js
@@ -1,17 +1,9 @@
 /**
- * WordPress dependencies
- */
-import { __ } from '@wordpress/i18n';
-
-/**
  * Question block multi-line answer component.
  */
 const MultiLineAnswer = () => {
 	return (
 		<div className="sensei-lms-question-block__answer sensei-lms-question-block__answer--multi-line">
-			<small className="sensei-lms-question-block__input-label">
-				{ __( 'Answer:', 'sensei-lms' ) }
-			</small>
 			<div className="sensei-lms-question-block__text-input-placeholder multi-line" />
 		</div>
 	);
