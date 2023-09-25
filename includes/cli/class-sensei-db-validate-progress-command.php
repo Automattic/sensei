d2f1f215421d02593c71e8587f91443fb4c8a0b0
Miroslav Mitev <m1r0@users.noreply.github.com>
2023-09-25 18:08:40 +0300

    Refactor the validate progress command

--- includes/cli/class-sensei-db-validate-progress-command.php
+++ includes/cli/class-sensei-db-validate-progress-command.php
@@ -6,10 +6,15 @@
  */
 
 use Sensei\Internal\Migration\Migration_Job_Scheduler;
+use Sensei\Internal\Student_Progress\Course_Progress\Models\Course_Progress_Interface;
 use Sensei\Internal\Student_Progress\Course_Progress\Repositories\Comments_Based_Course_Progress_Repository;
 use Sensei\Internal\Student_Progress\Course_Progress\Repositories\Tables_Based_Course_Progress_Repository;
+use Sensei\Internal\Student_Progress\Lesson_Progress\Models\Lesson_Progress_Interface;
 use Sensei\Internal\Student_Progress\Lesson_Progress\Repositories\Comments_Based_Lesson_Progress_Repository;
 use Sensei\Internal\Student_Progress\Lesson_Progress\Repositories\Tables_Based_Lesson_Progress_Repository;
+use Sensei\Internal\Student_Progress\Quiz_Progress\Models\Quiz_Progress_Interface;
+use Sensei\Internal\Student_Progress\Quiz_Progress\Repositories\Comments_Based_Quiz_Progress_Repository;
+use Sensei\Internal\Student_Progress\Quiz_Progress\Repositories\Tables_Based_Quiz_Progress_Repository;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -19,6 +24,13 @@ defined( 'ABSPATH' ) || exit;
  * @since $$next_version$$
  */
 class Sensei_DB_Validate_Progress_Command {
+	/**
+	 * Whether the progress data is valid.
+	 *
+	 * @var bool
+	 */
+	private $is_valid = true;
+
 	/**
 	 * Seed the database.
 	 *
@@ -30,16 +42,71 @@ class Sensei_DB_Validate_Progress_Command {
 			WP_CLI::error( 'The progress migration is not complete. Please run the progress migration first.' );
 		}
 
-		$courses = Sensei_Course::get_all_courses();
-
-		foreach ( $courses as $course ) {
-			$this->validate_course( $course );
-
-			$lessons = Sensei()->course->course_lessons( $course->ID, 'any' );
-			foreach ( $lessons as $lesson ) {
-				$this->validate_lesson( $lesson );
-			}
+		foreach ( $this->get_course_ids() as $course_id ) {
+			$this->validate_course_progress( $course_id );
 		}
+
+		foreach ( $this->get_lesson_ids() as $lesson_id ) {
+			$this->validate_lesson_progress( $lesson_id );
+		}
+
+		foreach ( $this->get_quiz_ids() as $quiz_id ) {
+			$this->validate_quiz_progress( $quiz_id );
+		}
+
+		if ( $this->is_valid ) {
+			WP_CLI::success( 'Progress data is valid.' );
+		} else {
+			WP_CLI::error( 'Progress data mismatch detected.' );
+		}
+	}
+
+	/**
+	 * Get the course IDs.
+	 *
+	 * @return int[]
+	 */
+	private function get_course_ids(): array {
+		return get_posts(
+			[
+				'post_type'      => 'course',
+				'post_status'    => 'any',
+				'posts_per_page' => -1,
+				'fields'         => 'ids',
+			]
+		);
+	}
+
+	/**
+	 * Get the lesson IDs.
+	 *
+	 * @return int[]
+	 */
+	private function get_lesson_ids(): array {
+		return get_posts(
+			[
+				'post_type'      => 'lesson',
+				'post_status'    => 'any',
+				'posts_per_page' => -1,
+				'fields'         => 'ids',
+			]
+		);
+	}
+
+	/**
+	 * Get the quiz IDs.
+	 *
+	 * @return int[]
+	 */
+	private function get_quiz_ids(): array {
+		return get_posts(
+			[
+				'post_type'      => 'quiz',
+				'post_status'    => 'any',
+				'posts_per_page' => -1,
+				'fields'         => 'ids',
+			]
+		);
 	}
 
 	/**
@@ -54,11 +121,9 @@ class Sensei_DB_Validate_Progress_Command {
 	/**
 	 * Validate the course progress.
 	 *
-	 * @param WP_Post $course Course post object.
+	 * @param int $course_id Course post ID.
 	 */
-	private function validate_course( WP_Post $course ): void {
-		WP_CLI::log( "Validating course {$course->post_title} ({$course->ID})..." );
-
+	private function validate_course_progress( int $course_id ): void {
 		global $wpdb;
 		// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
 		$user_ids = $wpdb->get_col(
@@ -66,7 +131,7 @@ class Sensei_DB_Validate_Progress_Command {
 				"SELECT user_id FROM {$wpdb->comments}
 				WHERE comment_type = 'sensei_course_status'
 				AND comment_post_ID = %d",
-				$course->ID
+				$course_id
 			)
 		);
 		if ( ! $user_ids ) {
@@ -77,8 +142,8 @@ class Sensei_DB_Validate_Progress_Command {
 		$tables_based_repository   = new Tables_Based_Course_Progress_Repository( $wpdb );
 
 		foreach ( $user_ids as $user_id ) {
-			$comments_based_progress = $comments_based_repository->get( $course->ID, $user_id );
-			$tables_based_progress   = $tables_based_repository->get( $course->ID, $user_id );
+			$comments_based_progress = $comments_based_repository->get( $course_id, $user_id );
+			$tables_based_progress   = $tables_based_repository->get( $course_id, $user_id );
 
 			if ( ! $comments_based_progress ) {
 				WP_CLI::warning( 'Comments based progress not found.' );
@@ -90,48 +155,16 @@ class Sensei_DB_Validate_Progress_Command {
 				continue;
 			}
 
-			if (
-				$comments_based_progress->get_course_id() !== $tables_based_progress->get_course_id()
-				|| $comments_based_progress->get_user_id() !== $tables_based_progress->get_user_id()
-				|| $comments_based_progress->get_status() !== $tables_based_progress->get_status()
-				|| $comments_based_progress->get_started_at() != $tables_based_progress->get_started_at() // phpcs:ignore WordPress.PHP.StrictComparisons.LooseComparison
-				|| $comments_based_progress->get_completed_at() != $tables_based_progress->get_completed_at() // phpcs:ignore WordPress.PHP.StrictComparisons.LooseComparison
-			) {
-				WP_CLI::warning( 'Data mismatch between comments and tables based progress.' );
-				WP_CLI\Utils\format_items(
-					'table',
-					[
-						[
-							'source'       => 'comments',
-							'course_id'    => $comments_based_progress->get_course_id(),
-							'user_id'      => $comments_based_progress->get_user_id(),
-							'status'       => $comments_based_progress->get_status(),
-							'started_at'   => $comments_based_progress->get_started_at(),
-							'completed_at' => $comments_based_progress->get_completed_at(),
-						],
-						[
-							'source'       => 'tables',
-							'course_id'    => $tables_based_progress->get_course_id(),
-							'user_id'      => $tables_based_progress->get_user_id(),
-							'status'       => $tables_based_progress->get_status(),
-							'started_at'   => $tables_based_progress->get_started_at(),
-							'completed_at' => $tables_based_progress->get_completed_at(),
-						],
-					],
-					[ 'source', 'course_id', 'user_id', 'status', 'started_at', 'completed_at' ]
-				);
-			}
+			$this->compare_progress( $comments_based_progress, $tables_based_progress );
 		}
 	}
 
 	/**
 	 * Validate the lesson progress.
 	 *
-	 * @param WP_Post $lesson Lesson post object.
+	 * @param int $lesson_id Lesson post ID.
 	 */
-	private function validate_lesson( WP_Post $lesson ): void {
-		WP_CLI::log( "Validating lesson {$lesson->post_title} ({$lesson->ID})..." );
-
+	private function validate_lesson_progress( int $lesson_id ): void {
 		global $wpdb;
 		// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
 		$user_ids = $wpdb->get_col(
@@ -139,7 +172,7 @@ class Sensei_DB_Validate_Progress_Command {
 				"SELECT user_id FROM {$wpdb->comments}
 				WHERE comment_type = 'sensei_lesson_status'
 				AND comment_post_ID = %d",
-				$lesson->ID
+				$lesson_id
 			)
 		);
 		if ( ! $user_ids ) {
@@ -150,8 +183,8 @@ class Sensei_DB_Validate_Progress_Command {
 		$tables_based_repository   = new Tables_Based_Lesson_Progress_Repository( $wpdb );
 
 		foreach ( $user_ids as $user_id ) {
-			$comments_based_progress = $comments_based_repository->get( $lesson->ID, $user_id );
-			$tables_based_progress   = $tables_based_repository->get( $lesson->ID, $user_id );
+			$comments_based_progress = $comments_based_repository->get( $lesson_id, $user_id );
+			$tables_based_progress   = $tables_based_repository->get( $lesson_id, $user_id );
 
 			if ( ! $comments_based_progress ) {
 				WP_CLI::warning( 'Comments based progress not found.' );
@@ -163,37 +196,124 @@ class Sensei_DB_Validate_Progress_Command {
 				continue;
 			}
 
-			if (
-				$comments_based_progress->get_lesson_id() !== $tables_based_progress->get_lesson_id()
-				|| $comments_based_progress->get_user_id() !== $tables_based_progress->get_user_id()
-				|| $comments_based_progress->get_status() !== $tables_based_progress->get_status()
-				|| $comments_based_progress->get_started_at() != $tables_based_progress->get_started_at() // phpcs:ignore WordPress.PHP.StrictComparisons.LooseComparison
-				|| $comments_based_progress->get_completed_at() != $tables_based_progress->get_completed_at() // phpcs:ignore WordPress.PHP.StrictComparisons.LooseComparison
-			) {
-				WP_CLI::warning( 'Data mismatch between comments and tables based progress.' );
-				WP_CLI\Utils\format_items(
-					'table',
-					[
-						[
-							'source'       => 'comments',
-							'lesson_id'    => $comments_based_progress->get_lesson_id(),
-							'user_id'      => $comments_based_progress->get_user_id(),
-							'status'       => $comments_based_progress->get_status(),
-							'started_at'   => $comments_based_progress->get_started_at(),
-							'completed_at' => $comments_based_progress->get_completed_at(),
-						],
-						[
-							'source'       => 'tables',
-							'lesson_id'    => $tables_based_progress->get_lesson_id(),
-							'user_id'      => $tables_based_progress->get_user_id(),
-							'status'       => $tables_based_progress->get_status(),
-							'started_at'   => $tables_based_progress->get_started_at(),
-							'completed_at' => $tables_based_progress->get_completed_at(),
-						],
-					],
-					[ 'source', 'lesson_id', 'user_id', 'status', 'started_at', 'completed_at' ]
-				);
-			}
+			$this->compare_progress( $comments_based_progress, $tables_based_progress );
 		}
+	}
+
+	/**
+	 * Validate the quiz progress.
+	 *
+	 * @param int $quiz_id Quiz post ID.
+	 */
+	private function validate_quiz_progress( int $quiz_id ): void {
+		$lesson_id = Sensei()->quiz->get_lesson_id( $quiz_id );
+
+		global $wpdb;
+		// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
+		$user_ids = $wpdb->get_col(
+			$wpdb->prepare(
+				"SELECT user_id FROM {$wpdb->comments}
+				WHERE comment_type = 'sensei_lesson_status'
+				AND comment_post_ID = %d",
+				$lesson_id
+			)
+		);
+		if ( ! $user_ids ) {
+			return;
+		}
+
+		$comments_based_repository = new Comments_Based_Quiz_Progress_Repository();
+		$tables_based_repository   = new Tables_Based_Quiz_Progress_Repository( $wpdb );
+
+		foreach ( $user_ids as $user_id ) {
+			$comments_based_progress = $comments_based_repository->get( $quiz_id, $user_id );
+			$tables_based_progress   = $tables_based_repository->get( $quiz_id, $user_id );
+
+			if ( ! $comments_based_progress ) {
+				WP_CLI::warning( 'Comments based progress not found.' );
+				continue;
+			}
+
+			if ( ! $tables_based_progress ) {
+				WP_CLI::warning( 'Tables based progress not found.' );
+				continue;
+			}
+
+			$this->compare_progress( $comments_based_progress, $tables_based_progress );
+		}
+	}
+
+	/**
+	 * Compare the comments and tables based progress.
+	 *
+	 * @param object $comments_based_progress Comments based progress.
+	 * @param object $tables_based_progress   Tables based progress.
+	 */
+	private function compare_progress( object $comments_based_progress, object $tables_based_progress ): void {
+		// phpcs:ignore WordPress.PHP.StrictComparisons.LooseComparison
+		if ( $this->get_progress_data( $comments_based_progress ) != $this->get_progress_data( $tables_based_progress ) ) {
+			$this->is_valid = false;
+			$this->log_progress_mismatch( $comments_based_progress, $tables_based_progress );
+		}
+	}
+
+	/**
+	 * Get the progress data.
+	 *
+	 * @param object $progress Progress.
+	 *
+	 * @return array
+	 * @throws \InvalidArgumentException When invalid progress type is provided.
+	 */
+	private function get_progress_data( object $progress ): array {
+		if ( $progress instanceof Course_Progress_Interface ) {
+			$type    = 'course';
+			$post_id = $progress->get_course_id();
+		} elseif ( $progress instanceof Lesson_Progress_Interface ) {
+			$type    = 'lesson';
+			$post_id = $progress->get_lesson_id();
+		} elseif ( $progress instanceof Quiz_Progress_Interface ) {
+			$type    = 'quiz';
+			$post_id = $progress->get_quiz_id();
+		} else {
+			throw new \InvalidArgumentException( 'Invalid progress type.' );
+		}
+
+		return [
+			'type'         => $type,
+			'post_id'      => $post_id,
+			'user_id'      => $progress->get_user_id(),
+			'status'       => $progress->get_status(),
+			'started_at'   => $progress->get_started_at(),
+			'completed_at' => $progress->get_completed_at(),
+		];
+	}
+
+	/**
+	 * Log a progress mismatch.
+	 *
+	 * @param object $comments_based_progress Comments based progress.
+	 * @param object $tables_based_progress   Tables based progress.
+	 */
+	private function log_progress_mismatch( object $comments_based_progress, object $tables_based_progress ): void {
+		WP_CLI::warning( 'Data mismatch between comments and tables based progress.' );
+		WP_CLI\Utils\format_items(
+			'table',
+			[
+				array_merge(
+					[
+						'source' => 'comments',
+					],
+					$this->get_progress_data( $comments_based_progress )
+				),
+				array_merge(
+					[
+						'source' => 'tables',
+					],
+					$this->get_progress_data( $tables_based_progress )
+				),
+			],
+			[ 'source', 'type', 'post_id', 'user_id', 'status', 'started_at', 'completed_at' ]
+		);
 	}
 }
