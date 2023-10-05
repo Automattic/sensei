eedb0e976723e29ec6400e51be103644a571ed3b
Donna Peplinskie <donnapep@gmail.com>
2023-10-05 13:55:46 -0400

    Add "Continue to next lesson" button for graded quizzes

--- includes/course-theme/class-sensei-course-theme-quiz.php
+++ includes/course-theme/class-sensei-course-theme-quiz.php
@@ -128,7 +128,14 @@ class Sensei_Course_Theme_Quiz {
 			);
 		}
 
-		// Prepare Restart Quiz button.
+		// "Continue to next lesson" button.
+		if ( in_array( $lesson_status->comment_approved, [ 'complete', 'graded', 'passed', 'failed' ], true ) ) {
+			$prev_next_urls  = sensei_get_prev_next_lessons( $lesson_id );
+			$next_lesson_url = $prev_next_urls['next']['url'] ?? null;
+			$actions[]       = Sensei_Quiz::get_primary_button_html( __( 'Continue to next lesson', 'sensei-lms' ), $next_lesson_url );
+		}
+
+		// "Restart Quiz" button.
 		$reset_allowed = Sensei_Quiz::is_reset_allowed( $lesson_id );
 
 		if ( $reset_allowed ) {
@@ -157,3 +164,4 @@ class Sensei_Course_Theme_Quiz {
 			</form>
 		" );
 	}
+}
