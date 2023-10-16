8567c10b1f21e66b00948d54149ba05da05c0aa3
Dmitry Merkushin <merkushin@gmail.com>
2023-10-16 08:00:01 -0600

    Update quiz submission repositories factories

--- tests/unit-tests/internal/quiz-submission/answer/repositories/test-class-answer-repository-factory.php
+++ tests/unit-tests/internal/quiz-submission/answer/repositories/test-class-answer-repository-factory.php
@@ -4,6 +4,8 @@ namespace SenseiTest\Internal\Quiz_Submission\Answer\Repositories;
 
 use Sensei\Internal\Quiz_Submission\Answer\Repositories\Comment_Reading_Aggregate_Answer_Repository;
 use Sensei\Internal\Quiz_Submission\Answer\Repositories\Answer_Repository_Factory;
+use Sensei\Internal\Quiz_Submission\Answer\Repositories\Comments_Based_Answer_Repository;
+use Sensei\Internal\Quiz_Submission\Answer\Repositories\Table_Reading_Aggregate_Answer_Repository;
 
 /**
  * Class Answer_Repository_Factory_Test
@@ -17,21 +19,23 @@ class Answer_Repository_Factory_Test extends \WP_UnitTestCase {
 	 *
 	 * @dataProvider providerCreate_WhenCalled_ReturnsAnswerRepository
 	 */
-	public function testCreate_WhenCalled_ReturnsAnswerRepository( bool $use_tables ): void {
+	public function testCreate_WhenCalled_ReturnsAnswerRepository( bool $tables_enabled, bool $read_tables, string $expected ): void {
 		/* Arrange. */
-		$factory = new Answer_Repository_Factory( $use_tables );
+		$factory = new Answer_Repository_Factory( $tables_enabled, $read_tables );
 
 		/* Act. */
 		$actual = $factory->create();
 
 		/* Assert. */
-		self::assertInstanceOf( Comment_Reading_Aggregate_Answer_Repository::class, $actual );
+		self::assertInstanceOf( $expected, $actual );
 	}
 
 	public function providerCreate_WhenCalled_ReturnsAnswerRepository(): array {
-		return [
-			'use tables'        => [ true ],
-			'do not use tables' => [ false ],
-		];
+		return array(
+			'tables disabled, don\'t read tables' => array( false, false, Comments_Based_Answer_Repository::class ),
+			'tables disabled, read tables'        => array( false, true, Comments_Based_Answer_Repository::class ),
+			'tables enabled, don\'t read tables'  => array( true, false, Comment_Reading_Aggregate_Answer_Repository::class ),
+			'tables enabled, read tables'         => array( true, true, Table_Reading_Aggregate_Answer_Repository::class ),
+		);
 	}
 }
--- tests/unit-tests/internal/quiz-submission/submission/repositories/test-class-submission-repository-factory.php
+++ tests/unit-tests/internal/quiz-submission/submission/repositories/test-class-submission-repository-factory.php
@@ -3,7 +3,9 @@
 namespace SenseiTest\Internal\Quiz_Submission\Submission\Repositories;
 
 use Sensei\Internal\Quiz_Submission\Submission\Repositories\Comment_Reading_Aggregate_Submission_Repository;
+use Sensei\Internal\Quiz_Submission\Submission\Repositories\Comments_Based_Submission_Repository;
 use Sensei\Internal\Quiz_Submission\Submission\Repositories\Submission_Repository_Factory;
+use Sensei\Internal\Quiz_Submission\Submission\Repositories\Table_Reading_Aggregate_Submission_Repository;
 
 /**
  * Class Submission_Repository_Factory_Test
@@ -17,21 +19,23 @@ class Submission_Repository_Factory_Test extends \WP_UnitTestCase {
 	 *
 	 * @dataProvider providerCreate_WhenCalled_ReturnsSubmissionRepository
 	 */
-	public function testCreate_WhenCalled_ReturnsSubmissionRepository( bool $use_tables ): void {
+	public function testCreate_WhenCalled_ReturnsSubmissionRepository( bool $tables_enabled, bool $read_tables, string $expected ): void {
 		/* Arrange. */
-		$factory = new Submission_Repository_Factory( $use_tables );
+		$factory = new Submission_Repository_Factory( $tables_enabled, $read_tables );
 
 		/* Act. */
 		$actual = $factory->create();
 
 		/* Assert. */
-		self::assertInstanceOf( Comment_Reading_Aggregate_Submission_Repository::class, $actual );
+		self::assertInstanceOf( $expected, $actual );
 	}
 
 	public function providerCreate_WhenCalled_ReturnsSubmissionRepository(): array {
 		return [
-			'use tables'        => [ true ],
-			'do not use tables' => [ false ],
+			'tables disabled, don\'t read tables' => [ false, false, Comments_Based_Submission_Repository::class ],
+			'tables disabled, read tables'        => [ false, true, Comments_Based_Submission_Repository::class ],
+			'tables enabled, don\'t read tables'  => [ true, false, Comment_Reading_Aggregate_Submission_Repository::class ],
+			'tables enabled, read tables'         => [ true, true, Table_Reading_Aggregate_Submission_Repository::class ],
 		];
 	}
 }
