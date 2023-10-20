6678751f47ae167c0977d71ca1acf06c88e54088
Miroslav Mitev <m1r0@users.noreply.github.com>
2023-10-20 15:53:15 +0300

    Revert "Use `wp-cli-stubs` in Psalm"
    This reverts commit ff35467e653efc585f73164dbec8187d9aba364a.

--- config/psalm/psalm-loader.php
+++ config/psalm/psalm-loader.php
@@ -16,5 +16,4 @@ if ( ! defined( 'SENSEI_LMS_PLUGIN_PATH' ) ) {
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../vendor/php-stubs/wordpress-stubs/wordpress-stubs.php';
-require_once __DIR__ . '/../../vendor/php-stubs/wp-cli-stubs/wp-cli-stubs.php';
 require_once __DIR__ . '/../../vendor/woocommerce/action-scheduler/functions.php';
