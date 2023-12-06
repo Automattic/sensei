d328e859478bc7f15eb833aa73e5586fd99ff996
Imran Hossain <imranh920@gmail.com>
2023-12-06 21:54:36 +0600

    Add notice file in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ const files = [
 	'js/admin/course-edit.js',
 	'js/admin/course-index.js',
 	'js/admin/event-logging.js',
+	'js/admin/first-course-creation-notice.js',
 	'js/admin/lesson-bulk-edit.js',
 	'js/admin/lesson-quick-edit.js',
 	'js/admin/message-menu-fix.js',
