198145c7c35434e0b2c77e970a21d3ae0041259d
Dmitry Merkushin <merkushin@gmail.com>
2023-12-22 02:07:19 -0600

    Set email template only for sensei_email post type

--- tests/unit-tests/internal/emails/test-class-email-page-template.php
+++ tests/unit-tests/internal/emails/test-class-email-page-template.php
@@ -104,7 +104,7 @@ class Email_Page_Template_Test extends \WP_UnitTestCase {
 			->willReturn( $template_to_be_added );
 
 		/* Act. */
-		$result = $page_template->add_email_template( $default_list, [], 'wp_template' );
+		$result = $page_template->add_email_template( $default_list, array( 'post_type' => 'sensei_email' ), 'wp_template' );
 
 		/* Assert. */
 		self::assertSame( $template_to_be_added, $result[1] );
@@ -141,6 +141,20 @@ class Email_Page_Template_Test extends \WP_UnitTestCase {
 
 		/* Act. */
 		$result = $page_template->add_email_template( $default_list, [ 'post_type' => 'page' ], 'wp_template' );
+
+		/* Assert. */
+		self::assertSame( $default_list, $result );
+	}
+
+	public function testAddEmailTemplate_GivenQueryWithoutPostType_ReturnsDefaultTemplate() {
+
+		/* Arrange. */
+		$repository    = $this->createMock( Email_Page_Template_Repository::class );
+		$page_template = new Email_Page_Template( $repository );
+		$default_list  = [ $this->createMock( \WP_Block_Template::class ) ];
+
+		/* Act. */
+		$result = $page_template->add_email_template( $default_list, [], 'wp_template' );
 
 		/* Assert. */
 		self::assertSame( $default_list, $result );
@@ -201,7 +215,7 @@ class Email_Page_Template_Test extends \WP_UnitTestCase {
 			->willReturn( $template_to_be_added );
 
 		/* Act. */
-		$result = $page_template->add_email_template( $default_list, [], 'wp_template' );
+		$result = $page_template->add_email_template( $default_list, array( 'post_type' => 'sensei_email' ), 'wp_template' );
 
 		/* Assert. */
 		self::assertSame( $template_to_be_added, $result[1] );
