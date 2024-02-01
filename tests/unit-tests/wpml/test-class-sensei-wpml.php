094a53e09561a5bbf24995b55a6065f05f7c7114
Dmitry Merkushin <merkushin@gmail.com>
2024-02-01 00:48:18 -0600

    Add tests for wpml hooks

--- tests/unit-tests/wpml/test-class-sensei-wpml.php
+++ tests/unit-tests/wpml/test-class-sensei-wpml.php
@@ -2,7 +2,8 @@
 
 namespace SenseiTest\WPML;
 
-use \Sensei_WPML;
+use Sensei_Factory;
+use Sensei_WPML;
 
 /**
 * Class Sensei_WPML_Test.
@@ -10,6 +11,24 @@ use \Sensei_WPML;
 * @covers \Sensei_WPML
 */
 class Sensei_WPML_Test extends \WP_UnitTestCase {
+
+	/**
+	 * Sensei Factory.
+	 *
+	 * @var Sensei_Factory
+	 */
+	protected $factory;
+
+	public function set_up(): void {
+		parent::set_up();
+		$this->factory = new Sensei_Factory();
+	}
+
+	public function tear_down(): void {
+		parent::tear_down();
+		$this->factory->tearDown();
+	}
+
 	public function testSetLanguageDetailsWhenLessonCreated_WhenCalled_AppliesWpmlElementLanguageCodeFilter() {
 		/* Arrange. */
 		$wpml = new Sensei_WPML();
@@ -146,7 +165,7 @@ class Sensei_WPML_Test extends \WP_UnitTestCase {
 		$this->assertTrue( $filter_applied );
 	}
 
-	public function UpdateCoursePrerequisiteBeforeCopied_WhenCalled_ReturnsMatchingPrerequisiteForNewCourse() {
+	public function testUpdateCoursePrerequisiteBeforeCopied_WhenCalled_ReturnsMatchingPrerequisiteForNewCourse() {
 		/* Arrange. */
 		$wpml = new Sensei_WPML();
 
@@ -162,14 +181,14 @@ class Sensei_WPML_Test extends \WP_UnitTestCase {
 
 		/* Act. */
 		$actual = $wpml->update_course_prerequisite_before_copied( 1, 2, 3, '_course_prerequisite' );
-		remove_filter( 'wpml_element_language_code', $language_code_filter, 10, 2 );
-		remove_filter( 'wpml_object_id', $object_id_fitler, 10, 4 );
 
-		/* Assert. */
+		/* Clean up & Assert. */
+		remove_filter( 'wpml_element_language_code', $language_code_filter );
+		remove_filter( 'wpml_object_id', $object_id_fitler );
 		$this->assertSame( 4, $actual );
 	}
 
-	public function updateLessonCourseBeforeCopied_WhenCalled_ReturnsMatchingCourseForNewLesson() {
+	public function testUpdateLessonCourseBeforeCopied_WhenCalled_ReturnsMatchingCourseForNewLesson() {
 		/* Arrange. */
 		$wpml = new Sensei_WPML();
 
@@ -185,10 +204,64 @@ class Sensei_WPML_Test extends \WP_UnitTestCase {
 
 		/* Act. */
 		$actual = $wpml->update_lesson_course_before_copied( 1, 2, 3, '_lesson_course' );
-		remove_filter( 'wpml_element_language_code', $language_code_filter, 10, 2 );
-		remove_filter( 'wpml_object_id', $object_id_fitler, 10, 4 );
 
-		/* Assert. */
+		/* Clean up & Assert. */
+		remove_filter( 'wpml_element_language_code', $language_code_filter );
+		remove_filter( 'wpml_object_id', $object_id_fitler );
+
 		$this->assertSame( 4, $actual );
+	}
+
+	public function testUpdateLessonPropertiesOnCourseTranslationCreated_WhenCalled_CreatesLessonTranslations() {
+		/* Arrange. */
+		$new_course_id = $this->factory->course->create();
+		$old_course    = $this->factory->get_course_with_lessons( array( 'lesson_count' => 2 ) );
+
+		$wpml = new Sensei_WPML();
+
+		$element_language_details_filter = function( $element_object, $args ) {
+			return array(
+				'language_code'        => 'a',
+				'source_language_code' => 'c',
+			);
+		};
+		add_filter( 'wpml_element_language_details', $element_language_details_filter, 10, 2 );
+
+		$object_id_fitler = function( $object_id, $element_type, $return_original_if_missing, $args ) use ( $new_course_id, $old_course ) {
+			if ( $new_course_id === $object_id && 'course' === $element_type ) {
+				return $old_course['course_id'];
+			}
+
+			return 4;
+		};
+		add_filter( 'wpml_object_id', $object_id_fitler, 10, 4 );
+
+		$element_has_translations_filter = function( $has_translations, $element_id, $element_type ) {
+			return false;
+		};
+		add_filter( 'wpml_element_has_translations', $element_has_translations_filter, 10, 3 );
+
+		$created_lessons                  = 0;
+		$admin_make_post_duplicates_acton = function( $post_id ) use ( &$created_lessons ) {
+			$created_lessons++;
+		};
+		add_action( 'wpml_admin_make_post_duplicates', $admin_make_post_duplicates_acton, 10, 1 );
+
+		$post_duplicates_filter = function( $post_id ) {
+			return array( 'a' => 1 );
+		};
+		add_filter( 'wpml_post_duplicates', $post_duplicates_filter, 10, 1 );
+
+		/* Act. */
+		$wpml->update_lesson_properties_on_course_translation_created( $new_course_id );
+
+		/* Clean up & Assert. */
+		remove_filter( 'wpml_element_language_details', $element_language_details_filter );
+		remove_filter( 'wpml_object_id', $object_id_fitler );
+		remove_filter( 'wpml_element_has_translations', $element_has_translations_filter );
+		remove_action( 'wpml_admin_make_post_duplicates', $admin_make_post_duplicates_acton );
+
+		$this->assertSame( 2, $created_lessons );
+
 	}
 }
