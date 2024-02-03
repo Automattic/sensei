fb8f89ee67b02687f3ee61222e848d8615839235
Dmitry Merkushin <merkushin@gmail.com>
2024-02-03 01:22:58 -0600

    Remove unnecessary useState usage

--- assets/blocks/course-outline/outline-block/existing-lessons-modal/filter.js
+++ assets/blocks/course-outline/outline-block/existing-lessons-modal/filter.js
@@ -3,7 +3,6 @@
  */
 import { search } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n';
-import { useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -19,12 +18,9 @@ import { debounce } from 'lodash';
  * Lessons filter.
  *
  * @param {Object}   props
- * @param {Object}   props.filters    Filters object.
  * @param {Function} props.setFilters Filters state setter.
  */
-const Filter = ( { filters, setFilters } ) => {
-	const { searchValue } = useState( filters.search );
-
+const Filter = ( { setFilters } ) => {
 	const createFilterChangeHandler = ( filterKey, wait ) =>
 		debounce( ( value ) => {
 			setFilters( ( prevFilters ) => ( {
@@ -40,7 +36,6 @@ const Filter = ( { filters, setFilters } ) => {
 					className="wp-block-sensei-lms-course-outline__existing-lessons-modal__search-input"
 					placeholder={ __( 'Search lessons', 'sensei-lms' ) }
 					iconRight={ search }
-					value={ searchValue }
 					onChange={ createFilterChangeHandler( 'search', 400 ) }
 				/>
 			</li>
