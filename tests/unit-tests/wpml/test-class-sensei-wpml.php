19307662409032e1783eedacd2cb24ad5fceda8c
Dmitry Merkushin <merkushin@gmail.com>
2024-02-16 13:09:38 -0600

    Fix linter issues in tests

--- tests/unit-tests/wpml/test-class-sensei-wpml.php
+++ tests/unit-tests/wpml/test-class-sensei-wpml.php
@@ -34,12 +34,12 @@ class Sensei_WPML_Test extends \WP_UnitTestCase {
 		$wpml = new Sensei_WPML();
 
 		$filter_applied  = false;
-		$filter_function = function( $language_code, $element_data ) use ( &$filter_applied ) {
+		$filter_function = function ( $language_code ) use ( &$filter_applied ) {
 			$filter_applied = true;
 			return $language_code;
 		};
 
-		add_filter( 'wpml_element_language_code', $filter_function, 10, 2 );
+		add_filter( 'wpml_element_language_code', $filter_function, 10, 1 );
 
 		/* Act. */
 		$wpml->set_language_details_when_lesson_created( 1, 2 );
@@ -54,13 +54,13 @@ class Sensei_WPML_Test extends \WP_UnitTestCase {
 		/* Arrange. */
 		$wpml = new Sensei_WPML();
 
-		$filter_language_code_function = function( $language_code, $element_data ) use ( &$filter_applied ) {
+		$filter_language_code_function = function () {
 			return null;
 		};
-		add_filter( 'wpml_element_language_code', $filter_language_code_function, 10, 2 );
+		add_filter( 'wpml_element_language_code', $filter_language_code_function, 10, 0 );
 
 		$filter_applied  = false;
-		$filter_function = function( $language_code ) use ( &$filter_applied ) {
+		$filter_function = function ( $language_code ) use ( &$filter_applied ) {
 			$filter_applied = true;
 			return $language_code;
 		};
@@ -81,7 +81,7 @@ class Sensei_WPML_Test extends \WP_UnitTestCase {
 		$wpml = new Sensei_WPML();
 
 		$filter_applied  = false;
-		$filter_function = function( $data ) use ( &$filter_applied ) {
+		$filter_function = function ( $data ) use ( &$filter_applied ) {
 			$filter_applied = true;
 			return $data;
 		};
@@ -102,12 +102,12 @@ class Sensei_WPML_Test extends \WP_UnitTestCase {
 		$wpml = new Sensei_WPML();
 
 		$filter_applied  = false;
-		$filter_function = function( $language_code, $element_data ) use ( &$filter_applied ) {
+		$filter_function = function ( $language_code ) use ( &$filter_applied ) {
 			$filter_applied = true;
 			return $language_code;
 		};
 
-		add_filter( 'wpml_element_language_code', $filter_function, 10, 2 );
+		add_filter( 'wpml_element_language_code', $filter_function, 10, 1 );
 
 		/* Act. */
 		$wpml->set_language_details_when_quiz_created( 1, 2 );
@@ -122,13 +122,13 @@ class Sensei_WPML_Test extends \WP_UnitTestCase {
 		/* Arrange. */
 		$wpml = new Sensei_WPML();
 
-		$filter_language_code_function = function( $language_code, $element_data ) use ( &$filter_applied ) {
+		$filter_language_code_function = function () {
 			return null;
 		};
-		add_filter( 'wpml_element_language_code', $filter_language_code_function, 10, 2 );
+		add_filter( 'wpml_element_language_code', $filter_language_code_function, 10, 0 );
 
 		$filter_applied  = false;
-		$filter_function = function( $language_code ) use ( &$filter_applied ) {
+		$filter_function = function ( $language_code ) use ( &$filter_applied ) {
 			$filter_applied = true;
 			return $language_code;
 		};
@@ -149,7 +149,7 @@ class Sensei_WPML_Test extends \WP_UnitTestCase {
 		$wpml = new Sensei_WPML();
 
 		$filter_applied  = false;
-		$filter_function = function( $data ) use ( &$filter_applied ) {
+		$filter_function = function ( $data ) use ( &$filter_applied ) {
 			$filter_applied = true;
 			return $data;
 		};
@@ -169,15 +169,15 @@ class Sensei_WPML_Test extends \WP_UnitTestCase {
 		/* Arrange. */
 		$wpml = new Sensei_WPML();
 
-		$language_code_filter = function( $language_code, $element_data ) {
+		$language_code_filter = function () {
 			return 'a';
 		};
-		add_filter( 'wpml_element_language_code', $language_code_filter, 10, 2 );
+		add_filter( 'wpml_element_language_code', $language_code_filter, 10, 0 );
 
-		$object_id_fitler = function( $object_id, $element_type, $return_original_if_missing, $args ) {
+		$object_id_fitler = function () {
 			return 4;
 		};
-		add_filter( 'wpml_object_id', $object_id_fitler, 10, 4 );
+		add_filter( 'wpml_object_id', $object_id_fitler, 10, 0 );
 
 		/* Act. */
 		$actual = $wpml->update_course_prerequisite_before_copied( 1, 2, 3, '_course_prerequisite' );
@@ -192,15 +192,15 @@ class Sensei_WPML_Test extends \WP_UnitTestCase {
 		/* Arrange. */
 		$wpml = new Sensei_WPML();
 
-		$language_code_filter = function( $language_code, $element_data ) {
+		$language_code_filter = function () {
 			return 'a';
 		};
-		add_filter( 'wpml_element_language_code', $language_code_filter, 10, 2 );
+		add_filter( 'wpml_element_language_code', $language_code_filter, 10, 0 );
 
-		$object_id_fitler = function( $object_id, $element_type, $return_original_if_missing, $args ) {
+		$object_id_fitler = function () {
 			return 4;
 		};
-		add_filter( 'wpml_object_id', $object_id_fitler, 10, 4 );
+		add_filter( 'wpml_object_id', $object_id_fitler, 10, 0 );
 
 		/* Act. */
 		$actual = $wpml->update_lesson_course_before_copied( 1, 2, 3, '_lesson_course' );
@@ -221,7 +221,7 @@ class Sensei_WPML_Test extends \WP_UnitTestCase {
 
 		$wpml = new Sensei_WPML();
 
-		$element_language_details_filter = function( $element_object, $args ) {
+		$element_language_details_filter = function () {
 			return array(
 				'language_code'        => 'a',
 				'source_language_code' => 'c',
@@ -229,34 +229,34 @@ class Sensei_WPML_Test extends \WP_UnitTestCase {
 		};
 		add_filter( 'wpml_element_language_details', $element_language_details_filter, 10, 2 );
 
-		$object_id_fitler = function( $object_id, $element_type, $return_original_if_missing, $args ) use ( $new_course_id, $old_course ) {
+		$object_id_fitler = function ( $object_id, $element_type ) use ( $new_course_id, $old_course ) {
 			if ( $new_course_id === $object_id && 'course' === $element_type ) {
 				return $old_course['course_id'];
 			}
 
 			return 0;
 		};
-		add_filter( 'wpml_object_id', $object_id_fitler, 10, 4 );
+		add_filter( 'wpml_object_id', $object_id_fitler, 10, 2 );
 
-		$element_has_translations_filter = function( $has_translations, $element_id, $element_type ) {
+		$element_has_translations_filter = function () {
 			return false;
 		};
-		add_filter( 'wpml_element_has_translations', $element_has_translations_filter, 10, 3 );
+		add_filter( 'wpml_element_has_translations', $element_has_translations_filter, 10, 0 );
 
 		$created_lessons                  = 0;
-		$admin_make_post_duplicates_acton = function( $post_id ) use ( &$created_lessons ) {
-			$created_lessons++;
+		$admin_make_post_duplicates_acton = function () use ( &$created_lessons ) {
+			++$created_lessons;
 		};
-		add_action( 'wpml_admin_make_post_duplicates', $admin_make_post_duplicates_acton, 10, 1 );
+		add_action( 'wpml_admin_make_post_duplicates', $admin_make_post_duplicates_acton, 10, 0 );
 
 		$new_lesson_ids         = array( $new_lesson1_id, $new_lesson2_id );
-		$post_duplicates_filter = function( $post_id ) use ( &$new_lesson_ids ) {
+		$post_duplicates_filter = function () use ( &$new_lesson_ids ) {
 			$lesson_id = array_shift( $new_lesson_ids );
 			return array(
 				'a' => $lesson_id,
 			);
 		};
-		add_filter( 'wpml_post_duplicates', $post_duplicates_filter, 10, 1 );
+		add_filter( 'wpml_post_duplicates', $post_duplicates_filter, 10, 0 );
 
 		/* Act. */
 		$wpml->update_lesson_properties_on_course_translation_created( $new_course_id );
