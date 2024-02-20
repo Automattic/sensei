fd8b5893fc38e489ab3f5b36e017d9308b14faad
Imran Hossain <imranh920@gmail.com>
2024-02-20 12:43:53 +0600

    Test lesson scripts get loaded for existing lesson edit

--- tests/unit-tests/admin/test-class-sensei-tour.php
+++ tests/unit-tests/admin/test-class-sensei-tour.php
@@ -91,4 +91,19 @@ class Sensei_Tour_Test extends WP_UnitTestCase {
 		$this->assertFalse( wp_script_is( 'sensei-course-tour' ) );
 		$this->assertTrue( wp_style_is( 'sensei-tour-styles' ) );
 	}
+
+	public function testEnqueueAdminScripts_WhenPostTypeIsLessonAndEditPage_EnqueuesLessonScriptsAndStyles() {
+		/* Arrange */
+		global $post;
+		$this->login_as_admin();
+		$post = $this->factory->lesson->create_and_get();
+
+		/* Act */
+		$this->instance->enqueue_admin_scripts( 'post.php' );
+
+		/* Assert */
+		$this->assertTrue( wp_script_is( 'sensei-lesson-tour' ) );
+		$this->assertFalse( wp_script_is( 'sensei-course-tour' ) );
+		$this->assertTrue( wp_style_is( 'sensei-tour-styles' ) );
+	}
 }
