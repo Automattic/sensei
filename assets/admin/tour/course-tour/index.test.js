4f1366e324f7fe03db61c7407ed46de13f9ed2ef
Imran Hossain <imranh920@gmail.com>
2024-02-21 11:41:57 +0600

    Check that step params are properly passed

--- assets/admin/tour/course-tour/index.test.js
+++ assets/admin/tour/course-tour/index.test.js
@@ -11,6 +11,7 @@ import { render } from '@testing-library/react';
  * WordPress dependencies
  */
 import { select } from '@wordpress/data';
+import getTourSteps from './steps';
 
 jest.mock( '../../../blocks/course-outline/data', () => ( {
 	getFirstBlockByName: jest.fn(),
@@ -33,19 +34,31 @@ describe( 'CourseTour', () => {
 		} );
 	} );
 
-	test( 'renders null when no outline block', () => {
-		// Mocking getOutlineBlock to return null
+	test( 'should render null when no outline block', () => {
+		// Mocking getOutlineBlock to return null.
 		getFirstBlockByName.mockReturnValueOnce( null );
 
 		const { queryByText } = render( <CourseTour /> );
 		expect( queryByText( 'Tour Kit Output' ) ).toBeFalsy();
 	} );
 
-	test( 'renders SenseiTourKit when outline block exists', () => {
-		// Mocking getOutlineBlock to return a block
+	test( 'should render SenseiTourKit when outline block exists', () => {
+		// Mocking getOutlineBlock to return true.
 		getFirstBlockByName.mockReturnValueOnce( true );
 
 		const { getAllByText } = render( <CourseTour /> );
 		expect( getAllByText( 'Tour Kit Output' ) ).toBeTruthy();
+	} );
+
+	test( 'should pass the correct steps to inner block', () => {
+		// Mocking getOutlineBlock to return true. Otherwise, the component will return null.
+		getFirstBlockByName.mockReturnValueOnce( true );
+
+		render( <CourseTour /> );
+
+		const tourSteps = getTourSteps();
+		expect( mockFunction.mock.calls[ 0 ][ 0 ].steps[ 5 ].slug ).toEqual(
+			tourSteps[ 5 ].slug
+		);
 	} );
 } );
