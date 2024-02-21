7e0d9d81bff35183c0fd44edb5e38d3d85ad042a
Imran Hossain <imranh920@gmail.com>
2024-02-21 11:47:39 +0600

    Test lesson tour renderer

---
+++ assets/admin/tour/lesson-tour/index.test.js
@@ -0,0 +1,56 @@
+/**
+ * Internal dependencies
+ */
+import { getFirstBlockByName } from '../../../blocks/course-outline/data';
+import LessonTour from './index';
+/**
+ * External dependencies
+ */
+import { render } from '@testing-library/react';
+/**
+ * WordPress dependencies
+ */
+import { select } from '@wordpress/data';
+import getTourSteps from './steps';
+
+jest.mock( '../../../blocks/course-outline/data', () => ( {
+	getFirstBlockByName: jest.fn(),
+} ) );
+jest.mock( '../components/sensei-tour-kit', () =>
+	jest.fn().mockImplementation( ( props ) => {
+		mockFunction( props );
+		return <>Tour Kit Output</>;
+	} )
+);
+jest.mock( '@wordpress/data' );
+
+const mockFunction = jest.fn();
+
+describe( 'LessonTour', () => {
+	beforeEach( () => {
+		jest.clearAllMocks();
+		select.mockReturnValue( {
+			getBlocks: () => null,
+		} );
+	} );
+
+	test( 'should render null when no quiz block', () => {
+		// Mocking getOutlineBlock to return null.
+		getFirstBlockByName.mockReturnValueOnce( null );
+
+		const { queryByText } = render( <LessonTour /> );
+		expect( queryByText( 'Tour Kit Output' ) ).toBeFalsy();
+	} );
+
+	test( 'should pass the correct steps to inner block', () => {
+		// Mocking getOutlineBlock to return true. Otherwise, the component will return null.
+		getFirstBlockByName.mockReturnValueOnce( true );
+
+		render( <LessonTour /> );
+
+		const tourSteps = getTourSteps();
+		expect( mockFunction.mock.calls[ 0 ][ 0 ].steps[ 5 ].slug ).toEqual(
+			tourSteps[ 5 ].slug
+		);
+	} );
+} );
