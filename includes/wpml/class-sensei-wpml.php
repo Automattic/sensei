5e7e60d52b2cfeeec97bce522d303b9ac7b04ed7
Dmitry Merkushin <merkushin@gmail.com>
2024-02-22 09:07:53 -0600

    Update lesson relations for existing lessons as well

--- includes/wpml/class-sensei-wpml.php
+++ includes/wpml/class-sensei-wpml.php
@@ -285,13 +285,13 @@ class Sensei_WPML {
 			if ( ! $is_translated ) {
 				// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
 				do_action( 'wpml_admin_make_post_duplicates', $lesson_id );
+			}
 
-				// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
-				$translations = apply_filters( 'wpml_post_duplicates', $lesson_id );
-				foreach ( $translations as $translated_lesson_id ) {
-					$this->update_lesson_course( (int) $translated_lesson_id, $new_course_id );
-					$this->update_translated_lesson_taxonomies( (int) $translated_lesson_id, $lesson_id );
-				}
+			// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
+			$translations = apply_filters( 'wpml_post_duplicates', $lesson_id );
+			foreach ( $translations as $translated_lesson_id ) {
+				$this->update_lesson_course( (int) $translated_lesson_id, $new_course_id );
+				$this->update_translated_lesson_taxonomies( (int) $translated_lesson_id, $lesson_id );
 			}
 
 			// Sync lesson course field across translations.
