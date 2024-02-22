f41a51698637bc5a67f32ae7ee8eee5466e3c1f2
Imran Hossain <imranh920@gmail.com>
2024-02-22 15:31:08 +0600

    Add test to make sure tour kit renders

---
+++ assets/admin/tour/components/sensei-tour-kit/index.test.js
@@ -0,0 +1,41 @@
+/**
+ * Internal dependencies
+ */
+import SenseiTourKit from './index';
+/**
+ * External dependencies
+ */
+import { render } from '@testing-library/react';
+import { WpcomTourKit } from '@automattic/tour-kit';
+/**
+ * WordPress dependencies
+ */
+import { when } from 'jest-when';
+import getTourSteps from '../../course-tour/steps';
+
+jest.mock( '@automattic/tour-kit', () => ( {
+	WpcomTourKit: jest.fn(),
+} ) );
+
+const mockFunction = jest.fn();
+
+describe( 'SenseiTourKit', () => {
+	beforeEach( () => {
+		jest.clearAllMocks();
+		when( WpcomTourKit ).mockImplementation( ( props ) => {
+			mockFunction( props );
+			return (
+				<div>
+					<h1>WpcomTourKit output</h1>
+				</div>
+			);
+		} );
+	} );
+
+	test( 'should render wpcomtourkit as expected', () => {
+		const steps = getTourSteps();
+
+		const { queryByText } = render( <SenseiTourKit steps={ steps } /> );
+		expect( queryByText( 'WpcomTourKit output' ) ).toBeTruthy();
+	} );
+} );
