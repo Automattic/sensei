194a8e1c694005a372aaa7b3cf296f766c8ddd4a
Dmitry Merkushin <merkushin@gmail.com>
2024-03-07 09:10:57 -0600

    Add notification when layouts are not available

--- assets/admin/editor-wizard/steps/patterns-step.js
+++ assets/admin/editor-wizard/steps/patterns-step.js
@@ -1,9 +1,10 @@
 /**
  * WordPress dependencies
  */
-import { Button, createSlotFill } from '@wordpress/components';
+import { Button, Notice, createSlotFill } from '@wordpress/components';
 import { store as editorStore } from '@wordpress/editor';
 import { useDispatch, useSelect } from '@wordpress/data';
+import { applyFilters } from '@wordpress/hooks';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -56,9 +57,33 @@ const PatternsStep = ( { title, replaces, onCompletion } ) => {
 			<h1 className="sensei-editor-wizard-step__sticky-title">
 				{ title }
 			</h1>
+			<NoLayoutsWarning layouts={ availableTemplates } />
 			<Slot />
 			<PatternsList onChoose={ onChoose } />
 		</div>
+	);
+};
+
+/**
+ * Output a warning message if no layouts are available.
+ *
+ * @param {Object} props         Compoent props.
+ * @param {Object} props.layouts Available layouts.
+ */
+const NoLayoutsWarning = ( { layouts } ) => {
+	if ( layouts && Object.keys( layouts ).length > 0 ) {
+		return <></>;
+	}
+
+	const warningMessage = applyFilters(
+		'sensei.editorWizard.noLayoutsWarning',
+		__( 'Layouts are not available', 'sensei-lms' )
+	);
+
+	return (
+		<Notice status="warning" isDismissible={ false }>
+			{ warningMessage }
+		</Notice>
 	);
 };
 
