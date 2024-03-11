9f6efd0174eac3feb48d636e8692b4c011148928
Imran Hossain <imranh920@gmail.com>
2024-03-11 18:15:29 +0600

    Add helper for common interactivity functions

---
+++ assets/admin/tour/helper.js
@@ -0,0 +1,47 @@
+export const HIGHLIGHT_CLASS = 'sensei-tour-highlight';
+
+/**
+ * Highlights the elements with a border.
+ *
+ * @param {Array} selectors An array of selectors to highlight.
+ */
+export function highlightElementsWithBorders( selectors ) {
+	selectors.forEach( function ( selector ) {
+		const element = document.querySelector( selector );
+		if ( element ) {
+			element.classList.add( HIGHLIGHT_CLASS );
+		}
+	} );
+}
+
+/**
+ * Removes the highlight classes from the elements.
+ */
+export function removeHighlightClasses() {
+	const highlightedElements = document.querySelectorAll(
+		'.sensei-tour-highlight'
+	);
+	highlightedElements.forEach( function ( element ) {
+		element.classList.remove( HIGHLIGHT_CLASS );
+	} );
+}
+
+/**
+ * Performs step actions one after another.
+ *
+ * @param {Array} stepActions An array of selectors to highlight.
+ */
+export async function performStepActionsAsync( stepActions ) {
+	removeHighlightClasses();
+
+	for ( const stepAction of stepActions ) {
+		if ( stepAction ) {
+			await new Promise( ( resolve ) =>
+				setTimeout( () => {
+					stepAction.action();
+					resolve();
+				}, stepAction.delay ?? 0 )
+			);
+		}
+	}
+}
