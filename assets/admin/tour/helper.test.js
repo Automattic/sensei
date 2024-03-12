557d588dea74773c000e81cf9586af0543650638
Imran Hossain <imranh920@gmail.com>
2024-03-12 15:40:23 +0600

    Test that highlighter is working properly

--- assets/admin/tour/helper.test.js
+++ assets/admin/tour/helper.test.js
@@ -1,7 +1,11 @@
 /**
  * Internal dependencies
  */
-import { PerformStepAction } from './helper';
+import {
+	PerformStepAction,
+	highlightElementsWithBorders,
+	HIGHLIGHT_CLASS,
+} from './helper';
 
 describe( 'PerformStepAction', () => {
 	it( 'should execute action if it exists for the given index', () => {
@@ -23,5 +27,38 @@ describe( 'PerformStepAction', () => {
 
 		expect( steps[ 0 ].action ).not.toHaveBeenCalled();
 		expect( steps[ 1 ].action ).not.toHaveBeenCalled();
+	} );
+} );
+
+describe( 'highlightElementsWithBorders', () => {
+	const mockQuerySelector = jest.spyOn( document, 'querySelector' );
+
+	beforeEach( () => {
+		mockQuerySelector.mockClear();
+	} );
+
+	it( 'should add highlight class to elements that exist', () => {
+		const element1 = document.createElement( 'div' );
+		const element2 = document.createElement( 'div' );
+
+		mockQuerySelector.mockImplementation( ( selector ) => {
+			if ( '.selector1' === selector ) {
+				return element1;
+			} else if ( '.selector2' === selector ) {
+				return element2;
+			}
+			return null;
+		} );
+
+		highlightElementsWithBorders( [ '.selector1' ] );
+
+		expect( mockQuerySelector ).toHaveBeenCalledTimes( 1 );
+
+		expect( element1.classList.contains( HIGHLIGHT_CLASS ) ).toBe( true );
+		expect( element2.classList.contains( HIGHLIGHT_CLASS ) ).toBe( false );
+
+		highlightElementsWithBorders( [ '.selector2' ] );
+		expect( mockQuerySelector ).toHaveBeenCalledTimes( 2 );
+		expect( element2.classList.contains( HIGHLIGHT_CLASS ) ).toBe( true );
 	} );
 } );
