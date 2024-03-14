f74272466b605c3d5cb60fa93933040baa07fc3e
Renatho De Carli Rosa <renatho@gmail.com>
2024-03-14 11:37:45 -0300

    Remove highlights on last step

--- assets/admin/tour/lesson-tour/steps.js
+++ assets/admin/tour/lesson-tour/steps.js
@@ -14,6 +14,7 @@ import { getFirstBlockByName } from '../../../blocks/course-outline/data';
 import {
 	highlightElementsWithBorders,
 	performStepActionsAsync,
+	removeHighlightClasses,
 } from '../helper';
 
 const getQuizBlock = () =>
@@ -497,6 +498,9 @@ export default function getTourSteps() {
 					desktop: '',
 					mobile: '',
 				},
+			},
+			action: () => {
+				removeHighlightClasses();
 			},
 		},
 	];
