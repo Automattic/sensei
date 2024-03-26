1ff083e5450db0ab92397ce611b0ce465ae7b5fc
Imran Hossain <imranh920@gmail.com>
2024-03-26 17:25:25 +0600

    Add basic tests

---
+++ tests/unit-tests/admin/home/tasks/task/test-class-sensei-home-task-pro-upsell.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * This file contains the Sensei_Home_Task_Pro_Upsell_Test class.
+ *
+ * @package sensei
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Tests for Sensei_Home_Task_Pro_Upsell_Test class.
+ *
+ * @covers Sensei_Home_Task_Pro_Upsell
+ */
+class Sensei_Home_Task_Pro_Upsell_Test extends WP_UnitTestCase {
+	use Sensei_Test_Login_Helpers;
+	use Sensei_Test_Redirect_Helpers;
+
+	/**
+	 * The task under test.
+	 *
+	 * @var Sensei_Home_Task_Pro_Upsell
+	 */
+	private $task;
+
+	public function setUp(): void {
+		parent::setUp();
+		$this->task = new Sensei_Home_Task_Pro_Upsell();
+	}
+
+	public function tearDown(): void {
+		delete_option( Sensei_Home_Task_Pro_Upsell::get_id() );
+		parent::tearDown();
+	}
+
+	public function testGetId_WhenCalled_ReturnsCorrectId() {
+		$this->assertSame( 'sensei-home-task-pro-upsell', Sensei_Home_Task_Pro_Upsell::get_id() );
+	}
+
+	public function testGetTitle_WhenCalled_ReturnsCorrectTitle() {
+		$this->assertSame( 'Sell your course with Sensei Pro', $this->task->get_title() );
+	}
+
+	public function testGetUrl_WhenCalled_ReturnsCorrectUrl() {
+		$this->assertStringContainsString( 'sensei-internal/v1/home/sensei-pro-upsell-redirect?_wpnonce=', $this->task->get_url() );
+	}
+
+	public function testIsCompleted_WhenCalled_IsNotCompletedByDefault() {
+		$this->assertFalse( $this->task->is_completed() );
+	}
+}
