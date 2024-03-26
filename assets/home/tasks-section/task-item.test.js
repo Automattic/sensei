5bed061ce37f75a4d66c641454deccd88aaa70c0
Renatho De Carli Rosa <renatho@gmail.com>
2024-03-26 17:03:15 -0300

    Add tests

--- assets/home/tasks-section/task-item.test.js
+++ assets/home/tasks-section/task-item.test.js
@@ -4,9 +4,16 @@
 import { render } from '@testing-library/react';
 
 /**
+ * WordPress dependencies
+ */
+import { Tooltip } from '@wordpress/components';
+
+/**
  * Internal dependencies
  */
 import TaskItem from './task-item';
+
+jest.mock( '@wordpress/components' );
 
 describe( '<TaskItem />', () => {
 	it( 'Should render an anchor when item is not completed', () => {
@@ -27,5 +34,23 @@ describe( '<TaskItem />', () => {
 		).tagName;
 
 		expect( renderedTag ).toEqual( 'SPAN' );
+	} );
+
+	it( 'Should render a span when item is disabled', () => {
+		const { container } = render( <TaskItem url="#" disabled /> );
+
+		const renderedTag = container.querySelector(
+			'.sensei-home-tasks__link'
+		).tagName;
+
+		expect( renderedTag ).toEqual( 'SPAN' );
+	} );
+
+	it( 'Should render a task with the info', () => {
+		Tooltip.mockImplementation( ( { text } ) => text );
+
+		const { queryByText } = render( <TaskItem url="#" info="Info text" /> );
+
+		expect( queryByText( 'Info text' ) ).toBeTruthy();
 	} );
 } );
