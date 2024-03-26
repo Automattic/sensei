53a5e00743c7ffb2f992caa6dade9672c7f3cadf
Imran Hossain <imranh920@gmail.com>
2024-03-26 21:59:05 +0600

    Test redirect api request takes user to senseilmscom

--- tests/unit-tests/rest-api/test-rest-class-sensei-rest-api-home-controller.php
+++ tests/unit-tests/rest-api/test-rest-class-sensei-rest-api-home-controller.php
@@ -13,6 +13,7 @@
  */
 class Sensei_REST_API_Home_Controller_REST_Test extends WP_Test_REST_TestCase {
 	use Sensei_Test_Login_Helpers;
+	use Sensei_Test_Redirect_Helpers;
 
 	/**
 	 * A server instance that we use in tests to dispatch requests.
@@ -80,11 +81,28 @@ class Sensei_REST_API_Home_Controller_REST_Test extends WP_Test_REST_TestCase {
 		$this->assertEquals( 200, $response->get_status() );
 	}
 
+	public function testSenseiProUpsellRedirect_WhenCalled_RedirectsToSenseiProUpsellPage() {
+		/* Arrange */
+		$this->login_as_admin();
+		$redirect_location = '';
+		$this->prevent_wp_redirect();
+
+		/* Act */
+		try {
+			$response = $this->dispatchRequest( 'GET', '/sensei-pro-upsell-redirect' );
+		} catch ( Sensei_WP_Redirect_Exception $e ) {
+			$redirect_location = $e->getMessage();
+		}
+
+		/* Assert */
+		$this->assertSame( 'https://senseilms.com/sensei-pro/?utm_source=plugin_sensei&utm_medium=upsell&utm_campaign=sensei-home', $redirect_location );
+	}
+
 	private function dispatchRequest( $method, $route = '' ) {
 		// Prevent requests.
 		add_filter(
 			'pre_http_request',
-			function() {
+			function () {
 				return [ 'body' => '[]' ];
 			}
 		);
