dfa5c54026035d365e872c229be894c298408e0e
Miroslav Mitev <m1r0@users.noreply.github.com>
2025-07-18 18:38:44 +0300

    Fix linter issue

--- tests/e2e-playwright/config/global-setup.ts
+++ tests/e2e-playwright/config/global-setup.ts
@@ -28,7 +28,9 @@ export default async (): Promise< void > => {
 };
 
 const cleanAllPlugins = async () => {
-	await cliAsync( 'wp plugin deactivate --all --exclude=sensei,sensei-lms,sensei-security' );
+	await cliAsync(
+		'wp plugin deactivate --all --exclude=sensei,sensei-lms,sensei-security'
+	);
 
 	if ( needActivateGutenberg() ) {
 		console.log( 'Installing and activating Gutenberg plugin...' );
