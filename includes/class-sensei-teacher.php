95dd85e3e40fb2999e0dec9a65117b54050c89ca
markcummins <markcummins87@gmail.com>
2025-08-11 11:33:42 +0100

    Fix Course Teacher going out of sync with Course Lessons

--- includes/class-sensei-teacher.php
+++ includes/class-sensei-teacher.php
@@ -107,6 +107,9 @@ class Sensei_Teacher {
 		// update lesson owner to course teacher before insert
 		add_filter( 'wp_insert_post_data', array( $this, 'update_lesson_teacher' ), 99, 2 );
 
+		// update lesson owner to course teacher when a lesson is updated.
+		add_action( 'updated_postmeta', array( $this, 'sync_lesson_teacher' ), 10, 4 );
+
 		// Redirect teachers to the WordPress admin dashboard after login.
 		add_filter( 'wp_login', array( $this, 'teacher_login_redirect' ), 99, 2 );
 
@@ -972,6 +975,42 @@ AND comments.comment_type = 'sensei_course_status'";
 		$args['post__in'] = $quiz_scope;
 
 		return $args;
+	}
+
+	/**
+	 * When the _lesson_course meta is updated, make sure the lesson author is the same as the course author
+	 *
+	 * @param int    $meta_id    ID of updated metadata entry.
+	 * @param int    $object_id  Post ID.
+	 * @param string $meta_key   Metadata key.
+	 * @param mixed  $meta_value Metadata value.
+	 */
+	public function sync_lesson_teacher( $meta_id, $object_id, $meta_key, $meta_value ) {
+		if ( '_lesson_course' !== $meta_key ) {
+			return;
+		}
+
+		// Check that the $object id is a Lesson ID.
+		if ( 'lesson' !== get_post_type( $object_id ) ) {
+			return;
+		}
+
+		// Check the $meta_value is a Course ID.
+		if ( 'course' !== get_post_type( $meta_value ) ) {
+			return;
+		}
+
+		$lesson_author_id = get_post_field( 'post_author', $object_id );
+		$course_author_id = get_post_field( 'post_author', $meta_value );
+
+		if ( $lesson_author_id !== $course_author_id ) {
+			wp_update_post(
+				array(
+					'ID'          => (int) $object_id,
+					'post_author' => (int) $course_author_id,
+				)
+			);
+		}
 	}
 
 	/**
