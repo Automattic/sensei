841600f46da0176ed7b2152b60a800322c369200
Donna Peplinskie <donna.peplinskie@automattic.com>
2026-09-11 11:06:44 -0400

    Restore HPPS test helper reset behavior

--- tests/unit-tests/test-trait-sensei-hpps-helpers.php
+++ tests/unit-tests/test-trait-sensei-hpps-helpers.php
@@ -11,20 +11,23 @@ class Sensei_HPPS_Helpers_Test extends WP_UnitTestCase {
 
 	public function testEnableHppsTablesRepository_Called_SetsResolvedConfigurationToTables() {
 		/* Arrange. */
+		$original_repository            = Sensei()->settings->settings['experimental_progress_storage_repository'] ?? Progress_Storage_Settings::COMMENTS_STORAGE;
 		$original_storage_configuration = Sensei()->progress_storage_configuration;
 
 		/* Act. */
 		$this->enable_hpps_tables_repository();
 		$actual = Sensei()->progress_storage_configuration->is_reading_from_tables();
 		$this->reset_hpps_repository();
-		Sensei()->progress_storage_configuration = $original_storage_configuration;
+		Sensei()->settings->settings['experimental_progress_storage_repository'] = $original_repository;
+		Sensei()->progress_storage_configuration                                 = $original_storage_configuration;
 
 		/* Assert. */
 		self::assertTrue( $actual );
 	}
 
-	public function testResetHppsRepository_TablesConfigurationGiven_RestoresOriginalConfiguration() {
+	public function testResetHppsRepository_TablesConfigurationGiven_SetsResolvedConfigurationToComments() {
 		/* Arrange. */
+		$original_repository                     = Sensei()->settings->settings['experimental_progress_storage_repository'] ?? Progress_Storage_Settings::COMMENTS_STORAGE;
 		$original_storage_configuration          = Sensei()->progress_storage_configuration;
 		Sensei()->progress_storage_configuration = Progress_Storage_Configuration::resolve(
 			array(
@@ -37,10 +40,12 @@ class Sensei_HPPS_Helpers_Test extends WP_UnitTestCase {
 
 		/* Act. */
 		$this->reset_hpps_repository();
-		$actual                                  = Sensei()->progress_storage_configuration->is_reading_from_tables();
-		Sensei()->progress_storage_configuration = $original_storage_configuration;
+		$actual = Sensei()->progress_storage_configuration->is_reading_from_tables();
+
+		Sensei()->settings->settings['experimental_progress_storage_repository'] = $original_repository;
+		Sensei()->progress_storage_configuration                                 = $original_storage_configuration;
 
 		/* Assert. */
-		self::assertSame( $original_storage_configuration->is_reading_from_tables(), $actual );
+		self::assertFalse( $actual );
 	}
 }
